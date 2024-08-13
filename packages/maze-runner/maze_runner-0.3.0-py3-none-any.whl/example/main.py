38327e96059c0af1e847897a228112cd47bbfb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:04:25 +0000

    Add package maze-runner/0.3.0/maze_runner-0.3.0-py3-none-any.whl

---
+++ packages/maze-runner/maze_runner-0.3.0-py3-none-any.whl/example/main.py
@@ -0,0 +1,19 @@
+from maze_runner import Maze
+
+if "__main__" == __name__:
+    maze = Maze("../maps/02.txt")
+    maze.clear_console()
+
+    maze.print_maze_status()
+
+    while not maze.has_finished():
+        if maze.can_move_left():
+            maze.move_left()
+        else:
+            maze.move_down()
+
+    maze.print_maze_status()
+    # Add your own GitHub Page
+    maze.generate_animation(
+        header="My amazing solution!\nBy https://github.com/matheusjardimb", fps=2
+    )
