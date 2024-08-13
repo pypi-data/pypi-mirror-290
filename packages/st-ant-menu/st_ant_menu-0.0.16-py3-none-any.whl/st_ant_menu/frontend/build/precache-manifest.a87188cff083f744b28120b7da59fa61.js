efb2cdc5e61b5a12f46fd40795906f5522b1f2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:04:06 +0000

    Add package st-ant-menu/0.0.16/st_ant_menu-0.0.16-py3-none-any.whl

---
+++ packages/st-ant-menu/st_ant_menu-0.0.16-py3-none-any.whl/st_ant_menu/frontend/build/precache-manifest.a87188cff083f744b28120b7da59fa61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79d746a7d0482d921a630876cd7c5d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "970a7a5bb8062b7fa24e",
+    "url": "./static/js/2.9216c681.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.9216c681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e7cb9b47d6d23b98bb",
+    "url": "./static/js/main.fc99ca09.chunk.js"
+  },
+  {
+    "revision": "23b81c6aad4ea3280af0",
+    "url": "./static/js/runtime-main.ac51b7c4.js"
+  }
+]);
---
+++ packages/st-ant-menu/st_ant_menu-0.0.16-py3-none-any.whl/st_ant_menu/frontend/build/precache-manifest.b3100fcdda99bc7575018310a5435793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60220bc5f6dd86d89477d010e65c6641",
+    "url": "./index.html"
+  },
+  {
+    "revision": "970a7a5bb8062b7fa24e",
+    "url": "./static/js/2.9216c681.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.9216c681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e454cf16fea1bad59885",
+    "url": "./static/js/main.81977b01.chunk.js"
+  },
+  {
+    "revision": "23b81c6aad4ea3280af0",
+    "url": "./static/js/runtime-main.ac51b7c4.js"
+  }
+]);
