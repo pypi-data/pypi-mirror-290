8adb2d099b614d0a6ada7be9c23bc4fde92f2136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:16 +0000

    Add package AnkurFKnapsack/1.0/AnkurFKnapsack-1.0.tar.gz

---
+++ packages/AnkurFKnapsack/AnkurFKnapsack-1.0.tar.gz/AnkurFKnapsack-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AnkurFKnapsack",
+    version="1.0",
+    packages=find_packages(),
+    description="A package for solving the Fractional Knapsack problem using a greedy approach",
+    author="Ankur Ahire",
+    author_email="ankurahire6@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
