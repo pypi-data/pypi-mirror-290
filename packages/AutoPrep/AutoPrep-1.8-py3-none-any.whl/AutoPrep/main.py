da03744118dfd7e5e982dcef9ceac1dd2cf9713a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:05:14 +0000

    Add package AutoPrep/1.8/AutoPrep-1.8-py3-none-any.whl

---
+++ packages/AutoPrep/AutoPrep-1.8-py3-none-any.whl/AutoPrep/main.py
@@ -0,0 +1,27 @@
+#%%
+
+############## dummy data #############
+import pandas as pd
+data = {
+    'ID': [1, 2, 3, 4],                 
+    'Name': ['Alice', 'Bob', 'Charlie', 'R2D2'],  
+    'Age': [25, 30, 35, 90],                 
+    'Salary': [50000.00, 60000.50, 75000.75, 80000.00], 
+    'Hire Date': pd.to_datetime(['2020-01-15', '2019-05-22', '2018-08-30', '2021-04-12']), 
+    'Is Manager': [False, True, False, True]  
+}
+data = pd.DataFrame(data)
+########################################
+
+
+from autoprep import AutoPrep
+
+pipeline = AutoPrep(
+    nominal_columns=["ID", "Name", "Is Manager"],
+    datetime_columns=["Hire Date"],
+    # pattern_recognition_columns=["Name"]
+
+)
+X_output = pipeline.preprocess(df=data)
+
+# pipeline.visualize_pipeline_structure_html()
