71e4376c5436552fdb52b6844405d63f6e60c5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:03:42 +0000

    Add package iudex/0.11.1/iudex-0.11.1-py3-none-any.whl

---
+++ packages/iudex/iudex-0.11.1-py3-none-any.whl/iudex/__init__.py
@@ -0,0 +1,16 @@
+# ruff: noqa: E402
+# ^ because monkeypatches must import and run before other imports
+from .monkeypatches.encode_value import monkeypatch_encode_value
+from .monkeypatches.clean_attribute import monkeypatch_clean_attribute
+from .monkeypatches.print import monkeypatch_print
+
+monkeypatch_encode_value()
+monkeypatch_clean_attribute()
+monkeypatch_print()
+
+from .config import IudexConfig, configure_logger
+from .instrumentation import instrument
+from .fastapi import instrument_fastapi
+from .trace import trace, trace_lambda
+
+__all__ = ["IudexConfig", "configure_logger", "instrument", "instrument_fastapi", "trace", "trace_lambda"]
