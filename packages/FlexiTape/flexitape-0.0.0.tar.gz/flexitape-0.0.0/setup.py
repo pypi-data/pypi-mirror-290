03aa5f329d3aaccd5fa0301559c7751461e2ecb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:15 +0000

    Add package FlexiTape/0.0.0/flexitape-0.0.0.tar.gz

---
+++ packages/FlexiTape/flexitape-0.0.0.tar.gz/flexitape-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='FlexiTape',
+    version='0.0.0',
+    description='A placeholder project',
+    author='Duckling Dean',
+    author_email='duckling.dean@proton.me',
+    packages=[],
+)
+
