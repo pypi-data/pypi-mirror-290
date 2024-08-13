2b9db5bf515503ebbb456d49b64ecf0f8b6bbe07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:09 +0000

    Add package leads/0.9.3a1/leads-0.9.3a1-py3-none-any.whl

---
+++ packages/leads/leads-0.9.3a1-py3-none-any.whl/leads_comm_serial/connection.py
@@ -0,0 +1,47 @@
+from typing import override as _override
+
+from serial import Serial as _Serial
+
+from leads.comm import ConnectionBase as _ConnectionBase, Service as _Service
+
+
+class SerialConnection(_ConnectionBase):
+    def __init__(self,
+                 service: _Service,
+                 serial: _Serial,
+                 port: str,
+                 remainder: bytes = b"",
+                 separator: bytes = b";") -> None:
+        super().__init__(service, remainder, separator)
+        self._serial: _Serial = serial
+        self._port: str = port
+
+    @_override
+    def closed(self) -> bool:
+        return self._serial.closed
+
+    def _require_open_serial(self, mandatory: bool = True) -> _Serial:
+        if mandatory and self.closed():
+            raise IOError("An open serial is required")
+        return self._serial
+
+    @_override
+    def receive(self, chunk_size: int = 1) -> bytes | None:
+        if self._remainder != b"":
+            return self.use_remainder()
+        try:
+            msg = chunk = b""
+            while self._separator not in chunk:
+                msg += (chunk := self._require_open_serial().read(chunk_size))
+            return self.with_remainder(msg)
+        except IOError:
+            return
+
+    @_override
+    def send(self, msg: bytes) -> None:
+        self._require_open_serial().write(msg + self._separator)
+
+    @_override
+    def close(self) -> None:
+        self.disconnect()
+        self._require_open_serial(False).close()
