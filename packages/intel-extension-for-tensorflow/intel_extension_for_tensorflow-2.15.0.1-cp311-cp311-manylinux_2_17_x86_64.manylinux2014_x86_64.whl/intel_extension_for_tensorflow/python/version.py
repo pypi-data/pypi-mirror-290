81e6ac24092a0b1dbb391e3e294bd51b89a5591d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:05:14 +0000

    Add package intel-extension-for-tensorflow/2.15.0.1/intel_extension_for_tensorflow-2.15.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/intel-extension-for-tensorflow/intel_extension_for_tensorflow-2.15.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/intel_extension_for_tensorflow/python/version.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2021-2022 Intel Corporation
+#
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+'''version information for Intel ® Extension for TensorFlow*'''
+
+__version__ = '2.15.0.1'
+__git_desc__= '569aef2e'
+VERSION = __version__
+GIT_VERSION = 'v' + __version__ + '-' + __git_desc__
+COMPILER_VERSION = 'dpcpp-2024.2.1.20240711'
+ONEDNN_GPU_GIT_VERSION = 'v3.4.3-8a037f77'
+TF_COMPATIBLE_VERSION = '>= 2.8.0'
