4d26c6d0f2e5169acb84c475f6b1f09591a9f583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:17 +0000

    Add package jenkins-pysdk/1.4.3/jenkins_pysdk-1.4.3.tar.gz

---
+++ packages/jenkins-pysdk/jenkins_pysdk-1.4.3.tar.gz/jenkins_pysdk-1.4.3/tests/conf.py
@@ -0,0 +1,10 @@
+credentials: dict = {
+    "username": "admin",
+    "password": "jenkins"
+}
+
+servers: dict = {
+    "port_lower": 8099,
+    "port_higher": 8103,
+    "host": "http://localhost"
+}
