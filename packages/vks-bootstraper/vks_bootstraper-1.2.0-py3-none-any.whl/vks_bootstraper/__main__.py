17df2ea2e6dee9dde136324d415cd96e13c7c18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:37 +0000

    Add package vks-bootstraper/1.2.0/vks_bootstraper-1.2.0-py3-none-any.whl

---
+++ packages/vks-bootstraper/vks_bootstraper-1.2.0-py3-none-any.whl/vks_bootstraper/__main__.py
@@ -0,0 +1,23 @@
+"""
+Author: Cuong. Duong Manh <cuongdm3@vng.com.vn>
+Description: The CLI tool to boostrap the instances of VKS clusters.
+"""
+
+import click
+from . import metadata
+
+
+@click.group()
+@click.version_option("1.2.0", prog_name="vks-bootstraper")
+def cli():
+    pass
+
+
+cli.add_command(metadata.get_instance_id)  # noqa
+cli.add_command(metadata.get_local_ipv4)  # noqa
+cli.add_command(metadata.prepare_kubeadm_config)  # noqa
+cli.add_command(metadata.waiting_instance_booted)  # noqa
+cli.add_command(metadata.add_host)  # noqa
+
+# if __name__ == "__main__":
+#     cli()
