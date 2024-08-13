0802f193605df3fc1bd86b085aac873844ec937c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:40 +0000

    Add package hidroconta/1.0.1/hidroconta-1.0.1.tar.gz

---
+++ packages/hidroconta/hidroconta-1.0.1.tar.gz/hidroconta-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='hidroconta',
+    version='1.0.1',
+    packages=['hidroconta'],
+    install_requires=[
+        'pandas',
+        'requests',
+        'datetime'
+    ],
+    author='JavierL',
+    author_email='javier.lopez@hidroconta.com',
+    description='API to easily access Demeter REST API provided by Hidroconta S.A.U.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
