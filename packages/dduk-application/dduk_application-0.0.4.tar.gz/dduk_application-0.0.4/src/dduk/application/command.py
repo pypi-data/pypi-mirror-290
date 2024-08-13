b9d2f1c28a6e5da8e7f823f1b90fb446663d3d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:16 +0000

    Add package dduk-application/0.0.4/dduk_application-0.0.4.tar.gz

---
+++ packages/dduk-application/dduk_application-0.0.4.tar.gz/dduk_application-0.0.4/src/dduk/application/command.py
@@ -0,0 +1,145 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Any, Final, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
+import builtins
+import sys
+import os
+
+
+#--------------------------------------------------------------------------------
+# 전역 상수 목록.
+#--------------------------------------------------------------------------------
+EMPTY : str = ""
+NONE : str = "NONE"
+COMMA : str = ","
+SLASH : str = "/"
+BACKSLASH : str = "\\"
+COLON : str = ":"
+SPACE : str = " "
+DEBUG : str = "DEBUG"
+READ : str = "r"
+WRITE : str = "w"
+UTF8 : str = "utf-8"
+
+
+#--------------------------------------------------------------------------------
+# 깃킵 파일 생성.
+#--------------------------------------------------------------------------------
+def CreateGitKeep(path : str) -> bool:
+	try:
+		if not path:
+			return False
+
+		path = path.replace(BACKSLASH, SLASH)
+		if not os.path.isdir(path):
+			return False
+		
+		gitkeepFilePath = f"{path}/.gitkeep"
+		if os.path.isfile(gitkeepFilePath):
+			return False
+
+		with open(gitkeepFilePath, mode = WRITE, encoding = UTF8) as file:
+			file.write(EMPTY)
+		return True
+	except Exception as exception:
+		builtins.print(exception)
+		return False
+
+
+
+
+#--------------------------------------------------------------------------------
+# 프로젝트 템플릿 생성.
+#--------------------------------------------------------------------------------
+def CreateProjectTemplate(rootPath : str) -> bool:
+	if not rootPath:
+		return False
+	if not os.path.isdir(rootPath):
+		return False
+
+	# builtins.print(f"{rootPath}/.git")
+	# os.mkdir(f"{rootPath}/.vscode")
+	# os.mkdir(f"{rootPath}/.vscode/launch.json")
+	# os.mkdir(f"{rootPath}/.vscode/settings.json")
+	# os.mkdir(f"{rootPath}/.vscode/tasks.json")
+	# builtins.print(f"{rootPath}/.venv")
+	os.mkdir(f"{rootPath}/build")
+	os.mkdir(f"{rootPath}/docs")
+	os.mkdir(f"{rootPath}/hints")
+	os.mkdir(f"{rootPath}/hooks")
+	os.mkdir(f"{rootPath}/libs")
+	builtins.print(f"{rootPath}/logs")
+	builtins.print(f"{rootPath}/res")
+	builtins.print(f"{rootPath}/src")
+	builtins.print(f"{rootPath}/tests")
+	builtins.print(f"{rootPath}/workspace")
+	builtins.print(f"{rootPath}/.gitignore")
+	builtins.print(f"{rootPath}/requirements.txt")
+
+
+#--------------------------------------------------------------------------------
+# 명령어 스크립트 생성.
+#--------------------------------------------------------------------------------
+def CreateCommandScript(rootPath : str) -> None:
+	builtins.print("dduk.application.command.CreateCommandScript()")
+
+
+#--------------------------------------------------------------------------------
+# 명령어 스크립트 제거.
+#--------------------------------------------------------------------------------
+def DestroyCommandScript(rootPath : str) -> None:
+	builtins.print("dduk.application.command.DestroyCommandScript()")
+
+
+#--------------------------------------------------------------------------------
+# 도움말.
+#--------------------------------------------------------------------------------
+def Help() -> None:
+	builtins.print("Usage: dduk-application {{project|bat|sh\}}")
+	builtins.print("ddukapp project: 현재 경로를 루트로 삼아 프로젝트 템플릿 생성.")
+	builtins.print("ddukapp bat: 현재 경로를 루트로 삼아 프로젝트 배치파일 생성. (Windows)")
+	builtins.print("ddukapp sh: 현재 경로를 루트로 삼아 프로젝트 쉘스크립트 생성. (Linux/MacOS)")
+
+
+#--------------------------------------------------------------------------------
+# 메인.
+#--------------------------------------------------------------------------------
+def Main() -> None:
+	rootPath = os.getcwd()
+	rootPath = rootPath.replace(BACKSLASH, SLASH)
+
+	argumentCount = len(sys.argv)
+	if sys.argv:
+		# 실행파일 추출.
+		executeName = sys.argv[0]
+		builtins.print(f"executeName: {executeName}")
+		sys.argv = sys.argv[1:]	
+		if sys.argv:
+			command = sys.argv[0]
+			command = command.lower()
+			if command == "project":
+				CreateProjectTemplate(rootPath)
+			elif command == "bat":
+				builtins.print(f"{rootPath}/build.bat")
+				builtins.print(f"{rootPath}/environment.bat")
+				builtins.print(f"{rootPath}/package.bat")
+				builtins.print(f"{rootPath}/run.bat")
+				builtins.print(f"{rootPath}/service.bat")
+				builtins.print(f"{rootPath}/variable.bat")
+				builtins.print(f"{rootPath}/venv.bat")
+			elif command == "sh":
+				builtins.print(f"{rootPath}/build.sh")
+				builtins.print(f"{rootPath}/environment.sh")
+				builtins.print(f"{rootPath}/package.sh")
+				builtins.print(f"{rootPath}/run.sh")
+				builtins.print(f"{rootPath}/service.sh")
+				builtins.print(f"{rootPath}/variable.sh")
+				builtins.print(f"{rootPath}/venv.sh")
+			else:
+				builtins.print(f"잘못된 명령어: {command}, 오류: 1")
+		else:
+			Help()
+	else:
+		Help()
