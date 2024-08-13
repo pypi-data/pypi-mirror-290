af7634ce3943de96e16c632897c2da73702454f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:04:10 +0000

    Add package scarx-api-client/0.6.1/scarx_api_client-0.6.1.tar.gz

---
+++ packages/scarx-api-client/scarx_api_client-0.6.1.tar.gz/scarx_api_client-0.6.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='scarx_api_client',
+   version='0.6.1',
+   description='Client for Scarx API',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   author='Scartz',
+   author_email='github@scarx.net',
+   packages=find_packages(),
+   install_requires=['grpclib', 'betterproto==2.0.0b6', 'pydantic'],
+)
