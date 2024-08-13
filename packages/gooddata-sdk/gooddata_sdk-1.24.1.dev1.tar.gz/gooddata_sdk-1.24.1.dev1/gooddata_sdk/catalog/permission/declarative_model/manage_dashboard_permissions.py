f116f02ea81df09dbfed4ebb2b60080313e2b723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:11 +0000

    Add package gooddata-sdk/1.24.1.dev1/gooddata_sdk-1.24.1.dev1.tar.gz

---
+++ packages/gooddata-sdk/gooddata_sdk-1.24.1.dev1.tar.gz/gooddata_sdk-1.24.1.dev1/gooddata_sdk/catalog/permission/declarative_model/manage_dashboard_permissions.py
@@ -0,0 +1,35 @@
+# (C) 2023 GoodData Corporation
+from typing import List, Type
+
+import attr
+from gooddata_api_client.model.manage_dashboard_permissions_request_inner import ManageDashboardPermissionsRequestInner
+
+from gooddata_sdk.catalog.base import Base
+from gooddata_sdk.catalog.identifier import CatalogAssigneeIdentifier
+from gooddata_sdk.catalog.rule import CatalogAssigneeRule
+
+
+# Kept for backward compatibility only
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogDashboardAssigneeIdentifier(CatalogAssigneeIdentifier):
+    pass
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogPermissionsForAssigneeIdentifier(Base):
+    permissions: List[str] = attr.field(factory=list)
+    assignee_identifier: CatalogAssigneeIdentifier
+
+    @staticmethod
+    def client_class() -> Type[ManageDashboardPermissionsRequestInner]:
+        return ManageDashboardPermissionsRequestInner
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogPermissionsForAssigneeRule(Base):
+    permissions: List[str] = attr.field(factory=list)
+    assignee_rule: CatalogAssigneeRule
+
+    @staticmethod
+    def client_class() -> Type[ManageDashboardPermissionsRequestInner]:
+        return ManageDashboardPermissionsRequestInner
---
+++ packages/gooddata-sdk/gooddata_sdk-1.24.1.dev1.tar.gz/gooddata_sdk-1.24.1.dev1/gooddata_sdk/catalog/setting.py
@@ -0,0 +1,32 @@
+# (C) 2022 GoodData Corporation
+from __future__ import annotations
+
+from typing import Any, Dict, Optional, Type
+
+import attr
+from gooddata_api_client.model.declarative_custom_application_setting import DeclarativeCustomApplicationSetting
+from gooddata_api_client.model.declarative_setting import DeclarativeSetting
+
+from gooddata_sdk.catalog.base import Base, value_in_allowed
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogDeclarativeSetting(Base):
+    id: str
+    type: str = attr.field(validator=value_in_allowed)
+    content: Optional[Dict[str, Any]] = None
+
+    @staticmethod
+    def client_class() -> Type[DeclarativeSetting]:
+        return DeclarativeSetting
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogDeclarativeCustomApplicationSetting(Base):
+    id: str
+    content: Dict[str, Any]
+    application_name: str
+
+    @staticmethod
+    def client_class() -> Type[DeclarativeCustomApplicationSetting]:
+        return DeclarativeCustomApplicationSetting
