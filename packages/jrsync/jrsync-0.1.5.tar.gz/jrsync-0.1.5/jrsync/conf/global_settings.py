79af002ba7442c7ffa4b84b2477fefb7e52b737a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:04:18 +0000

    Add package jrsync/0.1.5/jrsync-0.1.5.tar.gz

---
+++ packages/jrsync/jrsync-0.1.5.tar.gz/jrsync-0.1.5/jrsync/conf/global_settings.py
@@ -0,0 +1,21 @@
+DEFAULT_FORCE_MODE = False
+
+# sentinel value used to sync a directory
+ALL_DIRECTORY = ""
+
+DATE_PLACEHOLDER_REGEX = r"{{DATE(?:[+-]\d+)?}}"
+DATE_PLACEHOLDER = '{{DATE}}'
+
+DATE_FMT = "%Y%m%d"
+
+########################
+# LOG
+########################
+
+# The callable to use to configure logging
+LOGGING_CONFIG = "logging.config.dictConfig"
+
+# Custom logging configuration.
+LOGGING = {}
+
+DEBUG = False
