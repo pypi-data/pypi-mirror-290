ea98ba3596a36c0ce014e1ed44ec2581edb401a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:04:01 +0000

    Add package sparkpipelineframework/2.0.56/sparkpipelineframework-2.0.56-py3-none-any.whl

---
+++ packages/sparkpipelineframework/sparkpipelineframework-2.0.56-py3-none-any.whl/spark_pipeline_framework/utilities/fhir_helpers/get_fhir_client.py
@@ -0,0 +1,52 @@
+from logging import Logger
+from typing import Optional, List
+
+from helix_fhir_client_sdk.fhir_client import FhirClient
+
+from spark_pipeline_framework.utilities.fhir_helpers.token_helper import TokenHelper
+
+
+def get_fhir_client(
+    logger: Logger,
+    server_url: str,
+    auth_server_url: Optional[str] = None,
+    auth_client_id: Optional[str] = None,
+    auth_client_secret: Optional[str] = None,
+    auth_login_token: Optional[str] = None,
+    auth_access_token: Optional[str] = None,
+    auth_scopes: Optional[List[str]] = None,
+    log_level: Optional[str] = None,
+    auth_well_known_url: Optional[str] = None,
+) -> FhirClient:
+    assert server_url
+
+    fhir_client: FhirClient = FhirClient()
+    fhir_client = fhir_client.url(server_url)
+
+    if log_level:
+        fhir_client = fhir_client.log_level(log_level)
+
+    if auth_server_url:
+        fhir_client = fhir_client.auth_server_url(auth_server_url)
+    if auth_well_known_url:
+        auth_server_url_from_wellknown: Optional[str] = (
+            TokenHelper.get_auth_server_url_from_well_known_url(
+                well_known_url=auth_well_known_url
+            )
+        )
+        if auth_server_url_from_wellknown:
+            fhir_client = fhir_client.auth_server_url(auth_server_url_from_wellknown)
+    if auth_client_id and auth_client_secret:
+        fhir_client = fhir_client.client_credentials(auth_client_id, auth_client_secret)
+    if auth_login_token:
+        fhir_client = fhir_client.login_token(auth_login_token)
+    if auth_access_token:
+        fhir_client = fhir_client.set_access_token(auth_access_token)
+    if auth_scopes:
+        fhir_client = fhir_client.auth_scopes(auth_scopes)
+
+    if logger:
+        # noinspection PyTypeChecker
+        fhir_client = fhir_client.logger(logger)  # type: ignore
+
+    return fhir_client
