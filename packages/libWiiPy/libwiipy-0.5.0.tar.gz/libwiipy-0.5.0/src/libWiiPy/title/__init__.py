656661dce602aba64b958ced89de532cf72618eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:03:58 +0000

    Add package libWiiPy/0.5.0/libwiipy-0.5.0.tar.gz

---
+++ packages/libWiiPy/libwiipy-0.5.0.tar.gz/libwiipy-0.5.0/src/libWiiPy/title/__init__.py
@@ -0,0 +1,14 @@
+# "title/__init__.py" from libWiiPy by NinjaCheetah & Contributors
+# https://github.com/NinjaCheetah/libWiiPy
+
+from .content import *
+from .crypto import *
+from .emunand import *
+from .iospatcher import *
+from .nus import *
+from .sys import *
+from .ticket import *
+from .title import *
+from .tmd import *
+from .util import *
+from .wad import *
