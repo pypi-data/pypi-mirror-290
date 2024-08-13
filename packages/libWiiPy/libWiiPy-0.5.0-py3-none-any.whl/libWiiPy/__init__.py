f2af20dd60c3ef755077b69646647bc77464f739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:03:58 +0000

    Add package libWiiPy/0.5.0/libWiiPy-0.5.0-py3-none-any.whl

---
+++ packages/libWiiPy/libWiiPy-0.5.0-py3-none-any.whl/libWiiPy/__init__.py
@@ -0,0 +1,9 @@
+# "__init__.py" from libWiiPy by NinjaCheetah & Contributors
+# https://github.com/NinjaCheetah/libWiiPy
+#
+# These are the essential submodules from libWiiPy that you'd probably want imported by default.
+
+__all__ = ["archive", "title"]
+
+from . import archive
+from . import title
