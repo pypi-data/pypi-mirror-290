e732eef4e39ac3bc582e34a5ee882f81165db2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:31 +0000

    Add package AnkurKnapsack/1.0/AnkurKnapsack-1.0-py3-none-any.whl

---
+++ packages/AnkurKnapsack/AnkurKnapsack-1.0-py3-none-any.whl/AnkurKnapsack/knapsack.py
@@ -0,0 +1,47 @@
+
+
+class Item:
+    def __init__(self,profit,weight):
+        self.profit=profit
+        self.weight=weight
+        self.ratio=profit/weight
+
+
+def Knapsack():
+    
+    item=[]
+    
+    print()
+    n=int(input("Enter Total Number OF  Item : "))
+
+    for i in range(1,n+1):
+        print("Enter PROFIT For ITEM ",i," : ",end="")
+        p=int(input())
+        print("Enter WEIGHT For ITEM ",i," : ",end="")
+        w=int(input())
+        item.append(Item(p,w))
+
+    print()
+    capacity=int(input("ENTER CAPACITY : "))
+    item.sort(key=lambda x: x.ratio , reverse=True)
+
+    total_value=0.0
+
+    for items in item:
+        if capacity >= items.weight:
+            capacity-=items.weight
+            total_value += items.profit
+        else:
+            total_value += items.ratio * capacity
+            break
+    
+    print()
+    print("FRACTIONAL KNAPSACK : GREEDY APPROACH")
+    print("-----------------------------------")
+    print("MAX PROFIT : ",total_value)
+    print("-----------------------------------")
+    print()
+    print()
+
+
+
