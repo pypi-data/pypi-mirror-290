d8c53482b64885142382e5f3b0d45dd3a1468d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:45 +0000

    Add package pyobject/1.2.4/pyobject-1.2.4-cp38-cp38-win32.whl

---
+++ packages/pyobject/pyobject-1.2.4-cp38-cp38-win32.whl/pyobject/__init__.py
@@ -0,0 +1,122 @@
+"""一个提供操作Python对象底层工具的模块。
+A utility tool with some submodules for operating internal python objects.
+
+"""
+import sys
+from warnings import warn
+from pprint import pprint
+
+__email__="3076711200@qq.com"
+__author__="qfcy qq:3076711200"
+__version__="1.2.4"
+
+_ignore_names=["__builtins__","__doc__"]
+__all__=["objectname","bases","describe","desc"]
+
+def objectname(obj):
+    """objectname(obj) - 返回一个对象的名称,形如xxmodule.xxclass。
+如:objectname(int) -> 'builtins.int'"""
+    if not obj.__class__==type:obj=obj.__class__
+    if obj.__module__=="__main__":return obj.__name__
+    return "{}.{}".format(obj.__module__,obj.__name__)
+
+###无递归版本
+##def base(obj):
+##    while True:
+##        try:
+##            obj=obj.__bases__
+##        except AttributeError:obj=[obj.__class__]
+##        if not obj:break
+##        else:obj=obj[0]
+##        print(obj)
+def bases(obj,level=0,tab=4):
+    '''bases(obj) - 打印出该对象的基类
+tab:缩进的空格数,默认为4。'''
+    if not obj.__class__==type:obj=obj.__class__
+    if obj.__bases__:
+        if level:print(' '*(level*tab),end='')
+        print(*obj.__bases__,sep=',')
+        for cls in obj.__bases__:
+            bases(cls,level,tab)
+
+
+def _shortrepr(obj,maxlength=150):
+    result=repr(obj)
+    if len(result)>maxlength:
+        return result[:maxlength]+"..."
+    return result
+
+###无递归
+##def describe(obj,verbose=True,needhelp=False,**kwargs):
+##    "Prints the properties of an object."
+##    __builtins__.print(repr(obj)+" :",**kwargs)
+##    for attr in dir(obj):
+##        if verbose or not attr.startswith("_"):
+##            if not attr in ignore_names:
+##                value=getattr(obj,attr)
+##                pprint("{}:{}".format(attr,value),**kwargs)
+##                if needhelp:help(value)
+def describe(obj,level=0,maxlevel=1,tab=4,verbose=False,file=None):
+    '''"描述"一个对象,即打印出对象的各个属性。
+参数说明:
+maxlevel:打印对象属性的层数。
+tab:缩进的空格数,默认为4。
+verbose:一个布尔值,是否打印出对象的特殊方法(如__init__)。
+file:一个类似文件的对象，用于打印输出。
+'''
+    if file is None:file=sys.stdout
+    if level==maxlevel:
+        result=repr(obj)
+        if result.startswith('[') or result.startswith('{'):pprint(result)
+        else:print(result,file=file)
+    elif level>maxlevel:raise ValueError(
+        "Argument level is larger than maxlevel")
+    else:
+        print(_shortrepr(obj)+': ',file=file)
+        if type(obj) is type:
+            print("Base classes of the object:",file=file)
+            bases(obj,level+1,tab)
+            print(file=file)
+        for attr in dir(obj):
+            if verbose or not attr.startswith("_"):
+                print(' '*tab*(level+1)+attr+': ',end='',file=file)
+                try:
+                    if not attr in _ignore_names:
+                        describe(getattr(obj,attr),level+1,maxlevel,
+                                tab,verbose,file)
+                    else:print(_shortrepr(getattr(obj,attr)),file=file)
+                except AttributeError:
+                    print("<AttributeError!>",end='',file=file)
+
+desc=describe #别名
+# 导入其他子模块中的函数和类
+try:
+    from pyobject.browser import browse
+    __all__.append("browse")
+# SystemError: 修复Python 3.4下的bug
+except (ImportError,SystemError):warn("Failed to import module pyobject.browser.")
+try:
+    from pyobject.search import make_list,make_iter,search #,test_make_list,test_search
+    __all__.extend(["make_list","make_iter","search"])
+# 同上
+except (ImportError,SystemError):warn("Failed to import pyobject.search.")
+
+try:
+    from pyobject.code_ import Code
+    __all__.append("Code")
+except (ImportError,SystemError):warn("Failed to import pyobject.code_.")
+try:
+    from pyobj_extension import *
+    __all__.extend(["convptr","py_incref","py_decref","list_in"])
+except ImportError:warn("Failed to import module pyobj_extension.")
+
+def test():
+    try:
+        describe(type,verbose=True)
+    except BaseException as err:
+        print("STOPPED!",file=sys.stderr)
+        if not type(err) is KeyboardInterrupt:raise
+        return 1
+    else:return 0
+
+if __name__=="__main__":sys.exit(test())
---
+++ packages/pyobject/pyobject-1.2.4-cp38-cp38-win32.whl/pyobject/pyobj_extension.c
@@ -0,0 +1,133 @@
+﻿#include <Python.h>
+
+PyDoc_STRVAR(convptr_doc, u8"convptr(pointer)"
+    u8"\n"
+    u8"Convert a integer pointer to a Python object,as a reverse of id()."
+    u8"将整数指针转换为Python对象，与id()相反。\n"
+    u8"Warning:Converting an invalid pointer may lead to crashes.");
+PyDoc_STRVAR(py_inc_doc, u8"py_incref(object, n)"
+    u8"\n"
+    u8"Increase the reference count of an object for n."
+    u8"将对象的引用计数增加n。\n"
+    u8"Warning:Improper use of this function may lead to crashes.");
+PyDoc_STRVAR(py_dec_doc, u8"py_decref(object, n)"
+    u8"\n"
+    u8"Decrease the reference count of an object for n."
+    u8"将对象的引用计数减小n。\n"
+    u8"Warning:Improper use of this function may lead to crashes.");
+PyDoc_STRVAR(py_list_in_doc, u8"list_in(obj, lst)"
+    u8"\n"
+    u8"判断obj是否在列表或元组lst中。\n与Python内置的obj in lst调用多次\"==\"运算符(__eq__)相比，"
+    u8"本函数直接比较对象的指针，提高了效率。\n");
+
+PyObject* pyobj_extension_convptr(PyObject* self, PyObject* args, PyObject* kwargs) {
+    PyObject* obj = NULL;
+    unsigned long long number = 0; // 若使用long类型，则64位会引发OverflowError
+
+    static char* keywords[] = { "pointer", NULL };
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "L", keywords, &number)) {
+        return NULL;
+    }
+
+    obj = (PyObject*)number; // 获取指针对应的Python对象
+    return obj;
+}
+PyObject* py_incref(PyObject* self, PyObject* args, PyObject* kwargs) {
+    PyObject* obj = NULL;
+    unsigned int n;
+
+    static char* keywords[] = { "object", "n", NULL };
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OI", keywords, &obj, &n)) {
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < n; i++) { Py_INCREF(obj); }
+    return Py_None;
+}
+PyObject* py_decref(PyObject* self, PyObject* args, PyObject* kwargs) {
+    PyObject* obj = NULL;
+    unsigned int n;
+
+    static char* keywords[] = { "object", "n", NULL };
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OI", keywords, &obj, &n)) {
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < n; i++) { Py_DECREF(obj); }
+    return Py_None;
+}
+PyObject* list_in(PyObject* self, PyObject* args, PyObject* kwargs) {
+    PyObject *obj, *lst;
+
+    // 解析输入参数，参数为待查找对象和一个序列(列表或元组)
+    static char* keywords[] = { "obj", "lst", NULL };
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OO", keywords, &obj, &lst)) {
+        return NULL;
+    }
+
+    // 确保输入是列表或元组
+    if (!PyList_Check(lst) && !PyTuple_Check(lst)) {
+        PyErr_SetString(PyExc_TypeError, "Expected a list or a tuple");
+        return NULL;
+    }
+
+    Py_ssize_t n = PySequence_Size(lst);
+    PyObject* item;
+
+    for (Py_ssize_t i = 0; i < n; ++i) {
+        item = PySequence_GetItem(lst, i);  // 获取索引i的元素
+        if (obj == item) {
+            Py_RETURN_TRUE;
+        }
+    }
+
+    Py_RETURN_FALSE;
+}
+
+/*
+ * List of functions to add to pyobj_extension in exec_pyobj_extension().
+ */
+static PyMethodDef pyobj_extension_functions[] = {
+    { "convptr", (PyCFunction)pyobj_extension_convptr, METH_VARARGS | METH_KEYWORDS, convptr_doc },
+    { "py_incref", (PyCFunction)py_incref, METH_VARARGS | METH_KEYWORDS, py_inc_doc },
+    { "py_decref", (PyCFunction)py_decref, METH_VARARGS | METH_KEYWORDS, py_dec_doc },
+    { "list_in", (PyCFunction)list_in, METH_VARARGS | METH_KEYWORDS, py_list_in_doc },
+    { NULL, NULL, 0, NULL } /* marks end of array */
+};
+
+/*
+ * Initialize pyobj_extension. May be called multiple times, so avoid
+ * using static state.
+ */
+int exec_pyobj_extension(PyObject* module) {
+    PyModule_AddFunctions(module, pyobj_extension_functions);
+
+    PyModule_AddStringConstant(module, "__author__", "qfcy");
+    PyModule_AddStringConstant(module, "__version__", "1.2.1");
+
+    return 0; /* success */
+}
+
+/* Documentation for pyobj_extension. */
+PyDoc_STRVAR(pyobj_extension_doc, u8"模块 pyobj_extension - pyobject库的C扩展模块, 提供一系列操作Python对象底层的函数。");
+
+static PyModuleDef_Slot pyobj_extension_slots[] = {
+    { Py_mod_exec, exec_pyobj_extension },
+    { 0, NULL }
+};
+
+static PyModuleDef pyobj_extension_def = {
+    PyModuleDef_HEAD_INIT,
+    "pyobj_extension",
+    pyobj_extension_doc,
+    0,              /* m_size */
+    NULL,           /* m_methods */
+    pyobj_extension_slots,
+    NULL,           /* m_traverse */
+    NULL,           /* m_clear */
+    NULL,           /* m_free */
+};
+
+PyMODINIT_FUNC PyInit_pyobj_extension() {
+    return PyModuleDef_Init(&pyobj_extension_def);
+}
