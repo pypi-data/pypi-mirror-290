35f8a2cec73f02a2dae048381387fdcdf489b0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:02:55 +0000

    Add package sus-amongus/0.1.2/sus_amongus-0.1.2.tar.gz

---
+++ packages/sus-amongus/sus_amongus-0.1.2.tar.gz/sus_amongus-0.1.2/README.md
@@ -0,0 +1,71 @@
+amogus suspcious
+
+# Stuff
+
+* sus_amongus.capitalize_sus(text: str) - returns a string with every sus word capitalized.
+```
+>>> sus_amongus.capitalize_sus("there is an imposter among us.")
+'there is an IMPOSTER AMONG US.'
+```
+
+
+* sus_amongus.get_sus(text: str) - returns a ඞ named tuple of every sus word in the string.
+```
+>>> sus_amongus.get_sus("there is an imposter among us.")
+ඞ(sus0='imposter', sus1='among', sus2='us')
+```
+
+
+* sus_amongus.is_sus(text: str) - returns True if the string contains any sus words.
+```
+>>> sus_amongus.is_sus("example sus string")
+True
+>>> sus_amongus.is_sus("example normal string")
+False
+```
+
+
+* sus_amongus.susify(text: str) - achieve maximum sussiness.
+```
+>>> sus_amongus.susify("not very sus string")
+'ඞඞඞ ඞඞඞඞ ඞඞඞ ඞඞඞඞඞඞ'
+```
+
+
+* sus_amongus.sus_tuple(*args) - make the funni ඞ named tuple
+```
+>>> sus_amongus.sus_tuple("sussy", "among us")
+ඞ(sus0='sussy', sus1='among us')
+```
+
+
+sus_amongus.ඞ(*args) - stupid function that makes stuff useless
+```
+>>> sus_amongus.ඞ(3.14, "hello", [1, 2])
+(6.9, 'ඞඞඞඞඞ', ඞ(sus0=(420,), sus1=(420,)))
+```
+
+
+* sus_amongus.amongus - sus
+```
+>>> print(sus_amogus.amongus)
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ 
+⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ 
+⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ 
+⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ 
+⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ 
+⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ 
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ 
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ 
+⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ 
+⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ 
+⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+```
---
+++ packages/sus-amongus/sus_amongus-0.1.2.tar.gz/sus_amongus-0.1.2/src/sus_amongus/__init__.py
@@ -0,0 +1,60 @@
+del _sus_amongus
+import re as _re
+import collections as _collections
+
+def get_sus(text: str):
+    regex = _re.compile(r'imposter|impostor|suspicious|sus|amongus|among|amogus|us', _re.I)
+    mo = regex.findall(text)
+    named = _collections.namedtuple('ඞ', ('sus'+str(i) for i in range(len(mo))))
+    return named(*mo)
+
+def capitalize_sus(text: str) -> str:
+    sus = ['imposter', 'impostor', 'suspicious', 'amongus', 'amogus', 'among', 'sus', 'us']
+    for i in sus:
+        text = text.replace(i, i.upper())
+    return text
+
+def is_sus(text: str) -> bool:
+    regex = _re.compile(r'imposter|impostor|suspicious|sus|amongus|among|amogus|us', _re.I)
+    return bool(regex.findall(text))
+
+def susify(text: str) -> str:
+    result = ''
+    for i in text:
+        if i != ' ' and i != '\n' and i != '\t':
+            result += 'ඞ'
+        else:
+            result += i
+    return result
+
+def sus_tuple(*args):
+    named = _collections.namedtuple('ඞ', ('sus'+str(i) for i in range(len(args))))
+    return named(*args)
+
+def ඞ(*args) -> tuple:
+    output = []
+    for arg in args:
+        if type(arg) == int:
+            output.append(420)
+        elif type(arg) == float:
+            output.append(6.9)
+        elif type(arg) == str:
+            output.append(susify(arg))
+        elif type(arg) == list or type(arg) == tuple:
+            named_args = []
+            for i in arg:
+               named_args.append(ඞ(i))
+            output.append(sus_tuple(*named_args))
+        elif type(arg) == dict:
+            named = _collections.namedtuple('ඞ', arg.keys())
+            named_args = []
+            for i in arg.values():
+                named_args.append(ඞ(i))
+            output.append(named(*named_args))
+        else:
+            class ඞඞ:
+                pass
+            output.append(ඞඞ())
+    return tuple(output)
+
+amongus = '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ \n⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ \n⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ \n⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ \n⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ \n⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ \n⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ \n⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ \n⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ \n⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ \n⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
