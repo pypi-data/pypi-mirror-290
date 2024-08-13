5131e16b4670af0ac4ea9bd1cc80af1e3995a00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:31 +0000

    Add package moralis/0.1.49/moralis-0.1.49.tar.gz

---
+++ packages/moralis/moralis-0.1.49.tar.gz/moralis-0.1.49/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+REQUIRES = [
+    "certifi >= 14.5.14",
+    "frozendict ~= 2.3.4",
+    "python-dateutil >= 2.8.2",
+    "typing_extensions >= 4.3.0",
+    "urllib3 >= 1.26.7",
+]
+
+setup(
+    name="moralis",
+    version="0.1.49",
+    description="The official Moralis Python SDK",
+    keywords=["Moralis", "Moralis SDK", "Moralis Python SDK", "Web3", "Crypto", "Ethereum", "Avalanche",
+              "Blockchain", "Contracts", "Eth", "Evm", "Fantom", "Nft", "Dapps", "Binance", "Solana"],
+    python_requires=">=3.7",
+    install_requires=REQUIRES,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
