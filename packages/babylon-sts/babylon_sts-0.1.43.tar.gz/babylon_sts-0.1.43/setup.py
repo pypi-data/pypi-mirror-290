5c6c83d80e7462a1e113eb0fd761e828c922c98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:15 +0000

    Add package babylon-sts/0.1.43/babylon_sts-0.1.43.tar.gz

---
+++ packages/babylon-sts/babylon_sts-0.1.43.tar.gz/babylon_sts-0.1.43/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='babylon_sts',
+    py_modules=["babylon_sts"],
+    version='0.1.43',
+    description='A powerful library for audio processing '
+                'with advanced features for speech recognition, '
+                'text translation, and speech synthesis.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Artur Rieznik',
+    author_email='artuar1990@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'whisper_timestamped',
+        'torch',
+        'pydub',
+        'soundfile',
+        'sentencepiece',
+        'omegaconf',
+        'sacremoses',
+        'transformers',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    url="https://github.com/Artuar/babylon_sts",
+    include_package_data=True,
+)
