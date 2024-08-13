e8cdbc39eba9170793f7de128dc3c3651799f7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:04:06 +0000

    Add package SYMANTIC/1.0.1/SYMANTIC-1.0.1.tar.gz

---
+++ packages/SYMANTIC/SYMANTIC-1.0.1.tar.gz/SYMANTIC-1.0.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 17 09:33:17 2023
+
+@author: muthyala.7
+"""
+
+import setuptools
+
+
+setuptools.setup(
+    name="SYMANTIC",
+    version="1.0.1",
+    author="Madhav Muthyala",
+    author_email="madhavreddymuthyala@gmail.com",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "torch",
+        "pandas",
+        "numpy",
+        "scipy",
+        "scikit-learn"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
