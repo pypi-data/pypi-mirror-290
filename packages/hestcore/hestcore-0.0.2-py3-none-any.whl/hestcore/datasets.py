95f0a483c57d6f66179e215aba24dedb1fe6126f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:33 +0000

    Add package hestcore/0.0.2/hestcore-0.0.2-py3-none-any.whl

---
+++ packages/hestcore/hestcore-0.0.2-py3-none-any.whl/hestcore/datasets.py
@@ -0,0 +1,27 @@
+from torch.utils.data import Dataset
+
+from .wsi import WSIPatcher
+
+
+class WSIPatcherDataset(Dataset):
+    
+    def __init__(self, patcher: WSIPatcher, transform):
+        self.patcher = patcher
+        
+        self.transform = transform
+                              
+
+    def __len__(self):
+        return len(self.patcher)
+    
+    def __getitem__(self, index):
+        
+        if self.patcher.coords_only:
+            x, y = self.patcher[index]
+        else:
+            tile, x, y = self.patcher[index]
+        
+        if self.transform:
+            tile = self.transform(tile)
+
+        return tile, (x, y)
