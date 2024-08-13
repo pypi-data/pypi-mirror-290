c3c0be4556bff48882967f4e69bdd9964e51ba8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:03:33 +0000

    Add package dandi-s3-log-parser/0.1.0/dandi_s3_log_parser-0.1.0-py3-none-any.whl

---
+++ packages/dandi-s3-log-parser/dandi_s3_log_parser-0.1.0-py3-none-any.whl/dandi_s3_log_parser/_ip_utils.py
@@ -0,0 +1,135 @@
+"""Various private utility functions for handling IP address related tasks."""
+
+import datetime
+import hashlib
+import ipaddress
+import traceback
+from importlib.metadata import version as importlib_version
+
+import ipinfo
+import requests
+import yaml
+from pydantic import FilePath
+
+from ._config import (
+    _IP_HASH_TO_REGION_FILE_PATH,
+    DANDI_S3_LOG_PARSER_BASE_FOLDER_PATH,
+    IPINFO_CREDENTIALS,
+    IPINFO_HASH_SALT,
+)
+
+
+def _cidr_address_to_ip_range(*, cidr_address: str) -> list[str]:
+    """Convert a CIDR address to a list of IP addresses."""
+    cidr_address_class = type(ipaddress.ip_address(cidr_address.split("/")[0]))
+    ip_address_range = []
+    if cidr_address_class is ipaddress.IPv4Address:
+        ip_address_range = ipaddress.IPv4Network(address=cidr_address)
+    elif cidr_address_class is ipaddress.IPv6Address:  # pragma: no cover
+        ip_address_range = ipaddress.IPv6Network(address=cidr_address)
+
+    return [str(ip_address) for ip_address in ip_address_range]
+
+
+def _get_latest_github_ip_ranges() -> list[str]:
+    """Retrieve the latest GitHub CIDR ranges from their API and expand them into a list of IP addresses."""
+    github_ip_request = requests.get("https://api.github.com/meta").json()
+
+    skip_keys = ["domains", "ssh_key_fingerprints", "verifiable_password_authentication", "ssh_keys"]
+    keys = set(github_ip_request.keys()) - set(skip_keys)
+    github_cidr_addresses = [
+        cidr_address for key in keys for cidr_address in github_ip_request[key] if "::" not in cidr_address  # Skip IPv6
+    ]
+
+    all_github_ips = [
+        str(ip_address)
+        for cidr_address in github_cidr_addresses
+        for ip_address in _cidr_address_to_ip_range(cidr_address=cidr_address)
+    ]
+
+    return all_github_ips
+
+
+def _load_ip_hash_to_region_cache() -> dict[str, str]:
+    """Load the IP hash to region cache from disk."""
+    if not _IP_HASH_TO_REGION_FILE_PATH.exists():
+        return {}  # pragma: no cover
+
+    with open(file=_IP_HASH_TO_REGION_FILE_PATH) as stream:
+        return yaml.load(stream=stream, Loader=yaml.SafeLoader)
+
+
+def _save_ip_hash_to_region_cache(*, ip_hash_to_region: dict[str, str]) -> None:
+    """Save the IP hash to region cache to disk."""
+    with open(file=_IP_HASH_TO_REGION_FILE_PATH, mode="w") as stream:
+        yaml.dump(data=ip_hash_to_region, stream=stream)
+
+
+def _save_ip_address_to_region_cache(
+    ip_hash_to_region: dict[str, str],
+    ip_hash_to_region_file_path: FilePath | None = None,
+) -> None:
+    """Save the IP address to region cache to disk."""
+    ip_hash_to_region_file_path = ip_hash_to_region_file_path or _IP_HASH_TO_REGION_FILE_PATH
+
+    with open(file=ip_hash_to_region_file_path, mode="w") as stream:
+        yaml.dump(data=ip_hash_to_region, stream=stream)
+
+
+def _get_region_from_ip_address(ip_address: str, ip_hash_to_region: dict[str, str]) -> str | None:
+    """
+    If the parsed S3 logs are meant to be shared openly, the remote IP could be used to directly identify individuals.
+
+    Instead, identify the generic region of the world the request came from and report that instead.
+    """
+    if ip_address == "unknown":
+        return "unknown"
+
+    ip_hash = hashlib.sha1(string=bytes(ip_address, "utf-8") + IPINFO_HASH_SALT).hexdigest()
+
+    # Early return for speed
+    lookup_result = ip_hash_to_region.get(ip_hash)
+    if lookup_result is not None:
+        return lookup_result
+
+    # Log errors in IP fetching
+    # Lines cannot be covered without testing on a real IP
+    try:  # pragma: no cover
+        handler = ipinfo.getHandler(access_token=IPINFO_CREDENTIALS)
+        details = handler.getDetails(ip_address=ip_address)
+
+        country = details.details.get("country", None)
+        region = details.details.get("region", None)
+
+        region_string = ""  # Not technically necessary, but quiets the linter
+        match (country is None, region is None):
+            case (True, True):
+                region_string = "unknown"
+            case (True, False):
+                region_string = region
+            case (False, True):
+                region_string = country
+            case (False, False):
+                region_string = f"{country}/{region}"
+        ip_hash_to_region[ip_hash] = region_string
+
+        return region_string
+    except ipinfo.exceptions.RequestQuotaExceededError:  # pragma: no cover
+        # Return the generic 'unknown' but do not cache
+        return "unknown"
+    except Exception as exception:  # pragma: no cover
+        errors_folder_path = DANDI_S3_LOG_PARSER_BASE_FOLDER_PATH / "errors"
+        errors_folder_path.mkdir(exist_ok=True)
+
+        dandi_s3_log_parser_version = importlib_version(distribution_name="dandi_s3_log_parser")
+        date = datetime.datetime.now().strftime("%y%m%d")
+        lines_errors_file_path = errors_folder_path / f"v{dandi_s3_log_parser_version}_{date}_ipinfo_errors.txt"
+
+        with open(file=lines_errors_file_path, mode="a") as io:
+            io.write(
+                f"Error fetching IP information for {ip_address}!\n\n"
+                f"{type(exception)}: {exception!s}\n\n"
+                f"{traceback.format_exc()}",
+            )
+
+        return "unknown"
---
+++ packages/dandi-s3-log-parser/dandi_s3_log_parser-0.1.0-py3-none-any.whl/dandi_s3_log_parser/_s3_log_line_parser.py
@@ -0,0 +1,231 @@
+"""
+Primary functions for parsing a single line of a raw S3 log.
+
+The strategy is to...
+
+1) Parse the raw line into a list of strings using a regex pattern.
+2) Construct a FullLogLine object from the parsed line. A collections.namedtuple object is used for performance.
+3) Reduce and map the information from the FullLogLine into a ReducedLogLine object.
+   This uses a lot less memory than the full version.
+   Some of the mapping operations at this step include...
+      - Identifying the DANDI asset ID from the full blob.
+      - Parsing the timestamp in memory as a datetime.datetime object.
+      - Filtering out log lines from excluded IPs (such as Drogon or GitHub actions).
+      - Converting the full remote IP to a country and region, so it can be saved without violating privacy.
+"""
+
+import collections
+import datetime
+import importlib.metadata
+import pathlib
+import re
+
+from ._config import DANDI_S3_LOG_PARSER_BASE_FOLDER_PATH
+
+_FULL_PATTERN_TO_FIELD_MAPPING = [
+    "bucket_owner",
+    "bucket",
+    "timestamp",
+    "ip_address",
+    "requester",
+    "request_id",
+    "operation",
+    "asset_id",
+    "request_uri",
+    # "http_version",  # Regex not splitting this from the request_uri...
+    "status_code",
+    "error_code",
+    "bytes_sent",
+    "object_size",
+    "total_time",
+    "turn_around_time",
+    "referrer",
+    "user_agent",
+    "version",
+    "host_id",
+    "sigv",
+    "cipher_suite",
+    "auth_type",
+    "endpoint",
+    "tls_version",
+    "access_point_arn",
+]
+_REDUCED_PATTERN_TO_FIELD_MAPPING = ["asset_id", "timestamp", "bytes_sent", "ip_address"]
+
+_FullLogLine = collections.namedtuple("FullLogLine", _FULL_PATTERN_TO_FIELD_MAPPING)
+_ReducedLogLine = collections.namedtuple("ReducedLogLine", _REDUCED_PATTERN_TO_FIELD_MAPPING)
+
+_S3_LOG_REGEX = re.compile(pattern=r'"([^"]+)"|\[([^]]+)]|([^ ]+)')
+
+
+def _find_all_possible_substring_indices(*, string: str, substring: str) -> list[int]:
+    indices = list()
+    start = 0
+    while True:
+        next_index = string.find(substring, start)
+        if next_index == -1:  # .find(...) was unable to locate the substring
+            break
+        indices.append(next_index)
+        start = next_index + 1
+
+    return indices
+
+
+def _attempt_to_remove_quotes(*, raw_line: str, bad_parsed_line: str) -> str:
+    """
+    Attempt to remove bad quotes from a raw line of an S3 log file.
+
+    These quotes are not properly escaped and are causing issues with the regex pattern.
+    Various attempts to fix the regex failed, so this is the most reliable correction I could find.
+    """
+    starting_quotes_indices = _find_all_possible_substring_indices(string=raw_line, substring=' "')
+    ending_quotes_indices = _find_all_possible_substring_indices(string=raw_line, substring='" ')
+
+    # If even further unexpected structure, just return the bad parsed line so that the error reporter can catch it
+    if len(starting_quotes_indices) == 0:  # pragma: no cover
+        return bad_parsed_line
+    if len(starting_quotes_indices) != len(ending_quotes_indices):  # pragma: no cover
+        return bad_parsed_line
+
+    cleaned_raw_line = raw_line[0 : starting_quotes_indices[0]]
+    for counter in range(1, len(starting_quotes_indices) - 1):
+        next_block = raw_line[ending_quotes_indices[counter - 1] + 2 : starting_quotes_indices[counter]]
+        cleaned_raw_line += " - " + next_block
+    cleaned_raw_line += " - " + raw_line[ending_quotes_indices[-1] + 2 :]
+
+    return cleaned_raw_line
+
+
+def _parse_s3_log_line(*, raw_line: str) -> list[str]:
+    """
+    The current method of parsing lines of an S3 log file.
+
+    Bad lines reported in https://github.com/catalystneuro/dandi_s3_log_parser/issues/18 led to quote scrubbing
+    as a pre-step. No self-contained single regex was found that could account for this uncorrected strings.
+    """
+    parsed_log_line = [a or b or c for a, b, c in _S3_LOG_REGEX.findall(string=raw_line)]
+
+    number_of_parsed_items = len(parsed_log_line)
+
+    # Everything worked as expected
+    if number_of_parsed_items <= 26:
+        return parsed_log_line
+
+    potentially_cleaned_raw_line = _attempt_to_remove_quotes(raw_line=raw_line, bad_parsed_line=parsed_log_line)
+    parsed_log_line = [a or b or c for a, b, c in _S3_LOG_REGEX.findall(string=potentially_cleaned_raw_line)]
+
+    return parsed_log_line
+
+
+def _get_full_log_line(
+    *,
+    parsed_log_line: list[str],
+    log_file_path: pathlib.Path,
+    index: int,
+    raw_line: str,
+) -> _FullLogLine | None:
+    """Construct a FullLogLine from a single parsed log line, or dump to error collection file and return None."""
+    full_log_line = None
+
+    number_of_parsed_items = len(parsed_log_line)
+    match number_of_parsed_items:
+        # ARN not detected
+        case 24:
+            parsed_log_line.append("-")
+            full_log_line = _FullLogLine(*parsed_log_line)
+        # Expected length for good lines
+        case 25:
+            full_log_line = _FullLogLine(*parsed_log_line)
+        # Happens for certain types of HEAD requests; not sure what the extra element is
+        case 26:
+            full_log_line = _FullLogLine(*parsed_log_line[:25])
+
+    # Deviant log entry; usually some very ill-formed content in the URI
+    # Dump information to a log file in the base folder for easy sharing
+    if full_log_line is None:  # pragma: no cover
+        errors_folder_path = DANDI_S3_LOG_PARSER_BASE_FOLDER_PATH / "errors"
+        errors_folder_path.mkdir(exist_ok=True)
+
+        dandi_s3_log_parser_version = importlib.metadata.version(distribution_name="dandi_s3_log_parser")
+        date = datetime.datetime.now().strftime("%y%m%d")
+        lines_errors_file_path = errors_folder_path / f"v{dandi_s3_log_parser_version}_{date}_lines_errors.txt"
+
+        with open(file=lines_errors_file_path, mode="a") as io:
+            io.write(f"Line {index} of {log_file_path} (parsed {number_of_parsed_items} items): {raw_line}\n\n")
+
+    return full_log_line
+
+
+def _append_reduced_log_line(
+    *,
+    raw_line: str,
+    reduced_log_lines: list[_ReducedLogLine],
+    bucket: str,
+    request_type: str,
+    excluded_ips: collections.defaultdict[str, bool],
+    log_file_path: pathlib.Path,
+    index: int,
+) -> None:
+    """
+    Append the `reduced_log_lines` list with a ReducedLogLine constructed from a single raw log line, if it is valid.
+
+    Parameters
+    ----------
+    raw_line : string
+        A single line from the raw S3 log file.
+    reduced_log_lines : list of ReducedLogLine
+        The list of ReducedLogLine objects to mutate in place.
+        This is done to reduce overhead of copying/returning items in-memory via a return-based approach.
+    bucket : string
+        Only parse and return lines that match this bucket string.
+    request_type : string
+        The type of request to filter for.
+    excluded_ips : collections.defaultdict of strings to booleans
+        A lookup table / hash map whose keys are IP addresses and values are True to exclude from parsing.
+
+    """
+    bucket = "" if bucket is None else bucket
+    excluded_ips = excluded_ips or collections.defaultdict(bool)
+
+    parsed_log_line = _parse_s3_log_line(raw_line=raw_line)
+
+    full_log_line = _get_full_log_line(
+        parsed_log_line=parsed_log_line,
+        log_file_path=log_file_path,
+        index=index,
+        raw_line=raw_line,
+    )
+
+    if full_log_line is None:
+        return
+
+    # Various early skip conditions
+    if full_log_line.bucket != bucket:
+        return
+
+    # Skip all non-success status codes (those in the 200 block)
+    if full_log_line.status_code[0] != "2":
+        return
+
+    # Derived from operation string, e.g., "REST.GET.OBJECT"
+    parsed_request_type = full_log_line.operation.split(".")[1]
+    if parsed_request_type != request_type:
+        return
+
+    if excluded_ips[full_log_line.ip_address] is True:
+        return
+
+    assert (
+        full_log_line.timestamp[-5:] == "+0000"
+    ), f"Unexpected time shift attached to log! Have always seen '+0000', found '{full_log_line.timestamp[-5:]}'."
+
+    parsed_timestamp = datetime.datetime.strptime(full_log_line.timestamp[:-6], "%d/%b/%Y:%H:%M:%S")
+    parsed_bytes_sent = int(full_log_line.bytes_sent) if full_log_line.bytes_sent != "-" else 0
+    reduced_log_line = _ReducedLogLine(
+        asset_id=full_log_line.asset_id,
+        timestamp=parsed_timestamp,
+        bytes_sent=parsed_bytes_sent,
+        ip_address=full_log_line.ip_address,
+    )
+
+    reduced_log_lines.append(reduced_log_line)
