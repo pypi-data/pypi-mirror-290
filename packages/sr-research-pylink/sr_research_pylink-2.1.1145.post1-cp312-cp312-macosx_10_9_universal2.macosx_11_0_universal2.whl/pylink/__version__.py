9144d0930b5b016ddd6049b17c0866f8e2026958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:42 +0000

    Add package sr-research-pylink/2.1.1145.post1/sr_research_pylink-2.1.1145.post1-cp312-cp312-macosx_10_9_universal2.macosx_11_0_universal2.whl

---
+++ packages/sr-research-pylink/sr_research_pylink-2.1.1145.post1-cp312-cp312-macosx_10_9_universal2.macosx_11_0_universal2.whl/pylink/__version__.py
@@ -0,0 +1,9 @@
+
+__title__ = 'pylink'
+__description__ = 'Python module for interfacing SR Research EyeLink eye trackers'
+__url__ = 'https://www.sr-support.com'
+__version__ = '2.1.1145.post1'
+__author__ = 'SR Research.com'
+__author_email__ = 'support@sr-research.com'
+__license__ = 'SR Research Ltd.'
+__copyright__ = 'Copyright 2024 SR Research Ltd.'
