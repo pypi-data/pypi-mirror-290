e632110d5349f80f8c7d9286442c8fac92b1dcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:50 +0000

    Add package org.vuepy.core/0.1.4/org.vuepy.core-0.1.4.tar.gz

---
+++ packages/org.vuepy.core/org.vuepy.core-0.1.4.tar.gz/org.vuepy.core-0.1.4/src/ipywui/components/Combobox.vue
@@ -0,0 +1,39 @@
+<template>
+
+</template>
+
+<script>
+// el-autocomplete.
+export default {
+  name: "Combobox",
+  props: {
+    value: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      default: () => []
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    style: {
+      // background, description_width, font_size, text_color
+      type: string,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
