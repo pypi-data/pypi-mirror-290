e798b646afc846e49ac6135e3c827e1896061924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:20 +0000

    Add package tutorial-packaging-python-projects/2024.8.4/tutorial_packaging_python_projects-2024.8.4-py3-none-any.whl

---
+++ packages/tutorial-packaging-python-projects/tutorial_packaging_python_projects-2024.8.4-py3-none-any.whl/tutorial_packaging_python_projects/example.py
@@ -0,0 +1,11 @@
+"""Example methods to demonstrate a simple Python project setup."""
+
+
+def add_one(number: int) -> int:
+    """
+    Adds one to the given number.
+
+    :param number: The given number.
+    :return: Sum of the given number and one.
+    """
+    return number + 1
