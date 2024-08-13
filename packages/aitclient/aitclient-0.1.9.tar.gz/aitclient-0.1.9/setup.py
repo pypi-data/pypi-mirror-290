10edb295ffb56a848f82df97400831e2df1f4ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 03:03:33 +0000

    Add package aitclient/0.1.9/aitclient-0.1.9.tar.gz

---
+++ packages/aitclient/aitclient-0.1.9.tar.gz/aitclient-0.1.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aitclient',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'aitclient=aitclient:main',
+        ],
+    },
+    author='Bobby Svidron',
+    author_email='bobby.svidron@sap.com',
+    description='A Python library for AIT client operations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/I835834/aitclient',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
