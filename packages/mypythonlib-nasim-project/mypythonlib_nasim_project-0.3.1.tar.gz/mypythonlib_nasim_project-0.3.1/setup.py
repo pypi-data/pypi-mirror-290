f70963a5556f3c707e0a629c1b075cb01d630364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 05:04:08 +0000

    Add package mypythonlib-nasim-project/0.3.1/mypythonlib_nasim_project-0.3.1.tar.gz

---
+++ packages/mypythonlib-nasim-project/mypythonlib_nasim_project-0.3.1.tar.gz/mypythonlib_nasim_project-0.3.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mypythonlib_nasim_project',
+    packages=find_packages(),
+    version='0.3.1',
+    description='My first Python library',
+    author='Me',
+)
