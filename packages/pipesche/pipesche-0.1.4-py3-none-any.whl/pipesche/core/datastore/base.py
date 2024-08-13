148ae1eaca3f0ef2b7abd4ff514f2c6606a59c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:06 +0000

    Add package pipesche/0.1.4/pipesche-0.1.4-py3-none-any.whl

---
+++ packages/pipesche/pipesche-0.1.4-py3-none-any.whl/pipesche/core/datastore/base.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class BaseDataStore(ABC):
+    @abstractmethod
+    def get(self, key: str) -> Any:
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    @abstractmethod
+    def exist(self, key: str) -> bool:
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    @abstractmethod
+    def set(self, key: str, value: Any) -> None:
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    @abstractmethod
+    def delete(self, key: str) -> None:
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    @abstractmethod
+    def clear(self) -> None:
+        raise NotImplementedError("Subclasses should implement this method.")
