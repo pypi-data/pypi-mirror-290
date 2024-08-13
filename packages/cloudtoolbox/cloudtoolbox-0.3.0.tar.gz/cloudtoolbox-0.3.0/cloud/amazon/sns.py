fd3d0ecb2083ec3ca0d84fc7d1b92f21d31bce74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:05 +0000

    Add package cloudtoolbox/0.3.0/cloudtoolbox-0.3.0.tar.gz

---
+++ packages/cloudtoolbox/cloudtoolbox-0.3.0.tar.gz/cloudtoolbox-0.3.0/cloud/amazon/sns.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+import boto3
+
+
+class Publisher:
+    def __init__(self, *args: Any, **kwargs: Any):
+        self.client = boto3.client("sns", *args, **kwargs)
+
+    def publish(self, recipient: str, message: str, **attrs: Any) -> None:
+        self.client.publish(TargetArn=recipient, Message=message, **attrs)
---
+++ packages/cloudtoolbox/cloudtoolbox-0.3.0.tar.gz/cloudtoolbox-0.3.0/cloud/amazon/sqs.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+import boto3
+
+
+class Publisher:
+    def __init__(self, *args: Any, **kwargs: Any):
+        self.client = boto3.client("sqs", *args, **kwargs)
+
+    def publish(self, recipient: str, message: str, **attrs: Any) -> None:
+        self.client.send_message(QueueUrl=recipient, MessageBody=message, **attrs)
