20e9070541b5a68bfc75f4c21e0aa9383d312a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:13 +0000

    Add package tomifier/0.0.5/tomifier-0.0.5-py3-none-any.whl

---
+++ packages/tomifier/tomifier-0.0.5-py3-none-any.whl/tomifier/defaults.py
@@ -0,0 +1,150 @@
+LICENSE_TXT = '''MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+README_MD = '''# `<package_name>`
+
+Update this files with instructions for your package.
+
+## To run the package type:
+
+`<name> ui`
+'''
+
+MANIFEST = '''include-recursive <name>/root/static *
+'''
+
+PYPROJECT = '''[build-system]
+requires = ["setuptools", "setuptools-scm"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "<package_name>"
+authors = [
+  { name="<author>", email="<email>" },
+]
+description = "<description>"
+readme = "README.md"
+license = { file="LICENSE" }
+requires-python = ">=3.9"
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+dependencies = [
+<DEPS>]
+dynamic = ["version"]
+
+[tool.setuptools]
+include-package-data = true
+
+[tool.setuptools.dynamic]
+version = {attr = "<name>.version.VERSION"}
+readme = {file = ["README.md"]}
+
+[tool.setuptools.packages.find]
+include = ["<name>*"]
+exclude = ["*.tests*"]
+namespaces = false
+
+[tool.setuptools.package-data]
+"<name>" = ["*.*"]
+
+[project.urls]
+"Homepage" = "<home_page>"
+
+[project.scripts]
+<name> = "<name>.cmd.root:main"
+'''
+
+BUILD_SCRIPT = '''rm -rf dist
+pip uninstall <package_name> -y
+python -m build
+pip install -e .
+<name> ui
+'''
+
+ROOT_PY = '''import click
+import fastapi
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+import os
+
+@click.group()
+def cli():
+    click.echo("<name> CLI")
+
+@cli.command()
+def ui():
+    app = fastapi.FastAPI()
+    
+    @app.get('/api/status')
+    def read_root():
+        return {'status': 'healthy'}
+    
+    local_folder = os.path.dirname(os.path.abspath(__file__))
+    static_foler = os.path.join(local_folder, 'static')
+    print(static_foler)
+    app.mount("/", StaticFiles(directory=static_foler,html = True), name="static")
+
+    uvicorn.run(app)
+
+def main():
+    cli()
+
+if __name__ == "__main__":
+    main()
+'''
+
+VERSION_PY = '''version="0.0.1"
+__version__ = version
+VERSION=version
+'''
+
+SETUP_PY = '''from setuptools import setup
+
+setup()
+'''
+
+INIT_PY = '''from .root import *
+'''
+
+INDEX_HTML = '''<!DOCTYPE html>
+<html>
+<head>
+    <title>Tomifier</title>
+</head>
+<body>
+    <h1>Tomifier</h1>
+    <p>Tomify your Python projects</p>
+</body>
+</html>
+'''
+
+REQUIREMENTS_TXT = '''<DEPS>'''
+
+GIT_IGNORE = '''__pycache__
+*.egg-info
+dist
+
+.venv
+.env
+'''
