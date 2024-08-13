24fa4429b7aff70615d8514cd3032acf7b508efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 04:03:55 +0000

    Add package gibson-cli/0.3.2/gibson_cli-0.3.2-py3-none-any.whl

---
+++ packages/gibson-cli/gibson_cli-0.3.2-py3-none-any.whl/gibson/conf/Paths.py
@@ -0,0 +1,10 @@
+class ConfigPaths:
+    def __init__(self):
+        self.auth = None
+
+
+class ProjectPaths:
+    def __init__(self):
+        self.config = None
+        self.memory = None
+        self.top = None
