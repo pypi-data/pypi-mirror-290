4ae0794515c083a446e2e12af2fed3561f753719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:39 +0000

    Add package TicTacCode/0.0.1/tictaccode-0.0.1.tar.gz

---
+++ packages/TicTacCode/tictaccode-0.0.1.tar.gz/tictaccode-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name="TicTacCode",
+    version="0.0.1",
+    author="Amos Xiao",
+    author_email="AmosXiao1000020@proton.me",
+    description="Python Tic-Tac-Toe Library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[],
+    python_requires='>=3.10',
+)
