3be80a2e20066178fc991f70f1257deb31f015f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:04:03 +0000

    Add package rockset-v2-internal/0.0.9/rockset_v2_internal-0.0.9-py3-none-any.whl

---
+++ packages/rockset-v2-internal/rockset_v2_internal-0.0.9-py3-none-any.whl/rockset/apis/__init__.py
@@ -0,0 +1,35 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.api_keys_api import APIKeys
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from rockset.api.api_keys_api import APIKeys
+from rockset.api.aliases_api import Aliases
+from rockset.api.box_api import Box
+from rockset.api.collections_api import Collections
+from rockset.api.custom_roles_api import CustomRoles
+from rockset.api.deployment_settings_api import DeploymentSettings
+from rockset.api.deployments_api import Deployments
+from rockset.api.documents_api import Documents
+from rockset.api.integrations_api import Integrations
+from rockset.api.organizations_api import Organizations
+from rockset.api.queries_api import Queries
+from rockset.api.query_lambdas_api import QueryLambdas
+from rockset.api.scheduled_lambdas_api import ScheduledLambdas
+from rockset.api.search_api import Search
+from rockset.api.shared_lambdas_api import SharedLambdas
+from rockset.api.users_api import Users
+from rockset.api.views_api import Views
+from rockset.api.virtual_instances_api import VirtualInstances
+from rockset.api.workspaces_api import Workspaces
