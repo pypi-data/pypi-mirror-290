59bf68ea94a9cef86c4e769cf7c3fa1e6ed4be88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:34 +0000

    Add package mrn-behave/1.0.4/mrn_behave-1.0.4.tar.gz

---
+++ packages/mrn-behave/mrn_behave-1.0.4.tar.gz/mrn_behave-1.0.4/disrnn/fit_disrnn.py
@@ -0,0 +1,240 @@
+import sys
+sys.path.append('/Users/jeremyschroeter/Desktop/School_Work/Steinmetz_Lab/behavioral_modeling_2')
+from disrnn.disrnn import DisRNN
+from disrnn.disrnn_utils import DisRNNDataset
+from data.data_utils import custom_collate_fn
+import torch
+from torch import Tensor
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+
+
+def train(
+        subject: str,
+        fold: int,
+        epochs: int,
+        lr: float,
+        beta_weight: float
+) -> None:
+    
+    # Setup
+    model = DisRNN(
+        num_latents=5,
+        num_obs=3,
+        include_stimulus=True,
+        update_mlp_hidden_size=[5, 5, 5],
+        choice_mlp_hidden_size=[3, 3]
+    )
+    model = torch.compile(model)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    train_set = DisRNNDataset(subject, 'train', fold)
+    val_set = DisRNNDataset(subject, 'val', fold)
+    train_loader = DataLoader(
+        train_set,
+        batch_size=8,
+        collate_fn=custom_collate_fn
+    )
+    val_loader = DataLoader(
+        val_set,
+        batch_size=8,
+        collate_fn=custom_collate_fn
+    )
+
+    summary = {
+        split : {
+            'loss': {
+                'nll' : {
+                    'total' : [],
+                    'c_stim' : [],
+                    'o_stim' : []
+                },
+                'kld' : {
+                    'total' : [],
+                    'c_stim' : [],
+                    'o_stim' : []
+                }
+            },
+            'score' : {
+                'total' : [],
+                'c_stim' : [],
+                'o_stim' : []
+            },
+            'acc' : {
+                'total' : [],
+                'c_stim' : [],
+                'o_stim' : []
+            }
+
+        }
+        for split in ['train', 'val']
+    }
+
+    summary['model'] = {
+        'global_sigmas' : [],
+        'global_multipliers' : [],
+        'update_sigmas' : [],
+        'update_multipliers' : []
+    }
+
+    summary['grad_norm'] = []
+    model_checkpoint = None
+
+
+    # Begin training
+    for epoch in range(epochs):
+        for split in ['train', 'val']:
+            if split == 'val':
+                torch.set_grad_enabled(False)
+                model.eval()
+                loader = val_loader
+                dataset = val_set
+            else:
+                torch.set_grad_enabled(True)
+                model.train()
+                loader = train_loader
+                dataset = train_set
+            
+            epoch_c_stim_nll = 0
+            epoch_o_stim_nll = 0
+
+            epoch_c_stim_kld = 0
+            epoch_o_stim_kld = 0
+
+            epoch_c_stim_score = 0
+            epoch_o_stim_score = 0
+            
+            epoch_c_stim_correct = 0
+            epoch_o_stim_correct = 0
+
+
+            for (X, y, mask) in loader:
+                X = X.float()
+                y = y.long()
+                optimizer.zero_grad()
+                if split == 'train':
+                    summary['model']['global_sigmas'].append(model.global_sigmas)
+                    summary['model']['global_multipliers'].append(model.global_multipliers)
+                    summary['model']['update_sigmas'].append(model.update_sigmas)
+                    summary['model']['update_multipliers'].append(model.update_multipliers)
+
+                B, T, _ = X.shape
+
+                # Initialize containers for loss
+                cumulated_logits = torch.zeros(size=(B, T - 1, 2))
+                cumulated_kld_loss = torch.zeros(size=(B, T - 1))
+
+                # Iterate over timesteps
+                # Forward pass
+                for t in range(T - 1):
+                    obs = X[:, t]
+                    s_t = X[:, t + 1, 0].unsqueeze(-1)
+                    if t == 0:
+                        dummy_z0 = torch.zeros(size=(B, model.num_latents))
+                        logits, updated_z, global_kld, update_kld = model.forward(
+                            dummy_z0, obs, True, s_t
+                        )
+                    else:
+                        logits, updated_z, global_kld, update_kld = model.forward(
+                            updated_z, obs, False, s_t
+                        )
+
+                    cumulated_logits[:, t] = logits
+                    global_kld = global_kld.sum()
+                    update_kld = update_kld.sum()
+                    cumulated_kld_loss[:, t] = global_kld + update_kld
+
+                # Flatten and apply mask
+                X = X[:, 1:, :].reshape(-1, 3)
+                y = y.view(-1)
+                cumulated_logits = cumulated_logits.view(-1, 2)
+                kld_loss = cumulated_kld_loss.view(-1)
+                mask = mask.view(-1)
+
+                # Calculate cross entropy and apply mask to loss
+                probs = cumulated_logits.softmax(-1)
+                log_probs = probs.log()
+                nll = F.nll_loss(log_probs, y, reduction='none')
+                nll[mask == False] = 0
+                kld_loss[mask == False] = 0
+
+                # Backward pass
+                loss = (nll + kld_loss * beta_weight).mean()
+                print(split)
+                if split == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # Get trial identity indices
+                center_stim_trials = torch.where((X[:, 0] == 0) & (mask == True))
+                outside_stim_trials = torch.where((X[:, 0] != 0) & (mask == True))
+
+                # Filter loss by trial type
+                epoch_c_stim_nll += nll[center_stim_trials].sum().item()
+                epoch_o_stim_nll += nll[outside_stim_trials].sum().item()
+                epoch_c_stim_kld += kld_loss[center_stim_trials].sum().item() * beta_weight
+                epoch_o_stim_kld += kld_loss[outside_stim_trials].sum().item() * beta_weight
+
+                # Compute score
+                correct_probs = probs[torch.arange(len(probs)), y]
+                epoch_c_stim_score += correct_probs[center_stim_trials].sum().item()
+                epoch_o_stim_score += correct_probs[outside_stim_trials].sum().item()
+
+                # Compute correct trials
+                choice = probs.argmax(-1)
+                correct_choices = (choice == y)
+                epoch_c_stim_correct += correct_choices[center_stim_trials].sum()
+                epoch_o_stim_correct += correct_choices[outside_stim_trials].sum()
+
+            
+
+            summary[split]['loss']['nll']['total'].append((epoch_c_stim_nll + epoch_o_stim_nll) / dataset.length)
+            summary[split]['loss']['nll']['c_stim'].append(epoch_c_stim_nll / dataset.c_stim_trials)
+            summary[split]['loss']['nll']['o_stim'].append(epoch_o_stim_nll / dataset.o_stim_trials)
+
+            summary[split]['loss']['kld']['total'].append((epoch_c_stim_kld + epoch_o_stim_kld) / dataset.length)
+            summary[split]['loss']['kld']['c_stim'].append(epoch_c_stim_kld / dataset.c_stim_trials)
+            summary[split]['loss']['kld']['o_stim'].append(epoch_o_stim_kld / dataset.o_stim_trials)
+
+            summary[split]['score']['total'].append((epoch_c_stim_score + epoch_o_stim_score) / dataset.length)
+            summary[split]['score']['c_stim'].append(epoch_c_stim_score / dataset.c_stim_trials)
+            summary[split]['score']['o_stim'].append(epoch_o_stim_score / dataset.o_stim_trials)
+
+            summary[split]['acc']['total'].append((epoch_c_stim_correct + epoch_o_stim_correct) / dataset.length)
+            summary[split]['acc']['c_stim'].append(epoch_c_stim_correct / dataset.c_stim_trials)
+            summary[split]['acc']['o_stim'].append(epoch_o_stim_correct / dataset.o_stim_trials)
+
+        print('=' * 30)
+        print(f'Epoch {epoch + 1}')
+        print('Training')
+        print(f"NLL: {summary['train']['loss']['nll']['total'][-1]:0.4f}, KLD: {summary['train']['loss']['kld']['total'][-1]:0.4f}")
+        print(f"Score: {summary['train']['score']['total'][-1]:0.4f}, Accuracy: {summary['train']['acc']['total'][-1]}")
+        print('-' * 15)
+        print('Validation')
+        print(f"NLL: {summary['val']['loss']['nll']['total'][-1]:0.4f}, KLD: {summary['val']['loss']['kld']['total'][-1]:0.4f}")
+        print(f"Score: {summary['val']['score']['total'][-1]:0.4f}, Accuracy: {summary['val']['acc']['total'][-1]}")
+    return summary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/mrn-behave/mrn_behave-1.0.4.tar.gz/mrn_behave-1.0.4/linear_switching/linear_switching_utils.py
@@ -0,0 +1,171 @@
+import sys
+sys.path.append(r'c:\Users\jerem\Desktop\behavioral_modeling')
+from data.data_utils import load_master_dataset, load_split
+import torch
+from torch import Tensor
+import numpy as np
+from torch.utils.data import Dataset
+from linear_switching.linear_switching import LinearSwitching
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+
+def convert_dataset(dataset: list[dict]):
+    observations = {
+        (-1, 1, 1) : 0,
+        (0, -1, 1) : 1,
+        (0, 1, 1) : 2,
+        (1, -1, 1) : 3,
+        (-1, -1, 0) : 4,
+        (0, 1, 0) : 5,
+        (0, -1, 0) : 6,
+        (1, 1, 0) : 7
+    }
+    for session in dataset:
+        X = session['X']
+        session['X'] = np.array(list(map(lambda x: observations[tuple(x)], session['X'])))
+    return dataset
+
+
+class LinearSwitchingDataset(Dataset):
+    def __init__(self, subject: str, split: str, fold: int) -> None:
+        super().__init__()
+        full_dataset = load_master_dataset(subject)
+        self.split = split
+        self.fold = load_split(subject, fold)
+        self.sessions = self.fold[split]
+        self.dataset = list(filter(lambda x: x['session_id'] in self.sessions, full_dataset))
+        self.dataset = convert_dataset(self.dataset)
+
+    def __getitem__(self, index) -> Tensor:
+        session = self.dataset[index]
+        X, y = session['X'], session['y'][1:]
+        y[y == -1] = 0
+        return torch.from_numpy(X), torch.from_numpy(y)
+        
+    def __len__(self) -> int:
+        return len(self.dataset)
+    
+
+def evaluate(model: LinearSwitching, loader: DataLoader) -> dict:
+    summary = {
+        'total_loss' : [],
+        'center_stim_loss' : [],
+        'outside_stim_loss' : [],
+        'total_score' : [],
+        'center_stim_score' : [],
+        'outside_stim_score' : [],
+        'total_acc' : [],
+        'center_stim_acc' : [],
+        'outside_stim_acc' : []
+    }
+    # Data observation mapping
+    left_stim = torch.tensor([0, 4])
+    center_stim = torch.tensor([1, 2, 5, 6])
+    right_stim = torch.tensor([3, 7])
+    # Initialize metrics
+    total_loss = 0
+    outside_stim_loss = 0
+    center_stim_loss = 0
+    total_count = 0
+    outside_stim_count = 0
+    center_stim_count = 0
+    total_correct = 0
+    outside_stim_correct = 0
+    center_stim_correct = 0
+    probs = []
+    outside_stim_probs = []
+    center_stim_probs = []
+    # Iterate over batches
+    for (X, y, mask) in loader:
+
+        y[y == -1] = 0
+        B, T = X.shape
+        cumulated_log_probs = torch.zeros(size=(B, T - 1, 2))
+        z = model.z_0.expand(size=(B, 1))
+
+        for t in range(T - 1):
+            z = model(z, X[:, t])
+            l_stim_trials = torch.where(torch.isin(X[:, t + 1], left_stim))[0]
+            r_stim_trials = torch.where(torch.isin(X[:, t + 1], right_stim))[0]
+            c_stim_trials = torch.where(torch.isin(X[:, t + 1], center_stim))[0]
+
+            # For each trial, if center-stim transform latent into log probability
+            # Outside stim trials are log(1)
+            log_probs = model.temperature.exp() * z[c_stim_trials, 0]
+            p = F.sigmoid(log_probs)
+            cumulated_log_probs[c_stim_trials, t, 0] = p.log()
+            cumulated_log_probs[c_stim_trials, t, 1] = (1 - p).log()
+
+        # Flatten log probabilities
+        cumulated_log_probs = cumulated_log_probs.view(-1, 2)
+        y = y.view(-1)
+        mask = mask.view(-1)
+        X = X[:, 1:].reshape(-1)[mask == True]
+
+        # Indices of different trial types
+        l_stim_trials = torch.where(torch.isin(X, left_stim))[0]
+        r_stim_trials = torch.where(torch.isin(X, right_stim))[0]
+        c_stim_trials = torch.where(torch.isin(X, center_stim))[0]
+
+        # Counts for different trial types
+        total_count += mask.sum().item()
+        outside_stim_count += len(l_stim_trials) + len(r_stim_trials)
+        center_stim_count += len(c_stim_trials)
+
+        # Calculate loss on legit trials
+        loss = F.nll_loss(cumulated_log_probs, y.long(), reduction='none')
+        masked_loss = loss * mask
+        masked_loss[l_stim_trials] = 0
+        masked_loss[r_stim_trials] = 0
+
+        # Accumulate loss for different trial types
+        total_loss += masked_loss.sum().item()
+        outside_stim_loss += (masked_loss[l_stim_trials].sum() + masked_loss[r_stim_trials].sum()).item()
+        center_stim_loss += masked_loss[c_stim_trials].sum().item()
+
+        # Accumulate correct trials for computing accuracy
+        probabilities = cumulated_log_probs[mask == True].exp()
+        probabilities[l_stim_trials, 1] = 1
+        probabilities[l_stim_trials, 0] = 0
+        probabilities[r_stim_trials, 0] = 1
+        probabilities[r_stim_trials, 1] = 0
+        choices = probabilities.argmax(-1)
+        choices = choices == y[mask == True]
+        outside_stim_correct += torch.cat([choices[l_stim_trials], choices[r_stim_trials]]).sum()
+        center_stim_correct += choices[c_stim_trials].sum()
+        total_correct += choices.sum()
+        probabilities = probabilities[torch.arange(len(probabilities)), y[mask == True].int()]
+        probs.append(probabilities)
+        outside_stim_probs.append(torch.cat([probabilities[l_stim_trials], probabilities[r_stim_trials]]))
+        center_stim_probs.append(probabilities[c_stim_trials])
+
+    summary['total_acc'].append(total_correct / total_count)
+    summary['center_stim_acc'].append(center_stim_correct / center_stim_count)
+    summary['outside_stim_acc'].append(outside_stim_correct / outside_stim_count)
+
+    summary['total_loss'].append(total_loss / total_count)
+    summary['center_stim_loss'].append(center_stim_loss / center_stim_count)
+    summary['outside_stim_loss'].append(outside_stim_loss / outside_stim_count)
+
+    probs = torch.cat(probs)
+    outside_stim_probs = torch.cat(outside_stim_probs)
+    center_stim_probs = torch.cat(center_stim_probs)
+    summary['total_score'].append(probs.mean().item())
+    summary['center_stim_score'].append(center_stim_probs.mean().item())
+    summary['outside_stim_score'].append(outside_stim_probs.mean().item())
+
+    return summary
+
+def latent(model: LinearSwitching, dataset: LinearSwitchingDataset, session: int) -> np.ndarray:
+    zs = []
+    X, y = dataset[session]
+    with torch.no_grad():
+        T = len(X)
+        z = model.z_0.unsqueeze(0)
+        X = X.unsqueeze(0)
+        for t in range(T - 1):
+            zs.append(z)
+            z = model(z, X[:, t])
+    zs = torch.stack(zs).detach()
+    return zs[:, 0, 0]
