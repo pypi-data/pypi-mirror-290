3b42446752d4acb6738e202060302ee956466009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 16:04:37 +0000

    Add package pucktrick/0.4/pucktrick-0.4.tar.gz

---
+++ packages/pucktrick/pucktrick-0.4.tar.gz/pucktrick-0.4/tests/test_outliers.py
@@ -0,0 +1,61 @@
+import unittest
+from pucktrick.outliers import *
+from pucktrick.utils import *
+import pandas as pd
+import numpy as np
+from pandas.testing import assert_frame_equal
+
+class Test_outlies(unittest.TestCase):
+  
+
+  def test_outliersCategoricalString(self):
+        percentage=0.5
+        num_rows = 1000
+        fake_df=create_fake_table(num_rows)
+        column='f3'
+        noise_df=outlierCategoricalStringNew(fake_df,column,percentage)
+        dif = fake_df[column] != noise_df[column]
+        diff_number = dif.sum()
+        percentage=0.7
+        noise_df=outliercategoricalStringExtended(fake_df,noise_df,column,percentage)
+        dif = fake_df[column] != noise_df[column]
+        diff_number = dif.sum()
+        diff_p=diff_number/num_rows
+        self.assertEqual(diff_p, percentage)
+  
+  def test_outliersCategoricalInt(self):
+        percentage=0.5
+        num_rows = 1000
+        fake_df=create_fake_table(num_rows)
+        column='f1'
+        noise_df=outlierCategoricalIntegerNew(fake_df,column,percentage)
+        dif = fake_df[column] != noise_df[column]
+        diff_number = dif.sum()
+        percentage=0.7
+        noise_df=outliercategoricalIntegerExtended(fake_df,noise_df,column,percentage)
+        dif = fake_df[column] != noise_df[column]
+        diff_number = dif.sum()
+        diff_p=diff_number/num_rows
+        self.assertEqual(diff_p, percentage)
+  
+  def test_outliersdiscrete(self):
+        percentage=0.5
+        num_rows = 1000
+        fake_df=create_fake_table(num_rows)
+        column='f1'
+        noise_df=outlierDiscreteNew3Sigma(fake_df,column,percentage)
+        dif = fake_df[column] != noise_df[column]
+        diff_number = dif.sum()
+        percentage=0.7
+        noise_df=outlierDiscreteExtended3Sigma(fake_df,noise_df,column,percentage)
+        dif = fake_df[column] != noise_df[column]
+        diff_number = dif.sum()
+        diff_p=diff_number/num_rows
+        # Verifica che il numero di modifiche sia corretto
+        self.assertEqual(diff_p, percentage)
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
