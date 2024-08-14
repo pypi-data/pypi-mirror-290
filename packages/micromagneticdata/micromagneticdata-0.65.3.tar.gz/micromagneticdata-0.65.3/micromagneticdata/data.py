4b249d05eb702c6871557e55e6f88e541261395d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:03:43 +0000

    Add package micromagneticdata/0.65.3/micromagneticdata-0.65.3.tar.gz

---
+++ packages/micromagneticdata/micromagneticdata-0.65.3.tar.gz/micromagneticdata-0.65.3/micromagneticdata/data.py
@@ -0,0 +1,237 @@
+import glob
+import os
+
+import ipywidgets
+import pandas as pd
+import ubermagutil.typesystem as ts
+
+import micromagneticdata as md
+
+
+@ts.typesystem(name=ts.Typed(expected_type=str), dirname=ts.Typed(expected_type=str))
+class Data:
+    """Computational magnetism data class.
+
+    It requires the name of the system to be passed. If `dirname` was used when
+    the system was driven, it can be passed here via ``dirname``.
+
+    Parameters
+    ----------
+    name : str
+
+        System's name.
+
+    dirname : str, optional
+
+        Directory in which system's data is saved. Defults to ``'./'``.
+
+    Raises
+    ------
+    IOError
+
+        If the system's directory cannot be found.
+
+    Examples
+    --------
+    1. Creating data object.
+
+    >>> import os
+    >>> import micromagneticdata as md
+    ...
+    >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+    >>> data = md.Data(name='rectangle', dirname=dirname)
+
+    """
+
+    def __init__(self, name, dirname="./"):
+        self.name = name
+        self.dirname = dirname
+        self.path = os.path.join(dirname, name)
+
+        if not os.path.exists(self.path):
+            msg = f"Directory {self.path=} cannot be found."
+            raise OSError(msg)
+
+    def __repr__(self):
+        """Representation string.
+
+        Returns
+        -------
+        str
+
+            Representation string.
+
+        Examples
+        --------
+        1. Representation string.
+
+        >>> import os
+        >>> import micromagneticdata as md
+        ...
+        >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+        >>> data = md.Data(name='rectangle', dirname=dirname)
+        >>> data
+        Data(...)
+
+        """
+        return f"Data(name='{self.name}', dirname='{self.dirname}')"
+
+    @property
+    def info(self):
+        """Data information.
+
+        This property returns ``pandas.DataFrame`` with information about
+        different drives found. If columns of different drives mismatch,
+        ``NaN`` is added as the value.
+
+        Returns
+        -------
+        pandas.DataFrame
+
+            Data information.
+
+        Examples
+        --------
+        1. Getting information about data.
+
+        >>> import os
+        >>> import micromagneticdata as md
+        ...
+        >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+        >>> data = md.Data(name='rectangle', dirname=dirname)
+        >>> data.info
+           drive_number...
+        """
+        return pd.DataFrame.from_records([i.info for i in self])
+
+    @property
+    def n(self):
+        """Number of drives in data.
+
+        Returns
+        -------
+        int
+
+            Number of drives.
+
+        Examples
+        --------
+        1. Getting the number of drives.
+
+        >>> import os
+        >>> import micromagneticdata as md
+        ...
+        >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+        >>> data = md.Data(name='rectangle', dirname=dirname)
+        >>> data.n
+        7
+
+        """
+        return len(list(glob.iglob(os.path.join(self.path, "drive-*"))))
+
+    def __getitem__(self, item):
+        """Get drive with number ``item``.
+
+        If a negative value is passed as ``item``, the count starts from the end. For
+        example, passing ``-1`` would return the last drive.
+
+        Parameters
+        ----------
+        item : int
+
+            Drive number.
+
+        Returns
+        -------
+        micromagneticdata.Drive
+
+            Drive object.
+
+        Examples
+        --------
+        1. Getting drive.
+
+        >>> import os
+        >>> import micromagneticdata as md
+        ...
+        >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+        >>> data = md.Data(name='rectangle', dirname=dirname)
+        >>> data.n
+        7
+        >>> data[0]  # first (0th) drive
+        OOMMFDrive(...)
+        >>> data[1]  # second (1th) drive
+        Mumax3Drive(...)
+        >>> data[-1]  # last (6th) drive
+        OOMMFDrive(...)
+
+        """
+        return md.Drive(
+            name=self.name,
+            number=range(self.n)[item],
+            dirname=self.dirname,
+        )
+
+    def __iter__(self):
+        """Iterator.
+
+        This method yields all drives found in data.
+
+        Yields
+        ------
+        micromagneticdata.Drive
+
+            Drive object.
+
+        Examples
+        --------
+        1. Iterating data object.
+
+        >>> import os
+        >>> import micromagneticdata as md
+        ...
+        >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+        >>> data = md.Data(name='rectangle', dirname=dirname)
+        >>> data.n
+        7
+        >>> len(list(data))
+        7
+
+        """
+        for i in range(self.n):
+            yield self[i]
+
+    def selector(self, description="drive", **kwargs):
+        """Widget for selecting drive.
+
+        This method is based on ``ipywidgets.BoundedIntText``, so any keyword
+        argument accepted by it can be passed.
+
+        Parameters
+        ----------
+        description : str, optional
+
+            Widget description. Defaults to ``'drive'``.
+
+        Returns
+        -------
+        ipywidgets.BoundedIntText
+
+            Selection widget.
+
+        Examples
+        --------
+        1. Selection widget.
+
+        >>> import os
+        >>> import micromagneticdata as md
+        ...
+        >>> dirname = os.path.join(os.path.dirname(__file__), 'tests', 'test_sample')
+        >>> data = md.Data(name='rectangle', dirname=dirname)
+        >>> data.selector()
+        BoundedIntText(...)
+
+        """
+        return ipywidgets.BoundedIntText(
+            value=0, min=0, max=self.n - 1, step=1, description=description, **kwargs
+        )
---
+++ packages/micromagneticdata/micromagneticdata-0.65.3.tar.gz/micromagneticdata-0.65.3/micromagneticdata/tests/test_data.py
@@ -0,0 +1,43 @@
+import os
+
+import ipywidgets
+import pandas as pd
+import pytest
+
+import micromagneticdata as md
+
+
+class TestData:
+    def setup_method(self):
+        self.dirname = os.path.join(os.path.dirname(__file__), "test_sample")
+        self.name = "rectangle"
+        self.data = md.Data(name=self.name, dirname=self.dirname)
+        self.N_SAMPLES = 7
+
+    def test_init(self):
+        assert isinstance(self.data, md.Data)
+
+        # Exception
+        with pytest.raises(IOError):
+            md.Data(name="wrong", dirname=self.dirname)
+
+    def test_repr(self):
+        assert isinstance(repr(self.data), str)
+        assert "Data" in repr(self.data)
+
+    def test_info(self):
+        assert isinstance(self.data.info, pd.DataFrame)
+        assert len(self.data.info.index) == self.N_SAMPLES
+
+    def test_n(self):
+        assert self.data.n == self.N_SAMPLES
+
+    def test_getitem(self):
+        for i in range(-self.N_SAMPLES, self.N_SAMPLES):
+            assert isinstance(self.data[i], md.Drive)
+
+    def test_iter(self):
+        assert len(list(self.data)) == self.N_SAMPLES
+
+    def test_selector(self):
+        assert isinstance(self.data.selector(), ipywidgets.BoundedIntText)
