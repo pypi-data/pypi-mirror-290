ef690fa40085b49a1a9117c1e5e482c88ef5d6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:07 +0000

    Add package Mensajes-pmontt/6.0/mensajes_pmontt-6.0.tar.gz

---
+++ packages/Mensajes-pmontt/mensajes_pmontt-6.0.tar.gz/mensajes_pmontt-6.0/mensajes/__init__.py
@@ -0,0 +1,4 @@
+# Este archivo sirve cuando hago un import de la carpeta "mensajes".
+# Al hacer import de una carpeta, lo primero que busca el sistema es si existe una carpeta llamada "__init__" y la ejecuta.
+# No es necesario, pero es una buena practica tener este tipo de archivos cuando usamos paquetes.
+print("Cargando el paquete mensajes")
