ef7ddaebc7ac3b28237c8e319b66cf05ef7ce840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:26 +0000

    Add package blackboardsync/0.12.0a3/blackboardsync-0.12.0a3.tar.gz

---
+++ packages/blackboardsync/blackboardsync-0.12.0a3.tar.gz/blackboardsync-0.12.0a3/blackboard_sync/content/base.py
@@ -0,0 +1,33 @@
+import platform
+
+class BStream:
+    """Base class for content that can be downloaded as a byte stream."""
+    CHUNK_SIZE = 1024
+
+    def write(self, path, stream, executor) -> None:
+        """Schedule the write operation."""
+
+        if platform.system() == "Windows":
+            path = Path(u'\\\\?\\' + str(path))
+
+        def _write():
+            with path.open("wb") as f:
+                for chunk in stream.iter_content(chunk_size=self.CHUNK_SIZE):
+                    f.write(chunk)
+
+        executor.submit(_write)
+
+class FStream:
+    """Base class for content that can be written as text."""
+
+    def write(self, path, body, executor) -> None:
+        """Schedule the write operation."""
+
+        if platform.system() == "Windows":
+            path = Path(u'\\\\?\\' + str(path))
+
+        def _write():
+            with path.open('w', encoding='utf-8') as f:
+                f.write(body)
+
+        executor.submit(_write)
