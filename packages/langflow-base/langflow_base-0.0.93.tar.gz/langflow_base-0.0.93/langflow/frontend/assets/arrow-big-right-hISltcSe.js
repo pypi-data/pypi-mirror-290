721fddc1dff53d202ab873345dc811d025075bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:46 +0000

    Add package langflow-base/0.0.93/langflow_base-0.0.93.tar.gz

---
+++ packages/langflow-base/langflow_base-0.0.93.tar.gz/langflow_base-0.0.93/langflow/frontend/assets/arrow-big-right-hISltcSe.js
@@ -0,0 +1,6 @@
+import{eo as t}from"./index-DugHs1zq.js";/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=t("ArrowBigRight",[["path",{d:"M6 9h6V5l7 7-7 7v-4H6V9z",key:"7fvt9c"}]]);export{r as default};
---
+++ packages/langflow-base/langflow_base-0.0.93.tar.gz/langflow_base-0.0.93/langflow/frontend/assets/bookmark-minus-CVD1LVir.js
@@ -0,0 +1,6 @@
+import{eo as o}from"./index-DugHs1zq.js";/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=o("BookmarkMinus",[["path",{d:"m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z",key:"1fy3hk"}],["line",{x1:"15",x2:"9",y1:"10",y2:"10",key:"1gty7f"}]]);export{e as default};
