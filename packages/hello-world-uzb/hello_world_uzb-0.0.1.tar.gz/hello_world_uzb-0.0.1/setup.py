078ef44dec65279e25893e9175e5396ddb7b8bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:03:38 +0000

    Add package hello-world-uzb/0.0.1/hello_world_uzb-0.0.1.tar.gz

---
+++ packages/hello-world-uzb/hello_world_uzb-0.0.1.tar.gz/hello_world_uzb-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world_uzb',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        '',
+    ],
+    author='salohiddin',
+    author_email='salohiddinsalohiddin123@gmail.com',
+    description='siz bunda hello world so\'zini chiqarasiz',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
