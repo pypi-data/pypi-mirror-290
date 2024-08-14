c04264580eb904456c68002a50501adb24293fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:04:04 +0000

    Add package apigratis-sdk-python/1.1.6/apigratis-sdk-python-1.1.6.tar.gz

---
+++ packages/apigratis-sdk-python/apigratis-sdk-python-1.1.6.tar.gz/apigratis-sdk-python-1.1.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name='apigratis-sdk-python',
+    version='1.1.6',
+    author='APIBRASIL',
+    author_email='contato@apibrasil.com.br',
+    description=u'Transforme seus projetos em soluções inteligentes com nossa API...',
+    long_description_content_type="text/markdown",
+    url='https://github.com/APIBrasil/apigratis-sdk-python',
+    packages=find_packages(),
+    license='MIT License',
+    python_requires='>=3.2',
+    long_description=readme,
+    keywords='whatsapp api, apibrasil, cnpj, sms, cep, consulta, api, brasil, gratis, free, whatsapp, apiwhatsapp, apigratis, apifree',
+    install_requires=['requests'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
