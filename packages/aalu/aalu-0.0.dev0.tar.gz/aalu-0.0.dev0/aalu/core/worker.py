9180043bca9692d616d53e71cc4dd00df33657a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:58 +0000

    Add package aalu/0.0.dev0/aalu-0.0.dev0.tar.gz

---
+++ packages/aalu/aalu-0.0.dev0.tar.gz/aalu-0.0.dev0/aalu/core/worker.py
@@ -0,0 +1,25 @@
+"""
+Implements a generic worker to be used for all LROs
+"""
+
+import typing
+from multiprocessing import Pool
+
+from aalu import LOCAL_WORKERS
+
+WORKERPOOL = Pool(LOCAL_WORKERS)
+SCHEDULED_TASKS = []
+
+
+def schedule_task(func: typing.Callable, args: tuple[typing.Any, ...]) -> None:
+    """
+    Schedules the given task on the global worker and stores the future
+    """
+    SCHEDULED_TASKS.append(WORKERPOOL.apply_async(func, args))
+
+
+def safe_exit():
+    """
+    Waits for all pending tasks to complete
+    """
+    _ = [task.get() for task in SCHEDULED_TASKS]
