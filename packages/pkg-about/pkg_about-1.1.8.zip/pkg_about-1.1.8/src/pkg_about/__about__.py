e0785e90b26fd323dcf62ff8cbc4dd8831d84973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:00 +0000

    Add package pkg-about/1.1.8/pkg_about-1.1.8.zip

---
+++ packages/pkg-about/pkg_about-1.1.8.zip/pkg_about-1.1.8/src/pkg_about/__about__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2020 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+__import__("_about", globals=globals(), level=1).about()
+__copyright__ = f"Copyright (c) 2020-2024 {__author__}"  # noqa
