9cacb7f0a794ac2943f25c7e84d6cafdaae9f5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 14:08:40 +0000

    Add package ZhongYuuu/0.1/ZhongYuuu-0.1.tar.gz

---
+++ packages/ZhongYuuu/ZhongYuuu-0.1.tar.gz/ZhongYuuu-0.1/README.md
@@ -0,0 +1,3 @@
+# My Simple Package
+
+This is a minimal example of a Python package.
---
+++ packages/ZhongYuuu/ZhongYuuu-0.1.tar.gz/ZhongYuuu-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ZhongYuuu",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple example package",
+    author="ZhongYuuu",
+    # url="https://github.com/yourusername/my_simple_package",
+    python_requires='>=3.6',  # 指定支持的 Python 版本
+)
