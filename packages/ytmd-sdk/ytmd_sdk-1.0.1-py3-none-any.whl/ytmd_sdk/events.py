69489e76b5ccb355adce7fea30fef9b3d3b2a8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:28 +0000

    Add package ytmd-sdk/1.0.1/ytmd_sdk-1.0.1-py3-none-any.whl

---
+++ packages/ytmd-sdk/ytmd_sdk-1.0.1-py3-none-any.whl/ytmd_sdk/events.py
@@ -0,0 +1,7 @@
+class Events:
+    connect: str = "connect"
+    disconnect: str = "disconnect"
+    error: str = "error"
+    state_update: str = "state-update"
+    playlist_created: str = "playlist-created"
+    playlist_deleted: str = "playlist-deleted"
