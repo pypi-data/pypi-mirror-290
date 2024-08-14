337136793cbb21f9ba8e21a724d6b414e6fbd7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:03:39 +0000

    Add package pycvcqv/0.1.15/pycvcqv-0.1.15.tar.gz

---
+++ packages/pycvcqv/pycvcqv-0.1.15.tar.gz/pycvcqv-0.1.15/pycvcqv/is_numeric.py
@@ -0,0 +1,40 @@
+"""The is_numeric trait."""
+
+# --------------------------- Import libraries and functions --------------------------
+import functools
+
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+
+# -------------------------------- function definition --------------------------------
+
+
+def is_numeric(function):
+    """A decorator function to check whether the input_vector is numeric."""
+
+    # -------------------------------- wrapper function -------------------------------
+    @functools.wraps(function)
+    def wrapper(*args, **kw):
+        """The wrapper function."""
+        # ------------------------ if the **kwargs are not used -----------------------
+        if len(kw) == 0 or "data" not in kw:
+            # ------------------------------- DataFrame -------------------------------
+            if isinstance(args[0], pd.DataFrame):
+                return function(*args, **kw)
+            # --------------------- if the 1st argument is numeric --------------------
+            if is_numeric_dtype(pd.Series(args[0])):
+                # --------------------- return the actual function --------------------
+                return function(*args, **kw)
+            raise TypeError("The data is not numeric!")
+        # ------------------------- if the **kwargs include data ----------------------
+        # --------------------------------- DataFrame ---------------------------------
+        if isinstance(kw["data"], pd.DataFrame):
+            return function(*args, **kw)
+        # ----------------------- if the 1st argument is numeric ----------------------
+        if is_numeric_dtype(pd.Series(kw["data"])):
+            # ----------------------- return the actual function ----------------------
+            return function(*args, **kw)
+        raise TypeError("The data is not numeric!")
+
+    # ------------------------------- return the wrapper ------------------------------
+    return wrapper
