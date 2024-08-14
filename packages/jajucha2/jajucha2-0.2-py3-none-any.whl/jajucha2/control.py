18330f0f7ee3a645e840b0209d6ab7eb7cafd3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:14 +0000

    Add package jajucha2/0.2/jajucha2-0.2-py3-none-any.whl

---
+++ packages/jajucha2/jajucha2-0.2-py3-none-any.whl/jajucha2/control.py
@@ -0,0 +1,11 @@
+# jajucha/control.py
+
+def set_control(left,right,speed):
+    print('left',left,'right',right,'speed',speed)
+
+def get_control():
+    speed = 0
+    current = 0
+    
+    return speed,current
+
