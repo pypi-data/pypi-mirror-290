2580637f0503fc58dbeab9b189329960394307a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 05:03:08 +0000

    Add package scanspec/0.7.1/scanspec-0.7.1.tar.gz

---
+++ packages/scanspec/scanspec-0.7.1.tar.gz/scanspec-0.7.1/src/scanspec/plot.py
@@ -0,0 +1,235 @@
+from collections.abc import Iterator
+from itertools import cycle
+from typing import Any
+
+import numpy as np
+from matplotlib import colors, patches
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D, proj3d
+from scipy import interpolate
+
+from .core import Path
+from .regions import Circle, Ellipse, Polygon, Rectangle, Region, find_regions
+from .specs import DURATION, Spec
+
+__all__ = ["plot_spec"]
+
+
+def _plot_arrays(axes, arrays: list[np.ndarray], **kwargs):
+    if len(arrays) > 2:
+        axes.plot3D(arrays[2], arrays[1], arrays[0], **kwargs)
+    elif len(arrays) == 2:
+        axes.plot(arrays[1], arrays[0], **kwargs)
+    else:
+        axes.plot(arrays[0], np.zeros(len(arrays[0])), **kwargs)
+
+
+# https://stackoverflow.com/a/11156353
+class _Arrow3D(patches.FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    # Added here because of https://github.com/matplotlib/matplotlib/issues/21688
+    def do_3d_projection(self, renderer=None):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+
+        return np.min(zs)
+
+
+def _plot_arrow(axes, arrays: list[np.ndarray]):
+    if len(arrays) == 1:
+        arrays = [np.array([0, 0])] + arrays
+    if len(arrays) == 2:
+        head = [a[-1] for a in reversed(arrays)]
+        tail = [a[-1] - (a[-1] - a[-2]) * 0.1 for a in reversed(arrays)]
+        axes.annotate(
+            "",
+            head[:2],
+            tail[:2],
+            arrowprops={"color": "lightgrey", "arrowstyle": "-|>"},
+        )
+    elif len(arrays) == 3:
+        arrows = [a[-2:] for a in reversed(arrays)]
+        a = _Arrow3D(
+            *arrows[:3], mutation_scale=10, arrowstyle="-|>", color="lightgrey"
+        )
+        axes.add_artist(a)
+
+
+def _plot_spline(axes, ranges, arrays: list[np.ndarray], index_colours: dict[int, str]):
+    scaled_arrays = [a / r for a, r in zip(arrays, ranges, strict=False)]
+    # Define curves parametrically
+    t = np.zeros(len(arrays[0]))
+    t[1:] = np.sqrt(sum((arr[1:] - arr[:-1]) ** 2 for arr in scaled_arrays))
+    t = np.cumsum(t)
+    if t[-1] > 0:
+        # Can't make a spline that starts and ends in the same place, so add a small
+        # delta
+        for s, r in zip(scaled_arrays, ranges, strict=False):
+            if s[0] == s[-1]:
+                s += np.linspace(0, r * 1e-7, len(s))
+        # There are no duplicated points, plot a spline
+        t /= t[-1]
+        # Scale the arrays so splines don't favour larger scaled axes
+        tck, _ = interpolate.splprep(scaled_arrays, k=2, s=0)
+        starts = sorted(index_colours)
+        stops = starts[1:] + [len(arrays[0]) - 1]
+        for start, stop in zip(starts, stops, strict=False):
+            tnew = np.linspace(t[start], t[stop], num=1001)
+            spline = interpolate.splev(tnew, tck)
+            # Scale the splines back to the original scaling
+            unscaled_splines = [a * r for a, r in zip(spline, ranges, strict=False)]
+            _plot_arrays(axes, unscaled_splines, color=index_colours[start])
+            yield unscaled_splines
+
+
+def plot_spec(spec: Spec[Any], title: str | None = None):
+    """Plot a spec, drawing the path taken through the scan.
+
+    Uses a different colour for each frame, grey for the turnarounds, and
+    marks the midpoints with a filled circle if there are less than 200 of
+    them. If the scan is 2D then 2D regions are shown in black.
+
+    .. example_spec::
+
+        from scanspec.specs import Line
+        from scanspec.regions import Circle
+
+        cube = Line("z", 1, 3, 3) * Line("y", 1, 3, 10) * ~Line("x", 0, 2, 10)
+        spec = cube & Circle("x", "y", 1, 2, 0.9)
+    """
+    dims = spec.calculate()
+    dim = Path(dims).consume()
+    axes = [a for a in spec.axes() if a is not DURATION]
+    ndims = len(axes)
+
+    # Setup axes
+    if ndims > 2:
+        plt.figure(figsize=(6, 6))
+        plt_axes: Axes3D = plt.axes(projection="3d")
+        plt_axes.grid(False)
+        plt_axes.set_zlabel(axes[-3])
+        plt_axes.set_ylabel(axes[-2])
+        plt_axes.view_init(elev=15)
+    elif ndims == 2:
+        plt.figure(figsize=(6, 6))
+        plt_axes = plt.axes()
+        plt_axes.set_ylabel(axes[-2])
+    else:
+        plt.figure(figsize=(6, 2))
+        plt_axes = plt.axes()
+        plt_axes.yaxis.set_visible(False)
+    plt_axes.set_xlabel(axes[-1])
+
+    # Title with dimension sizes
+    title = title or ", ".join(f"Dim[{' '.join(d.axes())} len={len(d)}]" for d in dims)
+    plt.title(title)
+
+    # Plot any Regions
+    if ndims <= 2:
+        regions: Iterator[Region[Any]] = find_regions(spec)
+        for region in regions:
+            if isinstance(region, Rectangle):
+                xy = (region.x_min, region.y_min)
+                width = region.x_max - region.x_min
+                height = region.y_max - region.y_min
+                plt_axes.add_patch(
+                    patches.Rectangle(xy, width, height, angle=region.angle, fill=False)
+                )
+            elif isinstance(region, Circle):
+                xy = (region.x_middle, region.y_middle)
+                plt_axes.add_patch(patches.Circle(xy, region.radius, fill=False))
+            elif isinstance(region, Ellipse):
+                xy = (region.x_middle, region.y_middle)
+                width = region.x_radius * 2
+                height = region.y_radius * 2
+                angle = region.angle
+                plt_axes.add_patch(
+                    patches.Ellipse(xy, width, height, angle=angle, fill=False)
+                )
+            elif isinstance(region, Polygon):
+                # *xy_verts* is a numpy array with shape Nx2.
+                xy_verts = np.column_stack((region.x_verts, region.y_verts))
+                plt_axes.add_patch(patches.Polygon(xy_verts, fill=False))
+
+    # Plot the splines
+    tail: Any = {a: None for a in axes}
+    ranges = [max(np.max(v) - np.min(v), 0.0001) for k, v in dim.midpoints.items()]
+    seg_col = cycle(colors.TABLEAU_COLORS)
+    last_index = 0
+    splines = None
+    # The first element of gap is undefined (as there is no previous frame)
+    # so discard it
+    gap_indices = list(np.nonzero(dim.gap[1:])[0] + 1)
+    for index in gap_indices + [len(dim)]:
+        num_points = index - last_index
+        arrays = []
+        turnaround = []
+        for a in axes:
+            # Add the midpoints and the lower and upper bounds
+            arr = np.empty(num_points * 2 + 1)
+            arr[:-1:2] = dim.lower[a][last_index:index]
+            arr[1::2] = dim.midpoints[a][last_index:index]
+            arr[-1] = dim.upper[a][index - 1]
+            arrays.append(arr)
+            # Add the turnaround
+            if tail[a] is not None:
+                # Already had a tail, add lead in points
+                tail[a][2:] = np.linspace(-0.01, 0, 2) * (arr[1] - arr[0]) + arr[0]
+                turnaround.append(tail[a])
+            # Add tail off points
+            tail[a] = np.empty(4)
+            tail[a][:2] = np.linspace(0, 0.01, 2) * (arr[-1] - arr[-2]) + arr[-1]
+        last_index = index
+
+        arrow_arr = None
+        if turnaround:
+            # If we didn't move then plot a straight line from start to stop
+            if all(t[1] - t[0] == 0 for t in turnaround):
+                for t in turnaround:
+                    t[1] += (t[2] - t[1]) / 4
+            if all(t[3] - t[2] == 0 for t in turnaround):
+                for t in turnaround:
+                    t[2] -= (t[2] - t[1]) / 4
+            # Plot the turnaround
+            arrow_arr = list(
+                _plot_spline(plt_axes, ranges, turnaround, {0: "lightgrey"})
+            )[0]
+
+        # Plot the points
+        index_colours = {2 * i: next(seg_col) for i in range(num_points)}
+        splines = list(_plot_spline(plt_axes, ranges, arrays, index_colours))
+
+        if arrow_arr:
+            # Plot the arrow on the turnaround
+            _plot_arrow(plt_axes, arrow_arr)
+        elif splines:
+            # Plot the starting arrow in the direction of the first point
+            arrow_arr = [(2 * a[0] - a[1], a[0]) for a in splines[0]]
+            _plot_arrow(plt_axes, arrow_arr)
+        else:
+            # First point isn't moving, put a right caret marker
+            _plot_arrays(
+                plt_axes,
+                [np.array([dim.lower[a][0]]) for a in axes],
+                marker=5,
+                color="lightgrey",
+            )
+
+    # Plot the capture points
+    if len(dim) < 200:
+        arrays = [dim.midpoints[a] for a in axes]
+        _plot_arrays(plt_axes, arrays, linestyle="", marker=".", color="k")
+
+    # Plot the end
+    _plot_arrays(
+        plt_axes,
+        [np.array([dim.upper[a][-1]]) for a in axes],
+        marker="x",
+        color="lightgrey",
+    )
+
+    plt.show()
---
+++ packages/scanspec/scanspec-0.7.1.tar.gz/scanspec-0.7.1/src/scanspec/regions.py
@@ -0,0 +1,376 @@
+from __future__ import annotations
+
+from collections.abc import Iterator, Mapping
+from dataclasses import is_dataclass
+from typing import Any, Generic
+
+import numpy as np
+from pydantic import BaseModel, Field, TypeAdapter
+from pydantic.dataclasses import dataclass
+
+from .core import (
+    AxesPoints,
+    Axis,
+    StrictConfig,
+    discriminated_union_of_subclasses,
+    if_instance_do,
+)
+
+__all__ = [
+    "Region",
+    "get_mask",
+    "CombinationOf",
+    "UnionOf",
+    "IntersectionOf",
+    "DifferenceOf",
+    "SymmetricDifferenceOf",
+    "Range",
+    "Rectangle",
+    "Polygon",
+    "Circle",
+    "Ellipse",
+    "find_regions",
+]
+
+
+@discriminated_union_of_subclasses
+class Region(Generic[Axis]):
+    """Abstract baseclass for a Region that can `Mask` a `Spec`.
+
+    Supports operators:
+
+    - ``|``: `UnionOf` two Regions, midpoints present in either
+    - ``&``: `IntersectionOf` two Regions, midpoints present in both
+    - ``-``: `DifferenceOf` two Regions, midpoints present in first not second
+    - ``^``: `SymmetricDifferenceOf` two Regions, midpoints present in one not both
+    """
+
+    def axis_sets(self) -> list[set[Axis]]:
+        """Produce the non-overlapping sets of axes this region spans."""
+        raise NotImplementedError(self)
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        """Produce a mask of which points are in the region."""
+        raise NotImplementedError(self)
+
+    def __or__(self, other) -> UnionOf[Axis]:
+        return if_instance_do(other, Region, lambda o: UnionOf(self, o))
+
+    def __and__(self, other) -> IntersectionOf[Axis]:
+        return if_instance_do(other, Region, lambda o: IntersectionOf(self, o))
+
+    def __sub__(self, other) -> DifferenceOf[Axis]:
+        return if_instance_do(other, Region, lambda o: DifferenceOf(self, o))
+
+    def __xor__(self, other) -> SymmetricDifferenceOf[Axis]:
+        return if_instance_do(other, Region, lambda o: SymmetricDifferenceOf(self, o))
+
+    def serialize(self) -> Mapping[str, Any]:
+        """Serialize the Region to a dictionary."""
+        return TypeAdapter(Region).dump_python(self)
+
+    @staticmethod
+    def deserialize(obj) -> Region:
+        """Deserialize a Region from a dictionary."""
+        return TypeAdapter(Region).validate_python(obj)
+
+
+def get_mask(region: Region[Axis], points: AxesPoints[Axis]) -> np.ndarray:
+    """Return a mask of the points inside the region.
+
+    If there is an overlap of axes of region and points return a
+    mask of the points in the region, otherwise return all ones
+    """
+    axes = set(points)
+    needs_mask = any(ks & axes for ks in region.axis_sets())
+    if needs_mask:
+        return region.mask(points)
+    else:
+        return np.ones(len(list(points.values())[0]))
+
+
+def _merge_axis_sets(axis_sets: list[set[Axis]]) -> Iterator[set[Axis]]:
+    # Take overlapping axis sets and merge any that overlap into each
+    # other
+    for ks in axis_sets:  # ks = key_sets - left over from a previous naming standard
+        axis_set = ks.copy()
+        # Empty matching sets into this axis_set
+        for ks in axis_sets:
+            if ks & axis_set:
+                while ks:
+                    axis_set.add(ks.pop())
+        # It might be emptied already, only yield if it isn't
+        if axis_set:
+            yield axis_set
+
+
+@dataclass(config=StrictConfig)
+class CombinationOf(Region[Axis]):
+    """Abstract baseclass for a combination of two regions, left and right."""
+
+    left: Region[Axis] = Field(description="The left-hand Region to combine")
+    right: Region[Axis] = Field(description="The right-hand Region to combine")
+
+    def axis_sets(self) -> list[set[Axis]]:
+        axis_sets = list(
+            _merge_axis_sets(self.left.axis_sets() + self.right.axis_sets())
+        )
+        return axis_sets
+
+
+# Naming so we don't clash with typing.Union
+@dataclass(config=StrictConfig)
+class UnionOf(CombinationOf[Axis]):
+    """A point is in UnionOf(a, b) if in either a or b.
+
+    Typically created with the ``|`` operator
+
+    >>> r = Range("x", 0.5, 2.5) | Range("x", 1.5, 3.5)
+    >>> r.mask({"x": np.array([0, 1, 2, 3, 4])})
+    array([False,  True,  True,  True, False])
+    """
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        mask = get_mask(self.left, points) | get_mask(self.right, points)
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class IntersectionOf(CombinationOf[Axis]):
+    """A point is in IntersectionOf(a, b) if in both a and b.
+
+    Typically created with the ``&`` operator.
+
+    >>> r = Range("x", 0.5, 2.5) & Range("x", 1.5, 3.5)
+    >>> r.mask({"x": np.array([0, 1, 2, 3, 4])})
+    array([False, False,  True, False, False])
+    """
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        mask = get_mask(self.left, points) & get_mask(self.right, points)
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class DifferenceOf(CombinationOf[Axis]):
+    """A point is in DifferenceOf(a, b) if in a and not in b.
+
+    Typically created with the ``-`` operator.
+
+    >>> r = Range("x", 0.5, 2.5) - Range("x", 1.5, 3.5)
+    >>> r.mask({"x": np.array([0, 1, 2, 3, 4])})
+    array([False,  True, False, False, False])
+    """
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        left_mask = get_mask(self.left, points)
+        # Return the xor restricted to the left region
+        mask = left_mask ^ get_mask(self.right, points) & left_mask
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class SymmetricDifferenceOf(CombinationOf[Axis]):
+    """A point is in SymmetricDifferenceOf(a, b) if in either a or b, but not both.
+
+    Typically created with the ``^`` operator.
+
+    >>> r = Range("x", 0.5, 2.5) ^ Range("x", 1.5, 3.5)
+    >>> r.mask({"x": np.array([0, 1, 2, 3, 4])})
+    array([False,  True, False,  True, False])
+    """
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        mask = get_mask(self.left, points) ^ get_mask(self.right, points)
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class Range(Region[Axis]):
+    """Mask contains points of axis >= min and <= max.
+
+    >>> r = Range("x", 1, 2)
+    >>> r.mask({"x": np.array([0, 1, 2, 3, 4])})
+    array([False,  True,  True, False, False])
+    """
+
+    axis: Axis = Field(description="The name matching the axis to mask in spec")
+    min: float = Field(description="The minimum inclusive value in the region")
+    max: float = Field(description="The minimum inclusive value in the region")
+
+    def axis_sets(self) -> list[set[Axis]]:
+        return [{self.axis}]
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        v = points[self.axis]
+        mask = np.bitwise_and(v >= self.min, v <= self.max)
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class Rectangle(Region[Axis]):
+    """Mask contains points of axis within a rotated xy rectangle.
+
+    .. example_spec::
+
+        from scanspec.regions import Rectangle
+        from scanspec.specs import Line
+
+        grid = Line("y", 1, 3, 10) * ~Line("x", 0, 2, 10)
+        spec = grid & Rectangle("x", "y", 0, 1.1, 1.5, 2.1, 30)
+    """
+
+    x_axis: Axis = Field(description="The name matching the x axis of the spec")
+    y_axis: Axis = Field(description="The name matching the y axis of the spec")
+    x_min: float = Field(description="Minimum inclusive x value in the region")
+    y_min: float = Field(description="Minimum inclusive y value in the region")
+    x_max: float = Field(description="Maximum inclusive x value in the region")
+    y_max: float = Field(description="Maximum inclusive y value in the region")
+    angle: float = Field(
+        description="Clockwise rotation angle of the rectangle", default=0.0
+    )
+
+    def axis_sets(self) -> list[set[Axis]]:
+        return [{self.x_axis, self.y_axis}]
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        x = points[self.x_axis] - self.x_min
+        y = points[self.y_axis] - self.y_min
+        if self.angle != 0:
+            # Rotate src points by -angle
+            phi = np.radians(-self.angle)
+            rx = x * np.cos(phi) - y * np.sin(phi)
+            ry = x * np.sin(phi) + y * np.cos(phi)
+            x = rx
+            y = ry
+        mask_x = np.bitwise_and(x >= 0, x <= (self.x_max - self.x_min))
+        mask_y = np.bitwise_and(y >= 0, y <= (self.y_max - self.y_min))
+        return mask_x & mask_y
+
+
+@dataclass(config=StrictConfig)
+class Polygon(Region[Axis]):
+    """Mask contains points of axis within a rotated xy polygon.
+
+    .. example_spec::
+
+        from scanspec.regions import Polygon
+        from scanspec.specs import Line
+
+        grid = Line("y", 3, 8, 10) * ~Line("x", 1 ,8, 10)
+        spec = grid & Polygon("x", "y", [1.0, 6.0, 8.0, 2.0], [4.0, 10.0, 6.0, 1.0])
+    """
+
+    x_axis: Axis = Field(description="The name matching the x axis of the spec")
+    y_axis: Axis = Field(description="The name matching the y axis of the spec")
+    x_verts: list[float] = Field(
+        description="The Nx1 x coordinates of the polygons vertices", min_length=3
+    )
+    y_verts: list[float] = Field(
+        description="The Nx1 y coordinates of the polygons vertices", min_length=3
+    )
+
+    def axis_sets(self) -> list[set[Axis]]:
+        return [{self.x_axis, self.y_axis}]
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        x = points[self.x_axis]
+        y = points[self.y_axis]
+        v1x, v1y = self.x_verts[-1], self.y_verts[-1]
+        mask = np.full(len(x), False, dtype=np.int8)
+        for v2x, v2y in zip(self.x_verts, self.y_verts, strict=False):
+            # skip horizontal edges
+            if v2y != v1y:
+                vmask = np.full(len(x), False, dtype=np.int8)
+                vmask |= (y < v2y) & (y >= v1y)
+                vmask |= (y < v1y) & (y >= v2y)
+                t = (y - v1y) / (v2y - v1y)
+                vmask &= x < v1x + t * (v2x - v1x)
+                mask ^= vmask
+            v1x, v1y = v2x, v2y
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class Circle(Region[Axis]):
+    """Mask contains points of axis within an xy circle of given radius.
+
+    .. example_spec::
+
+        from scanspec.regions import Circle
+        from scanspec.specs import Line
+
+        grid = Line("y", 1, 3, 10) * ~Line("x", 0, 2, 10)
+        spec = grid & Circle("x", "y", 1, 2, 0.9)
+    """
+
+    x_axis: Axis = Field(description="The name matching the x axis of the spec")
+    y_axis: Axis = Field(description="The name matching the y axis of the spec")
+    x_middle: float = Field(description="The central x point of the circle")
+    y_middle: float = Field(description="The central y point of the circle")
+    radius: float = Field(description="Radius of the circle", gt=0)
+
+    def axis_sets(self) -> list[set[Axis]]:
+        return [{self.x_axis, self.y_axis}]
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        x = points[self.x_axis] - self.x_middle
+        y = points[self.y_axis] - self.y_middle
+        mask = x * x + y * y <= (self.radius * self.radius)
+        return mask
+
+
+@dataclass(config=StrictConfig)
+class Ellipse(Region[Axis]):
+    """Mask contains points of axis within an xy ellipse of given radius.
+
+    .. example_spec::
+
+        from scanspec.regions import Ellipse
+        from scanspec.specs import Line
+
+        grid = Line("y", 3, 8, 10) * ~Line("x", 1 ,8, 10)
+        spec = grid & Ellipse("x", "y", 5, 5, 2, 3, 75)
+    """
+
+    x_axis: Axis = Field(description="The name matching the x axis of the spec")
+    y_axis: Axis = Field(description="The name matching the y axis of the spec")
+    x_middle: float = Field(description="The central x point of the ellipse")
+    y_middle: float = Field(description="The central y point of the ellipse")
+    x_radius: float = Field(
+        description="The radius along the x axis of the ellipse", gt=0
+    )
+    y_radius: float = Field(
+        description="The radius along the y axis of the ellipse", gt=0
+    )
+    angle: float = Field(description="The angle of the ellipse (degrees)", default=0.0)
+
+    def axis_sets(self) -> list[set[Axis]]:
+        return [{self.x_axis, self.y_axis}]
+
+    def mask(self, points: AxesPoints[Axis]) -> np.ndarray:
+        x = points[self.x_axis] - self.x_middle
+        y = points[self.y_axis] - self.y_middle
+        if self.angle != 0:
+            # Rotate src points by -angle
+            phi = np.radians(-self.angle)
+            tx = x * np.cos(phi) - y * np.sin(phi)
+            ty = x * np.sin(phi) + y * np.cos(phi)
+            x = tx
+            y = ty
+        mask = (x / self.x_radius) ** 2 + (y / self.y_radius) ** 2 <= 1
+        return mask
+
+
+def find_regions(obj) -> Iterator[Region[Axis]]:
+    """Recursively yield Regions from obj and its children."""
+    if (
+        hasattr(obj, "__pydantic_model__")
+        and issubclass(obj.__pydantic_model__, BaseModel)
+        or is_dataclass(obj)
+    ):
+        if isinstance(obj, Region):
+            yield obj
+        for name in obj.__dict__.keys():
+            regions: Iterator[Region[Axis]] = find_regions(getattr(obj, name))
+            yield from regions
