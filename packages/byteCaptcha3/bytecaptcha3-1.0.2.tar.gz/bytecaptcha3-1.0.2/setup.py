3db46419a7a0ce53df520b978af527a798321bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 05:03:21 +0000

    Add package byteCaptcha3/1.0.2/bytecaptcha3-1.0.2.tar.gz

---
+++ packages/byteCaptcha3/bytecaptcha3-1.0.2.tar.gz/bytecaptcha3-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byteCaptcha3",
+    version="1.0.2",
+    description="A Python library for generating complex CAPTCHAs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="h3xcolor",
+    author_email="oktk0728@gmail.com",
+    url="https://github.com/sjskUw/ByteCaptcha3",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
