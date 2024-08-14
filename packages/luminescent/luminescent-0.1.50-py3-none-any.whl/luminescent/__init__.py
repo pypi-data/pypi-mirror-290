a14e7893d3cfcabaf0fa64a53a16625d7a131b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:45 +0000

    Add package luminescent/0.1.50/luminescent-0.1.50-py3-none-any.whl

---
+++ packages/luminescent/luminescent-0.1.50-py3-none-any.whl/luminescent/__init__.py
@@ -0,0 +1,22 @@
+from .gplugins.luminescent import *
+# __all__= .gplugins.luminescent.__all__
+#
+__all__ = [
+    "LAYER_STACK",
+    "LAYER",
+    "LAYER_VIEWS",
+    "MATERIALS",
+    "write_sparams",
+    "inverse_design_problem",
+    "sparams_problem",
+    "solve",
+    "apply_design",
+    "add_bbox",
+    "XMARGIN",
+    "YMARGIN",
+    "ZMARGIN",
+    "XYMARGIN",
+    "load_solution",
+    "show_solution",
+    "gcells",
+]
---
+++ packages/luminescent/luminescent-0.1.50-py3-none-any.whl/luminescent/gplugins/luminescent/run.jl
@@ -0,0 +1,18 @@
+
+using Luminescent, Random
+
+Random.seed!(1)
+if isempty(ARGS)
+    path = lastrun()
+    println("path: ", path)
+else
+    path = ARGS[1]
+end
+# gfrun(path; Courant=0.5)
+sol = gfrun(path)
+# @show sol.tparams
+# using Pkg
+# Pkg.resolve()
+# pkg"dev C:\Users\pxshe\OneDrive\Desktop\Porcupine.jl"
+# pkg"dev C:\Users\pxshe\OneDrive\Desktop\ArrayPadding.jl"
+# pkg"dev C:\Users\pxshe\OneDrive\Desktop\Jello.jl"
