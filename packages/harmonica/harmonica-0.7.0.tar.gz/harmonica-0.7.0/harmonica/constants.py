ad68200be3ca455ee8118af3bf65a1c648b7d98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:05 +0000

    Add package harmonica/0.7.0/harmonica-0.7.0.tar.gz

---
+++ packages/harmonica/harmonica-0.7.0.tar.gz/harmonica-0.7.0/harmonica/constants.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2018 The Harmonica Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+Fundamental constants used throughout the library.
+"""
+
+#: Gravitational constant :math:`G` in SI units (:math:`m^3 kg^{-1} s^{-2}`)
+GRAVITATIONAL_CONST = 6.6743e-11
