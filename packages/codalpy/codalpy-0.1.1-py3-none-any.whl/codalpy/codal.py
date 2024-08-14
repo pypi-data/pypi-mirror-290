a5fe5b8caebaaf02025c05465ee8f209f3e2efa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 15:04:15 +0000

    Add package codalpy/0.1.1/codalpy-0.1.1-py3-none-any.whl

---
+++ packages/codalpy/codalpy-0.1.1-py3-none-any.whl/codalpy/codal.py
@@ -0,0 +1,126 @@
+from typing import Literal
+from urllib.parse import urlparse, parse_qs
+import re
+import polars as pl
+from codalpy.models import QueryParam, Letter, IncomeStatement, GetIncomeStatement
+from codalpy.http import get
+from codalpy import gen_df
+
+
+class Codal:
+    def __init__(self, query: QueryParam, category: Literal["production"]) -> None:
+        self.base_url = "https://codal.ir"
+        self.search_url = "https://search.codal.ir/api/search/v2/q?"
+        self.api = "api/search/v2/q"
+        self._query = query
+        self._category = category
+
+    @property
+    def query(self):
+        return self._query
+
+    @query.setter
+    def query(self, value: QueryParam):
+        self._query = value
+
+    def letter(self):
+        r = get(
+            url=self.search_url,
+            params=self._query.model_dump(by_alias=True),
+            rtype="josn",
+        )
+        if isinstance(r, dict):
+            pages = str(r.get("Page"))
+            Letter.base_url = self.base_url
+            letters = [Letter.model_validate(i) for i in r["Letters"]]
+            if pages.isdigit:
+                pages = int(pages)
+                if pages > 1:
+
+                    for p in range(2, pages + 1):
+                        self._query.page_number = p
+                        r = get(
+                            url=self.search_url,
+                            params=self._query.model_dump(by_alias=True),
+                            rtype="josn",
+                        )
+                        if isinstance(r, dict):
+                            letters.extend(
+                                [Letter.model_validate(i) for i in r["Letters"]]
+                            )
+            return letters
+
+    def _get_incom_statemnt(self) -> GetIncomeStatement | None:
+        letters = self.letter()
+        if letters is not None:
+            records = []
+            get_error = []
+            match_error = []
+            validation_error = []
+            res = []
+            for i in letters:
+                urlp = urlparse(i.url)
+                params = parse_qs(urlp.query)
+                params["SheetId"] = ["1"]
+                r = get(url=f"{self.base_url}{urlp.path}", params=params, rtype="text")
+                if r is not None:
+                    pattern = r"var datasource = (.*?);"
+                    match = re.search(pattern, r)
+                    if match:
+                        text = match.group(1)
+                        try:
+                            records.append(
+                                (i, IncomeStatement.model_validate_json(text))
+                            )
+                        except Exception as e:
+                            validation_error.append((i, str(e)))
+                    else:
+                        match_error.append((i, str(r)))
+                else:
+                    get_error.append(i)
+            return GetIncomeStatement(
+                records=records,
+                get_error=get_error,
+                match_error=match_error,
+                validation_error=validation_error,
+            )
+
+    def income_statement(self) -> pl.DataFrame | None:
+        """
+        .. raw:: html
+
+            <div dir="rtl">
+                داده‌های نمایِ بازارِ اختیارِ-معامله‌یِ بورسِ تهران رو بهت می‌ده
+            </div>
+
+        Returns
+        -------
+        polars.DataFrame
+
+        example
+        -------
+        >>> from codalpy import Codal, QueryParam
+        >>> query = QueryParam(symbol="زاگرس",length=12, from_date="1400/01/01")
+        >>> codal = Codal(query=query, category="production")
+        >>> codal.income_statement()
+        shape: (8, 29)
+        ┌───────────┬───────────────┬──────────────┬────────────────────┬───┬─────────────────────────────────┬─────────────────────────────────┬─────────────────────────────────┬─────────────────────────────────┐
+        │ sales     ┆ cost_of_sales ┆ gross_profit ┆ operating_expenses ┆ … ┆ url                             ┆ attachment_url                  ┆ pdf_url                         ┆ excel_url                       │
+        │ ---       ┆ ---           ┆ ---          ┆ ---                ┆   ┆ ---                             ┆ ---                             ┆ ---                             ┆ ---                             │
+        │ i64       ┆ i64           ┆ i64          ┆ i64                ┆   ┆ str                             ┆ str                             ┆ str                             ┆ str                             │
+        ╞═══════════╪═══════════════╪══════════════╪════════════════════╪═══╪═════════════════════════════════╪═════════════════════════════════╪═════════════════════════════════╪═════════════════════════════════╡
+        │ 258734831 ┆ -192020455    ┆ 66714376     ┆ -57185171          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 258734831 ┆ -192020455    ┆ 66714376     ┆ -57363718          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 214213606 ┆ -145108587    ┆ 69105019     ┆ -44188435          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 214213606 ┆ -147350610    ┆ 66862996     ┆ -46301021          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 192628444 ┆ -132224423    ┆ 60404021     ┆ -32817902          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 192628444 ┆ -132224423    ┆ 60404021     ┆ -32834603          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 143234768 ┆ -61344224     ┆ 81890544     ┆ -34001119          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        │ 143234768 ┆ -61251730     ┆ 81983038     ┆ -31375649          ┆ … ┆ https://codal.ir/Reports/Decis… ┆ https://codal.ir/Reports/Attac… ┆ https://codal.ir/DownloadFile.… ┆ https://excel.codal.ir/service… │
+        └───────────┴───────────────┴──────────────┴────────────────────┴───┴─────────────────────────────────┴─────────────────────────────────┴─────────────────────────────────┴─────────────────────────────────┘
+        """
+        data = self._get_incom_statemnt()
+        if data:
+            if data.records:
+                df = gen_df.income_statement(data.records, self._category)
+                return df
