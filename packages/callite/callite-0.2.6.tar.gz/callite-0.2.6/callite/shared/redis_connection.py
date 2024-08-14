51e9bc951ee16f78d5aa13f07fc4d9892833722b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:03:38 +0000

    Add package callite/0.2.6/callite-0.2.6.tar.gz

---
+++ packages/callite/callite-0.2.6.tar.gz/callite-0.2.6/callite/shared/redis_connection.py
@@ -0,0 +1,22 @@
+import logging
+import uuid
+import redis
+from abc import ABC
+
+
+
+class RedisConnection(ABC):
+    def __init__(self, conn_url: str, service: str, *args, **kwargs):
+        self._methods = {}
+        self._service = service
+        self._running = True
+        self._running_threads = []
+        self._connection_id = uuid.uuid4().hex
+        self._queue_prefix = kwargs.get('queue_prefix', '/callite')
+        self._rds = redis.Redis.from_url(conn_url)
+        self._rds.ping()
+        self._logger = logging.getLogger(f'RPC{service}')
+        self._logger.setLevel(logging.INFO)
+
+    def close(self) -> None:
+        self._running = False
