f6a7e777b25c37edd13a175fefbe860fbc823adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:22 +0000

    Add package brady-django-template/0.1.3/brady-django-template-0.1.3.tar.gz

---
+++ packages/brady-django-template/brady-django-template-0.1.3.tar.gz/brady-django-template-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brady-django-template',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'Django',  
+    ],
+    entry_points={
+        'console_scripts': [
+            'brady-django-template=brady_django_template.main:create_django_project',
+        ],
+    },
+    description='A tool to create Django projects with a custom template.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KyawKoKoTun/brady-django-template',
+    author='Kyaw Ko Ko Tun',
+    author_email='kyawkokotunmm475157@gmail.com',
+    license='MIT',
+)
