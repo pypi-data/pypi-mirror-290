b026f5d708a7f97a3cae998a45fa5890f30aa6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 15:05:18 +0000

    Add package EasyFEA/1.2.0/EasyFEA-1.2.0.tar.gz

---
+++ packages/EasyFEA/EasyFEA-1.2.0.tar.gz/EasyFEA-1.2.0/EasyFEA/Materials.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2021-2024 Université Gustave Eiffel.
+# This file is part of the EasyFEA project.
+# EasyFEA is distributed under the terms of the GNU General Public License v3 or later, see LICENSE.txt and CREDITS.md for more information.
+
+"""Module containing material behavior. Such as elastic, damage, thermal and beam materials."""
+
+from .materials import Reshape_variable
+
+# ----------------------------------------------
+# Elastic
+# ----------------------------------------------
+
+from .materials._elastic import _Elas, Elas_Isot, Elas_IsotTrans, Elas_Anisot
+
+# ----------------------------------------------
+# PhaseField
+# ----------------------------------------------
+
+from .materials._phasefield import PhaseField
+
+# ----------------------------------------------
+# Beam
+# ----------------------------------------------
+
+from .materials._beam import _Beam, Beam_Structure, Beam_Elas_Isot
+
+# ----------------------------------------------
+# Thermal
+# ----------------------------------------------
+
+from .materials._thermal import Thermal
