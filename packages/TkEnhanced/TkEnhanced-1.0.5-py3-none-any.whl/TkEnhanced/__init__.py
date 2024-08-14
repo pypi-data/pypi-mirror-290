9d852eabc57fb1925c0d20d1a619c32db16ec42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:10 +0000

    Add package TkEnhanced/1.0.5/TkEnhanced-1.0.5-py3-none-any.whl

---
+++ packages/TkEnhanced/TkEnhanced-1.0.5-py3-none-any.whl/TkEnhanced/__init__.py
@@ -0,0 +1,34 @@
+"""
+MIT License
+
+Copyright (c) 2024 Bunnitown
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# local imports:
+from .enhanced_widgets import EnhListbox, EnhCanvas, EnhFrame, EnhText
+from .enhanced_label import EnhLabel
+from .enhanced_image import EnhImage
+from .enhanced_window import EnhToplevel, EnhTk
+from .enhanced_button import EnhButton
+from .enhanced_entry import EnhEntry
+from .enhanced_scrollableframe import EnhScrollableFrame
+
+__all__ = EnhScrollableFrame, EnhToplevel, EnhListbox, EnhCanvas, EnhButton, EnhFrame, EnhEntry, EnhImage, EnhLabel, EnhText, EnhTk
