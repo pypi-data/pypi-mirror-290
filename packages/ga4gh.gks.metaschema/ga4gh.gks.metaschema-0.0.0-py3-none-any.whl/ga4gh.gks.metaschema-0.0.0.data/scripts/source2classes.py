97c8146711846635d7add17c804c364be19d9690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:45 +0000

    Add package ga4gh.gks.metaschema/0.0.0/ga4gh.gks.metaschema-0.0.0-py3-none-any.whl

---
+++ packages/ga4gh.gks.metaschema/ga4gh.gks.metaschema-0.0.0-py3-none-any.whl/ga4gh.gks.metaschema-0.0.0.data/scripts/source2classes.py
@@ -0,0 +1,20 @@
+#!python
+import argparse
+from ga4gh.gks.metaschema.tools.source_proc import YamlSchemaProcessor
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile")
+
+
+def main(proc):
+    for cls in proc.processed_classes:
+        if proc.class_is_protected(cls):
+            continue
+        print(cls)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    p = YamlSchemaProcessor(Path(args.infile))
+    main(p)
---
+++ packages/ga4gh.gks.metaschema/ga4gh.gks.metaschema-0.0.0-py3-none-any.whl/ga4gh.gks.metaschema-0.0.0.data/scripts/source2mergedjsy.py
@@ -0,0 +1,10 @@
+#!python
+
+import pathlib
+import sys
+from ga4gh.gks.metaschema.tools.source_proc import YamlSchemaProcessor
+
+source_file = pathlib.Path(sys.argv[1])
+p = YamlSchemaProcessor(source_file)
+p.merge_imported()
+p.js_yaml_dump(sys.stdout)
