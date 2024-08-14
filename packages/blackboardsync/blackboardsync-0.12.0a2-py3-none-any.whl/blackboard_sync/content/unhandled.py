987733aed260230ecb711c22e8d044be27c3153d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:48 +0000

    Add package blackboardsync/0.12.0a2/blackboardsync-0.12.0a2-py3-none-any.whl

---
+++ packages/blackboardsync/blackboardsync-0.12.0a2-py3-none-any.whl/blackboard_sync/content/unhandled.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
+
+from blackboard.api_extended import BlackboardExtended
+from blackboard.blackboard import BBCourseContent
+
+from .job import DownloadJob
+
+
+class Unhandled:
+    """Content which we cannot handle yet, or at all."""
+
+    def __init__(self, content: BBCourseContent, _, job: DownloadJob):
+        pass
+
+    def write(self, path: Path, executor: ThreadPoolExecutor):
+        pass
+
+    @property
+    def create_dir(self) -> bool:
+        return False
