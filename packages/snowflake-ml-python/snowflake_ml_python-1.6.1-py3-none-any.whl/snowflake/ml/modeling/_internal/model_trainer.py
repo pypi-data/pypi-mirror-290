4dd8ffc99d608dbe47e0d875d29693c35f63f741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:59 +0000

    Add package snowflake-ml-python/1.6.1/snowflake_ml_python-1.6.1-py3-none-any.whl

---
+++ packages/snowflake-ml-python/snowflake_ml_python-1.6.1-py3-none-any.whl/snowflake/ml/modeling/_internal/model_trainer.py
@@ -0,0 +1,31 @@
+from typing import List, Optional, Protocol, Tuple, Union
+
+import pandas as pd
+
+from snowflake.snowpark import DataFrame
+
+
+class ModelTrainer(Protocol):
+    """
+    Interface for model trainer implementations.
+
+    There are multiple flavors of training like training with pandas datasets, training with
+    Snowpark datasets using sprocs, and out of core training with Snowpark datasets etc.
+    """
+
+    def train(self) -> object:
+        raise NotImplementedError
+
+    def train_fit_predict(
+        self,
+        expected_output_cols_list: List[str],
+        drop_input_cols: Optional[bool] = False,
+    ) -> Tuple[Union[DataFrame, pd.DataFrame], object]:
+        raise NotImplementedError
+
+    def train_fit_transform(
+        self,
+        expected_output_cols_list: List[str],
+        drop_input_cols: Optional[bool] = False,
+    ) -> Tuple[Union[DataFrame, pd.DataFrame], object]:
+        raise NotImplementedError
