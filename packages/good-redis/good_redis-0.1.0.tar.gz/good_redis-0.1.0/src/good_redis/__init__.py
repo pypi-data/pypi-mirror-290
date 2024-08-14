3b84c5db8e82e7559ea7c37f3d41f90de1652657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 09:03:44 +0000

    Add package good-redis/0.1.0/good_redis-0.1.0.tar.gz

---
+++ packages/good-redis/good_redis-0.1.0.tar.gz/good_redis-0.1.0/src/good_redis/__init__.py
@@ -0,0 +1,4 @@
+from ._client import Redis, RedisProvider, AsyncRedis, AsyncRedisProvider
+from ._dictproxy import DictProxy
+
+__all__ = ["Redis", "RedisProvider", "AsyncRedis", "AsyncRedisProvider", "DictProxy"]
