b58dc42e73ac66f8f32c359d96226ab477a42145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:10 +0000

    Add package metis-data/0.1.3/metis_data-0.1.3.tar.gz

---
+++ packages/metis-data/metis_data-0.1.3.tar.gz/metis_data-0.1.3/metis_data/config.py
@@ -0,0 +1,33 @@
+import re
+from dataclasses import dataclass, field
+from enum import Enum
+
+from metis_data import namespace
+
+normalise_pattern = pattern = re.compile(r'(?<!^)(?=[A-Z])')
+
+
+def normalise(token):
+    if not token:
+        return token
+    return normalise_pattern.sub('_', token).lower()
+
+
+class CatalogueMode(Enum):
+    SPARK = "spark"
+    UNITY = "unity"
+
+
+@dataclass
+class Config:
+    catalogue: str
+    data_product: str
+    service_name: str
+    catalogue_mode: CatalogueMode = field(default_factory=lambda: CatalogueMode.UNITY)
+    checkpoint_name: str | None = None
+    namespace_strategy_cls: namespace.CatalogueStrategyProtocol | None = None
+
+    def __post_init__(self):
+        self.catalogue = normalise(self.catalogue)
+        self.service_name = normalise(self.service_name)
+        self.data_product = normalise(self.data_product)
