d8339e5bf5e5d67d279459175b20a9b42d521d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 09:03:37 +0000

    Add package EZFF/1.0.9/ezff-1.0.9.tar.gz

---
+++ packages/EZFF/ezff-1.0.9.tar.gz/ezff-1.0.9/docs/ezff.rst
@@ -0,0 +1,21 @@
+EZFF - Easy forcefield fitting
+==============================
+
+.. autoclass:: ezff.FFParam
+   :members:
+
+   .. automethod:: __init__
+   .. automethod:: read_variable_bounds
+   .. automethod:: read_forcefield_template
+   .. automethod:: set_algorithm
+   .. automethod:: ask
+   .. automethod:: parameterize
+   .. automethod:: get_best_recommendation
+   .. automethod:: generate_pool
+   .. automethod:: save_evaluated
+   .. automethod:: load_evaluated
+   .. automethod:: normalize_errors
+
+
+.. automodule:: ezff
+   :members: get_pool_rank
---
+++ packages/EZFF/ezff-1.0.9.tar.gz/ezff-1.0.9/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(name='EZFF',
+      version='1.0.9', # Update __init__.py if the version changes!
+      description='Multiobjective forcefield optimization for Molecular Dynamics',
+      author='Aravind Krishnamoorthy',
+      author_email='arvk@users.noreply.github.com',
+      license='MIT License',
+      url='https://github.com/arvk/EZFF',
+      packages=find_packages(exclude=["docs", "examples"]),
+      install_requires=[
+          'numpy == 1.26.4',
+          'mpi4py >= 3.0.0',
+          'xtal >= 0.9.1',
+          'schwimmbad >= 0.3.2',
+          'nevergrad >= 0.11.0',
+          'pymoo >= 0.6.0.dev0',
+          'platypus-opt >= 1.0.3',
+          'deap >= 1.3.0',
+          'periodictable >= 1.6.1',
+          'cclib >= 1.8',
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering :: Chemistry',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Unix',
+      ],
+     )
