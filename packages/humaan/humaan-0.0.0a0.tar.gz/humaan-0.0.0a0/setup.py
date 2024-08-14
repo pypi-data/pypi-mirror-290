db16ac662abb9279797e24a17a79e121f6ab78e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:04:07 +0000

    Add package humaan/0.0.0a0/humaan-0.0.0a0.tar.gz

---
+++ packages/humaan/humaan-0.0.0a0.tar.gz/humaan-0.0.0a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='humaan',
+    version='0.0.0-alpha.0',
+    description='Humaan Python API library',
+    author='Humaan',
+    author_email='administrator@humaan.ai',
+    packages=find_packages(),
+    install_requires=[]
+)
