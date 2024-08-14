e92bc8cd8ea464491bb3b4c83294a5aebdd9a1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 09:03:44 +0000

    Add package clust-learn/0.2.7/clust_learn-0.2.7.tar.gz

---
+++ packages/clust-learn/clust_learn-0.2.7.tar.gz/clust_learn-0.2.7/clearn/classifier/classifier.py
@@ -0,0 +1,313 @@
+"""Classification class"""
+# Author: Miguel Alvarez-Garcia
+
+import logging
+import numpy as np
+
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+
+from .utils import compute_highly_related_pairs, get_shap_importances, run_feature_selection, run_hyperparameter_tuning
+from .viz_utils import *
+
+
+class Classifier:
+    """
+    Class to manage the classification model.
+
+    Parameters
+    ----------
+    df : `pandas:DatasFrame`
+        DataFrame with main data
+    predictor_cols : list
+        List of columns to use as predictors.
+    target : `numpy.array` or list
+        values of the target variable.
+    num_cols : list, default=None
+        List of numerical columns to use as predictors.
+    cat_cols : list, default=None
+        List of categorical columns to use as predictors.
+    """
+
+    def __init__(self, df, predictor_cols, target, num_cols=None, cat_cols=None):
+        self.df = df
+        self.original_features = predictor_cols
+        self.num_vars = num_cols
+        self.cat_vars = cat_cols
+        self.filtered_features_ = None
+        self.target = target
+        self.labels_ = list(np.sort(np.unique(self.target)))
+        self.model_ = None
+        self.X_train_, self.X_test_, self.y_train_, self.y_test_ = tuple([None]*4)
+        self.grid_result_ = None
+
+        # Initialize logger
+        logging.basicConfig(format='%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+                            level=logging.INFO,
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+        self.logger = logging.getLogger(__name__)
+
+    def train_model(self, model=None, feature_selection=True, features_to_keep=[],
+                    feature_selection_model=None, hyperparameter_tuning=False, param_grid=None,
+                    train_size=0.8, balance_classes=False):
+        """
+        This method trains a classification model.
+
+        By default, it uses XGBoost, but any other estimator (instance of `scikit-learn.Estimator`) can be used.
+
+        The building process consists of four steps:
+         - Train-test split
+         - Feature Selection (optional)
+           Feature removing highly correlated variables using a classification model and SHAP values
+           to determine which to keep, and Recursive Feature Elimination with Cross-Validation (RFECV)
+           on the remaining features.
+         - Hyperparameter tuning (optional)
+           Runs grid search with cross-validation for hyperparameter tuning. **Note** the parameter grid
+           must be passed.
+         - Model training
+           Trains a classification model with the selected features and hyperparameters. By default, an XGBoost
+           classifier will be trained.
+
+        **Note** both hyperparameter tuning and model training are run on a train set. Train-test split is performed
+        using `sklearn.model_selection.train_test_split`.
+
+        Parameters
+        ----------
+        model : `scikit-learn.Estimator`, default=None
+            Model to use as classifier. By default, `xgboost.XGBClassifier`.
+        feature_selection : boolean, default=True
+            If True, feature selection is performed on the original features.
+        features_to_keep : list, default=empty list
+            Features to be kept during the feature selection process.
+        feature_selection_model : `scikit-learn.Estimator`, default=None
+            Model to be used for feature selection. By default, `sklearn.ensemble.RandomForestClassifier`.
+        hyperparameter_tuning : boolean, default=False
+            If True, hyperparameter tuning is run using the parameter grid `param_grid`.
+        param_grid : dictionary, default=None
+            Parameter grid for hyperparameter tuning. **Note** if hyperparameter_tuning=True, a parameter grid is
+            needed.
+        train_size : float, default=0.8
+            Size of the train split of the data for model training.
+        balance_classes : booelan, default=False
+            Optional to balance classes using SMOTE
+        """
+        # Data is split into train and test sets
+        X = self.df[self.original_features]
+        transdict = dict(zip(self.labels_, range(len(self.labels_))))
+        y = np.array(list(map(lambda s: transdict[s], self.target)))
+        self.X_train_, self.X_test_, self.y_train_, self.y_test_ = train_test_split(X, y, train_size=train_size)
+
+        hi_rel = None
+        if balance_classes:
+            self.logger.info('Balancing classes...')
+            hi_rel = compute_highly_related_pairs(self.X_train_, self.num_vars, self.cat_vars)
+            oversample = SMOTE()
+            self.X_train_, self.y_train_ = oversample.fit_resample(self.X_train_, self.y_train_)
+
+        # Feature selection
+        if feature_selection:
+            self.logger.info('Running feature selection...')
+            if feature_selection_model is None:
+                min_samples_leaf = max(10, int(np.ceil(self.X_train_.shape[0] * 0.05) / len(self.labels_)))
+                feature_selection_model = RandomForestClassifier(max_depth=10,  min_samples_leaf=min_samples_leaf,
+                                                                 random_state=42)
+
+            self.filtered_features_ = run_feature_selection(self.X_train_, self.original_features, self.y_train_,
+                                                            feature_selection_model, self.num_vars, self.cat_vars,
+                                                            features_to_keep, hi_rel)
+
+            self.X_train_ = self.X_train_[self.filtered_features_]
+            self.X_test_ = self.X_test_[self.filtered_features_]
+        else:
+            self.filtered_features_ = self.original_features.copy()
+
+        # Model instantiation
+        if model is None:
+            model = XGBClassifier(eval_metric='auc', use_label_encoder=False, random_state=42)
+        self.model_ = model
+
+        # Hyperparameter tuning
+        if hyperparameter_tuning:
+            if param_grid is None:
+                raise RuntimeError('For hyperparameter tuning, some parameter grid must be passed - `param_grid`')
+            self.logger.info('Running hyperparameter tuning...')
+            self.grid_result_ = run_hyperparameter_tuning(self.X_train_, self.y_train_, self.model_, param_grid)
+            self.model_.set_params(**self.grid_result_.best_params_)
+
+        # Model training
+        self.logger.info('Training model...')
+        self.model_.fit(self.X_train_, self.y_train_)
+        self.logger.info('DONE!')
+
+    @property
+    def feature_importances(self):
+        return get_shap_importances(self.model_, self.X_train_)
+
+    def hyperparameter_tuning_metrics(self, output_path=None):
+        """
+        This method returns the average and standard deviation of the cross-validation runs for every hyperparameter
+        combination in hyperparameter tuning.
+
+        Parameters
+        ----------
+        output_path : str, default=None
+            If an output_path is passed, the resulting DataFame is saved as a CSV file.
+        """
+        htm = pd.DataFrame(self.grid_result_.cv_results_['params'])
+        htm.columns = pd.MultiIndex.from_product([[f'{self.model_.__class__.__name__} Hyperparameters'], htm.columns])
+        htm[('Performance metrics', 'mean_test_score')] = self.grid_result_.cv_results_['mean_test_score']
+        htm[('Performance metrics', 'std_test_score')] = self.grid_result_.cv_results_['std_test_score']
+
+        if output_path is not None:
+            htm.to_csv(output_path, index=False)
+
+        return htm
+
+    def confusion_matrix(self, test=True, sum_stats=True, output_path=None):
+        """
+        This method returns the confusion matrix of the classification model.
+
+        Parameters
+        ----------
+        test : boolean, default=True
+            If True, returns the confusion matrix calculated on the test set. If False, returns the confusion matrix on
+            the train set.
+        sum_stats : boolean, default=True
+            If True, adds row (recall), column (precision), and global accuracy metrics to the matrix.
+        output_path : str, default=None
+            If an output_path is passed, the resulting DataFame is saved as a CSV file.
+
+        Returns
+        ----------
+        cm : `pandas.DataFrame`
+            DataFrame with confusion matrix.
+        """
+        X = self.X_test_ if test else self.X_train_
+        y = self.y_test_ if test else self.y_train_
+
+        cm = pd.DataFrame(confusion_matrix(y, self.model_.predict(X)),
+                          columns=pd.MultiIndex.from_product([['Predicted values'], self.labels_]),
+                          index=pd.MultiIndex.from_product([['Observed values'], self.labels_]))
+
+        if sum_stats:
+            # Precision, recall, and global accuracy are appended to the table
+            recall = np.diag(cm) / cm.sum(1)
+            precision = np.diag(cm) / cm.sum()
+            accuracy = np.diag(cm).sum() / cm.sum().sum()
+            cm['recall'] = recall
+            cm = cm.transpose()
+            cm['precision'] = list(precision) + [accuracy]
+            cm = cm.transpose()
+
+        if output_path is not None:
+            cm.to_csv(output_path, index=False)
+
+        return cm
+
+    def classification_report(self, test=True, output_path=None):
+        """
+        This method returns the `sklearn.metrics.classification_report` in `pandas.DataFrame` format.
+        This report contains the intra-class metrics precision, recall and F1-score, together with the global accuracy,
+        and macro average and weighted average of the three intra-class metrics.
+
+        Parameters
+        ----------
+        test : boolean, default=True
+            If True, returns the confusion matrix calculated on the test set. If False, returns the confusion matrix on
+            the train set.
+        output_path : str, default=None
+            If an output_path is passed, the resulting DataFame is saved as a CSV file.
+
+        Returns
+        ----------
+        report : `pandas.DataFrame`
+            DataFrame with classification report.
+        """
+        X = self.X_test_ if test else self.X_train_
+        y = self.y_test_ if test else self.y_train_
+
+        report = pd.DataFrame(classification_report(y, self.model_.predict(X), output_dict=True)).transpose()
+
+        if output_path is not None:
+            # In this case we do want to keep the index
+            report.to_csv(output_path)
+
+        return report
+
+    def plot_shap_importances(self, n_top=7, output_path=None, savefig_kws=None):
+        """
+        Plots shap importance values, calculated as the combined average of the absolute values of the shap values
+        for all classes.
+
+        Parameters
+        ----------
+        n_top : int, default=7
+           Top n features to be displayed. The importance of the rest are aggregated and displayed under the tag "Rest".
+        output_path : str, default=None
+           Path to save figure as image.
+        savefig_kws : dict, default=None
+           Save figure options.
+        """
+        plot_shap_importances(self.model_, self.X_train_, n_top=n_top, output_path=output_path, savefig_kws=savefig_kws)
+
+    def plot_shap_importances_beeswarm(self, class_id, n_top=10, output_path=None, savefig_kws=None):
+        """
+        Plots a summary of shap values for a specific class of the target variable. This uses shap beeswarm plot
+        (https://shap.readthedocs.io/en/latest/example_notebooks/api_examples/plots/beeswarm.html).
+
+        Parameters
+        ----------
+        class_id : int
+            The class for which to show the SHAP values.
+        n_top : int, default=7
+            Top n features to be displayed. The importances of the rest are aggregated and displayed under the tag "Rest".
+        output_path : str, default=None
+            Path to save figure as image.
+        savefig_kws : dict, default=None
+            Save figure options.
+        """
+        plot_shap_importances_beeswarm(self.model_, self.X_train_, class_id, self.labels_[class_id], n_top=n_top,
+                                       output_path=output_path, savefig_kws=savefig_kws)
+
+    def plot_confusion_matrix(self, test=True, sum_stats=True, output_path=None, savefig_kws=None):
+        """
+        This function makes a pretty plot of an sklearn Confusion Matrix cf using a Seaborn heatmap visualization.
+
+        Parameters
+        ---------
+        test : boolean, default=True
+            If True, returns the confusion matrix calculated on the test set. If False, returns the confusion matrix on
+            the train set.
+        sum_stats : boolean, default=True
+            If True, show precision and recall per class, and global accuracy, appended to the matrix.
+        output_path : str, default=None
+            Path to save figure as image.
+        savefig_kws : dict, default=None
+            Save figure options.
+        """
+        cm = self.confusion_matrix(test, sum_stats)
+        plot_confusion_matrix(cm, sum_stats=sum_stats, figsize=(cm.shape[0]+1, cm.shape[0]),
+                              output_path=output_path, savefig_kws=savefig_kws)
+
+    def plot_roc_curves(self, test=True, output_path=None, savefig_kws=None):
+        """
+       Plots ROC curve for every class.
+
+       Parameters
+       ---------
+       test : boolean, default=True
+            If True, returns the confusion matrix calculated on the test set. If False, returns the confusion matrix on
+            the train set.
+       output_path : str, default=None
+           Path to save figure as image.
+       savefig_kws : dict, default=None
+           Save figure options.
+       """
+        X = self.X_test_ if test else self.X_train_
+        y = self.y_test_ if test else self.y_train_
+        plot_roc_curves(X, y, self.model_, self.labels_, output_path=output_path,
+                        savefig_kws=savefig_kws)
---
+++ packages/clust-learn/clust_learn-0.2.7.tar.gz/clust_learn-0.2.7/clearn/classifier/utils.py
@@ -0,0 +1,230 @@
+"""Utils for the classification module"""
+# Author: Miguel Alvarez-Garcia
+
+import logging
+import numpy as np
+import pandas as pd
+import shap
+
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import GridSearchCV
+from ..utils import compute_high_corr_pairs, compute_highly_related_categorical_vars, compute_highly_related_mixed_vars
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+
+def get_shap_importances(classifier, X):
+    """
+    Computes shap importance values as the combined average of the absolute values of the shap values
+    for all classes.
+
+    Parameters
+    ----------
+    classifier : object
+        Classification model supported by SHAP (e.g., sklearn models or xgboost).
+    X : `numpy.ndarray` or `pandas.DataFrame`
+        Observations.
+
+    Returns
+    ----------
+    importances : `pandas.DataFrame`
+        DataFrame with predictors and their corresponding importance.
+    """
+    explainer = shap.Explainer(classifier)
+    shap_values = explainer(X)
+    shap_importance_values = [np.abs(shap_values.values[:, i, :]).mean() for i in range(shap_values.values.shape[1])]
+    importances = pd.DataFrame(
+        data={'variable_name': shap_values.feature_names, 'shap_importance': shap_importance_values}).sort_values(
+        'shap_importance', ascending=False).reset_index(drop=True)
+    return importances
+
+
+def compute_highly_related_pairs(df, num_vars=None, cat_vars=None, num_kws=None, mixed_kws=None, cat_kws=None):
+    """
+    Computes strongly related pairs of variables. Depending on the type of variables, a correlation coefficient
+    (numerical variables), partial eta squared (mixed-type variables), or mutual information (categorical variables) is
+    used to measured the strength of the relationship of each pair.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    num_vars : str, list, series, or vector array
+        Numerical variable name(s).
+    cat_vars : str, list, series, or vector array
+        Categorical variable name(s).
+    {num,mixed,cat}_kws : dict, default=None
+        Additional keyword arguments to pass to `compute_high_corr_pairs()`, `compute_highly_related_mixed_vars()`, and
+        `compute_highly_related_categorical_vars()`.
+
+    Returns
+    ----------
+    final_pairs : `pandas.DataFrame`
+        DataFrame with pairs of highly correlated variables (var1: variable with values to impute; var2: variable to be
+        used as independent variable for model-based imputation).
+    """
+    if num_vars is None and cat_vars is None:
+        raise ValueError('Numerical or categorical variable lists are required.')
+
+    # Numerical variable pairs (correlation)
+    num_pairs = pd.DataFrame()
+    if num_vars:
+        num_kws = num_kws if num_kws else dict()
+        num_pairs = compute_high_corr_pairs(df[num_vars], **num_kws)
+
+    # Mixed variable pairs (cross correlation)
+    mixed_pairs = pd.DataFrame()
+    if num_vars and cat_vars:
+        mixed_kws = mixed_kws if mixed_kws else dict()
+        mixed_pairs = compute_highly_related_mixed_vars(df, num_vars, cat_vars, **mixed_kws)
+
+    # Categorical variable pairs (mutual information)
+    cat_pairs = pd.DataFrame()
+    if cat_vars:
+        cat_kws = cat_kws if cat_kws else dict()
+        cat_pairs = compute_highly_related_categorical_vars(df[cat_vars], **cat_kws)
+
+    final_pairs = pd.concat([num_pairs, mixed_pairs, cat_pairs], ignore_index=True)
+    return final_pairs
+
+
+def run_feature_selection(df, original_features, target, classifier, num_vars=None, cat_vars=None, features_to_keep=[],
+                          hi_rel=None, num_kws=None, mixed_kws=None, cat_kws=None, rfecv_kws=None):
+    """
+    Performs feature selection in three steps:
+        - First, if some features must be kept (informed in `features_to_keep`), those other
+          features that highly related with those in `features_to_keep` are removed.
+
+        - Next, a classifier is iteratively trained to obtain feature shap importances.
+          In each iteration, the feature with the highest shape importance which has not been previously
+          visited is selected and all other highly related features are removed.
+
+        - Finally, Recursive Feature Elimination with Cross-Validation (RFECV) is applied on the remaining
+          features.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing, at least, all variables in `original_features`
+    original_features : `numpy.array` or list
+        Array of original features to select from. `original_features` = `num_vars` + `cat_vars`.
+    target : `numpy.array` or list
+        Array with target values. This is the dependent variable values.
+    classifier : estimator
+        Should be an instance of `scikit-learn.Estimator`, `scikit-learn.Predictor`, `scikit-learn.Transformer`,
+        and `scikit-learn.Model`.
+    num_vars : `numpy.array` or list, default=None
+        Array of numerical features. All variable in `num_vars` must be in `original_features`.
+    cat_vars : `numpy.array` or list, default=None
+        Array of categorical features. All variable in `cat_vars` must be in `original_features`.
+    features_to_keep: list, default=[]
+        In case some features are of special interest to the analysis and should be kept.
+    hi_rel: `pandas.DataFrame`, default=None
+        DataFrame with pairs of highly correlated variables. If not passed, it is computed using all the data from `df`
+    {num,mixed,cat}_kws : dict, default=None
+        Additional keyword arguments to pass to `compute_high_corr_pairs()`, `compute_highly_related_mixed_vars()`, and
+        `compute_highly_related_categorical_vars()`.
+    rfecv_kws : dictionary, default=None
+        Dictionary for RFECV.
+
+    Returns
+    ----------
+    filtered_features : list
+        List with selected features.
+    """
+
+    tot_vars = 0
+    if cat_vars:
+        tot_vars += len(cat_vars)
+    if num_vars:
+        tot_vars += len(num_vars)
+    assert len(original_features) == tot_vars, "`original_features` != `num_vars` + `cat_vars`"
+
+    # First, we compute highly related pairs of variables
+    if num_kws is None:
+        num_kws = dict(corr_thres=0.8)
+    if hi_rel is None:
+        hi_rel = compute_highly_related_pairs(df, num_vars, cat_vars, num_kws, mixed_kws, cat_kws)
+
+    filtered_features = original_features.copy()
+
+    if hi_rel.shape[0] > 0:
+        # Next, we remove variables highly correlated with the ones indicated to be kept
+        for v in features_to_keep:
+            for v2 in hi_rel.loc[hi_rel['var1'] == v, 'var2'].to_list():
+                if v2 in filtered_features:
+                    if v2 in features_to_keep:
+                        logger.warning(
+                            f'Variables {v} and {v2} are highly correlated, and both were selected to be kept.')
+                    else:
+                        filtered_features.remove(v2)
+
+    if hi_rel.shape[0] > 0:
+        # Next, we remove highly correlated variables iteratively, keeping the most impactful one
+        # We identify the most impactful one using a classification algorithm and SHAP importances
+        # In every iteration, we remove all variables with a high correlation to the unvisited variable
+        # with the highest SHAP importance value
+        stop = False
+        visited = []
+        while not stop:
+            X = df[filtered_features]
+            y = target
+            classifier.fit(X, y)
+            importances = get_shap_importances(classifier, X)
+            updated = False
+            for feat in importances['variable_name']:
+                if feat in hi_rel['var1'].to_list() and feat not in visited:
+                    visited.append(feat)
+                    for var2 in hi_rel.loc[hi_rel['var1'] == feat, 'var2'].to_list():
+                        if var2 in filtered_features and var2 not in features_to_keep:
+                            filtered_features.remove(var2)
+                    updated = True
+                    break
+
+            stop = not updated
+
+    # Finally, we apply Recursive Feature Elimination with Cross-Validation (RFECV)
+    X = df[filtered_features]
+    y = target
+    if rfecv_kws is None:
+        rfecv_kws = dict(step=1, cv=3)
+    selector = RFECV(classifier, **rfecv_kws)
+    selector = selector.fit(X, y)
+    filtered_features = list(np.array(filtered_features)[np.where(selector.support_)])
+    return filtered_features
+
+
+def run_hyperparameter_tuning(X_train, y_train, classifier, param_grid, gridsearch_kws=None):
+    """
+    Runs grid search with cross-validation for hyperparameter tuning.
+
+    Parameters
+    ----------
+    X_train : `np.ndarray` or `pandas.DataFrame`
+        Training set.
+    y_train : `numpy.array` or list
+        Training target values.
+    classifier : estimator, default=None
+        Should be an instance of `scikit-learn.Estimator`, `scikit-learn.Predictor`, `scikit-learn.Transformer`,
+        and `scikit-learn.Model`.
+        If none is passed a `sklearn.ensemble.RandomForestClassifier` model is used.
+    param_grid : dictionary
+        Dictionary with the different hyperparameters and their corresponding values to be evaluated.
+    gridsearch_kws : dictionary, default=None
+        Dictionary for grid search.
+
+    Returns
+    ----------
+    grid_result : `sklearn.model_selection.GridSearchCV`
+        Instance of fitted estimator.
+    """
+    if gridsearch_kws is None:
+        gridsearch_kws = dict(n_jobs=-1, cv=3)
+    grid_search = GridSearchCV(classifier, param_grid, **gridsearch_kws)
+    grid_result = grid_search.fit(X_train, y_train)
+    return grid_result
