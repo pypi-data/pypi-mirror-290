929600bd342ecefa9d9955b001b96de7933ec98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 15:04:48 +0000

    Add package data-service-sdk/0.0.4/data_service_sdk-0.0.4.tar.gz

---
+++ packages/data-service-sdk/data_service_sdk-0.0.4.tar.gz/data_service_sdk-0.0.4/sais/autotrain/dataservice/__init__.py
@@ -0,0 +1,4 @@
+from sais.autotrain.dataservice.auth.auth_info import EnvVarCredentialsProvider
+from sais.autotrain.dataservice.config.const import ENDPOINT
+from sais.autotrain.dataservice.handler.query_handler import QueryStationRelatedHandler, QueryNWPHandler
+from sais.autotrain.dataservice.model.data_model import QueryStationRequest, QueryObservationRequest, QueryNWPRequest
