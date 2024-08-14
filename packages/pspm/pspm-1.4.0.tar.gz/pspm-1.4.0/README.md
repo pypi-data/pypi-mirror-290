a50c5c8be40c1aedfaff7937c194c6e225206613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:32 +0000

    Add package pspm/1.4.0/pspm-1.4.0.tar.gz

---
+++ packages/pspm/pspm-1.4.0.tar.gz/pspm-1.4.0/README.md
@@ -0,0 +1,31 @@
+# pspm
+
+pspm is Python's Simplest Package Manager.
+
+![logo-pspm](https://github.com/user-attachments/assets/6a35d219-1193-42f8-a1b6-0331818e8e4d)
+
+## Installation
+
+I recommend using [pipx](https://pipx.pypa.io/stable/) to install pspm.
+
+```bash
+pipx install pspm
+```
+
+## Quickstart
+### Install packages
+```bash
+spm add -g dev pytest
+```
+
+## Roadmap
+### Comands
+- [ ] Init
+- [x] Install
+- [x] Add
+- [x] Remove
+- [ ] Run
+
+## License
+
+Licensed under the [GNU GPLv3](LICENSE) license.
---
+++ packages/pspm/pspm-1.4.0.tar.gz/pspm-1.4.0/src/pspm/entities/installer.py
@@ -0,0 +1,93 @@
+"""Module with classes to deal with packages."""
+
+from __future__ import annotations
+
+import abc
+import subprocess
+from shutil import which
+
+import uv
+
+from pspm.errors.dependencies import InstallError
+
+
+class BaseInstaller(abc.ABC):
+    """Package installer."""
+
+    @abc.abstractmethod
+    def install(self, package: str, *, editable: bool = False) -> None:
+        """Install package.
+
+        Args:
+            package: Package to install
+            editable: Whether to install in editable mode
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def uninstall(self, package: str) -> None:
+        """Uninstall package.
+
+        Args:
+            package: Package to uninstall
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def sync(self, requirements_files: list[str]) -> None:
+        """Sync an environment with requirements files.
+
+        Install all dependencies listed in requirements files
+        and removes the ones that are not listed.
+
+        Args:
+            requirements_files: Path to files containing requirements
+        """
+        raise NotImplementedError
+
+
+class UVInstaller(BaseInstaller):
+    """Install packages with UV."""
+
+    def __init__(self) -> None:
+        """Initialize UV Installer."""
+        self._uv_path = which("uv") or uv.find_uv_bin()
+
+    def install(self, package: str, *, editable: bool = False) -> None:
+        """Install a package.
+
+        Args:
+            package: Package to install
+            editable: Whether to install in editable mode
+
+        Raises:
+            InstallError: If can't install package.
+        """
+        retcode = subprocess.call([
+            self._uv_path,
+            "pip",
+            "install",
+            *(["--editable"] if editable else []),
+            package,
+        ])
+        if retcode != 0:
+            raise InstallError(package)
+
+    def uninstall(self, package: str) -> None:
+        """Uninstall package.
+
+        Args:
+            package: Package to uninstall
+        """
+        raise NotImplementedError
+
+    def sync(self, requirements_files: list[str]) -> None:
+        """Sync an environment with requirements files.
+
+        Install all dependencies listed in requirements files
+        and removes the ones that are not listed.
+
+        Args:
+            requirements_files: Path to files containing requirements
+        """
+        subprocess.call([self._uv_path, "pip", "sync", *requirements_files])
