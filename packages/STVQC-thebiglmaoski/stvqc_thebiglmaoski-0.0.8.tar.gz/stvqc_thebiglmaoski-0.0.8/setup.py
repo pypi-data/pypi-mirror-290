1cfbc756f1856296b5c6bde486eb24e4b23a42cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:13 +0000

    Add package STVQC-thebiglmaoski/0.0.8/stvqc_thebiglmaoski-0.0.8.tar.gz

---
+++ packages/STVQC-thebiglmaoski/stvqc_thebiglmaoski-0.0.8.tar.gz/stvqc_thebiglmaoski-0.0.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="STVQC_thebiglmaoski",
+    version="0.0.8",
+    description="Implementation of the ST-VQC in TorchQuantum",
+    author="Noah Khan, Hanad Elmi",
+    author_email="noahkhan022@gmail.com, hanadelm@yahoo.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "torch>=1.7.0",
+        "numpy>=1.19.0",
+        "qiskit>=0.39.0",
+        "qiskit-ibm-runtime>=0.8.0",
+        "matplotlib>=3.8.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "torchquantum-cli=torchquantum.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
