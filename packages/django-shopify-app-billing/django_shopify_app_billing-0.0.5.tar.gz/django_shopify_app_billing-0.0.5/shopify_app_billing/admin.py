25aa7582d2586ac138292f5bab00e8dd2ab7cd17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 15:04:18 +0000

    Add package django-shopify-app-billing/0.0.5/django_shopify_app_billing-0.0.5.tar.gz

---
+++ packages/django-shopify-app-billing/django_shopify_app_billing-0.0.5.tar.gz/django_shopify_app_billing-0.0.5/shopify_app_billing/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import ShopBilling, RecurringApplicationCharge
+
+
+@admin.register(ShopBilling)
+class ShopBillingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecurringApplicationCharge)
+class RecurringApplicationChargeAdmin(admin.ModelAdmin):
+    list_display = ("name", "shop_billing", "price", "status", "test", "currency")
+    list_filter = ("status", "test")
