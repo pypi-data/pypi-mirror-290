7634dd6f4c8156e208f3b56ec23012e250c593ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:23 +0000

    Add package simple-calculator-yyl/0.2/simple_calculator_yyl-0.2.tar.gz

---
+++ packages/simple-calculator-yyl/simple_calculator_yyl-0.2.tar.gz/simple_calculator_yyl-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_calculator_yyl',
+    version='0.2',
+    packages=find_packages(),
+    description='A simple calculator Python package',
+    author='Yi-Yang Lin',
+    author_email='yiyanglin0102@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
