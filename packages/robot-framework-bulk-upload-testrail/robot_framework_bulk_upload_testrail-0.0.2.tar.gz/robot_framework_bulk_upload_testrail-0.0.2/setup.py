712ed445a8f75ff87d169c0b9d76f0ab261e468e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:04:15 +0000

    Add package robot-framework-bulk-upload-testrail/0.0.2/robot_framework_bulk_upload_testrail-0.0.2.tar.gz

---
+++ packages/robot-framework-bulk-upload-testrail/robot_framework_bulk_upload_testrail-0.0.2.tar.gz/robot_framework_bulk_upload_testrail-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robot-framework-bulk-upload-testrail',
+    version='0.0.2',
+    description='A simple Python library for bulk upload to testrail in robot framework.',
+    author='Adi Darmawan',
+    packages=find_packages(),
+)
