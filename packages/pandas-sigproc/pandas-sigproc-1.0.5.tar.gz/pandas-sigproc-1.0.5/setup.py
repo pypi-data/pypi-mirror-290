fa31627f0046867f5feabc5939981b1a59996414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 15:04:17 +0000

    Add package pandas-sigproc/1.0.5/pandas-sigproc-1.0.5.tar.gz

---
+++ packages/pandas-sigproc/pandas-sigproc-1.0.5.tar.gz/pandas-sigproc-1.0.5/setup.py
@@ -0,0 +1,31 @@
+import os
+import setuptools
+
+
+# Utility function to read a file at same level as this one.
+def read(f_name):
+    with open(os.path.join(os.path.dirname(__file__), f_name)) as f:
+        return f.read()
+
+
+def read_requirements():
+    requirements = []
+    for line in read('requirements.txt').split('\n'):
+        if line and line[0] != '-' and line[0] != '#':
+            requirements.append(line)
+    return requirements
+
+
+setuptools.setup(
+    name='pandas-sigproc',
+    version='1.0.5',
+    description='Useful pandas extensions for detailed signal processing',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/johns7591/pandas-sigproc',
+    author='John Scanlon',
+    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
+    install_requires=read_requirements()
+)
+
