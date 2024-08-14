2be5fd3f4d6e4908b8cf913ec158e4dccad5f9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 16:04:53 +0000

    Add package aiida-flexpart/0.1.5/aiida_flexpart-0.1.5.tar.gz

---
+++ packages/aiida-flexpart/aiida_flexpart-0.1.5.tar.gz/aiida_flexpart-0.1.5/aiida_flexpart/data/nc_data.py
@@ -0,0 +1,34 @@
+import os
+from aiida.orm import RemoteData
+
+
+class NetCdfData(RemoteData):
+
+    def __init__(
+        self, filepath=None, remote_path=None, g_att=None, nc_dimensions=None, **kwargs
+    ):
+        """
+        Data plugin for Netcdf files.
+        """
+        super().__init__(**kwargs)
+        if filepath is not None:
+            filename = os.path.basename(filepath)
+            self.set_remote_path(remote_path)
+            self.set_filename(filename)
+            self.set_global_attributes(g_att, nc_dimensions)
+
+    def set_filename(self, val):
+        self.base.attributes.set("filename", val)
+
+    def set_global_attributes(self, g_att, nc_dimensions):
+        self.base.attributes.set("global_attributes", g_att)
+        self.base.attributes.set("dimensions", nc_dimensions)
+
+    def ncdump(self):
+        """Small python version of ncdump."""
+        print("dimensions:")
+        for k, v in self.base.attributes.get("dimensions").items():
+            print(f"\t {k} = {v}")
+        print("// global attributes:")
+        for k, v in self.base.attributes.get("global_attributes").items():
+            print(f"\t :{k} = {v}")
