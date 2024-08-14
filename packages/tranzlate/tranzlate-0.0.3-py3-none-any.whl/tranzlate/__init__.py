b2c901abd45523c9ceab1af4ceb0648fed3b8da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:03:45 +0000

    Add package tranzlate/0.0.3/tranzlate-0.0.3-py3-none-any.whl

---
+++ packages/tranzlate/tranzlate-0.0.3-py3-none-any.whl/tranzlate/__init__.py
@@ -0,0 +1,12 @@
+"""
+#### tranzlate
+
+Translate text, files, markup and BeautifulSoup.
+
+@Author: Daniel T. Afolayan (ti-oluwa.github.io)
+"""
+
+from .translator import Translator, add_translatable_html_tag
+
+__all__ = ["Translator", "add_translatable_html_tag"]
+__version__ = "0.0.3"
