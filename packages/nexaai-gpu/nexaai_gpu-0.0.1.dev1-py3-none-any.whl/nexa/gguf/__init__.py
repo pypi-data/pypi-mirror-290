ee0b05fe3df384e6b3b6c56e56b1e5f7ef139ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 17:04:16 +0000

    Add package nexaai-gpu/0.0.1.dev1/nexaai_gpu-0.0.1.dev1-py3-none-any.whl

---
+++ packages/nexaai-gpu/nexaai_gpu-0.0.1.dev1-py3-none-any.whl/nexa/gguf/__init__.py
@@ -0,0 +1,3 @@
+# from nexa.gguf.nexa_inference_image import NexaImageInference
+# from nexa.gguf.nexa_inference_text import NexaTextInference
+# from nexa.gguf.nexa_inference_vlm import NexaVLMInference
