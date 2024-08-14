fb8a0b7771e3cf41c3fe0def470d67a1e6526582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:20 +0000

    Add package pyrovelocity/0.3.0b4/pyrovelocity-0.3.0b4.tar.gz

---
+++ packages/pyrovelocity/pyrovelocity-0.3.0b4.tar.gz/pyrovelocity-0.3.0b4/src/pyrovelocity/io/compressedpickle.py
@@ -0,0 +1,75 @@
+import pickle
+from os import PathLike
+from pathlib import Path
+from typing import Any
+
+import zstandard as zstd
+
+__all__ = ["CompressedPickle"]
+
+
+class CompressedPickle:
+    """
+    A class for reading and writing zstandard-compressed pickle files.
+
+    Examples:
+    >>> import pandas as pd
+    >>> tmp = getfixture("tmp_path")
+    >>> test_data_path = tmp / "test_data.pkl.zst"
+    >>> test_data = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+    >>> CompressedPickle.save(test_data_path, test_data)
+    >>> loaded_data = CompressedPickle.load(test_data_path)
+    >>> loaded_data.equals(test_data)
+    True
+    """
+
+    @staticmethod
+    def save(file_path: PathLike | str, obj: Any) -> None:
+        """
+        Save the given object to a zstandard-compressed pickle file.
+
+        Args:
+            file_path (str): The path of the file to save the object to.
+            obj (object): The object to be saved.
+
+        Examples:
+        >>> import pandas as pd
+        >>> tmp = getfixture("tmp_path")
+        >>> test_data_path = tmp / "test_data.pkl.zst"
+        >>> test_data = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+        >>> CompressedPickle.save(test_data_path, test_data)
+        """
+        file_path = Path(file_path)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with file_path.open("wb") as f:
+            compression_context = zstd.ZstdCompressor(level=3)
+            with compression_context.stream_writer(f) as compressor:
+                pickle.dump(obj, compressor)
+
+    @staticmethod
+    def load(file_path: PathLike | str) -> Any:
+        """
+        Load an object from a zstandard-compressed pickle file.
+
+        Args:
+            file_path (str): The path of the file to load the object from.
+
+        Returns:
+            object: The loaded object.
+
+        Examples:
+        >>> import pandas as pd
+        >>> tmp = getfixture("tmp_path")
+        >>> test_data_path = tmp / "test_data.pkl.zst"
+        >>> test_data = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+        >>> CompressedPickle.save(test_data_path, test_data)
+        >>> loaded_data = CompressedPickle.load(test_data_path)
+        >>> loaded_data.equals(test_data)
+        True
+        """
+        with open(file_path, "rb") as f:
+            decompression_context = zstd.ZstdDecompressor()
+            with decompression_context.stream_reader(f) as decompressor:
+                obj = pickle.load(decompressor)
+        return obj
