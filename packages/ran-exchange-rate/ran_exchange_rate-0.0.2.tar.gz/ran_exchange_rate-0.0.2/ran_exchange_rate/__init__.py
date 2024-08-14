5d6984fcf7a1d4d9ae7a52a7e5134f0bc7597874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:04 +0000

    Add package ran-exchange-rate/0.0.2/ran_exchange_rate-0.0.2.tar.gz

---
+++ packages/ran-exchange-rate/ran_exchange_rate-0.0.2.tar.gz/ran_exchange_rate-0.0.2/ran_exchange_rate/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+*------------------------------
+｜@Project   : ran_exchange_rate 
+｜@ Author   : ran <|-<
+｜@ Time     : 2024/8/13 3:41
+｜@ File     : __init__.py.py
+｜@ Software : PyCharm
+*------------------------------
+"""
