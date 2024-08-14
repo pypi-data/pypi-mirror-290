525b11c8d0c1263ee8f6de0d4b53abd30368e696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:04:11 +0000

    Add package resonate-sdk/0.1.21/resonate_sdk-0.1.21-py3-none-any.whl

---
+++ packages/resonate-sdk/resonate_sdk-0.1.21-py3-none-any.whl/resonate/itertools.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Generic
+
+from typing_extensions import ParamSpec, TypeVar, assert_never
+
+from resonate.result import Err, Ok, Result
+
+if TYPE_CHECKING:
+    from resonate.typing import Runnable, Yieldable
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+@dataclass(frozen=True)
+class FinalValue(Generic[T]):
+    v: Result[T, Exception]
+
+
+def iterate_coro(runnable: Runnable[T]) -> Yieldable | FinalValue[T]:
+    yieldable: Yieldable
+    try:
+        if runnable.next_value is None:
+            yieldable = next(runnable.coro_and_promise.coro)
+        elif isinstance(runnable.next_value, Ok):
+            yieldable = runnable.coro_and_promise.coro.send(
+                runnable.next_value.unwrap()
+            )
+        elif isinstance(runnable.next_value, Err):
+            yieldable = runnable.coro_and_promise.coro.throw(runnable.next_value.err())
+        else:
+            assert_never(runnable.next_value)
+    except StopIteration as e:
+        return FinalValue(Ok(e.value))
+    except Exception as e:  # noqa: BLE001
+        return FinalValue(Err(e))
+    return yieldable
