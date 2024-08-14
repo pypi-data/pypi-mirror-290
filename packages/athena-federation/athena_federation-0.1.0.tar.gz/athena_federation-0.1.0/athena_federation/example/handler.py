9637b057617e71f4cdf2c1bfd929fcd6c12e4bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:04:03 +0000

    Add package athena-federation/0.1.0/athena_federation-0.1.0.tar.gz

---
+++ packages/athena-federation/athena_federation-0.1.0.tar.gz/athena_federation-0.1.0/athena_federation/example/handler.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+from athena_federation.lambda_handler import AthenaLambdaHandler
+from sample_data_source import SampleDataSource
+
+# This needs to be a valid bucket that the Lambda function role has access to
+spill_bucket = os.environ["TARGET_BUCKET"]
+
+example_handler = AthenaLambdaHandler(
+    data_source=SampleDataSource(), spill_bucket=spill_bucket
+)
+
+
+def lambda_handler(event, context):
+    # For debugging purposes, we print both the event and the response :)
+    print(json.dumps(event))
+    response = example_handler.process_event(event)
+    print(json.dumps(response))
+
+    return response
