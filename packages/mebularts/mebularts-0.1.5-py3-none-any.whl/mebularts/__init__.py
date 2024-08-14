5e9f2ba9082531fbc87a619e77821bf70069798e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:04:00 +0000

    Add package mebularts/0.1.5/mebularts-0.1.5-py3-none-any.whl

---
+++ packages/mebularts/mebularts-0.1.5-py3-none-any.whl/mebularts/__init__.py
@@ -0,0 +1,13 @@
+from .fake_data import (
+    fake_male_first_name, 
+    fake_female_first_name, 
+    fake_last_name, 
+    fake_username, 
+    fake_email, 
+    fake_phone_number, 
+    fake_address, 
+    fake_birthdate, 
+    fake_company_name, 
+    fake_user_agent
+)
+
