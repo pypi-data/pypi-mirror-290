c4430652048a80e4e20ef448bef3a40ab4c33dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:34 +0000

    Add package worker-automate-hub/0.3.97/worker_automate_hub-0.3.97.tar.gz

---
+++ packages/worker-automate-hub/worker_automate_hub-0.3.97.tar.gz/worker_automate_hub-0.3.97/worker_automate_hub/tasks/task_definitions.py
@@ -0,0 +1,23 @@
+from worker_automate_hub.tasks.jobs.descartes import descartes
+from worker_automate_hub.tasks.jobs.fazer_pudim import fazer_pudim
+from worker_automate_hub.tasks.jobs.login_emsys import login_emsys
+from worker_automate_hub.tasks.jobs.descartes import descartes
+from worker_automate_hub.tasks.jobs.transferencias import transferencias
+
+
+task_definitions = {
+    "5b295021-8df7-40a1-a45e-fe7109ae3902": fazer_pudim,
+    "a0788650-de48-454f-acbf-3537ead2d8ed": login_emsys,
+    "abcfa1ba-d580-465a-aefb-c15ac4514407": descartes,
+    "2c8ee738-7447-4517-aee7-ce2c9d25cea9": transferencias
+}
+
+
+async def is_uuid_in_tasks(uuid_to_check):
+    """
+    Verifica se um UUID está presente nas definições de tarefas.
+
+    :param uuid_to_check: O UUID a ser verificado.
+    :return: True se o UUID estiver presente, False caso contrário.
+    """
+    return uuid_to_check in task_definitions.keys()
