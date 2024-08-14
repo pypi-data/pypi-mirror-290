db843841cbe71037ca94e86560f9d64ccc27d89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 05:03:29 +0000

    Add package napari-clusters-plotter/0.8.1/napari_clusters_plotter-0.8.1-py3-none-any.whl

---
+++ packages/napari-clusters-plotter/napari_clusters_plotter-0.8.1-py3-none-any.whl/napari_clusters_plotter/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.8.1"
+
+
+from ._dock_widget import (
+    ClusteringWidget,
+    DimensionalityReductionWidget,
+    PlotterWidget,
+    napari_experimental_provide_dock_widget,
+)
