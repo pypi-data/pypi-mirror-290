fed1e4568c28e9f452839dc6f1b6258d5c43334d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:33 +0000

    Add package magentic/0.31.0/magentic-0.31.0.tar.gz

---
+++ packages/magentic/magentic-0.31.0.tar.gz/magentic-0.31.0/src/magentic/__init__.py
@@ -0,0 +1,38 @@
+from magentic.chat_model.message import (
+    AnyMessage,
+    AssistantMessage,
+    FunctionResultMessage,
+    Placeholder,
+    SystemMessage,
+    ToolResultMessage,
+    UserMessage,
+)
+from magentic.chat_model.openai_chat_model import OpenaiChatModel
+from magentic.chatprompt import chatprompt
+from magentic.function_call import (
+    AsyncParallelFunctionCall,
+    FunctionCall,
+    ParallelFunctionCall,
+)
+from magentic.prompt_chain import prompt_chain
+from magentic.prompt_function import prompt
+from magentic.streaming import AsyncStreamedStr, StreamedStr
+
+__all__ = [
+    "AnyMessage",
+    "AssistantMessage",
+    "FunctionResultMessage",
+    "Placeholder",
+    "SystemMessage",
+    "ToolResultMessage",
+    "UserMessage",
+    "OpenaiChatModel",
+    "chatprompt",
+    "AsyncParallelFunctionCall",
+    "FunctionCall",
+    "ParallelFunctionCall",
+    "prompt_chain",
+    "prompt",
+    "AsyncStreamedStr",
+    "StreamedStr",
+]
