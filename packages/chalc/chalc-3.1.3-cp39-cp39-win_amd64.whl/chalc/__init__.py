1565a44f9e850207561dd703ef9e4346ad51a3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:04:09 +0000

    Add package chalc/3.1.3/chalc-3.1.3-cp39-cp39-win_amd64.whl

---
+++ packages/chalc/chalc-3.1.3-cp39-cp39-win_amd64.whl/chalc/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import platform
+
+from . import filtration
+if platform.system() == 'Windows':
+    # need to load up the GMP and MPFR DLLs
+    import os as _os
+    from importlib.metadata import files as _files
+    _bindir = [x 
+        for x in _files('chalc') 
+        if len(x.parent.parts) > 0 
+        and x.parent.parts[-1] == 'bin'][0].locate().parent
+    with _os.add_dll_directory(_bindir):
+        from . import chromatic
+else:
+    from . import chromatic
+
+from . import sixpack
+from . import plotting
+
+__all__ = ['chromatic', 'filtration', 'sixpack', 'plotting']
+
+__doc__ = "Chalc is a package to compute the persistent homology of chromatic complexes of points clouds in Euclidean space."
