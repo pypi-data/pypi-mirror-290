c39631d7842e2325116ae24127668122bed1024f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:12 +0000

    Add package apache-gravitino/0.1.0.dev10/apache_gravitino-0.1.0.dev10.tar.gz

---
+++ packages/apache-gravitino/apache_gravitino-0.1.0.dev10.tar.gz/apache_gravitino-0.1.0.dev10/gravitino/dto/responses/catalog_response.py
@@ -0,0 +1,50 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
+
+from dataclasses import dataclass, field
+
+from dataclasses_json import config
+
+from .base_response import BaseResponse
+from ..catalog_dto import CatalogDTO
+
+
+@dataclass
+class CatalogResponse(BaseResponse):
+    """Represents a response containing catalog information."""
+
+    _catalog: CatalogDTO = field(metadata=config(field_name="catalog"))
+
+    def validate(self):
+        """Validates the response data.
+
+        Raises:
+            IllegalArgumentException if the catalog name, type or audit is not set.
+        """
+        super().validate()
+
+        assert self.catalog is not None, "catalog must not be null"
+        assert (
+            self.catalog.name() is not None
+        ), "catalog 'name' must not be null and empty"
+        assert self.catalog.type() is not None, "catalog 'type' must not be null"
+        assert self.catalog.audit_info() is not None, "catalog 'audit' must not be null"
+
+    def catalog(self) -> CatalogDTO:
+        return self._catalog
---
+++ packages/apache-gravitino/apache_gravitino-0.1.0.dev10.tar.gz/apache_gravitino-0.1.0.dev10/gravitino/dto/responses/schema_response.py
@@ -0,0 +1,51 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
+
+from dataclasses import dataclass, field
+
+from dataclasses_json import DataClassJsonMixin, config
+
+from gravitino.dto.responses.base_response import BaseResponse
+from gravitino.dto.schema_dto import SchemaDTO
+
+
+@dataclass
+class SchemaResponse(BaseResponse, DataClassJsonMixin):
+    """Represents a response for a schema."""
+
+    _schema: SchemaDTO = field(metadata=config(field_name="schema"))
+
+    # TODO
+    # pylint: disable=arguments-differ
+    def schema(self) -> SchemaDTO:
+        return self._schema
+
+    def validate(self):
+        """Validates the response data.
+
+        Raises:
+            IllegalArgumentException if catalog identifiers are not set.
+        """
+        super().validate()
+
+        assert self._schema is not None, "schema must be non-null"
+        assert (
+            self._schema.name() is not None
+        ), "schema 'name' must not be null and empty"
+        assert self._schema.audit_info() is not None, "schema 'audit' must not be null"
