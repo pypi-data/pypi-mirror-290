9b6e3ff317af337906487bb4cb551dd4ea36ec8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:04:21 +0000

    Add package adi-distributions/1.1/adi_distributions-1.1.tar.gz

---
+++ packages/adi-distributions/adi_distributions-1.1.tar.gz/adi_distributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='adi_distributions',
+      version = '1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['adi_distributions'],
+      author = 'Adithya Chintala',
+      zip_safe=False)
