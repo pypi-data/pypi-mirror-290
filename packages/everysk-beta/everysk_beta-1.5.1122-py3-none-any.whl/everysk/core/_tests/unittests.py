4c0bf89e5d4edcc270340cd53c4b5e2fa8bd480f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:32 +0000

    Add package everysk-beta/1.5.1122/everysk_beta-1.5.1122-py3-none-any.whl

---
+++ packages/everysk-beta/everysk_beta-1.5.1122-py3-none-any.whl/everysk/core/_tests/unittests.py
@@ -0,0 +1,34 @@
+###############################################################################
+#
+# (C) Copyright 2024 EVERYSK TECHNOLOGIES
+#
+# This is an unpublished work containing confidential and proprietary
+# information of EVERYSK TECHNOLOGIES. Disclosure, use, or reproduction
+# without authorization of EVERYSK TECHNOLOGIES is prohibited.
+#
+###############################################################################
+from unittest.mock import MagicMock
+from everysk.core.unittests import TestCase
+
+
+class SDKUnittestTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.mock_method = MagicMock()
+        self.mock_method.return_value = 'different than None'
+
+    def test_obj_different_than_none(self):
+        obj = TestCase()
+
+        with self.assertWarns(DeprecationWarning) as context:
+            obj._callTestMethod(self.mock_method) # pylint: disable=protected-access
+
+        self.assertEqual(str(context.warning), f"It is deprecated to return a value that is not None from a test case ({self.mock_method})")
+
+    def test_assert_dict_equal_when_dicts_are_different(self):
+        first_dict = {'key': 'value'}
+        second_dict = {'chave': 'valor'}
+        with self.assertRaises(AssertionError) as context:
+            self.assertDictEqual(d1=first_dict, d2=second_dict)
+
+        self.assertEqual(str(context.exception), "{'key': 'value'} != {'chave': 'valor'}\n- {'key': 'value'}\n+ {'chave': 'valor'}")
