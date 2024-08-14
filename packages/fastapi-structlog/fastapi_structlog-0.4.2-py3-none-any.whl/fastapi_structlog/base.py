f21861acaef056d35cc6ca57ae9397113781e65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 05:03:21 +0000

    Add package fastapi-structlog/0.4.2/fastapi_structlog-0.4.2-py3-none-any.whl

---
+++ packages/fastapi-structlog/fastapi_structlog-0.4.2-py3-none-any.whl/fastapi_structlog/base.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from pydantic import model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from fastapi_structlog.utils import check_sub_settings_unset
+
+
+class BaseSettingsModel(BaseSettings):
+    model_config = SettingsConfigDict(
+        arbitrary_types_allowed=True,
+        env_ignore_empty=True,
+        env_nested_delimiter='__',
+        extra='ignore',
+    )
+
+    @model_validator(mode='before')
+    @classmethod
+    def _check_sub_settings_unset(cls, values: dict[str, Any]) -> dict[str, Any]:
+        return check_sub_settings_unset(cls.model_fields, values)
