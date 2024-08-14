95398b87359c5acb7bbbab3835aeec0e6d42a178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:59 +0000

    Add package django-polly/0.0.4/django_polly-0.0.4.tar.gz

---
+++ packages/django-polly/django_polly-0.0.4.tar.gz/django_polly-0.0.4/django_polly/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class DjangoPollyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_polly'
+
+    def ready(self):
+        pass
