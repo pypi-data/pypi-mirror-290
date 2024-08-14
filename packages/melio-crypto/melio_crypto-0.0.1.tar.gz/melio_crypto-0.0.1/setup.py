babab61a18366b3189a1d057292d70c71e408565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:06 +0000

    Add package melio-crypto/0.0.1/melio_crypto-0.0.1.tar.gz

---
+++ packages/melio-crypto/melio_crypto-0.0.1.tar.gz/melio_crypto-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="melio-crypto",
+    version="0.0.1",
+    py_modules=["test"],
+    author="Rafael",
+    author_email="rafael.nigri@melio.com",
+    description="A dummy package for testing purposes",
+    url="https://github.com/yourusernameasd/melio-crypto",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
