69077f0a6661a865a38ead34ca00763401ea1d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 15:05:14 +0000

    Add package zyx/0.2.31/zyx-0.2.31.tar.gz

---
+++ packages/zyx/zyx-0.2.31.tar.gz/zyx-0.2.31/zyx/__init__.py
@@ -0,0 +1,104 @@
+__all__ = [
+    "BaseModel",
+    "Field",
+    "logger",
+    "app",
+    "agents",
+    "cli",
+    "chainofthought",
+    "classify",
+    "completion",
+    "code",
+    "delegate",
+    "extract",
+    "function",
+    "generate",
+    "zyxModuleLoader",
+]
+
+# --- zyx ----------------------------------------------------------------
+
+from .core.ext import BaseModel, Field, zyxModuleLoader
+
+
+class agents(zyxModuleLoader):
+    pass
+
+
+agents.init("zyx.client.agents", "Agents")
+
+class app(zyxModuleLoader):
+    pass
+
+
+app.init("zyx.client.app", "app")
+
+
+class cli(zyxModuleLoader):
+    pass
+
+
+cli.init("zyx.client.app", "cli")
+
+
+class chainofthought(zyxModuleLoader):
+    pass
+
+
+chainofthought.init("zyx.client.fn", "chainofthought")
+
+
+class classify(zyxModuleLoader):
+    pass
+
+
+classify.init("zyx.client.fn", "classify")
+
+
+class code(zyxModuleLoader):
+    pass
+
+
+code.init("zyx.client.fn", "code")
+
+
+class extract(zyxModuleLoader):
+    pass
+
+
+extract.init("zyx.client.fn", "extract")
+
+
+class delegate(zyxModuleLoader):
+    pass
+
+
+delegate.init("zyx.client.fn", "delegate")
+
+
+class function(zyxModuleLoader):
+    pass
+
+
+function.init("zyx.client.fn", "function")
+
+
+class generate(zyxModuleLoader):
+    pass
+
+
+generate.init("zyx.client.fn", "generate")
+
+
+class completion(zyxModuleLoader):
+    pass
+
+
+completion.init("zyx.client.main", "completion")
+
+
+class logger(zyxModuleLoader):
+    pass
+
+
+logger.init("loguru", "logger")
