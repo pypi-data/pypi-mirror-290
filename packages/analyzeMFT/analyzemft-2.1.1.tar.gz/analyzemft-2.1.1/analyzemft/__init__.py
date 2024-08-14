eafb9ff76e1977c36d890065d20f1c0ef6bd615c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:04:18 +0000

    Add package analyzeMFT/2.1.1/analyzemft-2.1.1.tar.gz

---
+++ packages/analyzeMFT/analyzemft-2.1.1.tar.gz/analyzemft-2.1.1/analyzemft/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Version 2.1.1
+#
+# Author: Benjamin Cance (bjc@tdx.li)
+# Copyright Benjamin Cance 2024
+#
+# 2-Aug-24 
+# - Updating to current PEP
+
+
+from . import mft_utils
+from . import mft
+from . import mft_session
+
+__all__ = ["mft_utils", "mft", "mft_session"]
