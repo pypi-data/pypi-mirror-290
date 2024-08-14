5689ceb398e409fa476679fdfe1697a42cc9dd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:38 +0000

    Add package metis-data/0.1.0/metis_data-0.1.0.tar.gz

---
+++ packages/metis-data/metis_data-0.1.0.tar.gz/metis_data-0.1.0/metis_data/util/error_messages.py
@@ -0,0 +1,12 @@
+msgs = {
+    422: {
+        1: "Configuration failure, a job_mode is required.",
+        2: "perform_table_creation_protocol hook called but table_creation_protocol not defined on table",
+        3: """The repository requires an identity_merge_condition function 
+    to perform a delta merge. This function takes the name of the baseline and the name of the updates used in the merge.
+    Return a delta table condition that contains an identity column name (or sub column name). """
+    },
+    500: {
+        1: "init_schema_on_read decorated used on function which does not return a dataframe"
+    }
+}
