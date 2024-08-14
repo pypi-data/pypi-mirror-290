8210c2cf436a704e7ebdfda04bf87b8afe927c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:04:19 +0000

    Add package analyzeMFT/2.1.1.2/analyzeMFT-2.1.1.2-py3-none-any.whl

---
+++ packages/analyzeMFT/analyzeMFT-2.1.1.2-py3-none-any.whl/analyzemft/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Version 2.1.1
+#
+# Author: Benjamin Cance (bjc@tdx.li)
+# Copyright Benjamin Cance 2024
+
+from .config import Config
+from .mft_session import MftSession
+from .mft_analyzer import MFTAnalyzer
+from .mft_formatters import mft_to_csv, mft_to_body, mft_to_l2t, mft_to_json
+from .mft_utils import WindowsTime, decodeMFTmagic, decodeMFTisactive, decodeMFTrecordtype, decodeVolumeInfo, decodeObjectID
+
+__all__ = ['Config', 'MftSession', 'MFTAnalyzer', 'mft_to_csv', 'mft_to_body', 'mft_to_l2t', 'mft_to_json',
+           'WindowsTime', 'decodeMFTmagic', 'decodeMFTisactive', 'decodeMFTrecordtype', 'decodeVolumeInfo', 'decodeObjectID']
