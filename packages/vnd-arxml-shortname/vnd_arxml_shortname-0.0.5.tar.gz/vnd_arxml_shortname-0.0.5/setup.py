a27997725407fed6b76f620b4e997174bc116b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 05:03:32 +0000

    Add package vnd-arxml-shortname/0.0.5/vnd_arxml_shortname-0.0.5.tar.gz

---
+++ packages/vnd-arxml-shortname/vnd_arxml_shortname-0.0.5.tar.gz/vnd_arxml_shortname-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vnd_arxml_shortname',
+    version='0.0.5',
+    packages=find_packages(),
+    install_requires=[],
+    author='HJANSS16',
+    author_email='HJANNS16@volvocars.com',
+    description='Library for parsing and changing the names of arxml files',
+    url='https://github.com/volvo-cars/vnd_arxml_shortname',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
