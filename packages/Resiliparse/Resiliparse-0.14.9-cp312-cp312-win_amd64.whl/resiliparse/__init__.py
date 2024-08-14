b0e84802f400cbcc160abdb5aa0ed364e9729aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:03:43 +0000

    Add package Resiliparse/0.14.9/Resiliparse-0.14.9-cp312-cp312-win_amd64.whl

---
+++ packages/Resiliparse/Resiliparse-0.14.9-cp312-cp312-win_amd64.whl/resiliparse/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2021 Janek Bevendorff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_7_4():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'Resiliparse.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_4()
+del _delvewheel_patch_1_7_4
+# end delvewheel patch
