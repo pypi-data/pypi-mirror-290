c21dfb1846099b26a0f5b31aeed93f774326090c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:03:48 +0000

    Add package zdppy-captcha/0.1.0/zdppy_captcha-0.1.0.tar.gz

---
+++ packages/zdppy-captcha/zdppy_captcha-0.1.0.tar.gz/zdppy_captcha-0.1.0/zdppy_captcha/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+"""
+    Captcha
+    ~~~~~~~
+
+    A zdppy_captcha library that generates audio and image CAPTCHAs.
+
+    :copyright: (c) 2014 by Hsiaoming Yang.
+    :license: BSD, see LICENSE for more details.
+"""
+
+__version__ = '0.6.0'
+__author__ = 'Hsiaoming Yang <me@lepture.com>'
+__homepage__ = 'https://github.com/lepture/captcha'
