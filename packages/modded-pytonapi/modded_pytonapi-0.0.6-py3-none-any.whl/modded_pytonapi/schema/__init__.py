b5c0a194aa2af2f0fd39d229a7bd2f5bcf7864d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 14:08:13 +0000

    Add package modded-pytonapi/0.0.6/modded_pytonapi-0.0.6-py3-none-any.whl

---
+++ packages/modded-pytonapi/modded_pytonapi-0.0.6-py3-none-any.whl/modded_pytonapi/schema/__init__.py
@@ -0,0 +1,25 @@
+from modded_pytonapi.schema import accounts
+from modded_pytonapi.schema import blockchain
+from modded_pytonapi.schema import dns
+from modded_pytonapi.schema import domains
+from modded_pytonapi.schema import events
+from modded_pytonapi.schema import jettons
+from modded_pytonapi.schema import nft
+from modded_pytonapi.schema import rates
+from modded_pytonapi.schema import staking
+from modded_pytonapi.schema import storage
+from modded_pytonapi.schema import traces
+
+__all__ = [
+    "accounts",
+    "blockchain",
+    "dns",
+    "domains",
+    "events",
+    "jettons",
+    "nft",
+    "rates",
+    "staking",
+    "storage",
+    "traces",
+]
---
+++ packages/modded-pytonapi/modded_pytonapi-0.0.6-py3-none-any.whl/pytonapi/tonapi/methods/storage.py
@@ -0,0 +1,16 @@
+from modded_pytonapi.tonapi.client import TonapiClient
+from modded_pytonapi.schema.storage import StorageProviders
+
+
+class StorageMethod(TonapiClient):
+
+    def get_providers(self) -> StorageProviders:
+        """
+        Get TON storage providers deployed to the blockchain.
+
+        :return: :class:`StorageProviders`
+        """
+        method = f"v2/storage/providers"
+        response = self._get(method=method)
+
+        return StorageProviders(**response)
