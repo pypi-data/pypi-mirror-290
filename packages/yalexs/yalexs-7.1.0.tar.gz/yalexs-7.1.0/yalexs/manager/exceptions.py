47801e3ce5a062feb82189ed3a95beaf9699211f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:03:46 +0000

    Add package yalexs/7.1.0/yalexs-7.1.0.tar.gz

---
+++ packages/yalexs/yalexs-7.1.0.tar.gz/yalexs-7.1.0/yalexs/manager/exceptions.py
@@ -0,0 +1,19 @@
+"""Exceptions for errors."""
+
+from __future__ import annotations
+
+
+class RequireValidation(Exception):
+    """Error to indicate we require validation (2fa)."""
+
+
+class CannotConnect(Exception):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(Exception):
+    """Error to indicate there is invalid auth."""
+
+
+class YaleXSError(Exception):
+    """Base error."""
