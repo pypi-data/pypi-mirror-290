4edb3a620c95f0cc4873370555dd011c59d6b51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 16:04:36 +0000

    Add package wechatautomation/0.1.5/wechatautomation-0.1.5.tar.gz

---
+++ packages/wechatautomation/wechatautomation-0.1.5.tar.gz/wechatautomation-0.1.5/wechatautomation/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-----------------------------------------
+@ProjectName: 
+@Author: 
+@Date: 2024/8/12-下午3:42
+@Description: 
+-----------------------------------------
+"""
+
+from .wxutils import *
+__version__ = VERSION
+
+__all__ = [
+    "__version__",
+]
