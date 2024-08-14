600b1bd70145bbd90a193f8411fa0ac33dd4a98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:55 +0000

    Add package nonebot-plugin-dcqq-relay/1.0.3/nonebot_plugin_dcqq_relay-1.0.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-dcqq-relay/nonebot_plugin_dcqq_relay-1.0.3-py3-none-any.whl/nonebot_plugin_dcqq_relay/__init__.py
@@ -0,0 +1,135 @@
+import asyncio
+import re
+from typing import Union
+
+from nonebot import get_driver, logger, on, on_regex, require
+from nonebot.adapters.discord import (
+    Bot as dc_Bot,
+    MessageCreateEvent,
+    MessageDeleteEvent,
+)
+from nonebot.adapters.onebot.v11 import (
+    Bot as qq_Bot,
+    GroupMessageEvent,
+    GroupRecallNoticeEvent,
+)
+from nonebot.plugin import PluginMetadata
+
+require("nonebot_plugin_orm")
+
+from .config import Config, LinkWithoutWebhook, LinkWithWebhook, plugin_config
+from .dc_to_qq import create_dc_to_qq, delete_dc_to_qq
+from .qq_to_dc import create_qq_to_dc, delete_qq_to_dc
+from .utils import check_messages, get_webhook
+
+
+__plugin_meta__ = PluginMetadata(
+    name="QQ群-Discord 互通",
+    description="在QQ群与 Discord 之间同步消息的 nonebot2 插件",
+    usage="",
+    type="application",
+    homepage="https://github.com/Autuamn/nonebot-plugin-dcqq-relay",
+    config=Config,
+    supported_adapters={"~onebot.v11", "~discord"},
+)
+
+
+driver = get_driver()
+without_webhook_links: list[LinkWithoutWebhook] = plugin_config.dcqq_relay_channel_links
+with_webhook_links: list[LinkWithWebhook] = []
+discord_proxy = plugin_config.discord_proxy
+unmatch_beginning = plugin_config.dcqq_relay_unmatch_beginning
+
+just_delete = []
+
+unmatcher = on_regex(
+    rf'\A *?[{re.escape("".join(unmatch_beginning))}].*', priority=1, block=True
+)
+matcher = on(rule=check_messages, priority=2, block=False)
+
+
+@driver.on_bot_connect
+async def get_dc_bot(bot: dc_Bot):
+    global dc_bot
+    dc_bot = bot
+
+
+@driver.on_bot_connect
+async def get_qq_bot(bot: qq_Bot):
+    global qq_bot
+    qq_bot = bot
+
+
+@driver.on_bot_connect
+async def get_webhooks(bot: dc_Bot):
+    logger.info("prepare webhooks: start")
+    global with_webhook_links
+    task = [get_webhook(bot, link) for link in without_webhook_links]
+    links = await asyncio.gather(*task)
+    with_webhook_links.extend(
+        link for link in links if isinstance(link, LinkWithWebhook)
+    )
+    logger.info("prepare webhooks: done")
+    if failed := [link for link in links if isinstance(link, int)]:
+        logger.error(
+            f"{len(failed)} channels failed to get or create webhook: {failed}"
+        )
+
+
+@unmatcher.handle()
+async def unmatcher_pass():
+    pass
+
+
+@matcher.handle()
+async def create_message(
+    bot: Union[qq_Bot, dc_Bot],
+    event: Union[GroupMessageEvent, MessageCreateEvent],
+):
+    logger.debug("into create_message()")
+    try_times = 1
+    while True:
+        try:
+            if isinstance(bot, qq_Bot) and isinstance(event, GroupMessageEvent):
+                await create_qq_to_dc(bot, event, dc_bot, with_webhook_links)
+            elif isinstance(bot, dc_Bot) and isinstance(event, MessageCreateEvent):
+                await create_dc_to_qq(bot, event, qq_bot, with_webhook_links)
+            else:
+                logger.error(
+                    "bot type and event type not match: "
+                    + f"bot - {bot.type}, event - {type(event)}"
+                )
+            break
+        except NameError as e:
+            logger.warning(f"create_message() error: {e}, retry {try_times}")
+            if try_times == 3:
+                raise e
+            try_times += 1
+            await asyncio.sleep(5)
+
+
+@matcher.handle()
+async def delete_message(
+    bot: Union[qq_Bot, dc_Bot],
+    event: Union[GroupRecallNoticeEvent, MessageDeleteEvent],
+):
+    logger.debug("into delete_message()")
+    try_times = 1
+    while True:
+        try:
+            if isinstance(bot, qq_Bot) and isinstance(event, GroupRecallNoticeEvent):
+                await delete_qq_to_dc(event, dc_bot, with_webhook_links, just_delete)
+            elif isinstance(bot, dc_Bot) and isinstance(event, MessageDeleteEvent):
+                await delete_dc_to_qq(event, qq_bot, just_delete)
+            else:
+                logger.error(
+                    "bot type and event type not match: "
+                    + f"bot - {bot.type}, event - {type(event)}"
+                )
+            break
+        except NameError as e:
+            logger.warning(f"delete_message() error: {e}, retry {try_times}")
+            if try_times == 3:
+                raise e
+            try_times += 1
+            await asyncio.sleep(5)
---
+++ packages/nonebot-plugin-dcqq-relay/nonebot_plugin_dcqq_relay-1.0.3-py3-none-any.whl/nonebot_plugin_dcqq_relay/utils.py
@@ -0,0 +1,137 @@
+import re
+from typing import Optional, Union
+
+import aiohttp
+from nonebot import logger
+from nonebot.compat import model_dump
+from nonebot.adapters.discord import (
+    Bot as dc_Bot,
+    MessageCreateEvent,
+    MessageDeleteEvent,
+)
+from nonebot.adapters.discord.api import UNSET, Missing
+from nonebot.adapters.discord.exception import ActionFailed
+from nonebot.adapters.onebot.v11 import (
+    Bot as qq_Bot,
+    GroupMessageEvent,
+    GroupRecallNoticeEvent,
+)
+
+from .config import LinkWithoutWebhook, LinkWithWebhook, plugin_config
+
+
+channel_links: list[LinkWithoutWebhook] = plugin_config.dcqq_relay_channel_links
+discord_proxy = plugin_config.discord_proxy
+
+
+async def check_messages(
+    bot: Union[qq_Bot, dc_Bot],
+    event: Union[
+        GroupMessageEvent,
+        MessageCreateEvent,
+        GroupRecallNoticeEvent,
+        MessageDeleteEvent,
+    ],
+) -> bool:
+    """检查消息"""
+    logger.debug("into check_messages()")
+    if isinstance(event, GroupMessageEvent):
+        return any(event.group_id == link.qq_group_id for link in channel_links)
+    elif isinstance(event, MessageCreateEvent):
+        if not (
+            re.match(r".*?\[QQ:\d*?\]$", event.author.username)
+            and event.author.bot is True
+        ):
+            return any(
+                event.guild_id == link.dc_guild_id
+                and event.channel_id == link.dc_channel_id
+                for link in channel_links
+            )
+        else:
+            return False
+    elif isinstance(event, GroupRecallNoticeEvent):
+        return any(event.group_id == link.qq_group_id for link in channel_links)
+    elif isinstance(event, MessageDeleteEvent):
+        return any(
+            event.guild_id == link.dc_guild_id
+            and event.channel_id == link.dc_channel_id
+            for link in channel_links
+        )
+
+
+async def get_dc_member_name(
+    bot: dc_Bot, guild_id: Missing[int], user_id: int
+) -> tuple[str, str]:
+    try:
+        if guild_id is not UNSET:
+            member = await bot.get_guild_member(guild_id=guild_id, user_id=user_id)
+            if (nick := member.nick) and nick is not UNSET:
+                return nick, member.user.username if member.user is not UNSET else ""
+            elif member.user is not UNSET and (global_name := member.user.global_name):
+                return global_name, member.user.username
+            else:
+                return "", str(user_id)
+        else:
+            user = await bot.get_user(user_id=user_id)
+            return user.global_name or "", user.username
+    except ActionFailed as e:
+        if e.message == "Unknown User":
+            return "(error:未知用户)", str(user_id)
+        else:
+            raise e
+
+
+async def get_file_bytes(url: str, proxy: Optional[str] = None) -> bytes:
+    async with (
+        aiohttp.ClientSession() as session,
+        session.get(url, proxy=proxy) as response,
+    ):
+        return await response.read()
+
+
+async def get_webhook(
+    bot: dc_Bot, link: LinkWithoutWebhook
+) -> Union[LinkWithWebhook, int]:
+    if link.webhook_id and link.webhook_token:
+        return LinkWithWebhook(**model_dump(link))
+    try:
+        channel_webhooks = await bot.get_channel_webhooks(channel_id=link.dc_channel_id)
+        bot_webhook = next(
+            (
+                webhook
+                for webhook in channel_webhooks
+                if webhook.application_id == int(bot.self_id)
+            ),
+            None,
+        )
+        if bot_webhook and bot_webhook.token:
+            return await build_link(link, bot_webhook.id, bot_webhook.token)
+    except Exception as e:
+        logger.error(
+            f"get webhook error, Discord channel id: {link.dc_channel_id}, error: {e}"
+        )
+    try:
+        create_webhook = await bot.create_webhook(
+            channel_id=link.dc_channel_id, name=str(link.dc_channel_id)
+        )
+        if create_webhook.token:
+            return await build_link(link, create_webhook.id, create_webhook.token)
+    except Exception as e:
+        logger.error(
+            f"create webhook error, Discord channel id: {link.dc_channel_id}, "
+            + f"error: {e}"
+        )
+    logger.error(
+        f"failed to get or create webhook, Discord channel id: {link.dc_channel_id}"
+    )
+    return link.dc_channel_id
+
+
+async def build_link(
+    link: LinkWithoutWebhook, webhook_id: int, webhook_token: str
+) -> LinkWithWebhook:
+    return LinkWithWebhook(
+        webhook_id=webhook_id,
+        webhook_token=webhook_token,
+        **model_dump(link, exclude={"webhook_id", "webhook_token"}),
+    )
