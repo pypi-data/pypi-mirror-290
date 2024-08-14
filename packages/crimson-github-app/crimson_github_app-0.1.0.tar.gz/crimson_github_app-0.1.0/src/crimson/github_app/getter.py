476c76d88b867ef73e97e8b2cab0cbf079e0be17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 07:04:14 +0000

    Add package crimson-github-app/0.1.0/crimson_github_app-0.1.0.tar.gz

---
+++ packages/crimson-github-app/crimson_github_app-0.1.0.tar.gz/crimson_github_app-0.1.0/src/crimson/github_app/getter.py
@@ -0,0 +1,41 @@
+from github import Github
+from github.Repository import Repository
+from github.ContentFile import ContentFile
+from github.GithubObject import NotSet
+
+from typing import List, Dict, Optional
+
+
+def get_git_repos(app: Github, user_id: Optional[str] = NotSet) -> Dict[str, Repository]:
+    """
+    If your app is logged in, use it without user_id to keep the authority.
+    """
+    user = app.get_user(user_id)
+
+    repos = {}
+
+    for repo in user.get_repos():
+        repos[repo.name] = repo
+
+    return repos
+
+
+def get_repo_contents(repo: Repository, dir: str) -> List[ContentFile]:
+
+    contents: List[ContentFile] = repo.get_contents(dir)
+    file_contents: List[ContentFile] = []
+
+    while contents:
+        file_content: ContentFile = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            file_contents.append(file_content)
+
+    return file_contents
+
+
+def get_repo_content(repo: Repository, path: str) -> ContentFile:
+    content: List[ContentFile] = repo.get_contents(path)
+
+    return content
