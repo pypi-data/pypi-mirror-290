b002b16344fc0ecbbf578be015aff219e58c48e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 16:04:43 +0000

    Add package openmacro/0.0.4/openmacro-0.0.4-py3-none-any.whl

---
+++ packages/openmacro/openmacro-0.0.4-py3-none-any.whl/openmacro/core/core.py
@@ -0,0 +1,134 @@
+from openmacro.core.utils.computer import Computer
+from openmacro.core.utils.llm import LLM, to_lmc
+from pathlib import Path
+import importlib
+import os
+import toml
+
+from .defaults import (LLM_DEFAULT, CODE_DEFAULT, VISION_DEFAULT)
+
+class Apikey:
+    def __init__(self, key: str = "playwright", name: str = "google"):
+        self.key = key
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name}: {self.key}'
+
+class Profile:
+    """
+    store apikeys here. this is temp since its a bad setup omg.
+    """
+    def __init__(
+            self, 
+            keys: dict = {"llm": LLM_DEFAULT,
+                          "code": CODE_DEFAULT,
+                          "vision": VISION_DEFAULT,
+                          "browser": "playwright"}, 
+            search_engine: str = "google",
+            name: str = None):
+        self.keys = keys
+        self.search_engine = search_engine
+        self.name = "User" if name is None else name
+
+    def __str__(self):
+        return f'Profile({self.name}, {self.keys})'
+
+class Openmacro:
+    """
+    The core of all operations occurs here.
+    Where the system breaks down requests from the user and executes them.
+    """
+    def __init__(
+            self,
+            messages: list | None = None,
+            history_dir: Path | None = None,
+            skills_dir: Path | None = None,
+            prompts_dir: Path | None = None,
+            extensions_dir: Path | None= None,
+            verbose: bool = False,
+            local: bool = False,
+            computer = None,
+            llm = None,
+            tasks = False) -> None:
+        
+        # utils
+        self.computer = Computer() if computer is None else computer
+        self.llm = LLM(Profile(), messages=messages) if llm is None else llm
+
+        self.tasks = tasks
+
+        # logging + debugging
+        self.verbose = verbose
+        
+        # loop breakers
+        self.breakers = frozenset({"The task is done.", "The conversation is done."})
+        
+        # memory + history
+        self.history_dir = Path(Path(__file__).parent, "memory", "history") if history_dir is None else history_dir
+        self.skills_dir = Path(Path(__file__).parent, "memory", "skills") if skills_dir is None else skills_dir
+        self.prompts_dir = Path(Path(__file__).parent, "prompts") if prompts_dir is None else prompts_dir
+        self.extensions_dir = Path(Path(__file__).parent.parent, "extensions") if extensions_dir is None else extensions_dir
+        
+        self.llm.messages = [] if messages is None else messages
+
+        # experimental
+        self.local = local
+        with open(Path(Path(__file__).parent, "config.default.toml"), "r") as file:
+            self.settings = toml.load(file)
+        
+        # extensions including ['browser'] by default
+        for extension in os.listdir(self.extensions_dir):
+            module_path = os.path.join(self.extensions_dir, extension)
+            if os.path.isdir(module_path) and '__init__.py' in os.listdir(module_path):
+                try:
+                    module = importlib.import_module(extension)
+                    name = extension.title()
+                    setattr(self, name, getattr(module, name)())
+                except ImportError:
+                    config_path = Path(self.extensions_dir, extension, "config.default.toml")
+                    if config_path.exists():
+                        with open(config_path, "r") as f:
+                            # Handle the config file as needed
+                            pass
+
+        # prompts
+        self.prompts = {}
+        prompts = os.listdir(self.prompts_dir)
+        for filename in prompts:
+            with open(Path(self.prompts_dir, filename), "r") as f:
+                self.prompts[filename.split('.')[0]] = f.read().strip()
+                
+        self.prompts['initial'] = self.prompts['initial'].format(assistant=self.settings['assistant']['name'],
+                                                                 username=self.computer.user,
+                                                                 os=self.computer.os)
+
+    def chat(self, 
+            message: str = None, 
+            display: bool = True, 
+            stream: bool = False,
+            timeout=16):
+        
+        lmc, conversation = False, set()
+        for _ in range(timeout):
+            responses = self.llm.chat(message, system=self.prompts["initial"], lmc=lmc)
+            lmc = False
+            
+            for response in responses: 
+                if response.get("type", None) == "message":
+                    conversation.add("message")
+                    yield response
+                
+                if response.get("type", None) == "code":
+                    output = to_lmc(self.computer.run(response.get("content", None), format=response.get("format", "python")),
+                                    role="computer", format="output")
+                    if output.get("content", None):
+                        yield output
+                        message, lmc = output, True
+                    conversation.add("code")
+                    
+            if not ("code" in conversation) or response.get("content", None) in self.breakers:
+                return # "The task is done."
+                    
+        raise Warning("Openmacro has exceeded it's timeout stream of thoughts!")
+
---
+++ packages/openmacro/openmacro-0.0.4-py3-none-any.whl/openmacro/core/utils/llm.py
@@ -0,0 +1,158 @@
+from gradio_client import Client, exceptions
+from datetime import datetime
+from functools import partial
+
+from openmacro.core.defaults import (LLM_DEFAULT, LLM_SRC,
+                        CODE_DEFAULT, CODE_SRC,
+                        VISION_DEFAULT, VISION_SRC)
+# omg lmao this is so bad
+
+import re
+
+def interpret_input(input_str):
+    pattern = r'```(?P<format>\w+)\n(?P<content>[\s\S]+?)```|(?P<text>[^\n]+)'
+    
+    matches = re.finditer(pattern, input_str)
+    
+    blocks = []
+    current_message = None
+    
+    for match in matches:
+        if match.group("format"):
+            if current_message:
+                blocks.append(current_message)
+                current_message = None
+            block = {
+                "type": "code",
+                "format": match.group("format"),
+                "content": match.group("content").strip()
+            }
+            blocks.append(block)
+        else:
+            text = match.group("text").strip()
+            if current_message:
+                current_message["content"] += "\n" + text
+            else:
+                current_message = {
+                    "type": "message",
+                    "content": text
+                }
+    
+    if current_message:
+        blocks.append(current_message)
+    
+    return blocks
+
+def to_lmc(content: str, role: str = "assistant", type="message", format: str | None = None) -> dict:            
+    lmc = {"role": role, "type": type, "content": content} 
+    return lmc | ({} if format is None else {"format": format})
+    
+def to_chat(lmc: dict, logs=False) -> str:
+    #_defaults = {}
+    
+    _type, _role, _content, _format = (lmc.get("type", "message"), 
+                                       lmc.get("role", "assistant"), 
+                                       lmc.get("content", "None"), 
+                                       lmc.get("format", None))
+    
+    time = datetime.now().strftime("%I:%M %p %m/%d/%Y")
+    
+    if logs:
+        return f'\033[90m({time})\033[0m [type: {_type if _format is None else f"{_type}, format: {_format}"}] \033[1m{_role}\033[0m: {_content}'
+    
+    if _role == "system":
+        return ("----- SYSTEM PROMPT -----\n" +
+                _content + "\n----- END SYSTEM PROMPT -----")
+    
+    return f'({time}) [type: {_type if _format is None else f"{_type}, format: {_format}"}] *{_role}*: {_content}'
+
+class LLM:
+    def __init__(self, api_key, verbose=False, messages: list = []):
+        self.keys = api_key.keys
+
+        self.verbose = verbose
+        self.messages = messages
+
+        is_llm_default = self.keys.get("llm", LLM_DEFAULT) == LLM_DEFAULT
+        is_code_default = self.keys.get("code", CODE_DEFAULT) == CODE_DEFAULT
+        is_vision_default = self.keys.get("vision", VISION_DEFAULT) == VISION_DEFAULT
+
+        # self.chat = self.litellm_chat
+        if is_llm_default: 
+            self.llm = Client(LLM_SRC)
+            self.chat = self.gradio_chat
+
+        if is_code_default: pass
+        if is_vision_default: pass
+
+    # def perform_search(self, queries, complexity, widget=None):
+    #     # placeholder function
+    #     if self.verbose:
+    #         print(f"\nSearching results for `{queries}`")
+    #     return self.browser.search(queries, complexity, widget)
+        
+
+    def gradio_chat(self, 
+                    message: str, 
+                    role = "user", 
+                    remember=True, 
+                    context=True,
+                    lmc=False,
+                    system: str = 'You are a helpful assistant.'):
+        
+        system = to_lmc(system, role="system")
+        message = message if lmc else to_lmc(message, role=role)
+        
+        to_send = (self.messages if context is True else context) + [system] + [message]
+        
+        if remember:
+            self.messages.append(message)
+            
+        if self.verbose:
+            print('\n' + '\n'.join(map(partial(to_chat, logs=True), to_send)))
+        
+        try:
+            responses = interpret_input(self.llm.predict(user_message='\n'.join(map(to_chat, to_send)), api_name="/predict"))
+        except exceptions.AppError as e:
+            print(f"gradio_client.exceptions.AppError({e})")
+            exit()
+        
+        for response in responses:
+            if remember:
+                self.messages.append(response | {"role": "assistant"})
+                
+            if self.verbose:
+                print(to_chat(response | {"role": "assistant"}, logs=True))
+
+        return responses
+
+    # def chat(self, message: str):
+    #     # Classify the prompt
+    #     needs_search = self.classify_prompt(message)
+
+    #     # Check if a web search is needed
+    #     if not needs_search['search']:
+    #         # Generate a response using LLM
+    #         response = self.chat(message, role="user")
+    #         return response
+
+    #     # Check if widget is available
+    #     if needs_search['widget']:
+
+    #         # Perform the search
+    #         to_search, n, widget = needs_search["search"], needs_search["complexity"], needs_search["widget"]
+    #         search = self.perform_search(to_search, n, widget)
+
+    #         response = self.chat(message, role="user", context=[to_lmc(search, role="browser", type="info")]) 
+    #         return response
+        
+    #     searches = self.perform_search(needs_search['search'], needs_search.get('complexity', 1))
+    #     results = self.browser.load_searches(searches, complexity=needs_search.get('complexity', 1))
+        
+    #     response = self.chat(message, 
+    #                              role="user", 
+    #                              context=self.messages + [to_lmc(str(results))])
+
+    #     return response
+
+
