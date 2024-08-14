fc8beab48c3b0583a265279078c213c0366a507c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:04:25 +0000

    Add package xdfem/2024.8.1/xdfem-2024.8.1.tar.gz

---
+++ packages/xdfem/xdfem-2024.8.1.tar.gz/xdfem-2024.8.1/src/xdfem/decorators.py
@@ -0,0 +1,36 @@
+import time
+from functools import wraps
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Execution time of {func.__name__}: {end - start} seconds")
+        return result
+    return wrapper
+
+
+def debug(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with args: {args} and kwargs: {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned: {result}")
+        return result
+    return wrapper
+
+
+def memoize(func):
+    cache = {}
+    @wraps(func)
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            result = func(*args)
+            cache[args] = result
+            return result
+    return wrapper
