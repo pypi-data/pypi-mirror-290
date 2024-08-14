b67c0b95d2af94d33f8309efa52c830babd1e004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 04:43:11 +0000

    Add package pydase/0.9.0/pydase-0.9.0-py3-none-any.whl

---
+++ packages/pydase/pydase-0.9.0-py3-none-any.whl/pydase/config.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Literal
+
+from confz import BaseConfig, EnvSource
+
+
+class OperationMode(BaseConfig):  # type: ignore[misc]
+    environment: Literal["development", "production"] = "development"
+
+    CONFIG_SOURCES = EnvSource(allow=["ENVIRONMENT"])
+
+
+class ServiceConfig(BaseConfig):  # type: ignore[misc]
+    config_dir: Path = Path("config")
+    web_port: int = 8001
+
+    CONFIG_SOURCES = EnvSource(allow_all=True, prefix="SERVICE_", file=".env")
+
+
+class WebServerConfig(BaseConfig):  # type: ignore[misc]
+    generate_web_settings: bool = False
+
+    CONFIG_SOURCES = EnvSource(allow=["GENERATE_WEB_SETTINGS"])
