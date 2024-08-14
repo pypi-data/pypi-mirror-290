044bfce1d96ed1894eac7121f00105ce89b91de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 05:03:23 +0000

    Add package systembridgemodels/4.2.4/systembridgemodels-4.2.4.tar.gz

---
+++ packages/systembridgemodels/systembridgemodels-4.2.4.tar.gz/systembridgemodels-4.2.4/systembridgemodels/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides systembridgemodels version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update systembridgemodels` to change this file.
+
+from incremental import Version
+
+__version__ = Version("systembridgemodels", 4, 2, 4)
+__all__ = ["__version__"]
