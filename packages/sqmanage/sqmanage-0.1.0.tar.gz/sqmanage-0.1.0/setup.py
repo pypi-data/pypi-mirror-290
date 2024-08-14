4cc603fd96dffb5d5ed59ce0740c454e76e7c61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 14:08:18 +0000

    Add package sqmanage/0.1.0/sqmanage-0.1.0.tar.gz

---
+++ packages/sqmanage/sqmanage-0.1.0.tar.gz/sqmanage-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sqmanage",
+    version="0.1.0",
+    description="Gestor de sqlite",
+    author="Josue Santos",
+    author_email="santosarias502@gmail.com",
+    packages=find_packages(),
+)
