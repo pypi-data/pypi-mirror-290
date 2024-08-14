4ce8bbb22cdbc9d548baa5a700447259b1abc1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:07 +0000

    Add package parsl/2024.8.12/parsl-2024.8.12.tar.gz

---
+++ packages/parsl/parsl-2024.8.12.tar.gz/parsl-2024.8.12/parsl/tests/test_mpi_apps/test_bad_mpi_config.py
@@ -0,0 +1,33 @@
+import pytest
+
+from parsl import Config
+from parsl.executors import HighThroughputExecutor
+from parsl.launchers import AprunLauncher, SimpleLauncher, SrunLauncher
+from parsl.providers import SlurmProvider
+
+
+@pytest.mark.local
+def test_bad_launcher_with_mpi_mode():
+    """AssertionError if a launcher other than SimpleLauncher is supplied"""
+
+    for launcher in [SrunLauncher(), AprunLauncher()]:
+        with pytest.raises(AssertionError):
+            Config(executors=[
+                HighThroughputExecutor(
+                    enable_mpi_mode=True,
+                    provider=SlurmProvider(launcher=launcher),
+                )
+            ])
+
+
+@pytest.mark.local
+def test_correct_launcher_with_mpi_mode():
+    """Confirm that SimpleLauncher works with mpi_mode"""
+
+    config = Config(executors=[
+        HighThroughputExecutor(
+            enable_mpi_mode=True,
+            provider=SlurmProvider(launcher=SimpleLauncher()),
+        )
+    ])
+    assert isinstance(config.executors[0].provider.launcher, SimpleLauncher)
