678ad793288014b75e84f2460b1725b13e2f3953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:10 +0000

    Add package topsis-sidharth-102218069/0.4/topsis-sidharth-102218069-0.4.tar.gz

---
+++ packages/topsis-sidharth-102218069/topsis-sidharth-102218069-0.4.tar.gz/topsis-sidharth-102218069-0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis-sidharth-102218069',
+    version='0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'topsis_69=topsis.__main__:main',
+        ],
+    },
+    author='Sidharth Dhawan',
+    author_email='sidharthdhawan17@gmail.com',
+    description='A Python package to perform TOPSIS analysis.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sidharthd7/Topsis-Sidharth-102218069',
+    license='MIT',
+)
