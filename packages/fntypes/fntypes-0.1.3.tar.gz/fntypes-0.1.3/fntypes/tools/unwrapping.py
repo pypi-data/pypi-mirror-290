a757700ccca2ed396af8547661a0274470b97aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:04:08 +0000

    Add package fntypes/0.1.3/fntypes-0.1.3.tar.gz

---
+++ packages/fntypes/fntypes-0.1.3.tar.gz/fntypes-0.1.3/fntypes/tools/unwrapping.py
@@ -0,0 +1,38 @@
+from functools import wraps
+import typing
+
+from fntypes.result import Result, Error
+from fntypes.option import Option
+from fntypes.error import UnwrapError
+
+ParamSpec = typing.ParamSpec("ParamSpec")
+T = typing.TypeVar("T")
+Err = typing.TypeVar("Err")
+
+
+@typing.overload
+def unwrapping(
+    func: typing.Callable[ParamSpec, Option[T]],
+) -> typing.Callable[ParamSpec, Option[T]]: ...
+
+
+@typing.overload
+def unwrapping(
+    func: typing.Callable[ParamSpec, Result[T, Err]],
+) -> typing.Callable[ParamSpec, Result[T, Err]]: ... 
+
+
+def unwrapping(
+    func: typing.Callable[ParamSpec, T],
+) -> typing.Callable[ParamSpec, T]:
+    @wraps(func)
+    def __call__(*args: typing.Any, **kwargs: typing.Any) -> T:
+        try: 
+            return func(*args, **kwargs)
+        except UnwrapError as e:
+            return Error(e.err)  # type: ignore
+
+    return __call__  # type: ignore
+
+
+__all__ = ("unwrapping",)
