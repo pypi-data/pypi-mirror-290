82f4287a2c303cfbb8b0306a6512753d77a175cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:44 +0000

    Add package pro-website-maker/0.5.2/pro_website_maker-0.5.2-py3-none-any.whl

---
+++ packages/pro-website-maker/pro_website_maker-0.5.2-py3-none-any.whl/pro_website_maker/modules/resume/__init__.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from jinja2 import Template
+from os import system
+from pro_website_maker import Module
+from pro_website_maker.output_file import OutputFile
+
+class Resume(Module):
+    def get_output_files(self, globals, content):
+        template = self.load_template("resume.html")
+
+        # Render the content
+        rendered_website_content = template.render({
+            "content": content,
+            "globals": globals,
+            "pdf": False,
+        })
+
+        # Render the content intended for PDF
+        rendered_pdf_content = template.render({
+            "content": content,
+            "globals": globals,
+            "pdf": True,
+        })
+
+        # Render the PDF
+        with open("/tmp/Resume_PDF.html", "w") as f:
+            f.write(rendered_pdf_content)
+
+        print(f"        [+] Wrote /tmp/Resume_PDF.html")
+
+        resume_pdf_path = Path("/tmp/Resume.pdf")
+
+        if resume_pdf_path.exists():
+            resume_pdf_path.unlink()
+
+        system("/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless /tmp/Resume_PDF.html --print-to-pdf-no-header --print-to-pdf=/tmp/Resume.pdf")
+
+        print(f"        [+] Generated /tmp/Resume.pdf")
+
+        with open("/tmp/Resume.pdf", "rb") as f:
+            resume_blob = f.read()
+
+        print(f"        [+] Read /tmp/Resume.pdf blob")
+
+        return [
+            OutputFile(Path("Resume.html"), rendered_website_content),
+            OutputFile(Path(content['Name'].replace(' ', '-') + "-Resume.pdf"), resume_blob, "wb"),
+        ]
