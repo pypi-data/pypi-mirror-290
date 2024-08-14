62f24bfda0ae7f2d059f1a9cf65d09facf672e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:23 +0000

    Add package veox/0.1.6/veox-0.1.6-py3-none-any.whl

---
+++ packages/veox/veox-0.1.6-py3-none-any.whl/veox/__init__.py
@@ -0,0 +1,5 @@
+from .client import init, RemoteModel, VeoxServiceUnavailable, VeoxError, VeoxAPIKeyLocked
+from .client import DataProtector
+
+__all__ = ['init', 'RemoteModel', 'VeoxServiceUnavailable', 'VeoxError', 'VeoxAPIKeyLocked', 'DataProtector']
+__version__ = "0.1.6"  # Increment the version number
