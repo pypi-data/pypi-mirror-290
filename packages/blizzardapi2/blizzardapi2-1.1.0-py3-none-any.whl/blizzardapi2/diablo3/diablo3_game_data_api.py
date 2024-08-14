dd94fe44edc6103fcc737fbe9c6b7da93fb2dd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 03:03:47 +0000

    Add package blizzardapi2/1.1.0/blizzardapi2-1.1.0-py3-none-any.whl

---
+++ packages/blizzardapi2/blizzardapi2-1.1.0-py3-none-any.whl/blizzardapi2/diablo3/diablo3_game_data_api.py
@@ -0,0 +1,53 @@
+from typing import List, Dict
+from ..api import Api
+
+"""diablo3_game_data_api.py file."""
+
+
+class Diablo3GameDataApi(Api):
+    """All Diablo 3 Game Data API methods.
+
+    Attributes:
+        client_id: A string client id supplied by Blizzard.
+        client_secret: A string client secret supplied by Blizzard.
+    """
+
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        """Init Diablo3GameDataApi."""
+        super().__init__(client_id, client_secret)
+
+    # D3
+
+    def get_season_index(self, region: str) -> Dict:
+        """Return an index of available seasons."""
+        resource = "/data/d3/season/"
+        return super().get_resource(resource, region)
+
+    def get_season(self, region: str, season_id: int) -> Dict:
+        """Return a leaderboard list for the specified season."""
+        resource = f"/data/d3/season/{season_id}"
+        return super().get_resource(resource, region)
+
+    def get_season_leaderboard(
+        self, region: str, season_id: int, leaderboard_id: int
+    ) -> Dict:
+        """Return the specified leaderboard for the specified season."""
+        resource = f"/data/d3/season/{season_id}/leaderboard/{leaderboard_id}"
+        return super().get_resource(resource, region)
+
+    def get_era_index(self, region: str) -> Dict:
+        """Return an index of available eras."""
+        resource = "/data/d3/era/"
+        return super().get_resource(resource, region)
+
+    def get_era(self, region: str, era_id: int) -> Dict:
+        """Return a leaderboard list for a particular era."""
+        resource = f"/data/d3/era/{era_id}"
+        return super().get_resource(resource, region)
+
+    def get_era_leaderboard(
+        self, region: str, era_id: int, leaderboard_id: int
+    ) -> Dict:
+        """Return the specified leaderboard for the specified era."""
+        resource = f"/data/d3/era/{era_id}/leaderboard/{leaderboard_id}"
+        return super().get_resource(resource, region)
