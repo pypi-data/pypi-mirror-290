6dd4bed279d59fdc625bc9a19f2025d9a25ff911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 06:03:41 +0000

    Add package azure-graph-toolkit/1.0.7/azure_graph_toolkit-1.0.7.tar.gz

---
+++ packages/azure-graph-toolkit/azure_graph_toolkit-1.0.7.tar.gz/azure_graph_toolkit-1.0.7/azure_graph_toolkit/utils/decorators.py
@@ -0,0 +1,34 @@
+import requests
+from functools import wraps
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+
+class GraphHTTPError(Exception):
+    pass
+
+
+def handle_http_exceptions(func):
+    """
+    Decorator to handle HTTP exceptions in all functions.
+
+    Args:
+        function (func): The function to handle HTTP exceptions. 
+
+    Returns:
+        dict: A dictionary containings status code and error raised."""
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        
+        except requests.exceptions.HTTPError as http_err:
+
+            error_response = {
+            "status_code": http_err.response.status_code,
+            "error": http_err.response.json().get('error'),
+            "error_description": http_err.response.json().get('error_description')
+            }
+            raise GraphHTTPError(error_response) from None
+    return wrapper
