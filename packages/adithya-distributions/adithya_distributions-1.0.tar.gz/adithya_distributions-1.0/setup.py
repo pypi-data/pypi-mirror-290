7ff7fa973f12a9e2fb3cb018435bad8242d1fd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:23 +0000

    Add package adithya-distributions/1.0/adithya_distributions-1.0.tar.gz

---
+++ packages/adithya-distributions/adithya_distributions-1.0.tar.gz/adithya_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='adithya_distributions',
+      version = '1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['adithya_distributions'],
+      author = 'Adithya Chintala',
+      zip_safe=False)
