0707d6a774ce57977c96346f86301d166ea421a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 08:05:16 +0000

    Add package core-cortex/0.5.1/core_cortex-0.5.1-py3-none-any.whl

---
+++ packages/core-cortex/core_cortex-0.5.1-py3-none-any.whl/cortex/__init__.py
@@ -0,0 +1,17 @@
+"""
+Cortex Python library lets you interact with the Cortex platform -
+a Machine Learning hub developed by CORE studio at Thornton Tomasetti
+
+.. include:: ../../../introduction.md
+"""
+
+from .cortex import *
+from .cortex_cursor import *
+from .routes_controller import *
+from .schemas import *
+from .wrappers import *
+from .inference import *
+from .metadata import *
+
+__version__ = '0.5.1'
+__author__ = 'CORE studio | Thornton Tomasetti'
