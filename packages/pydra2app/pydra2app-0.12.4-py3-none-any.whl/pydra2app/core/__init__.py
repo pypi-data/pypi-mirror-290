9fec469ee03030432799296c7f8a2422c4d4c2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-14 14:08:37 +0000

    Add package pydra2app/0.12.4/pydra2app-0.12.4-py3-none-any.whl

---
+++ packages/pydra2app/pydra2app-0.12.4-py3-none-any.whl/pydra2app/core/__init__.py
@@ -0,0 +1,7 @@
+from ._version import __version__
+
+PACKAGE_NAME = "pydra2app"
+CODE_URL = f"https://github.com/ArcanaFramework/{PACKAGE_NAME}"
+
+
+__authors__ = [("Thomas G. Close", "tom.g.close@gmail.com")]
