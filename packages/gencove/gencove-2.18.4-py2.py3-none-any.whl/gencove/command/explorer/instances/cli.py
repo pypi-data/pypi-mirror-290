2b4f7a4c8c39ccf1e3786c92976f4fd68af21a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:05:33 +0000

    Add package gencove/2.18.4/gencove-2.18.4-py2.py3-none-any.whl

---
+++ packages/gencove/gencove-2.18.4-py2.py3-none-any.whl/gencove/command/explorer/instances/cli.py
@@ -0,0 +1,20 @@
+"""Commands to be executed from command line."""
+import click
+
+from .inactivity_stop.cli import inactivity_stop
+from .list.cli import list_instances
+from .shell.cli import shell
+from .start.cli import start
+from .stop.cli import stop
+
+
+@click.group()
+def instances():
+    """Explorer instances management commands."""
+
+
+instances.add_command(start)
+instances.add_command(stop)
+instances.add_command(list_instances)
+instances.add_command(inactivity_stop)
+instances.add_command(shell)
