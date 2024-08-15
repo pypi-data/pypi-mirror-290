3dc93b63ee094fa4ae304d1531c96d001f77d026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 15:06:29 +0000

    Add package quantile-forest/1.3.8/quantile_forest-1.3.8-cp311-cp311-win_amd64.whl

---
+++ packages/quantile-forest/quantile_forest-1.3.8-cp311-cp311-win_amd64.whl/examples/plot_predict_custom.py
@@ -0,0 +1,150 @@
+"""
+Computing User-Specified Functions with QRFs
+============================================
+
+This example demonstrates how to extract the empirical distribution from a
+quantile regression forest (QRF) to calculate user-specified functions of
+interest. While a QRF is designed to estimate quantiles, their empirical
+distributions can also be used to calculate other statistical quantities. In
+this scenario, we compute the empirical cumulative distribution function
+(ECDF) for test samples.
+"""
+
+from itertools import chain
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import scipy as sp
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.utils.validation import check_random_state
+
+from quantile_forest import RandomForestQuantileRegressor
+
+random_seed = 0
+rng = check_random_state(random_seed)
+
+n_test_samples = 100
+
+
+def predict(reg, X, quantiles=0.5, what=None):
+    """Custom prediction method that allows user-specified function.
+
+    Parameters
+    ----------
+    reg : BaseForestQuantileRegressor
+        Quantile forest model object.
+    X : array-like, of shape (n_samples, n_features)
+        New test data.
+    quantiles : float or list of floats, default=0.5
+        Scalar or vector of quantiles for quantile prediction.
+    what : Any, optional
+        User-specified function for prediction used instead of quantiles.
+        Must return a numeric vector.
+
+    Returns
+    -------
+    Output array with the user-specified function applied (if any).
+    """
+    if what is None:
+        return reg.predict(X, quantiles=quantiles)
+
+    # Get the complete set of proximities (training indices) for each sample.
+    proximities = reg.proximity_counts(X)
+
+    # Retrieve the unsorted training responses from the model (stored in sorted order).
+    reverse_sorter = np.argsort(reg.sorter_, axis=0)
+    y_train = np.empty_like(reg.forest_.y_train).T
+    for i in range(y_train.shape[1]):
+        y_train[:, i] = np.asarray(reg.forest_.y_train)[i, reverse_sorter[:, i]]
+
+    # For each sample, construct an array of the training responses used for prediction.
+    s = [np.concatenate([np.repeat(y_train[i[0]], i[1]) for i in prox]) for prox in proximities]
+
+    # Apply the user-specified function for each sample. Must return a numeric vector.
+    y_out = np.array([what(s_i) for s_i in s])
+
+    return y_out
+
+
+X, y = datasets.load_diabetes(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=n_test_samples, random_state=random_seed
+)
+
+reg = RandomForestQuantileRegressor(random_state=random_seed).fit(X_train, y_train)
+
+# Define a user-specified function; here we randomly sample 1000 values with replacement.
+func = lambda x: rng.choice(x, size=1000)
+
+# Output array with the user-specified function applied to each sample's empirical distribution.
+y_out = predict(reg, X_test, what=func)
+
+dfs = []
+for idx in range(n_test_samples):
+    # Calculate the ECDF from output array.
+    y_ecdf = [sp.stats.ecdf(y_i).cdf for y_i in y_out[idx].reshape(1, -1)]
+    n_quantiles = len(list(chain.from_iterable([y_i.quantiles for y_i in y_ecdf])))
+
+    df_i = pd.DataFrame(
+        {
+            "y_val": list(chain.from_iterable([y_i.quantiles for y_i in y_ecdf])),
+            "y_val2": list(chain.from_iterable([y_i.quantiles for y_i in y_ecdf]))[1:] + [np.nan],
+            "proba": list(chain.from_iterable([y_i.probabilities for y_i in y_ecdf])),
+            "index": [idx] * n_quantiles,
+        }
+    )
+    dfs.append(df_i)
+df = pd.concat(dfs, ignore_index=True)
+
+
+def plot_ecdf(df):
+    min_idx = df["index"].min()
+    max_idx = df["index"].max()
+
+    # Slider for determining the sample index for which the custom function is being visualized.
+    slider = alt.binding_range(min=min_idx, max=max_idx, step=1, name="Test Sample Index: ")
+    index_selection = alt.selection_point(value=0, bind=slider, fields=["index"])
+
+    tooltip = [
+        alt.Tooltip("y_val:Q", title="Response Value"),
+        alt.Tooltip("proba:Q", title="Probability"),
+    ]
+
+    circles = (
+        alt.Chart(df)
+        .mark_circle(color="#006aff", opacity=1, size=50)
+        .encode(
+            x=alt.X("y_val:Q", title="Response Value"),
+            y=alt.Y("proba:Q", title="Probability"),
+            tooltip=tooltip,
+        )
+    )
+
+    lines = (
+        alt.Chart(df)
+        .mark_line(color="#006aff", size=2)
+        .encode(
+            x=alt.X("y_val:Q", title="Response Value"),
+            x2=alt.X2("y_val2:Q"),
+            y=alt.Y("proba:Q", title="Probability"),
+            tooltip=tooltip,
+        )
+    )
+
+    chart = (
+        (circles + lines)
+        .add_params(index_selection)
+        .transform_filter(index_selection)
+        .properties(
+            height=400,
+            width=650,
+            title="Empirical Cumulative Distribution Function (ECDF) Plot",
+        )
+    )
+    return chart
+
+
+chart = plot_ecdf(df)
+chart
---
+++ packages/quantile-forest/quantile_forest-1.3.8-cp311-cp311-win_amd64.whl/examples/plot_quantile_multioutput.py
@@ -0,0 +1,167 @@
+"""
+Multi-target Quantile Regression with QRFs
+==========================================
+
+This example demonstrates how to fit a single quantile regressor for multiple
+target variables on a toy dataset. For each target, multiple quantiles can be
+estimated simultaneously. In this example, the target variable has two output
+values for each sample, with a single regressor used to estimate multiple
+quantiles simultaneously. Three of these quantiles are visualized concurrently
+for each target: the median line and the area defined by the interval points.
+"""
+
+import altair as alt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.utils.validation import check_random_state
+
+from quantile_forest import RandomForestQuantileRegressor
+
+random_seed = 0
+rng = check_random_state(random_seed)
+
+n_samples = 2500
+bounds = [0, 100]
+quantiles = np.linspace(0, 1, num=41, endpoint=True).round(3).tolist()
+
+# Define functions that generate targets; each function maps to one target.
+funcs = [
+    {
+        "signal": lambda x: np.log1p(x + 1),
+        "noise": lambda x: np.log1p(x) * rng.uniform(size=len(x)),
+    },
+    {
+        "signal": lambda x: np.log1p(np.sqrt(x)),
+        "noise": lambda x: np.log1p(x / 2) * rng.uniform(size=len(x)),
+    },
+]
+
+legend = {
+    "0": "#f2a619",
+    "1": "#006aff",
+}
+
+
+def make_func_Xy(funcs, bounds, n_samples):
+    x = np.linspace(*bounds, n_samples)
+    y = np.empty((len(x), len(funcs)))
+    for i, func in enumerate(funcs):
+        y[:, i] = func["signal"](x) + func["noise"](x)
+    return np.atleast_2d(x).T, y
+
+
+def format_frac(fraction):
+    return f"{fraction:.3g}".rstrip("0").rstrip(".") or "0"
+
+
+# Create the dataset with multiple target variables.
+X, y = make_func_Xy(funcs, bounds, n_samples)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_seed)
+
+qrf = RandomForestQuantileRegressor(max_samples_leaf=None, max_depth=4, random_state=random_seed)
+qrf.fit(X_train, y_train)  # fit on all of the targets simultaneously
+
+# Get multi-target predictions at specified quantiles.
+y_pred = qrf.predict(X, quantiles=quantiles)
+
+# Prepare the data frame.
+df = pd.DataFrame(
+    {
+        "x": np.tile(X.squeeze(), len(funcs)),
+        "y": y.reshape(-1, order="F"),
+        "y_true": np.concatenate([f["signal"](X.squeeze()) for f in funcs]),
+        "y_pred": np.concatenate([y_pred[:, i, len(quantiles) // 2] for i in range(len(funcs))]),
+        "target": np.concatenate([[str(i)] * len(X) for i in range(len(funcs))]),
+    }
+)
+
+# Add quantile predictions to the data frame.
+for idx, q in enumerate(quantiles):
+    df[f"q_{format_frac(q)}"] = y_pred[:, :, idx].flatten(order="F")
+
+
+def plot_multitargets(df, legend):
+    # Slider for varying the displayed prediction intervals.
+    slider = alt.binding_range(min=0, max=1, step=0.05, name="Prediction Interval: ")
+    interval_val = alt.param(value=0.95, bind=slider, name="interval")
+
+    click = alt.selection_point(fields=["target"], bind="legend")
+
+    color = alt.condition(
+        click,
+        alt.Color(
+            "target:N",
+            legend=alt.Legend(symbolOpacity=1),
+            scale=alt.Scale(range=list(legend.values())),
+            sort=list(legend.keys()),
+            title="Target",
+        ),
+        alt.value("lightgray"),
+    )
+
+    tooltip = [
+        alt.Tooltip("target:N", title="Target"),
+        alt.Tooltip("x:Q", format=",.3f", title="X"),
+        alt.Tooltip("y:Q", format=",.3f", title="Y"),
+        alt.Tooltip("y_true:Q", format=",.3f", title="Y"),
+        alt.Tooltip("y_pred:Q", format=",.3f", title="Predicted Y"),
+        alt.Tooltip("y_pred_low:Q", format=",.3f", title="Predicted Lower Y"),
+        alt.Tooltip("y_pred_upp:Q", format=",.3f", title="Predicted Upper Y"),
+        alt.Tooltip("quantile_low:Q", format=".3f", title="Lower Quantile"),
+        alt.Tooltip("quantile_upp:Q", format=".3f", title="Upper Quantile"),
+    ]
+
+    base = (
+        alt.Chart(df)
+        .transform_calculate(
+            quantile_low="round((0.5 - interval / 2) * 1000) / 1000",
+            quantile_upp="round((0.5 + interval / 2) * 1000) / 1000",
+            quantile_low_col="'q_' + datum.quantile_low",
+            quantile_upp_col="'q_' + datum.quantile_upp",
+        )
+        .transform_calculate(
+            y_pred_low="datum[datum.quantile_low_col]",
+            y_pred_upp="datum[datum.quantile_upp_col]",
+        )
+    )
+
+    points = base.mark_circle(color="black", opacity=0.25, size=25).encode(
+        x=alt.X("x:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("y:Q"),
+        color=alt.condition(click, alt.Color("target:N"), alt.value("lightgray")),
+        tooltip=tooltip,
+    )
+
+    line = base.mark_line(color="black", size=3).encode(
+        x=alt.X("x:Q", scale=alt.Scale(nice=False), title="X"),
+        y=alt.Y("y_pred:Q", title="Y"),
+        color=color,
+        tooltip=tooltip,
+    )
+
+    area = base.mark_area(opacity=0.25).encode(
+        x=alt.X("x:Q", scale=alt.Scale(nice=False), title="X"),
+        y=alt.Y("y_pred_low:Q", title="Y"),
+        y2=alt.Y2("y_pred_upp:Q", title=None),
+        color=color,
+        tooltip=tooltip,
+    )
+
+    chart = (
+        (points + area + line)
+        .add_params(interval_val, click)
+        .configure_range(category=alt.RangeScheme(list(legend.values())))
+        .properties(
+            height=400,
+            width=650,
+            title="Multi-target Predictions and Prediction Intervals on Toy Dataset",
+        )
+    )
+
+    return chart
+
+
+chart = plot_multitargets(df, legend)
+chart
