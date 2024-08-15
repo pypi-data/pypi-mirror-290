7185698e794629547ce7723664cd33f5cf41c435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 15:05:28 +0000

    Add package ytmdl/2024.8.15.1/ytmdl-2024.8.15.1-py3-none-any.whl

---
+++ packages/ytmdl/ytmdl-2024.8.15.1-py3-none-any.whl/ytmdl/__init__.py
@@ -0,0 +1,25 @@
+# Import entry to make it accessible
+from ytmdl.main import entry
+
+name = "ytmdl"
+
+
+__all__ = ['ytmdl',
+           'defaults',
+           'dir',
+           'prepend',
+           'setupConfig',
+           'song',
+           'yt',
+           'cache',
+           'stringutils',
+           'utility',
+           'gaana',
+           'metadata',
+           'manual',
+           'logger',
+           'trim',
+           'saavn',
+           'exceptions',
+           'core',
+           ]
