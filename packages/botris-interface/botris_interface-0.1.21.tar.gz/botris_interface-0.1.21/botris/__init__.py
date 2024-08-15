b16279286661e18ba0fb5fc647bd77fea928d533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:42 +0000

    Add package botris-interface/0.1.21/botris_interface-0.1.21.tar.gz

---
+++ packages/botris-interface/botris_interface-0.1.21.tar.gz/botris_interface-0.1.21/botris/__init__.py
@@ -0,0 +1,12 @@
+from . import bots, engine, interface
+from .engine import TetrisGame
+from .interface import Interface, connect
+
+__all__ = [
+    "bots",
+    "interface",
+    "engine",
+    "Interface",
+    "connect",
+    "TetrisGame",
+]
