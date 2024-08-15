5e09284666475526626a0d97a2e1749f6ee6f078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:58 +0000

    Add package docr/0.2.1/docr-0.2.1-py3-none-any.whl

---
+++ packages/docr/docr-0.2.1-py3-none-any.whl/docr/core/detection/__init__.py
@@ -0,0 +1,6 @@
+# from docrdata import IMG_FORMATS,MetaFile
+from .dbnet import DBNet
+from .lore import Lore
+from .yolov8 import YOLOv8
+
+__all__ = ("YOLOv8", "DBNet", "Lore")
---
+++ packages/docr/docr-0.2.1-py3-none-any.whl/docr/core/recognition/__init__.py
@@ -0,0 +1,12 @@
+# from .base import CVModel
+# from .detection import *
+# from .recognition import *
+# from .imageprocess import *
+# from .reading_order import *
+# from .classification import *
+from .crnn import CRNN
+from .latexocr import LatexOCR
+
+# __all__ = (
+#     "CVModel",
+# )
