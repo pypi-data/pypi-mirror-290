8c5374ec110fae91205b684e0ddd06fc9e05dcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:47 +0000

    Add package qualysdk/0.1.6/qualysdk-0.1.6-py3-none-any.whl

---
+++ packages/qualysdk/qualysdk-0.1.6-py3-none-any.whl/qualysdk/sql/__init__.py
@@ -0,0 +1,15 @@
+"""
+SQL server integration for qualysdk
+"""
+
+from .base import db_connect
+from .vmdr import (
+    upload_vmdr_ags,
+    upload_vmdr_kb,
+    upload_vmdr_hosts,
+    upload_vmdr_ips,
+    upload_vmdr_hld,
+    upload_vmdr_scanners,
+    upload_vmdr_static_searchlists,
+    upload_vmdr_users,
+)
