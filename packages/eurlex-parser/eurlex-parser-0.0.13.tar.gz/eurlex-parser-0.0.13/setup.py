b4d4d4942d49c750780f875e718d802763f37ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:51 +0000

    Add package eurlex-parser/0.0.13/eurlex-parser-0.0.13.tar.gz

---
+++ packages/eurlex-parser/eurlex-parser-0.0.13.tar.gz/eurlex-parser-0.0.13/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.13'  
+DESCRIPTION  = 'Eurlex parser for fetching and parsing Eurlex data.'
+
+def read_requirements():
+    with open('requirements.txt') as req_file:
+        return req_file.read().splitlines()
+
+def read_long_description():
+    with open('README.md', encoding='utf-8') as readme_file:
+        return readme_file.read()
+
+setup(
+    name='eurlex-parser',
+    version=VERSION,
+    packages=[''],    
+    package_dir={'': 'src'},    
+    include_package_data=True,
+    install_requires=read_requirements(),
+    description=DESCRIPTION,
+    long_description=read_long_description(),
+    long_description_content_type='text/markdown',
+    author='Yan-Ying Liao',
+    author_email='n9102125@gmail.com',
+    url='https://github.com/noworneverev/eurlex-parser',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    
+    python_requires='>=3.6',
+)
