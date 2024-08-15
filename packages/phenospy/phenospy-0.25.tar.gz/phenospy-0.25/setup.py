f4f0e8e906aa63309385d5a16d0383fc9bef963e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:04:37 +0000

    Add package phenospy/0.25/phenospy-0.25.tar.gz

---
+++ packages/phenospy/phenospy-0.25.tar.gz/phenospy-0.25/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='phenospy',
+    version='0.25',
+    packages=find_packages(exclude=['tests*', 'devel*', 'build*']),
+    license='MIT',
+    description='Tools for making and processing computable phenotypes',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    install_requires=['Owlready2',
+                      'PyYAML',
+                      'colorama',
+                      'markdown',
+                      'requests',
+                      'pyparsing==2.4.2'],
+    include_package_data=True,
+    url='https://github.com/sergeitarasov/PhenoScript',
+    author='Sergei Tarasov',
+    author_email='sergei.tarasov@helsinki.fi',
+    project_urls={
+        "Project Logo": "https://raw.githubusercontent.com/sergeitarasov/PhenoScript/main/Phenoscript_logo.png",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    
+    # Define entry points for command-line tools
+    entry_points={
+        "console_scripts": [
+            "phenospy = phenospy.cli:main",  # Replace with your CLI entry point
+        ],
+    },
+)
