2f3f3304f5f39d77ee5d613ea05d345029a19581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:39 +0000

    Add package tracer-extension/1.0.31/tracer-extension-1.0.31.tar.gz

---
+++ packages/tracer-extension/tracer-extension-1.0.31.tar.gz/tracer-extension-1.0.31/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+setup(
+    name='tracer-extension',
+    version='1.0.31',
+    author='Gordon',
+    author_email='gordon.hamilton@datadoghq.com',
+    description='Python Worker Extension for starting Datadog Tracer and a top level span to enable auto-instrumenting of Azure Function Apps',
+    include_package_data=True,
+    long_description=open('readme.md').read(),
+    install_requires=[
+        'azure-functions >= 1.7.0, < 2.0.0',
+        'ddtrace',
+    ],
+    license='',
+    packages=find_packages(where='.'),
+    zip_safe=False,
+)
