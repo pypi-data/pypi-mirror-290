9759816192dcc4b15427913fce2ebe97c3d3ad35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:32 +0000

    Add package horiba-sdk/0.4.0/horiba_sdk-0.4.0-py3-none-any.whl

---
+++ packages/horiba-sdk/horiba_sdk-0.4.0-py3-none-any.whl/horiba_sdk/__init__.py
@@ -0,0 +1,15 @@
+# mypy: disable-error-code="attr-defined"
+"""'horiba-sdk' is a package that provides source code for the development with Horiba devices"""
+
+__version__ = '0.2.0'  # It MUST match the version in pyproject.toml file
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return 'unknown'
+
+
+version: str = get_version()
