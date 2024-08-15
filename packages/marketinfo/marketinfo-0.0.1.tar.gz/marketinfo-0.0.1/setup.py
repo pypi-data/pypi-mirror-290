3ad5a93fc853a32260e2637a88eb78188ac95f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 16:04:29 +0000

    Add package marketinfo/0.0.1/marketinfo-0.0.1.tar.gz

---
+++ packages/marketinfo/marketinfo-0.0.1.tar.gz/marketinfo-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='marketinfo',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A package to fetch market data',
+)
