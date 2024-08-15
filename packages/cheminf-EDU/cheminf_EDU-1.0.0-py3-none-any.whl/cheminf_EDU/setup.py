8c7a8c2b5c72bfaff14536f45986eec5593ac177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:04:57 +0000

    Add package cheminf-EDU/1.0.0/cheminf_EDU-1.0.0-py3-none-any.whl

---
+++ packages/cheminf-EDU/cheminf_EDU-1.0.0-py3-none-any.whl/cheminf_EDU/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cheminf-EDU',
+    version='0.1.0',
+    description='A simple chemie educational Python package',
+    author='Ziheng Zhao',
+    author_email='zhacisbw4801@gmail.com',
+    url='https://github.com/Yakimochinai/che-edu',
+    packages=find_packages(),
+    install_requires=[],  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
