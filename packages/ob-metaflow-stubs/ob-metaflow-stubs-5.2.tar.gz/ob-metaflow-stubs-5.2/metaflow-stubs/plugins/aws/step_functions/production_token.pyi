fad3b8310471e8829aeb0b9907a7468d2ecf9263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:36 +0000

    Add package ob-metaflow-stubs/5.2/ob-metaflow-stubs-5.2.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-5.2.tar.gz/ob-metaflow-stubs-5.2/metaflow-stubs/plugins/aws/step_functions/production_token.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.12.0+ob(v1)                                                   #
+# Generated on 2024-08-14T18:23:51.934123                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def new_token(token_prefix, prev_token = None):
+    ...
+
+def load_token(token_prefix):
+    ...
+
+def store_token(token_prefix, token):
+    ...
+
