bf0fc267388104bc2ce53e87da2c6db102bd42b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 16:04:22 +0000

    Add package python-injection/0.10.2/python_injection-0.10.2.tar.gz

---
+++ packages/python-injection/python_injection-0.10.2.tar.gz/python_injection-0.10.2/injection/__init__.py
@@ -0,0 +1,38 @@
+from ._core.module import Injectable, Module
+from ._core.module import Mode as InjectableMode
+from ._core.module import Priority as ModulePriority
+
+__all__ = (
+    "Injectable",
+    "InjectableMode",
+    "Module",
+    "ModulePriority",
+    "constant",
+    "find_instance",
+    "get_instance",
+    "get_lazy_instance",
+    "inject",
+    "injectable",
+    "mod",
+    "set_constant",
+    "should_be_injectable",
+    "singleton",
+)
+
+
+def mod(name: str | None = None, /) -> Module:
+    if name is None:
+        return Module.default()
+
+    return Module.from_name(name)
+
+
+constant = mod().constant
+find_instance = mod().find_instance
+get_instance = mod().get_instance
+get_lazy_instance = mod().get_lazy_instance
+inject = mod().inject
+injectable = mod().injectable
+set_constant = mod().set_constant
+should_be_injectable = mod().should_be_injectable
+singleton = mod().singleton
