d17c094b6389c31ecdcdf6540c6168285df8a6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:04:48 +0000

    Add package funcnodes-span/0.1.3/funcnodes_span-0.1.3.tar.gz

---
+++ packages/funcnodes-span/funcnodes_span-0.1.3.tar.gz/funcnodes_span-0.1.3/funcnodes_span/__init__.py
@@ -0,0 +1,17 @@
+import funcnodes as fn
+
+from .normalization import NORM_NODE_SHELF as NORM
+from .smoothing import SMOOTH_NODE_SHELF as SMOOTH
+
+
+__version__ = "0.1.3"
+
+NODE_SHELF = fn.Shelf(
+    name="Spectral Analysis",
+    description="Spectral analysis for funcnodes",
+    nodes=[],
+    subshelves=[
+        NORM,
+        SMOOTH,
+    ],
+)
