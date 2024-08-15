b2da560d921ddba950ca86f10f3e16246ea9d80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:41 +0000

    Add package tradestream/0.4.2/tradestream-0.4.2.tar.gz

---
+++ packages/tradestream/tradestream-0.4.2.tar.gz/tradestream-0.4.2/tradestream/dashboard/__ini__.py
@@ -0,0 +1,3 @@
+from tradestream.dashboard.app import start_app_server, start_app_server_debug
+
+__all__ = ["start_app_server","start_app_server_debug"]
---
+++ packages/tradestream/tradestream-0.4.2.tar.gz/tradestream-0.4.2/tradestream/dashboard/app.py
@@ -0,0 +1,36 @@
+
+
+from dash import Dash, dcc, html, Input, Output, callback
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__,external_stylesheets=external_stylesheets,pages_folder='pages',)
+
+app.enable_pages = True
+app.enable_dev_tools = True
+
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Welcome to TradeStream'),
+    html.h3('Select a task to run:'),
+    dcc.Dropdown(['RUN_ALL', 'START_TRADING', 'START_STREAMING', 'NOTIFY_ONLY'],
+        'RUN_ALL',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@callback(Output('display-value', 'children'), Input('dropdown', 'value'))
+def display_value(value):
+    return f'Task {value} started successfully!'
+
+def start_app_server(debug:bool = False, run_local:bool = False):
+    print("Starting dashboard app server")
+    app.run(debug=debug,run_local=run_local)
+
+def start_app_server_debug():
+    start_app_server(True,False)
+
+if __name__ == '__main__':
+    start_app_server(False,False)
