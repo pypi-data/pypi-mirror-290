ae84b80b0d3c68cfb1819c389b6e1e8a7cd60bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:04:37 +0000

    Add package qontract-reconcile/0.10.1rc1051/qontract_reconcile-0.10.1rc1051-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc1051-py3-none-any.whl/reconcile/utils/elasticsearch_exceptions.py
@@ -0,0 +1,13 @@
+class ElasticSearchResourceNameInvalidError(Exception):
+    def __init__(self, msg):
+        super().__init__(str(msg))
+
+
+class ElasticSearchResourceMissingSubnetIdError(Exception):
+    def __init__(self, msg):
+        super().__init__(str(msg))
+
+
+class ElasticSearchResourceZoneAwareSubnetInvalidError(Exception):
+    def __init__(self, msg):
+        super().__init__(str(msg))
