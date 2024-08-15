ac33ae0927f73aec8a1da817858d80bc0bfabe3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 07:04:41 +0000

    Add package murnitur/0.1.4/murnitur-0.1.4.tar.gz

---
+++ packages/murnitur/murnitur-0.1.4.tar.gz/murnitur-0.1.4/murnitur/__init__.py
@@ -0,0 +1,20 @@
+import os
+from .main import (
+    load_preset,
+    format_prompt,
+    init,
+    set_api_key,
+    get_api_key,
+    tracer,
+    trace,
+    tool,
+    trace_agent,
+    task,
+    log,
+    Environment,
+    GuardConfig
+)
+from .guard.main import Guard
+
+
+os.environ["DEEPEVAL_TELEMETRY_OPT_OUT"] = "YES"
