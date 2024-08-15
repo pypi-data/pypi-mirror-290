6d12387c9f2200cc91f0a394b51e46f7254ccf1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:20 +0000

    Add package ftdcherub/0.1/ftdcherub-0.1.tar.gz

---
+++ packages/ftdcherub/ftdcherub-0.1.tar.gz/ftdcherub-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ftdcherub",
+    version="0.1",
+    author="Cherub",
+    author_email="zhaungxinwei@gmail.com",
+    description="Just use for myself",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.6',
+)
