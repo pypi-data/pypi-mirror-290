246cca9b9683c21bee4f48cb1198b220abefee5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:04:27 +0000

    Add package nguyenpanda/0.1.8.3/nguyenpanda-0.1.8.3-py3-none-any.whl

---
+++ packages/nguyenpanda/nguyenpanda-0.1.8.3-py3-none-any.whl/nguyenpanda/__init__.py
@@ -0,0 +1,42 @@
+"""
+Welcome to the world of nguyenpanda,
+    a Python library that brings you the essence of utility packages,
+    each named after animals, perfectly representing its unique domain.
+
+🦋 butterfly
+    Embark on a journey through randomness and chaos with the enigmatic butterfly package.
+    Delve into the realms of uncertainty, probability, and statistics.
+    Embrace the unpredictable nature of nguyenpanda's butterfly.
+    Remember, even the flutter of a butterfly's wings can cause significant changes in a complex system, so seize the opportunity to explore its mysteries.
+
+🦉 owl
+    Illuminate the path to mathematical enlightenment with the wisdom of the owl.
+    Ascend to mastery in calculus, conquer the realms of linear algebra,
+    and traverse the diverse landscapes of mathematics.
+    Allow nguyenpanda's owl to be your steadfast companion on your intellectual odyssey.
+
+🦢 swan
+    Indulge in the beauty of aesthetics and user interfaces with the elegant swan package.
+    Dive into the world of colors, front-end development, GUI, and more.
+    Transform your applications into visual masterpieces with swan.
+
+🐦‍⬛ raven
+    Enter the realm of the raven, where intelligence and innovation
+    converge in a comprehensive library of data structures
+    Inspired by the ingenuity of the raven, this package offers elegant data structures classes.
+"""
+
+__version__ = '0.1.8.1'
+
+from .ant import *
+from .butterfly import *
+from .owl import *
+from .swan import *
+
+__all__ = (
+    'ant',
+    'butterfly',
+    'owl',
+    'raven',
+    'swan',
+)
