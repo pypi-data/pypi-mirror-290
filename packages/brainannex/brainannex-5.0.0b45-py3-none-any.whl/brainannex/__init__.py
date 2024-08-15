c983b5d36f48a58ad4a8163e0453b218050db803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 14:04:42 +0000

    Add package brainannex/5.0.0b45/brainannex-5.0.0b45-py3-none-any.whl

---
+++ packages/brainannex/brainannex-5.0.0b45-py3-none-any.whl/brainannex/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "5.0.0-beta.45"
+
+
+from brainannex.neo_schema.neo_schema import NeoSchema
+from brainannex.data_manager import DataManager
+from brainannex.user_manager import UserManager
+from brainannex.categories import Categories
+from brainannex.collections import Collections
+from brainannex.full_text_indexing import FullTextIndexing
+
+
+__all__ = [
+    'NeoSchema',
+    'DataManager',
+    'UserManager',
+    'Categories',
+    'Collections',
+    'FullTextIndexing',
+    'version'
+]
+
+
+def version():
+    return __version__
