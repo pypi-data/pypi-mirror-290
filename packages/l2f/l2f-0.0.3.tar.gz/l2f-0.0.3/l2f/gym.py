0fd619950f9c6f1e454849240053753951b9b1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:48 +0000

    Add package l2f/0.0.3/l2f-0.0.3.tar.gz

---
+++ packages/l2f/l2f-0.0.3.tar.gz/l2f-0.0.3/l2f/gym.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+
+class LearningToFlyEnv(gym.Env):
+    metadata = {"render_modes": [], "render_fps": 4}
+
+    def __init__(self, render_mode=None):
+        self.observation_space = spaces.spaces.Box(np.full(-np.inf, 13+), , shape=(2,), dtype=float)
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = spaces.Discrete(4)
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.
+        """
+        self.window = None
+        self.clock = None
