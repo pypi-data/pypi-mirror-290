f979965d49013c6786f48161f080148233e29538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:05:17 +0000

    Add package ask-question/1.2.6/ask_question-1.2.6.tar.gz

---
+++ packages/ask-question/ask_question-1.2.6.tar.gz/ask_question-1.2.6/ask_question/__init__.py
@@ -0,0 +1,28 @@
+"""
+File in charge of automating and simplify the process of asking a question and expecting a specific type of response
+"""
+
+from .ask_question import AskQuestion
+
+__all__ = ["AskQuestion"]
+__version__ = "1.2.0"
+
+
+class ask_question(AskQuestion):
+    """ Ask a question to the user while expecting a specific format """
+
+
+class askquestion(AskQuestion):
+    """ Ask a question to the user while expecting a specific format """
+
+
+class Ask_Question(AskQuestion):
+    """ Ask a question to the user while expecting a specific format """
+
+
+class ASK_QUESTION(AskQuestion):
+    """ Ask a question to the user while expecting a specific format """
+
+
+class ASKQUESTION(AskQuestion):
+    """ Ask a question to the user while expecting a specific format """
