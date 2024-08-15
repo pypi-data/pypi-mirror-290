c45c8521b31ca6e3cd524975069d84e11d532ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:25 +0000

    Add package agentbuddy/0.1.1.dev0/agentbuddy-0.1.1.dev0.tar.gz

---
+++ packages/agentbuddy/agentbuddy-0.1.1.dev0.tar.gz/agentbuddy-0.1.1.dev0/agentbuddy/app.py
@@ -0,0 +1,15 @@
+import uvicorn
+import os
+from agentbuddy.agent.api import get_agent_app
+
+app = get_agent_app(
+    name=os.getenv("AGENT_NAME", default="generic"),
+    persona=os.getenv("PERSONA_NAME", default="generic"),
+    purpose=os.getenv("AGENT_PURPOSE", default=None),
+    hostname=os.getenv("AGENT_HOST", default=None),
+    port=os.getenv("AGENT_PORT", default=None),
+    parent_hostname=os.getenv("AGENT_P_HOST", default=None),
+    parent_port=os.getenv("AGENT_P_PORT", default=None),
+    )
+
+uvicorn.run(app, host="0.0.0.0", port=os.getenv("AGENT_PORT", default=8080), workers=1)
