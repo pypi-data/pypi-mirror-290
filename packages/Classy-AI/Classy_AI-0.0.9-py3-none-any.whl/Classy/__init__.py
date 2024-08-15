b54fcecad9ae3d04f6b39e0859bc5d320c6dda51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:30 +0000

    Add package Classy-AI/0.0.9/Classy_AI-0.0.9-py3-none-any.whl

---
+++ packages/Classy-AI/Classy_AI-0.0.9-py3-none-any.whl/Classy/__init__.py
@@ -0,0 +1,18 @@
+from .New_ai import question
+from . import model
+from .basic import classify
+from . import New_ai
+from . import basic
+import time
+import os
+import nltk
+def init(location,key,user_name):
+    New_ai.init(location,key,user_name)
+    basic.init(location)
+def download():
+    print('Downloading the model from GitHub. Press Ctrl+c to quit.')
+    time.sleep(3)
+    os.system('wget https://github.com/Mrpi314tech/Classy/raw/main/train/data.pth')
+    print('Downloading nltk tokenizer. Press Ctrl+c to quit.')
+    time.sleep(3)
+    nltk.download('punkt')
