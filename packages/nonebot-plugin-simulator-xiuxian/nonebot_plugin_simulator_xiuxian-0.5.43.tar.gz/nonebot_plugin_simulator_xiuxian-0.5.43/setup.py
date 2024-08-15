c369d445828366f5591771fddecaa05df9ea230e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:53 +0000

    Add package nonebot-plugin-simulator-xiuxian/0.5.43/nonebot_plugin_simulator_xiuxian-0.5.43.tar.gz

---
+++ packages/nonebot-plugin-simulator-xiuxian/nonebot_plugin_simulator_xiuxian-0.5.43.tar.gz/nonebot_plugin_simulator_xiuxian-0.5.43/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup,find_namespace_packages,find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='nonebot_plugin_simulator_xiuxian',
+    version='0.5.43',
+    description='修仙',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='甘城菜月',
+    author_email='2859385794@qq.com',
+    license='MIT license',
+    include_package_data=True,
+    packages=find_namespace_packages(include=["nonebot_plugin_simulator_xiuxian"]),
+    platforms='all',
+    install_requires=["nonebot2",
+                      "nonebot-adapter-onebot",
+                      'wget',
+                      "nonebot-plugin-apscheduler",
+                      "Pillow==9.5.0",
+                      "pydantic",
+                      "wcwidth",
+                      "ujson",
+                      "xiuxian-base",
+                      "xiuxian-bank",
+                      "xiuxian-back",
+                      "xiuxian-boss",
+                      "xiuxian-buff",
+                      "xiuxian-info",
+                      "xiuxian-mixelixir",
+                      "xiuxian-rift",
+                      "xiuxian-sect",
+                      "xiuxian-work"],
+    url='https://github.com/luoyefufeng/nonebot_plugin_simulator_xiuxian',
+)
+
+
