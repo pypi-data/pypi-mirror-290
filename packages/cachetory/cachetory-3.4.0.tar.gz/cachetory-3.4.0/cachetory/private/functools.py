b8d9c35a20c0f2f1d84b50e42f91ff6c3f37fe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:37 +0000

    Add package cachetory/3.4.0/cachetory-3.4.0.tar.gz

---
+++ packages/cachetory/cachetory-3.4.0.tar.gz/cachetory-3.4.0/cachetory/private/functools.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from functools import wraps
+from inspect import iscoroutinefunction
+from typing import Any, Awaitable, Callable, TypeVar, cast
+
+T = TypeVar("T")
+
+
+def into_callable(value: Callable[..., T] | T) -> Callable[..., T]:
+    if not callable(value):
+
+        def callable_(*args: Any, **kwargs: Any) -> T:
+            return value
+
+        return callable_
+
+    else:
+        return value
+
+
+def into_async_callable(value: Callable[..., Awaitable[T]] | Callable[..., T] | T) -> Callable[..., Awaitable[T]]:
+    if not callable(value):
+
+        async def callable_(*args: Any, **kwargs: Any) -> T:
+            return value
+
+        return callable_
+
+    if not iscoroutinefunction(value):
+
+        @wraps(value)
+        async def callable_(*args: Any, **kwargs: Any) -> T:
+            return cast(T, value(*args, **kwargs))
+
+        return callable_
+
+    else:
+        return value
