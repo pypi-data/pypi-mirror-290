0ed6c225d7fb1d24589b770485ef53fcfd86c4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 06:05:12 +0000

    Add package multiples-library/0.1.0/multiples_library-0.1.0.tar.gz

---
+++ packages/multiples-library/multiples_library-0.1.0.tar.gz/multiples_library-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multiples_library',  # Replace with your package’s name
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Your name',  
+    author_email='your_email@example.com',
+    description='A library for checking multiples of 2 and 5.',
+    classifiers=[
+        
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
+
+
+
