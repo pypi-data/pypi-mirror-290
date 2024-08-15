82214f74b8e2bb457d473f5787c5f63aa4b71ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:30 +0000

    Add package tradestream/0.4.3/tradestream-0.4.3-py3-none-any.whl

---
+++ packages/tradestream/tradestream-0.4.3-py3-none-any.whl/tradestream/tests/main.py
@@ -0,0 +1,12 @@
+from dotenv_vault import load_dotenv
+load_dotenv()
+
+def run_tests():
+    print("Running tests...")
+    # TODO: run tests
+    print("Tests complete.")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    run_tests()
