5eed7a75c21fce393a8946a481c3dc8c7e31ec97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:04:43 +0000

    Add package maidr/0.5.1/maidr-0.5.1-py3-none-any.whl

---
+++ packages/maidr/maidr-0.5.1-py3-none-any.whl/maidr/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.5.1"
+
+from .core import Maidr
+from .core.enum import PlotType
+from .patch import (
+    barplot,
+    boxplot,
+    clear,
+    heatmap,
+    highlight,
+    histogram,
+    lineplot,
+    scatterplot,
+)
+from .maidr import close, save_html, show, stacked
+
+__all__ = [
+    "close",
+    "save_html",
+    "show",
+    "stacked",
+]
