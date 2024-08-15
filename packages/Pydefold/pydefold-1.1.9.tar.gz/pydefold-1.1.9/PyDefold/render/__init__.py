3b1210d14d64c544f043cf5252ea7452961c37b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 14:04:46 +0000

    Add package Pydefold/1.1.9/pydefold-1.1.9.tar.gz

---
+++ packages/Pydefold/pydefold-1.1.9.tar.gz/pydefold-1.1.9/PyDefold/render/__init__.py
@@ -0,0 +1,3 @@
+from .material_ddf_pb2 import MaterialDesc 
+from .font_ddf_pb2 import FontDesc,FontMap,GlyphBank 
+from .render_ddf_pb2 import ClearColor,DisplayProfile,DisplayProfileQualifier,DisplayProfiles,DrawDebugText,DrawLine,DrawText,RenderPrototypeDesc,Resize,WindowResized 
---
+++ packages/Pydefold/pydefold-1.1.9.tar.gz/pydefold-1.1.9/PyDefold/script/__init__.py
@@ -0,0 +1,3 @@
+from .ddf_script_pb2 import LuaRef 
+from .sys_ddf_pb2 import Exit,Reboot,SetUpdateFrequency,SetVsync,StartRecord,StopRecord,TogglePhysicsDebug,ToggleProfile 
+from .lua_source_ddf_pb2 import LuaSource 
