2b24de358bf9a8a96e9617755289ab1f8919610c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 06:04:46 +0000

    Add package protonmail-api-client/1.6.1/protonmail-api-client-1.6.1.tar.gz

---
+++ packages/protonmail-api-client/protonmail-api-client-1.6.1.tar.gz/protonmail-api-client-1.6.1/src/protonmail/exceptions.py
@@ -0,0 +1,29 @@
+"""Exceptions"""
+
+
+class SendMessageError(Exception):
+    """Error when try to send message"""
+
+
+class InvalidTwoFactorCode(Exception):
+    """Invalid Two-Factor Authentication(2FA) code"""
+
+
+class NoKeysForDecryptThisMessage(Exception):
+    """
+    No keys for decrypt this message,
+    maybe you created new key then re-login,
+    if you deleted key then you can't decrypt this message
+    """
+
+
+class LoadSessionError(Exception):
+    """Error while load session, maybe this session file was created in other version? then re-login"""
+
+
+class AddressNotFound(Exception):
+    """Email address was not found"""
+
+
+class CantUploadAttachment(Exception):
+    """Error when try to upload attachment"""
