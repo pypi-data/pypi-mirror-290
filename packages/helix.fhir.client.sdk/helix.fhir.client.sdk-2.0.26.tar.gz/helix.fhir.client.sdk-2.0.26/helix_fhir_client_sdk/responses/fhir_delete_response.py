3339649943bd3cd6cc0d4fcd812773726721b3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 14:04:52 +0000

    Add package helix.fhir.client.sdk/2.0.26/helix.fhir.client.sdk-2.0.26.tar.gz

---
+++ packages/helix.fhir.client.sdk/helix.fhir.client.sdk-2.0.26.tar.gz/helix.fhir.client.sdk-2.0.26/helix_fhir_client_sdk/responses/fhir_delete_response.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class FhirDeleteResponse:
+    def __init__(
+        self,
+        request_id: Optional[str],
+        url: str,
+        responses: str,
+        error: Optional[str],
+        access_token: Optional[str],
+        status: int,
+        count: Optional[int] = None,
+    ) -> None:
+        """
+        Class that encapsulates the response from FHIR server
+
+        :param request_id: request id
+        :param url: url that was being accessed
+        :param responses: response text
+        :param error: Any error returned by FHIR server
+        :param access_token: access token that was used
+        """
+        self.request_id: Optional[str] = request_id
+        self.url: str = url
+        self.responses: str = responses
+        """ Response text """
+        self.error: Optional[str] = error
+        self.access_token: Optional[str] = access_token
+        self.status: int = status
+        self.count: Optional[int] = count
+        """ Number of resources deleted """
