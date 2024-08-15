a0523b5c27e6da22daa5de04706945157c850ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 14:04:37 +0000

    Add package bayesfilter/0.0.4/bayesfilter-0.0.4.tar.gz

---
+++ packages/bayesfilter/bayesfilter-0.0.4.tar.gz/bayesfilter-0.0.4/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name='bayesfilter',
+    version='0.0.4',
+    packages=['bayesfilter'],
+    install_requires=[
+        'numpy',
+    ],
+    license='MIT',
+    author='Hugo Hadfield',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
