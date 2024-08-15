d8813d9a1997dd0d1e19d4f6a2a7284972763c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:04:50 +0000

    Add package MNHPy/0.3.3/MNHPy-0.3.3.tar.gz

---
+++ packages/MNHPy/MNHPy-0.3.3.tar.gz/MNHPy-0.3.3/README.md
@@ -0,0 +1,13 @@
+# MNHPy
+Meso-NH Python library to plot quick figures
+
+Gallery and script examples : http://mesonh.aero.obs-mip.fr/mesonh55/PythonLIB
+
+Notebook Tutorial in /examples
+
+Feel free to contribute !
+
+
+# Install
+- clone or download the repo
+- pip install [--user] MNHPy
---
+++ packages/MNHPy/MNHPy-0.3.3.tar.gz/MNHPy-0.3.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+MAINTAINER_EMAIL = 'mesonhsupport@obs-mip.fr'
+AUTHORS = 'Quentin Rodier & Meso-NH Team'
+
+setup(
+   name='MNHPy',
+   version='0.3.3',
+   description=('Python visulization tools for MesoNH atmospheric research model'),
+   long_description=('Compatible with Meso-NH model version >=5.7.1'),
+   author=AUTHORS,
+   author_email=MAINTAINER_EMAIL,
+   url='https://github.com/QuentinRodier/MNHPy',
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: CeCILL-C Free Software License Agreement (CECILL-C)"],
+   packages=['MNHPy'],	#name of package (dir with the modules)
+   # external dependencies packages
+   install_requires=['numpy>=1.26.4', 'matplotlib==3.9.1', 'cartopy>=0.21.1', 'scipy>=1.14.0', 'netCDF4>=1.7.1'],
+   python_requires=">=3.12.3",
+   package_dir={"": "src"},
+   )
