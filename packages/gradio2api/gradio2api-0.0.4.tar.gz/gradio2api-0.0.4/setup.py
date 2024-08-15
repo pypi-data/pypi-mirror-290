b7fe2072b520cfaf2bcb8432786bd0c810acb69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 06:05:09 +0000

    Add package gradio2api/0.0.4/gradio2api-0.0.4.tar.gz

---
+++ packages/gradio2api/gradio2api-0.0.4.tar.gz/gradio2api-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="gradio2api",
+  version="0.0.4",
+  description="Transfer gradio application to fastapi.",
+  url="https://github.com/mistake0316/gradio2api",
+  author="湯沂達(Tang Yi Dar)",
+  author_email="changethewhat@gmail.com",
+  license="MIT",
+  packages=find_packages(),
+  install_requires=[
+    "fastapi[standard]",
+    "gradio_client",
+    "gradio",
+  ],
+  classifiers=[
+    "License :: OSI Approved :: MIT License",
+  ],
+  python_requires=">=3.9"
+)
