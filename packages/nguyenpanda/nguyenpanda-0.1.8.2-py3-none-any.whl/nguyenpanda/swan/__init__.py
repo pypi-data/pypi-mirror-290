b9029de419029b953507787c8793bb0c106dff84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:10 +0000

    Add package nguyenpanda/0.1.8.2/nguyenpanda-0.1.8.2-py3-none-any.whl

---
+++ packages/nguyenpanda/nguyenpanda-0.1.8.2-py3-none-any.whl/nguyenpanda/swan/__init__.py
@@ -0,0 +1,22 @@
+"""
+🦢 swan
+    Indulge in the beauty of aesthetics and user interfaces with the elegant swan package.
+    Dive into the world of colors, front-end development, GUI, and more.
+    Transform your applications into visual masterpieces with swan.
+
+    - ColorClass: class that contains color codes and methods to print colored text to the console.
+    - Color: an instance of ColorClass.
+"""
+
+from .color import Color, ColorClass
+
+__all__ = (
+    'Color',        # an instance of ColorClass
+    'ColorClass',
+)
+
+__version__ = '0.1.0'
+
+__description__ = '''
+\'swan\' package provides things related to beautiful, font-end, GUI,...
+'''
