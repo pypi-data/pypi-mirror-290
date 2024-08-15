a6ab8a67b3d75985e56a4e6757fba798d41b3513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 07:04:32 +0000

    Add package pyseq/0.7.0/pyseq-0.7.0.tar.gz

---
+++ packages/pyseq/pyseq-0.7.0.tar.gz/pyseq-0.7.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2011-2024 Ryan Galloway (ryan@rsgalloway.com)
+#
+# This module is part of Shotman and is released under
+# the BSD License: http://www.opensource.org/licenses/bsd-license.php
+
+from setuptools import setup
+from pyseq import __version__
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="pyseq",
+    version=__version__,
+    description="Compressed File Sequence String Module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ryan Galloway",
+    author_email="ryan@rsgalloway.com",
+    url="http://github.com/rsgalloway/pyseq",
+    py_modules=["pyseq"],
+    scripts=["lss"],
+    python_requires=">=3.6",
+    zip_safe=False,
+)
