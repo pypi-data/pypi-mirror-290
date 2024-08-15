d4f8caeeaf346ba12015bc793721429a2e2b33e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:04:35 +0000

    Add package asciimatics-overlay-ov/1.0.7/asciimatics_overlay_ov-1.0.7.tar.gz

---
+++ packages/asciimatics-overlay-ov/asciimatics_overlay_ov-1.0.7.tar.gz/asciimatics_overlay_ov-1.0.7/asciimatics_overlay_ov/__main__.py
@@ -0,0 +1,29 @@
+"""
+File in charge of acting as the main script of the library when it is called as a standalone
+"""
+from time import sleep
+from random import randint
+from asciimatics.event import Event
+from asciimatics.screen import Screen
+from asciimatics_overlay_ov import AsciimaticsOverlay
+import asciimatics_overlay_ov.example_scripts as ES
+
+
+print("Hello world")
+
+if __name__ == "__main__":
+    SUCCESS = 0
+    ERROR = 1
+    SCREEN = None
+    LAST_SCENE = None
+    WII = ES.Main(
+        success=SUCCESS,
+        error=ERROR,
+        screen=SCREEN,
+        last_scene=LAST_SCENE
+    )
+    try:
+        STATUS = WII.run()
+    except Exception as err:
+        print(f"Error: {err}")
+        exit(ERROR)
