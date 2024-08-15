d6f21ab274393fca4be80c1d938126742ca0b0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:10 +0000

    Add package unitprompt/0.0.8/unitprompt-0.0.8.tar.gz

---
+++ packages/unitprompt/unitprompt-0.0.8.tar.gz/unitprompt-0.0.8/unitprompt/matchers/test_fulfills.py
@@ -0,0 +1,27 @@
+import pytest
+from unitprompt.llm import LLM
+
+async def assert_fulfills(received: str, prompt):
+    
+    # Check if prompt is a list, if not, convert it to a list
+    if not isinstance(prompt, list):
+        prompt = [prompt]
+    
+    llm = LLM()
+    
+    unfulfilled_conditions = []
+    for condition in prompt:
+        complete_prompt = "<Goal>Check whether the input satisfies the condition</Goal>"
+        complete_prompt += f"<Condition>{condition}</Condition>"
+        complete_prompt += f"<Input>{received}</Input>"
+        
+        llm.model.max_tokens = 1
+        complete_prompt += "<Response>If condition satisfied: 1, if condition is not satisfied: 0</Response>"
+            
+        result = await llm.invoke(complete_prompt, { "received": received, "condition": condition })
+        
+        if result != "1":
+            unfulfilled_conditions.append(condition)
+
+    if len(unfulfilled_conditions) > 0:
+        raise pytest.fail(f"Expected {received} to satisfy the following conditions: {unfulfilled_conditions}, but it did not.")
