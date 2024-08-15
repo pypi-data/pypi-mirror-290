c470be20db2d85de8ea374873e521a600ac1f55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 15:07:20 +0000

    Add package leesah-game/0.3.4/leesah_game-0.3.4.tar.gz

---
+++ packages/leesah-game/leesah_game-0.3.4.tar.gz/leesah_game-0.3.4/leesah/__init__.py
@@ -0,0 +1,2 @@
+"""Entry point for the Leesah package."""
+from leesah.quiz_rapid import QuizRapid, Question, ARITHMETIC, BANK_ACCOUNT, BASE64, DEDUPLICATION, GRUNNBELØP, MIN_MAX, NAV, PING_PONG, PRIME, TEAM_REGISTRATION
