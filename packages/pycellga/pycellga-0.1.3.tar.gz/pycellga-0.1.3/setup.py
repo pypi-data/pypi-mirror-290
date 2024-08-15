6176b2d18d25f4c044ee8af8c173599b7ab080a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:05:16 +0000

    Add package pycellga/0.1.3/pycellga-0.1.3.tar.gz

---
+++ packages/pycellga/pycellga-0.1.3.tar.gz/pycellga-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='pycellga',  
+    version='0.1.3',  
+    description='A Python Package for Improved Cellular Genetic Algorithms',  
+    author='Sevgi Akten Karakaya, Mehmet Hakan Satman',  
+    author_email='sevgiakten@gmail.com, mhsatman@gmail.com', 
+    packages=find_packages('src'),
+    package_dir={'': 'src'}, 
+    install_requires=required, 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+)
