16dcc969d61b87924dd1d623bd92e7c13d7acae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:30 +0000

    Add package starbear/0.2.1/starbear-0.2.1.tar.gz

---
+++ packages/starbear/starbear-0.2.1.tar.gz/starbear-0.2.1/oldshit/carcajou.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import uvicorn
+from hrepr import H
+from starbear import bear
+from starlette.applications import Starlette
+from starlette.routing import Mount
+
+
+@bear
+async def allo(page):
+    page.print(H.link(rel="stylesheet", href=Path("moldule/style.css")))
+    page.print(H.div["moldy"](H.b("allo")))
+
+
+@bear
+async def boop(page):
+    page.print(H.link(rel="stylesheet", href=Path("moldule/style.css")))
+    page.print(H.div["moldy"](H.b("boop!")))
+
+
+if __name__ == "__main__":
+    app = Starlette(
+        routes=[
+            Mount("/allo", allo),
+            Mount("/boop", boop),
+        ]
+    )
+    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+
+
+# @bear("/allo")
+# async def allo(page):
+#     page.print(
+#         H.link(rel="stylesheet", href=Path("moldule/style.css"))
+#     )
+#     page.print(
+#         H.div["moldy"](
+#             H.b("allo")
+#         )
+#     )
+
+# @bear("/boop")
+# async def boop(page):
+#     page.print(
+#         H.link(rel="stylesheet", href=Path("moldule/style.css"))
+#     )
+#     page.print(
+#         H.div["moldy"](
+#             H.b("boop!")
+#         )
+#     )
+
+
+# if __name__ == "__main__":
+#     app = Starlette(routes=[allo, boop])
+#     uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
