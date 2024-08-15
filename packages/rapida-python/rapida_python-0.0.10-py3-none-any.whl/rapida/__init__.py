db8a84c96ed0d065024ee603427ea64b42ac31b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 14:06:02 +0000

    Add package rapida-python/0.0.10/rapida_python-0.0.10-py3-none-any.whl

---
+++ packages/rapida-python/rapida_python-0.0.10-py3-none-any.whl/rapida/__init__.py
@@ -0,0 +1,29 @@
+"""
+author: prashant.srivastav
+"""
+
+from rapida.rapida_client import RapidaClient
+from rapida.rapida_client_options import (
+    RapidaClientOptions,
+    RapidaEnvironment,
+    RapidaRegion,
+)
+from rapida.exceptions import (
+    RapidaException,
+    RapidaInternalServerException,
+    RapidaInvalidAPIException,
+    RapidaConfigurationException,
+)
+from rapida.version import VERSION
+
+__all__ = [
+    "RapidaClient",
+    "RapidaClientOptions",
+    "RapidaException",
+    "RapidaInternalServerException",
+    "RapidaInvalidAPIException",
+    "RapidaConfigurationException",
+    "VERSION",
+    "RapidaEnvironment",
+    "RapidaRegion",
+]
