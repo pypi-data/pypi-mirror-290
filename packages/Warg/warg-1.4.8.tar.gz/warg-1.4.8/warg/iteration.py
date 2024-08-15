1ea66a2cf60eceeaf0a3417ac4c11139458554f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:05:16 +0000

    Add package Warg/1.4.8/warg-1.4.8.tar.gz

---
+++ packages/Warg/warg-1.4.8.tar.gz/warg-1.4.8/warg/iteration.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import logging
+from typing import Any, Callable, Iterable, List, Sequence, Tuple
+
+logger = logging.getLogger(__name__)
+__all__ = ["pairs", "chunks", "leaf_apply", "leaf_type_apply"]
+
+
+def pairs(s: Sequence) -> Tuple[Any, Any]:
+    """
+    Iterate over a list in overlapping pairs.
+
+    Usage:
+        lst = [4, 7, 11, 2]
+        pairs(lst) yields (4, 7), (7, 11), (11, 2)
+
+    https://stackoverflow.com/questions/1257413/1257446#1257446
+
+
+    :param s: An iterable/list
+    :return: Yields a pair of consecutive elements (lst[k], lst[k+1]) of lst. Last call yields (lst[-2], lst[-1]).
+    """
+    i = iter(s)
+    prev = next(i)
+
+    for item in i:
+        yield prev, item
+        prev = item
+
+
+def leaf_apply(seq: Iterable, func: Callable) -> List:
+    sub = []
+    for element in seq:
+        if isinstance(element, Iterable):
+            sub.append(leaf_apply(element, func))
+        else:
+            sub.append(func(element))
+    return sub
+
+
+def leaf_type_apply(seq: Iterable, func: Callable, leaf_type: type = tuple) -> List:
+    sub = []
+    for element in seq:
+        if not isinstance(element, leaf_type):
+            sub.append(leaf_type_apply(element, func, leaf_type))
+        else:
+            sub.append(func(element))
+    return sub
+
+
+def chunks(lst: Sequence, n: int) -> Any:
+    """
+    Yield successive n-sized chunks from lst.
+
+     :param lst:
+     :param n:
+     :return:
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+if __name__ == "__main__":
+    logger.info(list(chunks(list(range(10)), 3)))
---
+++ packages/Warg/warg-1.4.8.tar.gz/warg-1.4.8/warg/map_itertools.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+__author__ = "Christian Heider Lindbjerg"
+__doc__ = r"""
+
+           Created on 09-09-2020
+           """
+
+__all__ = [
+    "map_value_product",
+    "map_product",
+    "map_permutations",
+    "map_combinations",
+    "map_combinations_with_replacement",
+]
+
+import itertools
+import logging
+from typing import Any, Mapping, Tuple
+
+logger = logging.getLogger(__name__)
+
+
+def map_value_product(dicts: Mapping) -> Any:
+    """description"""
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
+
+
+def map_product(dicts: Mapping, repeat: int = 2) -> Any:
+    """description"""
+    yield from zip(
+        itertools.product(dicts.keys(), repeat=repeat),
+        itertools.product(dicts.values(), repeat=repeat),
+    )
+
+
+def map_permutations(dicts: Mapping, repeat: int = 2) -> Tuple:
+    """description"""
+    yield from zip(
+        itertools.permutations(dicts.keys(), repeat),
+        itertools.permutations(dicts.values(), repeat),
+    )
+
+
+def map_combinations(dicts: Mapping, repeat: int = 2) -> Tuple:
+    """description"""
+    yield from zip(
+        itertools.combinations(dicts.keys(), repeat),
+        itertools.combinations(dicts.values(), repeat),
+    )
+
+
+def map_combinations_with_replacement(dicts: Mapping, repeat: int = 2) -> Tuple:
+    """description"""
+    yield from zip(
+        itertools.combinations_with_replacement(dicts.keys(), repeat),
+        itertools.combinations_with_replacement(dicts.values(), repeat),
+    )
+
+
+if __name__ == "__main__":
+
+    def asdijha() -> None:
+        """
+        :rtype: None
+        """
+        from warg import NOD
+
+        a = NOD(a=[1], b=[4], c=[8])
+        logger.info(f"ValueMapProduct{str(list(map_value_product(a.as_dict())))}")
+        logger.info(f"MapProduct{str(list(map_product(a.as_dict())))}")
+        logger.info(f"map_combinations{str(list(map_combinations(a.as_dict())))}")
+        logger.info(f"map_permutations{str(list(map_permutations(a.as_dict())))}")
+        logger.info(
+            f"map_combinations_with_replacement{str(list(map_combinations_with_replacement(a.as_dict())))}"
+        )
+
+    def asdijhsadasdad() -> None:
+        """
+        :rtype: None
+        """
+        from warg import NOD
+
+        a = NOD(a=[1, 2, 8], b=[4, 3, 99])
+        logger.info(f"ValueMapProduct{str(list(map_value_product(a.as_dict())))}")
+        logger.info(f"MapProduct{str(list(map_product(a.as_dict())))}")
+        logger.info(f"map_combinations{str(list(map_combinations(a.as_dict())))}")
+        logger.info(f"map_permutations{str(list(map_permutations(a.as_dict())))}")
+        logger.info(
+            f"map_combinations_with_replacement{str(list(map_combinations_with_replacement(a.as_dict())))}"
+        )
+
+    asdijha()
+    # asdijhsadasdad()
