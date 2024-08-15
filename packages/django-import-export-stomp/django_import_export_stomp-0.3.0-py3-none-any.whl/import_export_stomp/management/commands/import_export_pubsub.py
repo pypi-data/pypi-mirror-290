0c6112222cc020a41a83e922837669d8c7155286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 06:04:55 +0000

    Add package django-import-export-stomp/0.3.0/django_import_export_stomp-0.3.0-py3-none-any.whl

---
+++ packages/django-import-export-stomp/django_import_export_stomp-0.3.0-py3-none-any.whl/import_export_stomp/management/commands/import_export_pubsub.py
@@ -0,0 +1,19 @@
+from django_stomp.management.commands.pubsub import Command as PubsubCommand
+
+from import_export_stomp.utils import IMPORT_EXPORT_STOMP_PROCESSING_QUEUE
+
+
+class Command(PubsubCommand):
+    help = "Listens to queue to process messages"
+
+    def add_arguments(self, parser):
+        ...
+
+    def handle(self, *args, **options):
+        super().handle(
+            *args,
+            **{
+                "source_destination": IMPORT_EXPORT_STOMP_PROCESSING_QUEUE,
+                "callback_function": "import_export_stomp.pubsub.consumer",
+            },
+        )
