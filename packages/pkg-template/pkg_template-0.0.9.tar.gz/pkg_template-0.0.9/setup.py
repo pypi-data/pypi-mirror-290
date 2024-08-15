a58f30e66fb9f188e71477c5ee15aeffce27de91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 15:04:40 +0000

    Add package pkg-template/0.0.9/pkg_template-0.0.9.tar.gz

---
+++ packages/pkg-template/pkg_template-0.0.9.tar.gz/pkg_template-0.0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pkg_template",
+    version="0.0.9",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pkg-template = pkg_template.cli:main',  # This connects the `pkg_template` command to the `main` function in `cli.py`.
+        ],
+    },
+    install_requires=[
+        # 'requests',
+    ],
+    author="amirgard",
+    author_email="",
+    description="",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
