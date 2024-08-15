213509c8e1f1590c6313211cecb19abfded0c86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:04:34 +0000

    Add package asciimatics-overlay-ov/1.0.7/asciimatics_overlay_ov-1.0.7-py3-none-any.whl

---
+++ packages/asciimatics-overlay-ov/asciimatics_overlay_ov-1.0.7-py3-none-any.whl/asciimatics_overlay_ov/__init__.py
@@ -0,0 +1,8 @@
+"""
+File in charge of doing a rebind for the asciimatics library
+"""
+from .asciimatics_overlay_main import AsciiMaticsOverlayMain
+
+
+class AsciimaticsOverlay(AsciiMaticsOverlayMain):
+    """ The class in charge of simplifying the usage of some functionalities from asciimatics """
