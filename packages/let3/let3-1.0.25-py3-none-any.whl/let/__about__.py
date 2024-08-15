1e40e36616c3f13c88d6583e612e4368526250e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:29 +0000

    Add package let3/1.0.25/let3-1.0.25-py3-none-any.whl

---
+++ packages/let3/let3-1.0.25-py3-none-any.whl/let/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2016 Taylor Marks
+# Copyright (c) 2016 Adam Karpierz
+# SPDX-License-Identifier: MIT
+
+__import__("pkg_about").about("let3")
+__copyright__ = f"Copyright (c) 2016-2024 {__author__}"  # noqa
