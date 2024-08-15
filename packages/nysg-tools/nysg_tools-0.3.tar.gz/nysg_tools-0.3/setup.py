be1dadceadbdf8687bd21703e020d402a5728f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 04:05:02 +0000

    Add package nysg-tools/0.3/nysg_tools-0.3.tar.gz

---
+++ packages/nysg-tools/nysg_tools-0.3.tar.gz/nysg_tools-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+
+setup(
+    name="nysg_tools",
+    version="0.3",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    description="Una colección de herramientas utiles para los laboratorios de la UBA FCEN",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="Santiago Noya",
+    author_email="noyasantiagomail@gmail.com",
+)
