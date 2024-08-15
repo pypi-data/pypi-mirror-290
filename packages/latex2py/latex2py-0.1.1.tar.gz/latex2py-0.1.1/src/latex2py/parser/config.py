e593be0e2e1efbaf5fde0ab410bf4844bc97d6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 06:05:11 +0000

    Add package latex2py/0.1.1/latex2py-0.1.1.tar.gz

---
+++ packages/latex2py/latex2py-0.1.1.tar.gz/latex2py-0.1.1/src/latex2py/parser/config.py
@@ -0,0 +1,28 @@
+class Config:
+  INFINITY = "float('inf')"
+  FACTORIAL = "math.factorial"
+
+  FLOOR = "math.floor"
+  CEIL = "math.ceil"
+
+  SQRT = "math.sqrt"
+
+  SIN = "math.sin"
+  COS = "math.cos"
+  TAN = "math.tan"
+  SINH = "math.sinh"
+  COSH = "math.cosh"
+  TANH = "math.tanh"
+
+  ASIN = "math.asin"
+  ACOS = "math.acos"
+  ATAN = "math.atan"
+  ASINH = "math.asinh"
+  ACOSH = "math.acosh"
+  ATANH = "math.atanh"
+
+  LOG = "math.log"
+  EXP = "math.exp"
+  ABS = "abs"
+
+  E = "math.e"
