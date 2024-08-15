6fc64b938600ca8d3a6ce0aa8c6479347de38bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 05:04:40 +0000

    Add package DeeFuzzer/0.9.0/DeeFuzzer-0.9.0.tar.gz

---
+++ packages/DeeFuzzer/DeeFuzzer-0.9.0.tar.gz/DeeFuzzer-0.9.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup and build script for the library."""
+
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.11',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Multimedia :: Sound/Audio',
+    'Topic :: Multimedia :: Sound/Audio :: Players',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+]
+
+setup(
+    name="DeeFuzzer",
+    url="http://github.com/yomguy/DeeFuzzer",
+    description="open, light and instant media streaming tool",
+    long_description=open('README.rst').read(),
+    author="Guillaume Pellerin",
+    author_email="yomguy@parisson.com",
+    version='0.9.0',
+    install_requires=[
+        'setuptools',
+        'wheel',
+        'python-shout==0.2.8',
+        'python-twitter==3.5',
+        'mutagen==1.45.1',
+        'pyliblo3==0.12.0',
+        'pycurl==7.44.1',
+        'pyyaml==6.0.1',
+        'mysqlclient==2.0.3',
+    ],
+    platforms=['OS Independent'],
+    license='GPL v3',
+    scripts=['scripts/deefuzzer'],
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3',
+)
