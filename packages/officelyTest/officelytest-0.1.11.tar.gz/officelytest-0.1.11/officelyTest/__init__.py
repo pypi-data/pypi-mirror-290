151813b0d354f042aede07417af7792d1672f1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 16:04:30 +0000

    Add package officelyTest/0.1.11/officelytest-0.1.11.tar.gz

---
+++ packages/officelyTest/officelytest-0.1.11.tar.gz/officelytest-0.1.11/officelyTest/__init__.py
@@ -0,0 +1,4 @@
+from officelyTest.team_builder.nodes.interface import IHistory
+from officelyTest.team_builder.nodes.enums import MessageType
+from officelyTest.team_builder.nodes.interface import IHistory
+from officelyTest.run import invokeTeam
