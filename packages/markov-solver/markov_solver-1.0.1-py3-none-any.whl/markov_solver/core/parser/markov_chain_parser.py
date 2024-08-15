2fc1544e2233d494f140c05c5626d8bf02c18a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 03:05:32 +0000

    Add package markov-solver/1.0.1/markov_solver-1.0.1-py3-none-any.whl

---
+++ packages/markov-solver/markov_solver-1.0.1-py3-none-any.whl/markov_solver/core/parser/markov_chain_parser.py
@@ -0,0 +1,25 @@
+import yaml  # type: ignore
+
+from markov_solver.core.model.markov_chain import MarkovChain
+from markov_solver.core.model.markov_link import MarkovLink
+from markov_solver.core.model.markov_state import MarkovState
+
+
+def create_chain_from_file(path):
+    with open(path, "r") as definition_file:
+        definition = yaml.load(definition_file, Loader=yaml.FullLoader)
+
+    mc = MarkovChain()
+
+    if "symbols" in definition:
+        mc.add_symbols(**definition["symbols"])
+
+    for edge in definition["chain"]:
+        head = MarkovState(edge["from"])
+        tail = MarkovState(edge["to"])
+        link = MarkovLink(head, tail, edge["value"])
+        mc.add_state(head)
+        mc.add_state(tail)
+        mc.add_link(link)
+
+    return mc
