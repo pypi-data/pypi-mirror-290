848fbbeddec7893cc726d7c6e46a2ea1bbc05d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:05:02 +0000

    Add package myutilitiesplus/0.2/myutilitiesplus-0.2-py3-none-any.whl

---
+++ packages/myutilitiesplus/myutilitiesplus-0.2-py3-none-any.whl/myutilitiesplus/math_operations.py
@@ -0,0 +1,30 @@
+def add(num1, num2):
+    """
+    Performs addition of the given two numbers
+    """
+    return num1 + num2
+
+def subtract(num1, num2):
+    """
+    Performs subtraction of the given two numbers
+    """
+    return num1 - num2
+
+def multiply(num1, num2):
+    """
+    Performs multiplication of the given two numbers
+    """
+    return num1 * num2
+
+def divide(num1, num2):
+    """
+    Performs division of the given two numbers
+    """
+    return num1 / num2
+
+if __name__ == '__main__':
+    print(add(7,2))
+    print(subtract(9,3))
+    print(multiply(4,8))
+    print(divide(5,2))
+
---
+++ packages/myutilitiesplus/myutilitiesplus-0.2-py3-none-any.whl/myutilitiesplus/string_operations/string_ops.py
@@ -0,0 +1,11 @@
+def reverse(word):
+    """
+    Converts given string to its reverse
+    """
+    return word[::-1]
+
+def capitalize(word):
+    """
+    Converts given string to its capitalized version
+    """
+    return word.capitalize()
