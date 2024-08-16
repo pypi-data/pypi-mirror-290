27a8017ceebfbf04aab56f2e3f9a14996aacad06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:04:45 +0000

    Add package prompt-enhance-service/0.1.0/prompt_enhance_service-0.1.0.tar.gz

---
+++ packages/prompt-enhance-service/prompt_enhance_service-0.1.0.tar.gz/prompt_enhance_service-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prompt_enhance_service',
+    version='0.1.0',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jinka Sai Jagadeesh',
+    author_email='saijagadeesh.jinka@gavstech.com',
+    license='GAVS Proprietary License',  # Or another license
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+        'google-cloud-firestore'
+        # List of dependencies e.g. 'numpy', 'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',  # Specify your minimum Python version
+)
