0101daa83ed700817398ff9dfa9262735a9f7570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:05:10 +0000

    Add package pypst/0.2.0/pypst-0.2.0.tar.gz

---
+++ packages/pypst/pypst-0.2.0.tar.gz/pypst-0.2.0/pypst/__init__.py
@@ -0,0 +1,19 @@
+from pypst.table import Table
+from pypst.cell import Cell
+from pypst.figure import Figure
+from pypst.document import Document
+from pypst.heading import Heading
+from pypst.itemize import Itemize, Enumerate
+from pypst.image import Image
+
+__all__ = [
+    "Table",
+    "Cell",
+    "Figure",
+    "Document",
+    "Heading",
+    "Itemize",
+    "Enumerate",
+    "Image",
+]
+__version__ = "0.2.0"
