aed3288532482172b53fc00e72944f100b000367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:03 +0000

    Add package pkg-template/0.1.6/pkg_template-0.1.6.tar.gz

---
+++ packages/pkg-template/pkg_template-0.1.6.tar.gz/pkg_template-0.1.6/README.md
@@ -0,0 +1,13 @@
+# pkg-template
+
+its a simple cli program developed by amirgard0.
+
+## what does pkg-template do
+
+it create a pypi pkg template.<br>
+
+## how to install it
+
+1. > pip install pkg-template
+
+2. > pip3 install pkg-template
---
+++ packages/pkg-template/pkg_template-0.1.6.tar.gz/pkg_template-0.1.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pkg_template",
+    version="0.1.6",
+    packages=find_packages(),
+    package_data={
+        'pkg_template': ['data/django_app/**/*', "data/*"],
+    },
+    include_package_data=True,  # Make sure this is included
+    entry_points={
+        'console_scripts': [
+            'pkg-template = pkg_template.cli:main',
+        ],
+    },
+    install_requires=[
+        # 'requests',
+    ],
+    author="amirgard",
+    author_email="",
+    description="",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
