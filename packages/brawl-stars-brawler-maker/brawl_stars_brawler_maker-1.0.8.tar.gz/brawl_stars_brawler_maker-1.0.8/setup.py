c980a37c53fc7d772718c4c3b3566c8d67683d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:25 +0000

    Add package brawl-stars-brawler-maker/1.0.8/brawl_stars_brawler_maker-1.0.8.tar.gz

---
+++ packages/brawl-stars-brawler-maker/brawl_stars_brawler_maker-1.0.8.tar.gz/brawl_stars_brawler_maker-1.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brawl_stars_brawler_maker',
+    version='1.0.8',
+    author='AngelFire',
+    description='A package to mod Brawl Stars game files for custom brawler creation.',
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AngelFireLA/Brawler-Maker-Python-Library',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'apk_signer',
+        'tenacity',
+        'tqdm',
+        'pycryptodome',
+    ],
+    package_data={'': ['default_files/csv_logic/*.csv', 'default_files/localization/*.csv']},
+)
