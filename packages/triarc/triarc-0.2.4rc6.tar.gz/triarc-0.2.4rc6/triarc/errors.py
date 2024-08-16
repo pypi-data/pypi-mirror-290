a26ea19fc890903af4205f7e5db8f17f664862f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 16:05:12 +0000

    Add package triarc/0.2.4rc6/triarc-0.2.4rc6.tar.gz

---
+++ packages/triarc/triarc-0.2.4rc6.tar.gz/triarc-0.2.4rc6/triarc/errors.py
@@ -0,0 +1,48 @@
+"""
+A module with generic Triarc errors.
+"""
+
+
+class TriarcError(Exception):
+    """
+    A common superclass for all
+    exceptions regarding Triarc.
+    """
+
+    pass
+
+
+# == Backend errors ==
+
+
+class TriarcBackendError(TriarcError):
+    """
+    A common superclass for all exceptions
+    involving triarc.backend.Backend and
+    subclasses thereof.
+    """
+
+    pass
+
+
+# == Bot errors ==
+
+
+class TriarcBotError(TriarcError):
+    """
+    A common superclass for all exceptions
+    involving triarc.bot.Bot and subclasses
+    thereof.
+    """
+
+    pass
+
+
+class TriarcBotBackendRefusedError(TriarcBotError):
+    """
+    Raised when a backend refuses to be registered
+    by a Bot, when such register operation is
+    called with `required=True`.
+    """
+
+    pass
