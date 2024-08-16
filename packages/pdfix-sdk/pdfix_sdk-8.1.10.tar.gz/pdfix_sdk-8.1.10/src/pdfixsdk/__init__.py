adef07e6044d6d177eed2bacd37389d03afdfd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:31 +0000

    Add package pdfix-sdk/8.1.10/pdfix_sdk-8.1.10.tar.gz

---
+++ packages/pdfix-sdk/pdfix_sdk-8.1.10.tar.gz/pdfix_sdk-8.1.10/src/pdfixsdk/__init__.py
@@ -0,0 +1,35 @@
+__version__="8.1.10"
+
+import platform, os
+from ctypes import cdll
+import pdfixsdk.Pdfix
+# import pdfixsdk.OcrTesseract
+
+# get the shared library name based on the platform
+def getModuleName(module):
+  machine = platform.uname().machine
+  pltfm = platform.system()
+  if pltfm == 'Darwin':
+    if machine == "arm64":
+      return 'arm64/lib' + module + '.dylib'  
+    else:
+      return 'x86_64/lib' + module + '.dylib'
+  elif pltfm == "Windows":
+    if machine == "arm64":
+      return 'arm64/' + module + '.dll'  
+    else:
+      return 'x86_64/' + module + '.dll'
+  elif pltfm == "Linux":
+    if machine == "aarch64":
+      return 'aarch64/lib' + module + '.so'  
+    else:
+      return 'x86_64/lib' + module + '.so'
+  
+# load pdfix library from the current folder
+basePath = os.path.dirname(os.path.abspath(__file__))
+pdfixsdk.Pdfix.Pdfix_init(basePath + "/bin/" + getModuleName('pdf'))
+# pdfixsdk.OcrTesseract.OcrTesseract_init(basePath + "/bin/" + getModuleName('ocr_tesseract'))
+
+# load additional dependencies
+if platform.system() == "Windows":
+  cdll.LoadLibrary(basePath + "/bin/x64/LicenseSpringVMD.dll")
