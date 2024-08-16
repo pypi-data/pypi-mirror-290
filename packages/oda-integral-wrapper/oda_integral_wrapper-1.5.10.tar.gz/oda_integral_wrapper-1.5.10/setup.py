b73841bd663fe0dc77e005f364fe5ef004370c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:05:32 +0000

    Add package oda-integral-wrapper/1.5.10/oda_integral_wrapper-1.5.10.tar.gz

---
+++ packages/oda-integral-wrapper/oda_integral_wrapper-1.5.10.tar.gz/oda_integral_wrapper-1.5.10/setup.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import, division, print_function
+
+__author__ = 'Carlo Ferrigno'
+
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import glob
+
+packs = find_packages()
+
+print('packs', packs)
+
+include_package_data = True
+
+scripts_list = glob.glob('./bin/*')
+setup(name='oda_integral_wrapper',
+      version="1.5.10",
+      description='wrapper for INTEGRAL analysis using the API plugin for CDCI online data analysis',
+      author='Carlo Ferrigno',
+      author_email='carlo.ferrigno@unige.ch',
+      url="https://gitlab.astro.unige.ch/oda/api-clients/oda_api_wrapper",
+      scripts=scripts_list,
+      packages=packs,
+      package_data={'oda_integral_wrapper': ['config_dir/*']},
+      include_package_data=True,
+      install_requires=[
+                        "astropy",
+                        "matplotlib",
+                        "numpy",
+                        "oda_api",
+                        "requests",
+                        "astroquery",
+                        "pymosaic-fits",
+                        "nlopt",
+                        "autologging",
+                        "PyYAML"
+                    ],
+    entry_points={
+    'console_scripts': [
+        'group_spectrum_adaptively.py = oda_integral_wrapper.wrapper:group_spectrum_adaptively',
+    ],
+},
+      python_requires='>=3.0',
+      )
