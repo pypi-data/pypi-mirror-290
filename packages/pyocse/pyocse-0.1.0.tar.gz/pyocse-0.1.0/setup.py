104da79944758bbdeb38f070355496fd9af4a3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 16:05:29 +0000

    Add package pyocse/0.1.0/pyocse-0.1.0.tar.gz

---
+++ packages/pyocse/pyocse-0.1.0.tar.gz/pyocse-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#from setuptools import find_packages, setup
+from distutils.core import setup
+import setuptools  # noqa
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyocse",
+    version="0.1.0",
+    author="Qiang Zhu, Shinnosule Hattori",
+    description="Python Organic Crystal Simulation Environment",
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaterSim/OST",
+    packages=[
+        "ost",
+        "ost.interfaces",
+        "ost.lmp",
+        "ost.charmm",
+        "ost.data",
+        "ost.templates",
+    ],
+    package_data={
+        "ost.data": ["*"],
+        "ost.templates": ['*.in'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    #install_requires=[
+        #"parmed>=3.4.3",
+        #"openmm>=7.6.0",
+        #"toml",
+        #"xmltodict"
+    #],
+    #python_requires=">=3.7, <=3.10",
+    license="MIT",
+)
