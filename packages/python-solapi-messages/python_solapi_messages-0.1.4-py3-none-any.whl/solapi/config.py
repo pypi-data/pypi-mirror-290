29aea3cec9335d1ac764e62e5b7bedaf7f08e2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 04:04:34 +0000

    Add package python-solapi-messages/0.1.4/python_solapi_messages-0.1.4-py3-none-any.whl

---
+++ packages/python-solapi-messages/python_solapi_messages-0.1.4-py3-none-any.whl/solapi/config.py
@@ -0,0 +1,10 @@
+class SolApiConfig:
+    def __init__(self, api_key: str, secret_key: str, protocol: str = "https", domain: str = "api.solapi.com", prefix: str = ""):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.protocol = protocol
+        self.domain = domain
+        self.prefix = prefix
+
+    def get_url(self, path: str) -> str:
+        return f"{self.protocol}://{self.domain}{self.prefix}{path}"
