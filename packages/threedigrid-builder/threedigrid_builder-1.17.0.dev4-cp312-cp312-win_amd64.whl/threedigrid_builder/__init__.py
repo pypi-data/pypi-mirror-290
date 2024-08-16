c31738b4f7f4734e4650898cd7a6cb9b67398ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:14 +0000

    Add package threedigrid-builder/1.17.0.dev4/threedigrid_builder-1.17.0.dev4-cp312-cp312-win_amd64.whl

---
+++ packages/threedigrid-builder/threedigrid_builder-1.17.0.dev4-cp312-cp312-win_amd64.whl/threedigrid_builder/__init__.py
@@ -0,0 +1,18 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_7_4():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'threedigrid_builder.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_4()
+del _delvewheel_patch_1_7_4
+# end delvewheel patch
+
+from .application import *  # NOQA
+from .exceptions import *  # NOQA
+
+# fmt: off
+__version__ = '1.17.0.dev4'
+# fmt: on
