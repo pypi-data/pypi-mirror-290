844c3acfe54a469e4a9abf76909c6a88cf490ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:27 +0000

    Add package QyLang/0.0.1/QyLang-0.0.1-py3-none-any.whl

---
+++ packages/QyLang/QyLang-0.0.1-py3-none-any.whl/qy/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+__all__ = ['qy', 'symbol', 'symbolproxy']
+__version__ = '0.0.1'
+__author__ = 'Ge'
+
+"""
+Created on 2024-08-15
+
+"""
+
+from qy.core import qy, symbol, symbolproxy
