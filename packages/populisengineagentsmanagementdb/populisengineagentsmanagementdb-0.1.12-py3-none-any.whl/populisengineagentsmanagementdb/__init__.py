7ae4688fb0840ae98ae56c7d67cb6124f5acf878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 04:04:47 +0000

    Add package populisengineagentsmanagementdb/0.1.12/populisengineagentsmanagementdb-0.1.12-py3-none-any.whl

---
+++ packages/populisengineagentsmanagementdb/populisengineagentsmanagementdb-0.1.12-py3-none-any.whl/populisengineagentsmanagementdb/__init__.py
@@ -0,0 +1,4 @@
+from .crud import CRUD
+from .models import UserModel, CompanyModel, SessionModel, SessionItem, LogModel, HistoryModel, HistoryItem
+
+__all__ = ["CRUD", "UserModel", "CompanyModel", "SessionModel", "SessionItem", "LogModel", "HistoryModel", "HistoryItem"]
---
+++ packages/populisengineagentsmanagementdb/populisengineagentsmanagementdb-0.1.12-py3-none-any.whl/populisengineagentsmanagementdb/models.py
@@ -0,0 +1,74 @@
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from typing import List, Optional, Dict, Any
+import datetime
+
+class UserModel(BaseModel):
+    id: Optional[ObjectId] = Field(alias="_id") 
+    userEmail: str
+    userWhatsApp: str
+    companyId: str
+    language: str
+    name: str
+    personasStart: List[str]
+    personasResume: List[str]
+    personas: List[str]
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True  # To allow ObjectId
+        json_encoders = {ObjectId: str} 
+
+class CompanyModel(BaseModel):
+    id: Optional[ObjectId] = Field(alias="_id")
+    name: str
+    language: str
+    step: str
+    erp: List[str]
+    model: str
+    keyModel : str
+    personasStart: List[str]
+    personasResume: List[str]
+    personas: List[str]
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        
+class LogModel(BaseModel):
+    id: Optional[ObjectId] = Field(alias="_id")
+    timestamp: datetime.datetime
+    collection: str
+    operation: str
+    document: Optional[Dict[str, Any]]
+    old_document: Optional[Dict[str, Any]]
+    user: Optional[str]
+    company: Optional[str]
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
+class SessionItem(BaseModel):
+    context: Optional[Dict[str, Any]] = None
+    response: Optional[Dict[str, Any]] = None
+    vectors: Optional[Dict[str, Any]] = None
+
+class SessionModel(BaseModel):
+    userId: str
+    timestamp: datetime.datetime
+    content: List[SessionItem]
+
+class HistoryItem(BaseModel):
+    context: Optional[Dict[str, Any]] = None
+    response: Optional[Dict[str, Any]] = None
+    vectors: Optional[Dict[str, Any]] = None
+
+class HistoryModel(BaseModel):
+    userId: str
+    timestamp: datetime.datetime
+    content: List[HistoryItem]
+
+
