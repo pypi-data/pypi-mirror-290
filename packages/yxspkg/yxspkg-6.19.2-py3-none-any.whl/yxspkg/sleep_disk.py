00fcb79c66c126be896e4356c053e8f8d959e1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:02 +0000

    Add package yxspkg/6.19.2/yxspkg-6.19.2-py3-none-any.whl

---
+++ packages/yxspkg/yxspkg-6.19.2-py3-none-any.whl/yxspkg/sleep_disk.py
@@ -0,0 +1,46 @@
+import os,socket
+import time,subprocess
+import click
+import struct
+
+@click.command()
+@click.option('--disk','-d',default='sda,sdb')
+@click.option('--time_l','-t',default=10,help="停止活动指定时长后进入休眠 默认10分钟")
+@click.option('--sleep_cmd','-p',default='sudo hdparm')
+def main(disk,time_l,sleep_cmd):
+    ddold = dict()
+    nt = 0
+    disk = [i.strip() for i in disk.split(',')]
+    st = time.time()
+    ddold = {i:'none' for i in disk}
+    while True:
+   
+        for di in disk:
+            stat,newstate = subprocess.getstatusoutput(f'grep {di} /proc/diskstats')
+            oldstate = ddold[di]
+            if stat != 0:
+                continue
+            print(newstate)
+            if oldstate == newstate:
+                nt += 1
+                if nt == time_l:
+                    stat1,ott = subprocess.getstatusoutput(f'{sleep_cmd} -C /dev/{di}')
+
+                    if stat1 == 0 and ott.find('standby') == -1:
+                        os.system(f'{sleep_cmd} -y /dev/{di}')
+                        print(f'standby disk: {di}')
+            else:
+                nt = 0
+                ddold[di] = newstate 
+            time.sleep(0.1)
+        st += 60
+        tt = st - time.time()
+        if tt < 0:
+            tt = 60
+        tt = min(60,tt)
+        
+        print('sleep ',tt)
+        time.sleep(tt)
+
+if __name__=='__main__':
+    main()
