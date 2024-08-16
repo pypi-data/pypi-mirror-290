ea4e1a7298db48d22e453c9ff97faeab775f6054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:25 +0000

    Add package tbparse/0.0.9/tbparse-0.0.9.tar.gz

---
+++ packages/tbparse/tbparse-0.0.9.tar.gz/tbparse-0.0.9/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+github_url = "https://github.com/j3soon/tbparse"
+
+setuptools.setup(
+    name="tbparse",
+    version="0.0.9",
+    author="Johnson",
+    author_email="j3.soon777@gmail.com",
+    description="Load tensorboard event logs as pandas DataFrames; " + \
+        "Read, parse, and plot tensorboard event logs with ease!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=github_url,
+    project_urls={
+        "Changelog": f"{github_url}/blob/master/docs/pages/changelog.rst",
+        "Issues": f"{github_url}/issues",
+        "Source Code": github_url,
+    },
+    keywords=(
+        "package, parser, plot, python, pytorch, reader, tensorboard, tensorboardx, tensorflow"
+    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where=".", exclude=("tests*",)),
+    python_requires=">=3.8",
+    install_requires=[
+        "pandas>=1.3.0",
+        "tensorboard>=2.12.0",
+    ],
+    # The following error occurs if tensorboardX>2.6
+    #    ImportError: cannot import name 'builder' from 'google.protobuf.internal'
+    extras_require={
+        "testing": ["pytest", "mypy", "flake8", "pylint", "sphinx",
+                    "sphinx-rtd-theme", "torch", "tensorboardX==2.6", "seaborn",
+                    "soundfile", "pytest-cov", "sphinx-tabs", "nbsphinx"],
+    },
+)
+
+# Note: PyPI seems to only recognize double-quoted strings
---
+++ packages/tbparse/tbparse-0.0.9.tar.gz/tbparse-0.0.9/tbparse/version.py
@@ -0,0 +1,6 @@
+"""Contains the version string."""
+
+VERSION = "0.0.9"
+
+if __name__ == "__main__":
+    print(VERSION)
