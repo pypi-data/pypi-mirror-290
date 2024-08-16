625e1715b2ff050e5a5bded8af4b44861f6e6d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:06 +0000

    Add package pywwise/0.0.3/pywwise-0.0.3.tar.gz

---
+++ packages/pywwise/pywwise-0.0.3.tar.gz/pywwise-0.0.3/setup.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Matheus Vilano
+# SPDX-License-Identifier: Apache-2.0
+
+from setuptools import find_packages, setup
+from pathlib import Path
+
+
+def main():
+	setup()
+
+
+if __name__ == "__main__":
+	main()
