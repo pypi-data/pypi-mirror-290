f097f79a96bdf2872e48bfd5af1706933156f533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:09 +0000

    Add package jolescraper/0.1.0/jolescraper-0.1.0-py3-none-any.whl

---
+++ packages/jolescraper/jolescraper-0.1.0-py3-none-any.whl/jolescraper/__init__.py
@@ -0,0 +1,14 @@
+"""
+    Jole Scraper
+    ============
+
+    A simple web scraper made using BeautifulSoup and requests libraries.  
+
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Joel Crasta'
+
+from .JoleScraper import JoleScraper
+
+__all__ = ['JoleScraper']
