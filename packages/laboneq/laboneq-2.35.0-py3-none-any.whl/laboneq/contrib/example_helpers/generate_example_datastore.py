26653785e53840ec1f8feeb5402b2dfa6caeca98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:04:56 +0000

    Add package laboneq/2.35.0/laboneq-2.35.0-py3-none-any.whl

---
+++ packages/laboneq/laboneq-2.35.0-py3-none-any.whl/laboneq/contrib/example_helpers/generate_example_datastore.py
@@ -0,0 +1,326 @@
+# Copyright 2020 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+"""Function to generate and fill a datastore for use with the tutorials and how_to notebooks in the LabOne Q repository"""
+
+import datetime
+
+from laboneq.implementation.data_storage.laboneq_database import DataStore
+
+from .generate_device_setup import generate_device_setup_qubits
+
+
+def generate_example_datastore(
+    in_memory=False,
+    path="./laboneq_data/",
+    filename="dummy_datastore.db",
+    server_host="localhost",
+    include_qubits=True,
+) -> DataStore:
+    """A function to generate a DataStore for use with the public LabOne Q examples.
+
+    The generated Datastore is loaded with a selection of DeviceSetups and Qubits preconfigured to be used
+    with the public example notebooks.
+
+    Args:
+        in_memory: Whether to generate the Datastore in-memory instead of on the filesystem.
+        path: Path to the location of the Datastore on the filesystem. Only applies if `in_memory=False`.
+        filename: Filename of the generated Datastore on the filesystem. Only applies if `in_memory=False`.
+        server_host: IP address of the LabOne dataserver used to communicate to the instruments of the QCCS.
+            Defaults to "localhost".
+        include_qubits: Whether to include qubits directly in the DeviceSetup, through the `DeviceSetup.qubits` property.
+            If set to "False", no qubits will be generated.
+
+    Returns:
+        A LabOne Q Datastore loaded with a selection of device setups for use with the public LabOne Q examples.
+    """
+
+    # create (connection to) datastore
+    if in_memory:
+        setup_db = DataStore(":memory:")
+    else:
+        setup_db = DataStore(path + filename)
+
+    # specify your device ids here
+    pqsc = ["dev10001"]
+    hdawg = ["dev8001", "dev8002", "dev8003"]
+    shfqc = ["dev12003", "dev12004", "dev12005", "dev12006"]
+    shfsg = ["dev12001"]
+    shfqa = ["dev12002"]
+    uhfqa = ["dev2001"]
+    zsync = {
+        "dev8001": 1,
+        "dev8002": 2,
+        "dev12001": 3,
+        "dev12002": 4,
+        "dev12003": 5,
+        "dev12004": 6,
+        "dev12005": 7,
+        "dev12006": 8,
+    }
+    dio = {"dev8001": "dev2001"}
+
+    # generic 6 qubit device setup - including flux lines
+    num_qubits = 6
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        hdawg=hdawg[:1],
+        shfqc=shfqc[:1],
+        zsync=zsync,
+        number_multiplex=6,
+        include_flux_lines=True,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_tuneable_qubit_setup_shfqc_hdawg_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_tuneable_qubit_setup_shfqc_hdawg_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # large 24 qubit device setup - including flux lines
+    num_qubits = 24
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        hdawg=hdawg[:3],
+        shfqc=shfqc,
+        zsync=zsync,
+        number_multiplex=6,
+        include_flux_lines=True,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_tuneable_qubit_setup_shfqc_hdawg_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_tuneable_qubit_setup_shfqc_hdawg_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # generic 6 qubit device setup - without flux lines
+    num_qubits = 6
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        hdawg=hdawg[:1],
+        shfqc=shfqc[:1],
+        zsync=zsync,
+        number_multiplex=6,
+        include_flux_lines=False,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_fixed_qubit_setup_shfqc_hdawg_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_fixed_qubit_setup_shfqc_hdawg_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # shfqc standalone
+    num_qubits = 6
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        shfqc=shfqc[:1],
+        number_multiplex=6,
+        include_flux_lines=False,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_fixed_qubit_setup_shfqc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_fixed_qubit_setup_shfqc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # shfsg_shfqa_pqsc
+    num_qubits = 6
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        shfsg=shfsg,
+        shfqa=shfqa,
+        zsync=zsync,
+        number_multiplex=6,
+        include_flux_lines=False,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_fixed_qubit_setup_shfsg_shfqa_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_fixed_qubit_setup_shfsg_shfqa_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # shfsg_shfqa_hdawg_pqsc
+    num_qubits = 6
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        hdawg=hdawg[:1],
+        shfsg=shfsg,
+        shfqa=shfqa,
+        zsync=zsync,
+        number_multiplex=6,
+        include_flux_lines=True,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_tuneable_qubit_setup_shfsg_shfqa_hdawg_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_tuneable_qubit_setup_shfsg_shfqa_hdawg_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # shfsg_shfqa_shfqc_hdawg_pqsc
+    num_qubits = 12
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        hdawg=hdawg[:2],
+        shfsg=shfsg,
+        shfqa=shfqa,
+        shfqc=shfqc[:1],
+        zsync=zsync,
+        number_multiplex=6,
+        include_flux_lines=True,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_tuneable_qubit_setup_shfsg_shfqa_shfqc_hdawg_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_tuneable_qubit_setup_shfsg_shfqa_shfqc_hdawg_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # hdawg_uhfqa_pqsc
+    num_qubits = 2
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        pqsc=pqsc,
+        hdawg=hdawg[:1],
+        uhfqa=uhfqa,
+        zsync=zsync,
+        dio=dio,
+        number_multiplex=6,
+        include_flux_lines=False,
+        multiplex_drive_lines=False,
+        setup_name=f"{num_qubits}_fixed_qubit_setup_hdawg_uhfqa_pqsc",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_fixed_qubit_setup_hdawg_uhfqa_pqsc_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # shfsg standalone
+    num_qubits = 8
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        shfsg=shfsg,
+        drive_only=True,
+        number_multiplex=6,
+        include_flux_lines=False,
+        multiplex_drive_lines=True,
+        setup_name=f"{num_qubits}_fixed_qubit_setup_shfsg",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_fixed_qubit_setup_shfsg_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    # hdawg standalone
+    num_qubits = 4
+    device_setup, qubits = generate_device_setup_qubits(
+        number_qubits=num_qubits,
+        hdawg=hdawg[:1],
+        drive_only=True,
+        number_multiplex=6,
+        include_flux_lines=False,
+        multiplex_drive_lines=False,
+        setup_name=f"{num_qubits}_fixed_qubit_setup_hdawg",
+        server_host=server_host,
+        include_qubits=include_qubits,
+        calibrate_setup=True,
+    )
+    # store calibrated device_setup in datastore
+    setup_db.store(
+        data=device_setup,
+        metadata={
+            "name": f"{num_qubits}_fixed_qubit_setup_hdawg_calibrated",
+            "type": "device_setup_calibrated",
+            "creation_date": datetime.datetime.now(),
+        },
+    )
+
+    return setup_db
+
+
+def get_first_named_entry(db: DataStore, name: str):
+    """Return the first entry in a LabOne Q Datastore whose metadata contains the matching name"""
+    key = next(db.find(metadata={"name": name}))
+    return db.get(key)
---
+++ packages/laboneq/laboneq-2.35.0-py3-none-any.whl/laboneq/controller/devices/device_shf_base.py
@@ -0,0 +1,73 @@
+# Copyright 2022 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from enum import IntEnum
+
+from laboneq.controller.devices.device_zi import DeviceZI
+from laboneq.controller.devices.zi_node_monitor import (
+    Command,
+    Setting,
+    Condition,
+    NodeControlBase,
+    Response,
+    WaitCondition,
+)
+
+OPT_SHF_PLUS = "PLUS"
+
+
+class ReferenceClockSourceSHF(IntEnum):
+    INTERNAL = 0
+    EXTERNAL = 1
+    ZSYNC = 2
+
+
+class DeviceSHFBase(DeviceZI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._reference_clock_source = ReferenceClockSourceSHF.ZSYNC
+        self._is_plus: bool = False  # SHF+
+
+    def _process_shf_opts(self):
+        self._is_plus = OPT_SHF_PLUS in self.dev_opts
+
+    def update_clock_source(self, force_internal: bool | None):
+        if self.is_standalone() and force_internal is not False:
+            # Internal is the default (or explicit) for standalone SHF
+            self._reference_clock_source = ReferenceClockSourceSHF.INTERNAL
+        elif self.is_standalone() and force_internal is not True:
+            # External specified explicitly for standalone SHF
+            self._reference_clock_source = ReferenceClockSourceSHF.EXTERNAL
+        else:
+            # ZSync is the only possible source when device is not standalone
+            self._reference_clock_source = ReferenceClockSourceSHF.ZSYNC
+
+    def load_factory_preset_control_nodes(self) -> list[NodeControlBase]:
+        return [
+            Command(f"/{self.serial}/system/preset/load", 1),
+            Response(f"/{self.serial}/system/preset/busy", 0),
+            # TODO(2K): Remove once https://zhinst.atlassian.net/browse/HULK-1800 is resolved
+            WaitCondition(f"/{self.serial}/system/clocks/referenceclock/in/source", 0),
+            WaitCondition(f"/{self.serial}/system/clocks/referenceclock/in/status", 0),
+        ]
+
+    def clock_source_control_nodes(self) -> list[NodeControlBase]:
+        expected_freq = {
+            ReferenceClockSourceSHF.INTERNAL: None,
+            ReferenceClockSourceSHF.EXTERNAL: 10e6,
+            ReferenceClockSourceSHF.ZSYNC: 100e6,
+        }[self._reference_clock_source]
+        source = self._reference_clock_source.value
+
+        return [
+            Condition(
+                f"/{self.serial}/system/clocks/referenceclock/in/freq", expected_freq
+            ),
+            Condition(
+                f"/{self.serial}/system/clocks/referenceclock/in/sourceactual", source
+            ),
+            Setting(f"/{self.serial}/system/clocks/referenceclock/in/source", source),
+            Response(f"/{self.serial}/system/clocks/referenceclock/in/status", 0),
+        ]
