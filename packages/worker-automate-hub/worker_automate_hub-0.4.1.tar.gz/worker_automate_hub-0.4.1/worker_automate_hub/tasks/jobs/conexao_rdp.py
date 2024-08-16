7c723fa6843dcb13d44f44e404b04bde5ec9c9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:22 +0000

    Add package worker-automate-hub/0.4.1/worker_automate_hub-0.4.1.tar.gz

---
+++ packages/worker-automate-hub/worker_automate_hub-0.4.1.tar.gz/worker_automate_hub-0.4.1/worker_automate_hub/tasks/jobs/conexao_rdp.py
@@ -0,0 +1,38 @@
+import subprocess
+import pyautogui
+import asyncio
+from rich.console import Console
+from worker_automate_hub.utils.logger import logger
+
+console = Console()
+
+async def conexao_rdp(task):
+
+    try:
+        ip = task["configEntrada"]["ip"]
+        user = task["configEntrada"]["user"]
+        password = task["configEntrada"]["password"]
+
+        subprocess.Popen('mstsc')
+        await asyncio.sleep(2)
+
+        pyautogui.write(ip)
+        pyautogui.press('enter')
+        await asyncio.sleep(3)
+
+        pyautogui.write(user)
+        pyautogui.press('enter')
+        await asyncio.sleep(2)
+
+        pyautogui.write(password)
+        pyautogui.press('enter')
+
+        await asyncio.sleep(5)
+
+        return {"sucesso": True, "retorno": "Processo de conexão ao RDP executado com sucesso."}
+
+    except Exception as ex:
+        err_msg = f"Erro ao executar conexao_rdp: {ex}"
+        logger.error(err_msg)
+        console.print(err_msg, style="bold red")
+        return {"sucesso": False, "retorno": err_msg}
