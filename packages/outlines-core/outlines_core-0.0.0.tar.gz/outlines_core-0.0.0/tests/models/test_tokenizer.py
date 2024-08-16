7aea126268096317f74e622d84c256072eaecf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:04:56 +0000

    Add package outlines-core/0.0.0/outlines_core-0.0.0.tar.gz

---
+++ packages/outlines-core/outlines_core-0.0.0.tar.gz/outlines_core-0.0.0/tests/models/test_tokenizer.py
@@ -0,0 +1,8 @@
+import pytest
+
+from outlines_core.models.tokenizer import Tokenizer
+
+
+def test_tokenizer():
+    with pytest.raises(TypeError, match="instantiate abstract"):
+        Tokenizer()
