743dd5940e905789d3bb648375d5cfedeb8d44a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:19 +0000

    Add package object-case-switcher/0.0.5/object_case_switcher-0.0.5.tar.gz

---
+++ packages/object-case-switcher/object_case_switcher-0.0.5.tar.gz/object_case_switcher-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+NAME = "object_case_switcher"
+VERSION = "0.0.5"
+
+setup(
+    version=VERSION,
+    name=NAME,
+    description="The easiest way to switch case of prop in return of your functions",
+
+    long_description=open(os.path.join(os.path.dirname(__file__), './README.md')).read(),
+    long_description_content_type='text/markdown',
+    
+    packages=find_packages(),
+
+    keywords=["python", "switch", "switcher", "case", "snake", "camel"],
+
+    author="Aleksandrychev Andrey",
+    author_email="aleks-andr-19@yandex.ru",
+    url="https://github.com/Alex-Andr-19/case_switcher",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
