a82c79640cd3eb54570a6614542d2bc5698a787e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:05:02 +0000

    Add package fastapi-easystart/0.0.1/fastapi_easystart-0.0.1.tar.gz

---
+++ packages/fastapi-easystart/fastapi_easystart-0.0.1.tar.gz/fastapi_easystart-0.0.1/fastapi_easystart/utils/enums/base.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class BaseCustomEnum(str, Enum):
+
+    def __str__(self):
+        return self.value.title()
+
+    @property
+    def response_key(self):
+        # Replace single underscores with double underscores and convert to lowercase
+        return self.name.lower().replace("_", "__")
+
+    @property
+    def name_key(self):
+        # convert to lowercase
+        return self.name.lower()
