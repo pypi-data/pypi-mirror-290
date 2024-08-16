497ce219051889491d754bcf40930edb7a37b6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:11 +0000

    Add package cameo-big-query-log/0.1.15/cameo_big_query_log-0.1.15.tar.gz

---
+++ packages/cameo-big-query-log/cameo_big_query_log-0.1.15.tar.gz/cameo_big_query_log-0.1.15/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cameo_big_query_log',
+    version='0.1.15',
+    packages=find_packages(),
+    install_requires=[
+        'google-cloud-bigquery',
+        'cryptography',
+        'pydantic'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cameo_big_query_log=cameo_big_query_log.cameo_big_query_log:main',
+        ],
+    },
+    author='bear',
+    author_email='panda1993127@gmail.com',
+    description='A package for logging data to Google BigQuery',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
