694de2a6996c9fa78345ee74b5ef9b2ca09530c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:04:52 +0000

    Add package prefect/3.0.0rc18/prefect-3.0.0rc18.tar.gz

---
+++ packages/prefect/prefect-3.0.0rc18.tar.gz/prefect-3.0.0rc18/src/prefect/server/ui/assets/usePageTitle-CKKzQl14.js
@@ -0,0 +1,2 @@
+import{i as o,bA as a,n as c}from"./index-BvWlZEEM.js";function u(...t){const r=[...t,"Prefect Server"],n=o(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));a(()=>document.title=n.value)}export{u};
+//# sourceMappingURL=usePageTitle-CKKzQl14.js.map
