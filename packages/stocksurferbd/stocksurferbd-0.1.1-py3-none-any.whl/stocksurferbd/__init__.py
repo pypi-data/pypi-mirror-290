72defe882417fd7fc937cdd2399c50f13f857544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:06:13 +0000

    Add package stocksurferbd/0.1.1/stocksurferbd-0.1.1-py3-none-any.whl

---
+++ packages/stocksurferbd/stocksurferbd-0.1.1-py3-none-any.whl/stocksurferbd/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__author__ = "Sk Farhad"
+__copyright__ = "Copyright (c) 2021 The Python Packaging Authority"
+
+
+from .price_data_scraper import PriceData
+from .fundamental_data_scraper import FundamentalData
+from .price_plots import CandlestickPlot
