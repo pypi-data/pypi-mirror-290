ca4ecf37720518d62ac05559478e14f8f78fe190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 16:04:53 +0000

    Add package py-rtf-framework/1.1.6/py_rtf_framework-1.1.6-py3-none-any.whl

---
+++ packages/py-rtf-framework/py_rtf_framework-1.1.6-py3-none-any.whl/py_framework/data/shaper/data_shaper.py
@@ -0,0 +1,20 @@
+from pandas import DataFrame
+import json
+import os
+from py_framework.data.shaper.workflow import Workflow
+
+
+def run_data_shaper(work_flow_schema_path: str) -> DataFrame:
+    """加载配置文件中WorkFlow的schema"""
+
+    if not os.path.exists(work_flow_schema_path):
+        raise ValueError(work_flow_schema_path + '不存在')
+
+    with open(work_flow_schema_path, 'r', encoding='utf-8') as json_file:
+        workflow_schema = json.load(json_file)
+
+    work_flow = Workflow(schema=workflow_schema)
+
+    work_flow_result_df: DataFrame = work_flow.run_until_complete()
+
+    return work_flow_result_df
