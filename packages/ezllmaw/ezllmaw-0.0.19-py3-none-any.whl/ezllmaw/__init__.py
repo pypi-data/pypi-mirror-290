fa36049f7bc135760029bc2023b9ad61df1f801e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:09 +0000

    Add package ezllmaw/0.0.19/ezllmaw-0.0.19-py3-none-any.whl

---
+++ packages/ezllmaw/ezllmaw-0.0.19-py3-none-any.whl/ezllmaw/__init__.py
@@ -0,0 +1,5 @@
+from .parser import JsonParser
+from .agent import Agent, InputField, OutputField
+from .utils import settings, configure, context
+from .lm import OllamaLLM, GroqLLM
+from .program import Program
