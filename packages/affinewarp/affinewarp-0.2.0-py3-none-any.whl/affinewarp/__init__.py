77e0852c184035dc337490a8ff168b3be7e6da30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:13 +0000

    Add package affinewarp/0.2.0/affinewarp-0.2.0-py3-none-any.whl

---
+++ packages/affinewarp/affinewarp-0.2.0-py3-none-any.whl/affinewarp/__init__.py
@@ -0,0 +1,11 @@
+"""affinewarp - Time warping under affine warping functions"""
+
+__version__ = '0.2.0'
+__author__ = 'Alex Williams'
+
+import sys
+import warnings
+
+from .piecewisewarp import PiecewiseWarping
+from .shiftwarp import ShiftWarping
+from .spikedata import SpikeData
