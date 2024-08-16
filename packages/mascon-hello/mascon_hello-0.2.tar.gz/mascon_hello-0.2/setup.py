8ec0b6597fbecfaf3214bcfacc14978191ab57d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:05:11 +0000

    Add package mascon-hello/0.2/mascon_hello-0.2.tar.gz

---
+++ packages/mascon-hello/mascon_hello-0.2.tar.gz/mascon_hello-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "mascon_hello",
+    version = "0.2",
+    package = find_packages(),
+    install_requires = [
+        # numpy
+    ],
+)
