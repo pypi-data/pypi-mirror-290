969443de5414eb0a063dacb8e630cbec9dcd1cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:05:37 +0000

    Add package bclib/3.32.1/bclib-3.32.1.tar.gz

---
+++ packages/bclib/bclib-3.32.1.tar.gz/bclib-3.32.1/bclib/logger/ilogger.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from .log_object import LogObject
+
+class ILogger(ABC):
+    """Base class for logger"""
+
+    @abstractmethod
+    async def log_async(self, log_object: LogObject):
+        """log data async"""
+
+    def new_object_log(self, schema_name: str, **kwargs) -> LogObject:
+        """New object log"""
+        return LogObject(schema_name, **kwargs)
