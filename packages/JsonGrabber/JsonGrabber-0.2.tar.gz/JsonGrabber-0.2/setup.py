9060559ed87db44a1e24e24c7cf72c9279c0060e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:05:09 +0000

    Add package JsonGrabber/0.2/JsonGrabber-0.2.tar.gz

---
+++ packages/JsonGrabber/JsonGrabber-0.2.tar.gz/JsonGrabber-0.2/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='JsonGrabber',
+    version='0.2',
+    packages=find_packages(),
+    description='A utility library for extracting JSON from text strings',
+    author='YEVHRAFOV Artem',
+    author_email='all@power-display.com',
+    url='https://github.com/artyomevgrafov/JsonGrabber',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
