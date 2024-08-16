2a201d1565201083aaa30889d9e7f73eb6def02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:06:11 +0000

    Add package cymulate-oauth2-client/1.0.6/cymulate_oauth2_client-1.0.6.tar.gz

---
+++ packages/cymulate-oauth2-client/cymulate_oauth2_client-1.0.6.tar.gz/cymulate_oauth2_client-1.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cymulate_oauth2_client',
+    version='1.0.6',
+    description='A Python cymulate_oauth2_client for OAuth2 authentication with Cymulate API',
+    author='Cymulate',
+    author_email='roys@cymulate.com',
+    url='https://github.com/royscymulate/Cymulate-Oauth2-Client',
+    packages=find_packages(),
+    long_description=open("./README.md").read(),
+    install_requires=[
+        'requests>=2.20.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
