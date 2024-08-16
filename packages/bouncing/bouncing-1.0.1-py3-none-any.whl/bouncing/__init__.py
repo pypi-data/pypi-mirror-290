edd483964cf2692c11910fbeccf6c9da68556e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:20 +0000

    Add package bouncing/1.0.1/bouncing-1.0.1-py3-none-any.whl

---
+++ packages/bouncing/bouncing-1.0.1-py3-none-any.whl/bouncing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# @File    ：__init__.py.py
+# @Author  ：Jay
+# @Date    ：2024/8/16 13:39 
+# @Remark  ：
+from .register import identify as identifyHsv
+from .slider import discern as identifySlider
+
+__all__ = ["identifyHsv", "identifySlider"]
