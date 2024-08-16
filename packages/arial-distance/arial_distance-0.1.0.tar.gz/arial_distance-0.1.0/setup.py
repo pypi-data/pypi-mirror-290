5b74b81f91be3730c86452655712923b0cc64e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:05:05 +0000

    Add package arial-distance/0.1.0/arial_distance-0.1.0.tar.gz

---
+++ packages/arial-distance/arial_distance-0.1.0.tar.gz/arial_distance-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arial_distance',
+    version='0.1.0',
+    description='A library for calculating arial distance between two coordinates',
+    author='Sharad Ingle',
+    author_email='shrdingle@gmail.com',
+    url='https://github.com/Sharad2124Ingle/arial_distance',
+    packages=find_packages(),
+    install_requires=[
+        # list dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
