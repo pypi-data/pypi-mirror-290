b31285c6162a7e78ba1ae8ca44890a3954b88865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:04:56 +0000

    Add package ipulse-shared-base-ftredge/2.2.3/ipulse_shared_base_ftredge-2.2.3.tar.gz

---
+++ packages/ipulse-shared-base-ftredge/ipulse_shared_base_ftredge-2.2.3.tar.gz/ipulse_shared_base_ftredge-2.2.3/src/ipulse_shared_base_ftredge/logging/__init__.py
@@ -0,0 +1,1 @@
+from .utils_logging import get_logger, log_error, log_warning, log_info, log_debug
---
+++ packages/ipulse-shared-base-ftredge/ipulse_shared_base_ftredge-2.2.3.tar.gz/ipulse_shared_base_ftredge-2.2.3/src/ipulse_shared_base_ftredge/utils/utils_common.py
@@ -0,0 +1,6 @@
+
+
+    
+def list_as_strings(*enums):
+    """Converts a list of Enum members to their string values."""
+    return [str(enum) for enum in enums]
