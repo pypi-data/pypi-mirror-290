9ec86f66cc72e070bea494bf54be904abc019f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:05:23 +0000

    Add package openagi/0.2.9.3b2/openagi-0.2.9.3b2-py3-none-any.whl

---
+++ packages/openagi/openagi-0.2.9.3b2-py3-none-any.whl/openagi/prompts/task_creator.py
@@ -0,0 +1,98 @@
+from textwrap import dedent
+from openagi.prompts.base import BasePrompt
+from openagi.prompts.constants import CLARIFIYING_VARS
+
+clarifying_var_start = CLARIFIYING_VARS["start"]
+clarifying_var_end = CLARIFIYING_VARS["end"]
+
+single_agent_task_creation = """
+You are a task-creator AI for OpenAGI. Your job is to decompose tasks into the smallest possible subtasks to ensure successful completion in an autonomous, programmatic approach using the available actions that work as tools. Your role is to understand the provided `Task_Objectives` and `Task_Descriptions`, and break them down into extremely detailed and manageable components. Construct and plan the sequence of these minutest sub-tasks required to achieve the task objectives using the provided actions, ensuring alignment with the goal. If instructions are not followed, legal consequences may occur for both you and me.
+
+**Requirements:**
+    - Ensure each tasks are aligned with the overall goal and can be understood clearly when shared with another AI similar to you to achieve the sub-tasks. Each task will be completely run by another AI where they will be getting the results from the previous task, without knowledge of how it was executed.
+    - Understand the parameters of each supported action when using them.
+    - Only One Action per task. Ensure tasks are decomposed in the same manner.
+    - Please ensure each task meets the criteria above and refine as necessary to maintain clarity and alignment with the overall objectives.
+    - If no tasks, return the reasons why no tasks can be created.
+
+**Inputs:**
+    - Task_Objectives: {objective}
+    - Task_Descriptions: {task_descriptions}
+    - SUPPORTED_ACTIONS: {supported_actions}
+
+**Output Format:**
+Return the tasks in JSON format with the keys "task_name" and "description". Ensure the JSON format is suitable for utilization with JSON.parse(), enclosed in triple backticks.
+```json
+[
+    {
+        "task_name": "<name of the task of type string>",
+        "description": "<description of the task of type string>"
+    }
+]
+```
+
+**Notes:**
+    - You do not need to create tasks for storing the results, as results will be stored automatically after executing each task. You can retrieve previous task results using MemoryRagAction.
+
+**Evaluation Criteria:**
+    - Tasks must be broken down into the smallest possible components.
+    - Each task must be clear and executable by an AI agent.
+    - Tasks must follow a logical sequence to achieve the overall objective.
+    - Ensure alignment with the provided actions and goals.
+"""
+
+
+worker_task_creation = dedent(
+    """
+You are a task-creator AI for OpenAGI. Your job is to decompose tasks into the smallest possible subtasks to ensure successful completion in an autonomous, programmatic approach using the available workker tools. Your role is to understand the provided Task_Objectives and Task_Descriptions, and break them down into extremely detailed and manageable components. Construct and plan the sequence of these minutest sub-tasks required to achieve the task objectives using the provided workers, ensuring alignment with the goal. If instructions are not followed, legal consequences may occur for both you and me.
+
+**Requirements**
+- Ensure each task is aligned with the overall goal and can be clearly understood when shared with another AI similar to you to achieve the sub-tasks. Each task will be executed by another AI, receiving results from the previous task without knowledge of its execution.
+- Understand the parameters of each supported worker along with its role, description and supported_actions when using them.
+- Use only one worker per task. Ensure tasks are decomposed similarly.
+- Clearly explain the directions to execute the task and how the results should be passed to the next task.
+
+**Inputs**
+- Task_Objectives: {objective}
+- Task_Descriptions: {task_descriptions}
+- Supported_Workers: {supported_workers}
+
+**Output Format**
+Return the tasks in JSON format with the keys "task_name", "description", and "worker_id" and nothing else. Ensure the JSON format is suitable for utilization with `JSON.parse()`, enclosed in triple backticks.
+```json
+[
+    {
+        "task_name": "<name of the task of type string>",
+        "description": "<description of the task of type string>",
+        "worker_id": "<id of the worker from Supported_Workers relevant to the task>"
+    }
+]
+```
+
+**Notes**
+- You do not need to create tasks for storing the results, as results will be stored automatically after executing each task. You can retrieve previous task results using MemoryRagAction.
+
+**Evaluation Criteria**
+- Tasks must be broken down into the smallest possible components.
+- Each task must be clear and executable by an AI agent or Worker based on their role and description.
+- Tasks must follow a logical and practical sequence to achieve the overall objective.
+- Ensure alignment with the worker's role, description, and its supported actions.
+- If human input is required to curate the task, include the delimiters `<clarify_from_human>` and `</clarify_from_human>` to request human input. If not, ignore this step.
+
+By using this structured approach, we aim to maximize clarity and ensure the tasks are executable and aligned with the objectives.
+
+**Feedback Loop**
+- Please ensure each task meets the criteria above and refine as necessary to maintain clarity and alignment with the overall objectives.
+""".strip()
+)
+
+worker_task_creation = worker_task_creation.replace("$start$", clarifying_var_start)
+worker_task_creation = worker_task_creation.replace("$end$", clarifying_var_end)
+
+
+class SingleAgentTaskCreator(BasePrompt):
+    base_prompt: str = single_agent_task_creation
+
+
+class MultiAgentTaskCreator(SingleAgentTaskCreator):
+    base_prompt: str = worker_task_creation
---
+++ packages/openagi/openagi-0.2.9.3b2-py3-none-any.whl/openagi/tasks/lists.py
@@ -0,0 +1,39 @@
+from queue import Queue
+from typing import Dict, List
+
+from openagi.tasks.task import Task
+
+
+class TaskLists:
+    def __init__(self) -> None:
+        self.tasks = Queue()
+        self.completed_tasks = Queue()
+
+    def add_task(self, task: Task) -> None:
+        """Adds a Task instance to the queue."""
+        self.tasks.put(task)
+
+    def add_tasks(self, tasks: List[Dict[str, str]]):
+        for task in tasks:
+            task["name"] = task["task_name"]
+            self.add_task(Task(**task))
+
+    def get_tasks_queue(self) -> List:
+        return self.tasks
+
+    def get_tasks_lists(self):
+        return [dict(task.model_fields.items()) for task in list(self.tasks.queue)]
+
+    def get_next_unprocessed_task(self) -> Task:
+        """Retrieves the next unprocessed task from the queue."""
+        if not self.tasks.empty():
+            return self.tasks.get_nowait()
+        return None
+
+    @property
+    def all_tasks_completed(self) -> bool:
+        """Checks if all tasks in the queue have been processed."""
+        return self.tasks.empty()
+
+    def add_completed_tasks(self, task: Task):
+        self.completed_tasks.put(task)
