6761eae31939d7381a0e334ee36301e5ee9a6444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:08 +0000

    Add package stocksurferbd/0.1.2a0/stocksurferbd-0.1.2a0.tar.gz

---
+++ packages/stocksurferbd/stocksurferbd-0.1.2a0.tar.gz/stocksurferbd-0.1.2a0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stocksurferbd",
+    version="0.1.2a",
+    author="Sk Farhad",
+    author_email="sk.farhad.eee@gmail.com",
+    description="This is a tool to download stock market data of Dhaka Stock Exchange.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skfarhad/stocksurferbd",
+    project_urls={
+        "Bug Tracker": "https://github.com/skfarhad/stocksurferbd/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests==2.25.1',
+        'pandas==2.2.2',
+        'openpyxl==3.1.5',
+        'beautifulsoup4==4.9.3',
+        'matplotlib==3.9.2',
+        'mplfinance==0.12.7a17',
+        'pyti==0.2.2',
+        'tapy==1.9.1',
+    ],
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="dse_data_loader"),
+    packages=['stocksurferbd'],
+    python_requires=">=3.10",
+    zip_safe=False
+)
+
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
