1543a70a2199e624109c1cb2a541859b60c49997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:04:47 +0000

    Add package restgfdl/1.0.3/restgfdl-1.0.3.tar.gz

---
+++ packages/restgfdl/restgfdl-1.0.3.tar.gz/restgfdl-1.0.3/restgfdl/GetInstruments.py
@@ -0,0 +1,15 @@
+from .base_api import BaseAPI
+
+class GetInstruments(BaseAPI):
+    
+    
+    def Get_Instruments(self, exchange, product=None, instrument_type=None):
+        endpoint = "GetInstruments/"
+        params = {
+            'exchange': exchange
+        }
+        if product:
+            params['product'] = product
+        if instrument_type:
+            params['instrumentType'] = instrument_type
+        return self._get(endpoint, params) 
