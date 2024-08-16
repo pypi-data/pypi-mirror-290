713c56e4b0b8b4cf4f244406bcef1296c0e79c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 04:04:47 +0000

    Add package logger-json-file/0.1/logger_json_file-0.1.tar.gz

---
+++ packages/logger-json-file/logger_json_file-0.1.tar.gz/logger_json_file-0.1/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="logger_json_file",
+    version="0.1",
+    author="",
+    author_email="",
+    description="A simple logging utility for saving logs as JSON files.",
+    long_description_content_type="text/markdown",
+    url="",  
+    packages=find_packages(),
+  
+    python_requires=">=3.6",
+)
