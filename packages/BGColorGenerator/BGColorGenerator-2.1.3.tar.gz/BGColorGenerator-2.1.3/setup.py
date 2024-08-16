e27ebe114f5a90ce2ce0d66ab0c15c2cc572d291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:31 +0000

    Add package BGColorGenerator/2.1.3/BGColorGenerator-2.1.3.tar.gz

---
+++ packages/BGColorGenerator/BGColorGenerator-2.1.3.tar.gz/BGColorGenerator-2.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BGColorGenerator',  
+    version='2.1.3',
+    packages=['BGColorGenerator', 'BGColorGenerator.utils'],  # Encuentra automáticamente todos los paquetes
+    include_package_data=True,
+    install_requires=[
+        'tensorflow>=2.0.0',
+        'matplotlib',
+        'pandas',
+        'numpy'
+    ],
+    package_data={
+        'BGColorGenerator': ['**/*'],
+    },
+    description='A package for predicting the background color based on text color using a trained neural network.',
+    long_description=open('README_updated.md').read(),
+    long_description_content_type='text/markdown',
+    author='Joaquín Francisco Solórzano Corea',
+    author_email='joaquinscorea@gmail.com',
+    url='https://github.com/joaquinsc999/BGColorGenerator',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
