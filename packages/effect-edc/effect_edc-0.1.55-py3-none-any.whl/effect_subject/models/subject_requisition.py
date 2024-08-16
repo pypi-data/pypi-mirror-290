d9949362a3eb95ae2683a3ddb2b5d0a5707e216f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 16:04:39 +0000

    Add package effect-edc/0.1.55/effect_edc-0.1.55-py3-none-any.whl

---
+++ packages/effect-edc/effect_edc-0.1.55-py3-none-any.whl/effect_subject/models/subject_requisition.py
@@ -0,0 +1,8 @@
+from edc_lab.managers import RequisitionManager as Manager  # noqa
+from edc_lab.model_mixins import RequisitionModelMixin
+from edc_model.models import BaseUuidModel
+
+
+class SubjectRequisition(RequisitionModelMixin, BaseUuidModel):
+    class Meta(RequisitionModelMixin.Meta, BaseUuidModel.Meta):
+        pass
