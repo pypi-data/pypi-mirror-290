88d5cca6bb921d781942c0054fa8676d3bb0be7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 04:04:49 +0000

    Add package achatbot/0.0.5.1/achatbot-0.0.5.1.tar.gz

---
+++ packages/achatbot/achatbot-0.0.5.1.tar.gz/achatbot-0.0.5.1/pypi_build/app/achatbot/modules/speech/asr/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from achatbot.common.types import MODELS_DIR
+from achatbot.common import interface
+from achatbot.common.factory import EngineClass, EngineFactory
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+
+class ASREnvInit():
+
+    @staticmethod
+    def initASREngine() -> interface.IAsr | EngineClass:
+        from . import whisper_asr
+        from . import sense_voice_asr
+        from . import whisper_groq_asr
+        # asr
+        tag = os.getenv('ASR_TAG', "whisper_timestamped_asr")
+        kwargs = {}
+        kwargs["model_name_or_path"] = os.getenv(
+            'ASR_MODEL_NAME_OR_PATH', 'base')
+        kwargs["download_path"] = MODELS_DIR
+        kwargs["verbose"] = bool(os.getenv('ASR_VERBOSE', 'True'))
+        kwargs["language"] = os.getenv('ASR_LANG', 'zh')
+        engine = EngineFactory.get_engine_by_tag(EngineClass, tag, **kwargs)
+        logging.info(f"initASREngine: {tag}, {engine}")
+        return engine
