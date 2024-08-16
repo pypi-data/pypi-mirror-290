5e31587f41d3364389ab8dfc6445ca41fa496bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:03 +0000

    Add package MaixPy/4.4.22/MaixPy-4.4.22-cp312-cp312-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/MaixPy-4.4.22-cp312-cp312-manylinux2014_x86_64.whl/maix/_maix/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+{'breif': 'MaixPy C/C++ API from MaixCDK'}
+"""
+from __future__ import annotations
+from . import app
+from . import audio
+from . import camera
+from . import comm
+from . import display
+from . import err
+from . import example
+from . import fs
+from . import http
+from . import i18n
+from . import image
+from . import network
+from . import nn
+from . import peripheral
+from . import protocol
+from . import rtmp
+from . import rtsp
+from . import sys
+from . import tensor
+from . import thread
+from . import time
+from . import touchscreen
+from . import util
+from . import video
+__all__ = ['app', 'audio', 'camera', 'comm', 'display', 'err', 'example', 'fs', 'http', 'i18n', 'image', 'network', 'nn', 'peripheral', 'protocol', 'rtmp', 'rtsp', 'sys', 'tensor', 'thread', 'time', 'touchscreen', 'util', 'video']
