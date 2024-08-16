2b4a365e15b6b6b2d837aad3e9a72ccafcbdee62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:05:07 +0000

    Add package mns-scheduler/1.1.4.1/mns_scheduler-1.1.4.1-py3-none-any.whl

---
+++ packages/mns-scheduler/mns_scheduler-1.1.4.1-py3-none-any.whl/mns_scheduler/common/redis/redis_msg_push_service.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 16
+project_path = file_path[0:end]
+sys.path.append(project_path)
+import redis
+import mns_common.constant.redis_msg_constant as redis_msg_constant
+
+# 连接到Redis服务器
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+# 发送消息
+def send_redis_msg(message):
+    # 将消息推送到队列中
+    r.publish(redis_msg_constant.REDIS_MSG_TOPIC, message)
