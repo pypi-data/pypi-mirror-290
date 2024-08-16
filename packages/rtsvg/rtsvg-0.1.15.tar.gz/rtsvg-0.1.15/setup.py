9694f9b4aa3ab57ca18984319a982b9807f9b735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 16:04:35 +0000

    Add package rtsvg/0.1.15/rtsvg-0.1.15.tar.gz

---
+++ packages/rtsvg/rtsvg-0.1.15.tar.gz/rtsvg-0.1.15/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+name='rtsvg',
+version='0.1.15',
+author='D. Trimm',
+author_email='dave.trimm@gmail.com',
+description='Visualization Package Leveraging SVG in Jupyter Notebooks',
+packages=['rtsvg'],
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: Apache Software License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.11',
+url='https://github.com/datrcode/racetrack_svg_framework'
+)
+
