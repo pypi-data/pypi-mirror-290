4453450d2be4d3eba88094f764f70d2b120548bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 04:04:48 +0000

    Add package mixiu-app-helper/0.1.9/mixiu_app_helper-0.1.9-py3-none-any.whl

---
+++ packages/mixiu-app-helper/mixiu_app_helper-0.1.9-py3-none-any.whl/mixiu_app_helper/info.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  mixiu-app-helper
+# FileName:     info.py
+# Description:  TODO
+# Author:       zhouhanlin
+# CreateDate:   2024/07/16
+# Copyright ©2011-2024. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
+from enum import Enum
+
+
+class Constant(Enum):
+    ANDROID_APP_NAME = "com.mixiu.com"
+    iOS_APP_NAME = "iOS"
