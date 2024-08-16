d7574e40a0fce0ffce3fe44d28a7f6f834be8c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:04:58 +0000

    Add package swing-sitemap/0.0.0/swing_sitemap-0.0.0.tar.gz

---
+++ packages/swing-sitemap/swing_sitemap-0.0.0.tar.gz/swing_sitemap-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="swing_sitemap",
+    version="0.0.0",
+    author="Swing Collection",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/swing-collection/swing-sitemap",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
