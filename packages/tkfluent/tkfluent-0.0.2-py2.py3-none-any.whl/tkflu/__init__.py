a51c2e81210eb50ce4575067ee405a247afc9f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 03:04:57 +0000

    Add package tkfluent/0.0.2/tkfluent-0.0.2-py2.py3-none-any.whl

---
+++ packages/tkfluent/tkfluent-0.0.2-py2.py3-none-any.whl/tkflu/__init__.py
@@ -0,0 +1,26 @@
+"""、
+
+Fluent设计的tkinter组件库（模板）
+
+-------------
+作者：XiangQinxi
+-------------
+"""
+
+from .badge import FluBadge
+from .button import FluButton
+from .entry import FluEntry
+from .frame import FluFrame
+from .label import FluLabel
+from .text import FluText
+from .thememanager import FluThemeManager
+from .togglebutton import FluToggleButton
+from .window import FluWindow
+
+FluChip = FluBadge
+FluPushButton = FluButton
+FluTextInput = FluEntry
+FluTextBox = FluText
+FluPanel = FluFrame
+
+# 
---
+++ packages/tkfluent/tkfluent-0.0.2-py2.py3-none-any.whl/tkflu/__main__.py
@@ -0,0 +1,50 @@
+from tkflu import *
+from tkinter import *
+from tkinter.font import *
+
+root = FluWindow()
+root.iconify
+root.wincustom(way=0)
+root.wm_geometry("180x360")
+
+thememanager = FluThemeManager()
+
+frame = FluFrame()
+
+badge1 = FluBadge(frame, text="FluBadge", width=60)
+badge1.pack(padx=5, pady=5)
+
+badge2 = FluBadge(frame, text="FluBadge (Accent)", width=120, style="accent")
+badge2.pack(padx=5, pady=5)
+
+button1 = FluButton(
+    frame, text="FluButton", command=lambda: print("FluButton -> Clicked")
+)
+button1.pack(fill="x", padx=5, pady=5)
+
+button2 = FluButton(
+    frame, text="FluButton (Accent)", command=lambda: print("FluButton (Accent) -> Clicked"), style="accent"
+)
+button2.pack(fill="x", padx=5, pady=5)
+
+def toggle1():
+    print(f"FluToggleButton -> Toggled -> Checked: {togglebutton1.dcget('checked')}")
+    if togglebutton1.dcget('checked'):
+        thememanager.mode("dark")
+    else:
+        thememanager.mode("light")
+
+togglebutton1 = FluToggleButton(
+    frame, text="FluToggleButton", command=toggle1
+)
+togglebutton1.pack(fill="x", padx=5, pady=5)
+
+entry1 = FluEntry(frame)
+entry1.pack(fill="x", padx=5, pady=5)
+
+text1 = FluText(frame)
+text1.pack(fill="x", padx=5, pady=5)
+
+frame.pack(fill="both", expand="yes", side="right", padx=5, pady=5)
+
+root.mainloop()
