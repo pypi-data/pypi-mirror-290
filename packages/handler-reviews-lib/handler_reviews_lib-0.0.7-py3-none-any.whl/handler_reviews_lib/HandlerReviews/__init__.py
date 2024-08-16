c8e49340a2736a55a00a5d4dc6996683fe3cf714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 16:04:56 +0000

    Add package handler-reviews-lib/0.0.7/handler_reviews_lib-0.0.7-py3-none-any.whl

---
+++ packages/handler-reviews-lib/handler_reviews_lib-0.0.7-py3-none-any.whl/handler_reviews_lib/HandlerReviews/__init__.py
@@ -0,0 +1,12 @@
+from .enums_type import TypeModelChatGpt , HandlerType
+from .gpt_reviews_handler import GPTReviewsHandler
+from .handler_reviews_provider import HandlerReviewsProvider
+from .template_reviews_handler import TemplateReviewsHandler
+
+__all__ = [
+    'TypeModelChatGpt',
+    'HandlerType',
+    'TemplateReviewsHandler',
+    'GPTReviewsHandler',
+    'HandlerReviewsProvider'
+]
---
+++ packages/handler-reviews-lib/handler_reviews_lib-0.0.7-py3-none-any.whl/handler_reviews_lib/Interfaces/__init__.py
@@ -0,0 +1,7 @@
+from .interface_handler_reviews import IHandlerReviews
+from .interface_reviews_handler_provider import IHandlerProvider
+
+__all__ = [
+    'IHandlerReviews',
+    'IHandlerProvider'
+]
