6bb37fe3a3a4a5f70b06e3f807b6ea3b13598d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:05:23 +0000

    Add package viessmann-gridbox-connector/1.5.2/viessmann_gridbox_connector-1.5.2.tar.gz

---
+++ packages/viessmann-gridbox-connector/viessmann_gridbox_connector-1.5.2.tar.gz/viessmann_gridbox_connector-1.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viessmann-gridbox-connector',
+    version='1.5.2',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'authlib'
+    ],
+    package_data={
+        'viessmann_gridbox_connector': ['config.json'],
+    },
+    entry_points={
+        'console_scripts': [
+            'viessmann=viessmann_gridbox_connector.cli:main',
+        ],
+    },
+)
