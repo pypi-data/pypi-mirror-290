b371b5ed1ef4371c5c137f90a4ab18f1e76dc4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:05:08 +0000

    Add package ai-parrot/0.2.3/ai_parrot-0.2.3.tar.gz

---
+++ packages/ai-parrot/ai_parrot-0.2.3.tar.gz/ai_parrot-0.2.3/parrot/llms/openai.py
@@ -0,0 +1,49 @@
+from langchain_openai import (
+    OpenAI,
+    ChatOpenAI,
+    OpenAIEmbeddings
+)
+from navconfig import config
+from .abstract import AbstractLLM
+
+
+class OpenAILLM(AbstractLLM):
+    """OpenAI.
+    Interact with OpenAI Language Model.
+
+    Returns:
+        _type_: an instance of OpenAI LLM Model.
+    """
+    model: str = "gpt-4-turbo"
+    embed_model: str = "text-embedding-3-large"
+    max_tokens: int = 1024
+
+    def __init__(self, *args, **kwargs):
+        self.model_type = kwargs.get("model_type", "text")
+        super().__init__(*args, **kwargs)
+        self.model = kwargs.get("model", "davinci")
+        self._api_key = kwargs.pop('api_key', config.get('OPENAI_API_KEY'))
+        organization = config.get("OPENAI_ORGANIZATION")
+        if self.model_type == 'chat':
+            base_llm = ChatOpenAI
+        else:
+            base_llm = OpenAI
+        self._llm = base_llm(
+            model_name=self.model,
+            api_key=self._api_key,
+            organization=organization,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            **self.args
+        )
+        # Embedding
+        embed_model = kwargs.get("embed_model", "text-embedding-3-large")
+        self._embed = OpenAIEmbeddings(
+            model=embed_model,
+            dimensions=self.max_tokens,
+            api_key=self._api_key,
+            organization=organization,
+            temperature=self.temperature,
+            top_p=self.top_p,
+            top_k=self.top_k,
+        )
