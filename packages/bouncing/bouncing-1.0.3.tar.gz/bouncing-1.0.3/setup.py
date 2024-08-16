dbb3498d845f778fb2a890e2316951f6b70bf0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 18:04:51 +0000

    Add package bouncing/1.0.3/bouncing-1.0.3.tar.gz

---
+++ packages/bouncing/bouncing-1.0.3.tar.gz/bouncing-1.0.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# @File    ：setup.py
+# @Author  ：Jay
+# @Date    ：2024/6/21 18:41 
+# @Remark  ：
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bouncing',
+    version='1.0.3',
+    author='Jay',
+    description='Verification code image recognition!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jaygarage/bouncing',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'xxhash',
+        'cachetools',
+        'numpy',
+        'scikit-image',
+        'requests',
+        'fake_useragent',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7'
+)
