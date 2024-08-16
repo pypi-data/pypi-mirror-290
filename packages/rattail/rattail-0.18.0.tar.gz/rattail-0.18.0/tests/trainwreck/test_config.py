a2ef875672ab0b4a7c642e3f565a91f1737fa629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:13 +0000

    Add package rattail/0.18.0/rattail-0.18.0.tar.gz

---
+++ packages/rattail/rattail-0.18.0.tar.gz/rattail-0.18.0/tests/trainwreck/test_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8; -*-
+
+from rattail.trainwreck import config as mod
+from rattail.testing import DataTestCase
+
+
+class TestTrainwreckConfig(DataTestCase):
+
+    def test_configure(self):
+        self.assertFalse(hasattr(self.config, 'trainwreck_engines'))
+        ext = mod.TrainwreckConfig()
+        ext.configure(self.config)
+        self.assertEqual(self.config.trainwreck_engines, {})
