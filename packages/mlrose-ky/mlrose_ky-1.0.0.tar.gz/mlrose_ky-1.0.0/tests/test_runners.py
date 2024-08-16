e8155a6c5598f0beda7e3ba0db60975e6e47f7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:05:20 +0000

    Add package mlrose-ky/1.0.0/mlrose_ky-1.0.0.tar.gz

---
+++ packages/mlrose-ky/mlrose_ky-1.0.0.tar.gz/mlrose_ky-1.0.0/tests/test_runners.py
@@ -0,0 +1,14 @@
+"""Unit tests for runners/"""
+
+# Author: Kyle Nakamura
+# License: BSD 3 clause
+
+# import numpy as np
+
+try:
+    import mlrose_ky
+except ImportError:
+    import sys
+
+    sys.path.append("..")
+    import mlrose_ky
