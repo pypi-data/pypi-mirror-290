5ac670a12bfea987a2d7ac2e1673799f6b822eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 15:05:13 +0000

    Add package dc-converter/0.1.8/dc_converter-0.1.8-cp310-cp310-manylinux_2_34_x86_64.whl

---
+++ packages/dc-converter/dc_converter-0.1.8-cp310-cp310-manylinux_2_34_x86_64.whl/dc_converter/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+from dc_converter.convert import (
+    write_color_images,
+    write_multi_view,
+    convert_file_inplace,
+    convert_to_folder,
+    convert_depth_file_target,
+)
