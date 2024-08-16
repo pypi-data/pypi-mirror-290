f2df21d3498228739cc6c7140a38f2b61fb74f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 14:05:21 +0000

    Add package swarms-cloud/0.3.2/swarms_cloud-0.3.2-py3-none-any.whl

---
+++ packages/swarms-cloud/swarms_cloud-0.3.2-py3-none-any.whl/swarms_cloud/schema/__init__.py
@@ -0,0 +1,14 @@
+from agent_api_schemas import (
+    AgentInput,
+    ModelSchema,
+    ModelList,
+    AgentOutput,
+)
+
+
+__all__ = [
+    "AgentInput",
+    "ModelSchema",
+    "ModelList",
+    "AgentOutput",
+]
