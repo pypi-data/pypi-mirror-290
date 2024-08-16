a66d386ad495d108b84cc4536172f4d893434a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-16 17:04:53 +0000

    Add package searchbot/1.0.5/searchbot-1.0.5.tar.gz

---
+++ packages/searchbot/searchbot-1.0.5.tar.gz/searchbot-1.0.5/src/searchbot/rag.py
@@ -0,0 +1,34 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_ollama import OllamaLLM
+from . import bot
+from . import retriever
+import template
+
+
+def search(query, bot_name='baidu', lang='chinese', verbose=False, llm=OllamaLLM(model='llama3.1', temperature=0.3)):
+    # Set the language template
+    if lang == 'chinese':
+        prompt_template = template.chinese
+    else:
+        prompt_template = template.english
+
+    # Create the prompt
+    prompt = ChatPromptTemplate.from_template(prompt_template)
+
+    # Create the retriever
+    search_bot = bot.make_bot(bot_name, verbose=verbose)  # Default to Baidu bot
+    r = retriever.SearchBotRetriever(bot=search_bot)
+
+    # Construct the RAG chain
+    rag_chain = (
+            {"context": r, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+    )
+
+    # Invoke the RAG chain with the query
+    answer = rag_chain.invoke(query)
+    return answer
