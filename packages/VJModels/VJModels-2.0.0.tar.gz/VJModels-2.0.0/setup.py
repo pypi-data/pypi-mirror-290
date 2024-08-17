87a41e2490f934cbd1f672521104bdd9f81ac5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:17 +0000

    Add package VJModels/2.0.0/VJModels-2.0.0.tar.gz

---
+++ packages/VJModels/VJModels-2.0.0.tar.gz/VJModels-2.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name='VJModels',
+  packages=find_packages(),  
+  version='2.0.0',
+  license='MIT',  
+  description='My personal machine learning models',
+  long_description=long_description,
+  long_description_content_type="text/markdown", 
+  author='Vanderval Borges de Souza Junior',  
+  author_email='vander31bs@gmail.com',  
+  url='https://github.com/Vanderval31bs/VJModels',  
+  download_url='https://github.com/Vanderval31bs/VJModels/archive/refs/tags/v2.0.0-alpha.tar.gz',  
+  keywords=['MachineLearning', 'Models', 'Forests'],
+  install_requires=[
+      'pandas',
+      'scikit-learn',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',  
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.11', 
+  ],
+)
