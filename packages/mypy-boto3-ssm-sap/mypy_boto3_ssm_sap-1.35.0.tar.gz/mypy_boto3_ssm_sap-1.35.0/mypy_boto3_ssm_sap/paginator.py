68059f3c4b734cdfdc6ffe26a47d40f3979349aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:11 +0000

    Add package mypy-boto3-ssm-sap/1.35.0/mypy_boto3_ssm_sap-1.35.0.tar.gz

---
+++ packages/mypy-boto3-ssm-sap/mypy_boto3_ssm_sap-1.35.0.tar.gz/mypy_boto3_ssm_sap-1.35.0/mypy_boto3_ssm_sap/paginator.py
@@ -0,0 +1,150 @@
+"""
+Type annotations for ssm-sap service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_ssm_sap.client import SsmSapClient
+    from mypy_boto3_ssm_sap.paginator import (
+        ListApplicationsPaginator,
+        ListComponentsPaginator,
+        ListDatabasesPaginator,
+        ListOperationEventsPaginator,
+        ListOperationsPaginator,
+    )
+
+    session = Session()
+    client: SsmSapClient = session.client("ssm-sap")
+
+    list_applications_paginator: ListApplicationsPaginator = client.get_paginator("list_applications")
+    list_components_paginator: ListComponentsPaginator = client.get_paginator("list_components")
+    list_databases_paginator: ListDatabasesPaginator = client.get_paginator("list_databases")
+    list_operation_events_paginator: ListOperationEventsPaginator = client.get_paginator("list_operation_events")
+    list_operations_paginator: ListOperationsPaginator = client.get_paginator("list_operations")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    FilterTypeDef,
+    ListApplicationsOutputTypeDef,
+    ListComponentsOutputTypeDef,
+    ListDatabasesOutputTypeDef,
+    ListOperationEventsOutputTypeDef,
+    ListOperationsOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "ListApplicationsPaginator",
+    "ListComponentsPaginator",
+    "ListDatabasesPaginator",
+    "ListOperationEventsPaginator",
+    "ListOperationsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListApplicationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListApplications)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listapplicationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        Filters: Sequence[FilterTypeDef] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListApplicationsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListApplications.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listapplicationspaginator)
+        """
+
+
+class ListComponentsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListComponents)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listcomponentspaginator)
+    """
+
+    def paginate(
+        self, *, ApplicationId: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListComponentsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListComponents.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listcomponentspaginator)
+        """
+
+
+class ListDatabasesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListDatabases)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listdatabasespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ApplicationId: str = ...,
+        ComponentId: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListDatabasesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListDatabases.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listdatabasespaginator)
+        """
+
+
+class ListOperationEventsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListOperationEvents)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listoperationeventspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        OperationId: str,
+        Filters: Sequence[FilterTypeDef] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListOperationEventsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListOperationEvents.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listoperationeventspaginator)
+        """
+
+
+class ListOperationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListOperations)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listoperationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ApplicationId: str,
+        Filters: Sequence[FilterTypeDef] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListOperationsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-sap.html#SsmSap.Paginator.ListOperations.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ssm_sap/paginators/#listoperationspaginator)
+        """
