54c0724d3718dbd13ce671386ff583a212a3f7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:04:50 +0000

    Add package nekobox/0.1.2/nekobox-0.1.2.tar.gz

---
+++ packages/nekobox/nekobox-0.1.2.tar.gz/nekobox-0.1.2/src/nekobox/apis/utils.py
@@ -0,0 +1,16 @@
+from satori import Api
+from lagrange.client.client import Client
+from satori.server import Adapter, Request
+
+from .types import API_HANDLER
+
+
+def register_api(
+    api: Api,
+    adapter: Adapter,
+    client: Client,
+    handler: API_HANDLER,
+):
+    @adapter.route(api)
+    async def handler_wrapper(request: Request):
+        return await handler(client, request)
