ab4460564e3f937467110a5ca6f85aff3b198d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:10 +0000

    Add package tetgen/0.6.5/tetgen-0.6.5.tar.gz

---
+++ packages/tetgen/tetgen-0.6.5.tar.gz/tetgen-0.6.5/tetgen/cython/tetgen/tetgen_wrap.h
@@ -0,0 +1,21 @@
+#include "tetgen.h"
+
+
+// Adds additional functionality to original tetgen object
+class tetgenio_wrap : public tetgenio
+{
+    public:
+        //constructor
+        tetgenio_wrap();
+
+        facet *f;
+        polygon *p;
+        void LoadArray(int, double*, int, int*);
+        void LoadMTRArray(int, double*, int, int*, double*);
+        bool LoadTetMesh(char*, int);
+
+        //destructor
+//        ~myRectangle();
+
+};
+
