7a546a5f33585665f99c37e597e1f620637d18ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:54 +0000

    Add package DataSolutions/0.0.14/datasolutions-0.0.14.tar.gz

---
+++ packages/DataSolutions/datasolutions-0.0.14.tar.gz/datasolutions-0.0.14/src/chainalysis_data_solutions/utils/utils.py
@@ -0,0 +1,11 @@
+from chainalysis_data_solutions.utils.stringify import Stringify
+
+
+class Utils:
+    """
+    This class contains util/helper functions for users
+    of the SDK.
+    """
+
+    def __init__(self):
+        self.stringify = Stringify()
