390149a5ae98c05811ef025c0bb199e73230905d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:51 +0000

    Add package mypy-boto3-cloudfront-keyvaluestore/1.35.0/mypy_boto3_cloudfront_keyvaluestore-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-cloudfront-keyvaluestore/mypy_boto3_cloudfront_keyvaluestore-1.35.0-py3-none-any.whl/mypy_boto3_cloudfront_keyvaluestore/client.py
@@ -0,0 +1,168 @@
+"""
+Type annotations for cloudfront-keyvaluestore service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_cloudfront_keyvaluestore.client import CloudFrontKeyValueStoreClient
+
+    session = Session()
+    client: CloudFrontKeyValueStoreClient = session.client("cloudfront-keyvaluestore")
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from botocore.client import BaseClient, ClientMeta
+
+from .paginator import ListKeysPaginator
+from .type_defs import (
+    DeleteKeyRequestListItemTypeDef,
+    DeleteKeyResponseTypeDef,
+    DescribeKeyValueStoreResponseTypeDef,
+    GetKeyResponseTypeDef,
+    ListKeysResponseTypeDef,
+    PutKeyRequestListItemTypeDef,
+    PutKeyResponseTypeDef,
+    UpdateKeysResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("CloudFrontKeyValueStoreClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class CloudFrontKeyValueStoreClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        CloudFrontKeyValueStoreClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#can_paginate)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#close)
+        """
+
+    def delete_key(self, *, KvsARN: str, Key: str, IfMatch: str) -> DeleteKeyResponseTypeDef:
+        """
+        Deletes the key value pair specified by the key.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.delete_key)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#delete_key)
+        """
+
+    def describe_key_value_store(self, *, KvsARN: str) -> DescribeKeyValueStoreResponseTypeDef:
+        """
+        Returns metadata information about Key Value Store.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.describe_key_value_store)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#describe_key_value_store)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#generate_presigned_url)
+        """
+
+    def get_key(self, *, KvsARN: str, Key: str) -> GetKeyResponseTypeDef:
+        """
+        Returns a key value pair.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.get_key)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#get_key)
+        """
+
+    def list_keys(
+        self, *, KvsARN: str, NextToken: str = ..., MaxResults: int = ...
+    ) -> ListKeysResponseTypeDef:
+        """
+        Returns a list of key value pairs.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.list_keys)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#list_keys)
+        """
+
+    def put_key(self, *, Key: str, Value: str, KvsARN: str, IfMatch: str) -> PutKeyResponseTypeDef:
+        """
+        Creates a new key value pair or replaces the value of an existing key.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.put_key)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#put_key)
+        """
+
+    def update_keys(
+        self,
+        *,
+        KvsARN: str,
+        IfMatch: str,
+        Puts: Sequence[PutKeyRequestListItemTypeDef] = ...,
+        Deletes: Sequence[DeleteKeyRequestListItemTypeDef] = ...,
+    ) -> UpdateKeysResponseTypeDef:
+        """
+        Puts or Deletes multiple key value pairs in a single, all-or-nothing operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.update_keys)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#update_keys)
+        """
+
+    def get_paginator(self, operation_name: Literal["list_keys"]) -> ListKeysPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront-keyvaluestore.html#CloudFrontKeyValueStore.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloudfront_keyvaluestore/client/#get_paginator)
+        """
