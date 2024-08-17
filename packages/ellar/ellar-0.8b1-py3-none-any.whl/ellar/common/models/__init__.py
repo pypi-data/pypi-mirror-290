a1efdd83821c981eaf5abcb60e689f5260386cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:06:38 +0000

    Add package ellar/0.8b1/ellar-0.8b1-py3-none-any.whl

---
+++ packages/ellar/ellar-0.8b1-py3-none-any.whl/ellar/common/models/__init__.py
@@ -0,0 +1,14 @@
+from .controller import ControllerBase, ControllerType
+from .guard import GlobalGuard, GuardCanActivate
+from .identity import AnonymousIdentity, Identity
+from .interceptor import EllarInterceptor
+
+__all__ = [
+    "AnonymousIdentity",
+    "ControllerBase",
+    "ControllerType",
+    "EllarInterceptor",
+    "Identity",
+    "GuardCanActivate",
+    "GlobalGuard",
+]
