bae9776f1a23364cc96264629f0465f3af06905c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:26 +0000

    Add package localizesh-sdk/0.1.1/localizesh_sdk-0.1.1.tar.gz

---
+++ packages/localizesh-sdk/localizesh_sdk-0.1.1.tar.gz/localizesh_sdk-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import os
+
+setup(
+    name='localizesh-sdk',
+    version=os.environ.get('VERSION'),
+    description="Localize.sh SDK",
+    package_dir={"localizesh_sdk": "src"},
+    long_description=open('README.md').read()
+)
