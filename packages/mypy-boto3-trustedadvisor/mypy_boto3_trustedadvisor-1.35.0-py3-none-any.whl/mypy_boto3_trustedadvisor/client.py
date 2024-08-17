8e05e8e6f1f1af5ecc605a860e0c6ed65088d456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:13 +0000

    Add package mypy-boto3-trustedadvisor/1.35.0/mypy_boto3_trustedadvisor-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-trustedadvisor/mypy_boto3_trustedadvisor-1.35.0-py3-none-any.whl/mypy_boto3_trustedadvisor/client.py
@@ -0,0 +1,355 @@
+"""
+Type annotations for trustedadvisor service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_trustedadvisor.client import TrustedAdvisorPublicAPIClient
+
+    session = Session()
+    client: TrustedAdvisorPublicAPIClient = session.client("trustedadvisor")
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type, overload
+
+from botocore.client import BaseClient, ClientMeta
+
+from .literals import (
+    ExclusionStatusType,
+    RecommendationLanguageType,
+    RecommendationPillarType,
+    RecommendationSourceType,
+    RecommendationStatusType,
+    RecommendationTypeType,
+    ResourceStatusType,
+    UpdateRecommendationLifecycleStageReasonCodeType,
+    UpdateRecommendationLifecycleStageType,
+)
+from .paginator import (
+    ListChecksPaginator,
+    ListOrganizationRecommendationAccountsPaginator,
+    ListOrganizationRecommendationResourcesPaginator,
+    ListOrganizationRecommendationsPaginator,
+    ListRecommendationResourcesPaginator,
+    ListRecommendationsPaginator,
+)
+from .type_defs import (
+    BatchUpdateRecommendationResourceExclusionResponseTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetOrganizationRecommendationResponseTypeDef,
+    GetRecommendationResponseTypeDef,
+    ListChecksResponseTypeDef,
+    ListOrganizationRecommendationAccountsResponseTypeDef,
+    ListOrganizationRecommendationResourcesResponseTypeDef,
+    ListOrganizationRecommendationsResponseTypeDef,
+    ListRecommendationResourcesResponseTypeDef,
+    ListRecommendationsResponseTypeDef,
+    RecommendationResourceExclusionTypeDef,
+    TimestampTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("TrustedAdvisorPublicAPIClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class TrustedAdvisorPublicAPIClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        TrustedAdvisorPublicAPIClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#exceptions)
+        """
+
+    def batch_update_recommendation_resource_exclusion(
+        self, *, recommendationResourceExclusions: Sequence[RecommendationResourceExclusionTypeDef]
+    ) -> BatchUpdateRecommendationResourceExclusionResponseTypeDef:
+        """
+        Update one or more exclusion status for a list of recommendation resources See
+        also: [AWS API
+        Documentation](https://docs.aws.amazon.com/goto/WebAPI/trustedadvisor-2022-09-15/BatchUpdateRecommendationResourceExclusion).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.batch_update_recommendation_resource_exclusion)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#batch_update_recommendation_resource_exclusion)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#can_paginate)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#close)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#generate_presigned_url)
+        """
+
+    def get_organization_recommendation(
+        self, *, organizationRecommendationIdentifier: str
+    ) -> GetOrganizationRecommendationResponseTypeDef:
+        """
+        Get a specific recommendation within an AWS Organizations organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_organization_recommendation)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_organization_recommendation)
+        """
+
+    def get_recommendation(
+        self, *, recommendationIdentifier: str
+    ) -> GetRecommendationResponseTypeDef:
+        """
+        Get a specific Recommendation See also: [AWS API
+        Documentation](https://docs.aws.amazon.com/goto/WebAPI/trustedadvisor-2022-09-15/GetRecommendation).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_recommendation)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_recommendation)
+        """
+
+    def list_checks(
+        self,
+        *,
+        awsService: str = ...,
+        language: RecommendationLanguageType = ...,
+        maxResults: int = ...,
+        nextToken: str = ...,
+        pillar: RecommendationPillarType = ...,
+        source: RecommendationSourceType = ...,
+    ) -> ListChecksResponseTypeDef:
+        """
+        List a filterable set of Checks See also: [AWS API
+        Documentation](https://docs.aws.amazon.com/goto/WebAPI/trustedadvisor-2022-09-15/ListChecks).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.list_checks)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#list_checks)
+        """
+
+    def list_organization_recommendation_accounts(
+        self,
+        *,
+        organizationRecommendationIdentifier: str,
+        affectedAccountId: str = ...,
+        maxResults: int = ...,
+        nextToken: str = ...,
+    ) -> ListOrganizationRecommendationAccountsResponseTypeDef:
+        """
+        Lists the accounts that own the resources for an organization aggregate
+        recommendation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.list_organization_recommendation_accounts)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#list_organization_recommendation_accounts)
+        """
+
+    def list_organization_recommendation_resources(
+        self,
+        *,
+        organizationRecommendationIdentifier: str,
+        affectedAccountId: str = ...,
+        exclusionStatus: ExclusionStatusType = ...,
+        maxResults: int = ...,
+        nextToken: str = ...,
+        regionCode: str = ...,
+        status: ResourceStatusType = ...,
+    ) -> ListOrganizationRecommendationResourcesResponseTypeDef:
+        """
+        List Resources of a Recommendation within an Organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.list_organization_recommendation_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#list_organization_recommendation_resources)
+        """
+
+    def list_organization_recommendations(
+        self,
+        *,
+        afterLastUpdatedAt: TimestampTypeDef = ...,
+        awsService: str = ...,
+        beforeLastUpdatedAt: TimestampTypeDef = ...,
+        checkIdentifier: str = ...,
+        maxResults: int = ...,
+        nextToken: str = ...,
+        pillar: RecommendationPillarType = ...,
+        source: RecommendationSourceType = ...,
+        status: RecommendationStatusType = ...,
+        type: RecommendationTypeType = ...,
+    ) -> ListOrganizationRecommendationsResponseTypeDef:
+        """
+        List a filterable set of Recommendations within an Organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.list_organization_recommendations)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#list_organization_recommendations)
+        """
+
+    def list_recommendation_resources(
+        self,
+        *,
+        recommendationIdentifier: str,
+        exclusionStatus: ExclusionStatusType = ...,
+        maxResults: int = ...,
+        nextToken: str = ...,
+        regionCode: str = ...,
+        status: ResourceStatusType = ...,
+    ) -> ListRecommendationResourcesResponseTypeDef:
+        """
+        List Resources of a Recommendation See also: [AWS API
+        Documentation](https://docs.aws.amazon.com/goto/WebAPI/trustedadvisor-2022-09-15/ListRecommendationResources).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.list_recommendation_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#list_recommendation_resources)
+        """
+
+    def list_recommendations(
+        self,
+        *,
+        afterLastUpdatedAt: TimestampTypeDef = ...,
+        awsService: str = ...,
+        beforeLastUpdatedAt: TimestampTypeDef = ...,
+        checkIdentifier: str = ...,
+        maxResults: int = ...,
+        nextToken: str = ...,
+        pillar: RecommendationPillarType = ...,
+        source: RecommendationSourceType = ...,
+        status: RecommendationStatusType = ...,
+        type: RecommendationTypeType = ...,
+    ) -> ListRecommendationsResponseTypeDef:
+        """
+        List a filterable set of Recommendations See also: [AWS API
+        Documentation](https://docs.aws.amazon.com/goto/WebAPI/trustedadvisor-2022-09-15/ListRecommendations).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.list_recommendations)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#list_recommendations)
+        """
+
+    def update_organization_recommendation_lifecycle(
+        self,
+        *,
+        lifecycleStage: UpdateRecommendationLifecycleStageType,
+        organizationRecommendationIdentifier: str,
+        updateReason: str = ...,
+        updateReasonCode: UpdateRecommendationLifecycleStageReasonCodeType = ...,
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Update the lifecycle of a Recommendation within an Organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.update_organization_recommendation_lifecycle)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#update_organization_recommendation_lifecycle)
+        """
+
+    def update_recommendation_lifecycle(
+        self,
+        *,
+        lifecycleStage: UpdateRecommendationLifecycleStageType,
+        recommendationIdentifier: str,
+        updateReason: str = ...,
+        updateReasonCode: UpdateRecommendationLifecycleStageReasonCodeType = ...,
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Update the lifecyle of a Recommendation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.update_recommendation_lifecycle)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#update_recommendation_lifecycle)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_checks"]) -> ListChecksPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_organization_recommendation_accounts"]
+    ) -> ListOrganizationRecommendationAccountsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_organization_recommendation_resources"]
+    ) -> ListOrganizationRecommendationResourcesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_organization_recommendations"]
+    ) -> ListOrganizationRecommendationsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_recommendation_resources"]
+    ) -> ListRecommendationResourcesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_recommendations"]
+    ) -> ListRecommendationsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/client/#get_paginator)
+        """
---
+++ packages/mypy-boto3-trustedadvisor/mypy_boto3_trustedadvisor-1.35.0-py3-none-any.whl/mypy_boto3_trustedadvisor/paginator.py
@@ -0,0 +1,206 @@
+"""
+Type annotations for trustedadvisor service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_trustedadvisor.client import TrustedAdvisorPublicAPIClient
+    from mypy_boto3_trustedadvisor.paginator import (
+        ListChecksPaginator,
+        ListOrganizationRecommendationAccountsPaginator,
+        ListOrganizationRecommendationResourcesPaginator,
+        ListOrganizationRecommendationsPaginator,
+        ListRecommendationResourcesPaginator,
+        ListRecommendationsPaginator,
+    )
+
+    session = Session()
+    client: TrustedAdvisorPublicAPIClient = session.client("trustedadvisor")
+
+    list_checks_paginator: ListChecksPaginator = client.get_paginator("list_checks")
+    list_organization_recommendation_accounts_paginator: ListOrganizationRecommendationAccountsPaginator = client.get_paginator("list_organization_recommendation_accounts")
+    list_organization_recommendation_resources_paginator: ListOrganizationRecommendationResourcesPaginator = client.get_paginator("list_organization_recommendation_resources")
+    list_organization_recommendations_paginator: ListOrganizationRecommendationsPaginator = client.get_paginator("list_organization_recommendations")
+    list_recommendation_resources_paginator: ListRecommendationResourcesPaginator = client.get_paginator("list_recommendation_resources")
+    list_recommendations_paginator: ListRecommendationsPaginator = client.get_paginator("list_recommendations")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import (
+    ExclusionStatusType,
+    RecommendationLanguageType,
+    RecommendationPillarType,
+    RecommendationSourceType,
+    RecommendationStatusType,
+    RecommendationTypeType,
+    ResourceStatusType,
+)
+from .type_defs import (
+    ListChecksResponseTypeDef,
+    ListOrganizationRecommendationAccountsResponseTypeDef,
+    ListOrganizationRecommendationResourcesResponseTypeDef,
+    ListOrganizationRecommendationsResponseTypeDef,
+    ListRecommendationResourcesResponseTypeDef,
+    ListRecommendationsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    TimestampTypeDef,
+)
+
+__all__ = (
+    "ListChecksPaginator",
+    "ListOrganizationRecommendationAccountsPaginator",
+    "ListOrganizationRecommendationResourcesPaginator",
+    "ListOrganizationRecommendationsPaginator",
+    "ListRecommendationResourcesPaginator",
+    "ListRecommendationsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListChecksPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListChecks)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listcheckspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        awsService: str = ...,
+        language: RecommendationLanguageType = ...,
+        pillar: RecommendationPillarType = ...,
+        source: RecommendationSourceType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListChecksResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListChecks.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listcheckspaginator)
+        """
+
+
+class ListOrganizationRecommendationAccountsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListOrganizationRecommendationAccounts)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listorganizationrecommendationaccountspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        organizationRecommendationIdentifier: str,
+        affectedAccountId: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListOrganizationRecommendationAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListOrganizationRecommendationAccounts.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listorganizationrecommendationaccountspaginator)
+        """
+
+
+class ListOrganizationRecommendationResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListOrganizationRecommendationResources)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listorganizationrecommendationresourcespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        organizationRecommendationIdentifier: str,
+        affectedAccountId: str = ...,
+        exclusionStatus: ExclusionStatusType = ...,
+        regionCode: str = ...,
+        status: ResourceStatusType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListOrganizationRecommendationResourcesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListOrganizationRecommendationResources.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listorganizationrecommendationresourcespaginator)
+        """
+
+
+class ListOrganizationRecommendationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListOrganizationRecommendations)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listorganizationrecommendationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        afterLastUpdatedAt: TimestampTypeDef = ...,
+        awsService: str = ...,
+        beforeLastUpdatedAt: TimestampTypeDef = ...,
+        checkIdentifier: str = ...,
+        pillar: RecommendationPillarType = ...,
+        source: RecommendationSourceType = ...,
+        status: RecommendationStatusType = ...,
+        type: RecommendationTypeType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListOrganizationRecommendationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListOrganizationRecommendations.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listorganizationrecommendationspaginator)
+        """
+
+
+class ListRecommendationResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListRecommendationResources)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listrecommendationresourcespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        recommendationIdentifier: str,
+        exclusionStatus: ExclusionStatusType = ...,
+        regionCode: str = ...,
+        status: ResourceStatusType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListRecommendationResourcesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListRecommendationResources.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listrecommendationresourcespaginator)
+        """
+
+
+class ListRecommendationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListRecommendations)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listrecommendationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        afterLastUpdatedAt: TimestampTypeDef = ...,
+        awsService: str = ...,
+        beforeLastUpdatedAt: TimestampTypeDef = ...,
+        checkIdentifier: str = ...,
+        pillar: RecommendationPillarType = ...,
+        source: RecommendationSourceType = ...,
+        status: RecommendationStatusType = ...,
+        type: RecommendationTypeType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListRecommendationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Paginator.ListRecommendations.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_trustedadvisor/paginators/#listrecommendationspaginator)
+        """
