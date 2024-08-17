7ea0de18a526c160e0bb9ac31619231661fc4a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:11 +0000

    Add package mypy-boto3-snow-device-management/1.35.0/mypy_boto3_snow_device_management-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-snow-device-management/mypy_boto3_snow_device_management-1.35.0-py3-none-any.whl/mypy_boto3_snow_device_management/client.pyi
@@ -0,0 +1,281 @@
+"""
+Type annotations for snow-device-management service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_snow_device_management.client import SnowDeviceManagementClient
+
+    session = Session()
+    client: SnowDeviceManagementClient = session.client("snow-device-management")
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type, overload
+
+from botocore.client import BaseClient, ClientMeta
+
+from .literals import ExecutionStateType, TaskStateType
+from .paginator import (
+    ListDeviceResourcesPaginator,
+    ListDevicesPaginator,
+    ListExecutionsPaginator,
+    ListTasksPaginator,
+)
+from .type_defs import (
+    CancelTaskOutputTypeDef,
+    CommandTypeDef,
+    CreateTaskOutputTypeDef,
+    DescribeDeviceEc2OutputTypeDef,
+    DescribeDeviceOutputTypeDef,
+    DescribeExecutionOutputTypeDef,
+    DescribeTaskOutputTypeDef,
+    EmptyResponseMetadataTypeDef,
+    ListDeviceResourcesOutputTypeDef,
+    ListDevicesOutputTypeDef,
+    ListExecutionsOutputTypeDef,
+    ListTagsForResourceOutputTypeDef,
+    ListTasksOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("SnowDeviceManagementClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+class SnowDeviceManagementClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        SnowDeviceManagementClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#can_paginate)
+        """
+
+    def cancel_task(self, *, taskId: str) -> CancelTaskOutputTypeDef:
+        """
+        Sends a cancel request for a specified task.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.cancel_task)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#cancel_task)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#close)
+        """
+
+    def create_task(
+        self,
+        *,
+        command: CommandTypeDef,
+        targets: Sequence[str],
+        clientToken: str = ...,
+        description: str = ...,
+        tags: Mapping[str, str] = ...,
+    ) -> CreateTaskOutputTypeDef:
+        """
+        Instructs one or more devices to start a task, such as unlocking or rebooting.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.create_task)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#create_task)
+        """
+
+    def describe_device(self, *, managedDeviceId: str) -> DescribeDeviceOutputTypeDef:
+        """
+        Checks device-specific information, such as the device type, software version,
+        IP addresses, and lock
+        status.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.describe_device)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#describe_device)
+        """
+
+    def describe_device_ec2_instances(
+        self, *, instanceIds: Sequence[str], managedDeviceId: str
+    ) -> DescribeDeviceEc2OutputTypeDef:
+        """
+        Checks the current state of the Amazon EC2 instances.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.describe_device_ec2_instances)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#describe_device_ec2_instances)
+        """
+
+    def describe_execution(
+        self, *, managedDeviceId: str, taskId: str
+    ) -> DescribeExecutionOutputTypeDef:
+        """
+        Checks the status of a remote task running on one or more target devices.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.describe_execution)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#describe_execution)
+        """
+
+    def describe_task(self, *, taskId: str) -> DescribeTaskOutputTypeDef:
+        """
+        Checks the metadata for a given task on a device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.describe_task)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#describe_task)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#generate_presigned_url)
+        """
+
+    def list_device_resources(
+        self, *, managedDeviceId: str, maxResults: int = ..., nextToken: str = ..., type: str = ...
+    ) -> ListDeviceResourcesOutputTypeDef:
+        """
+        Returns a list of the Amazon Web Services resources available for a device.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.list_device_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#list_device_resources)
+        """
+
+    def list_devices(
+        self, *, jobId: str = ..., maxResults: int = ..., nextToken: str = ...
+    ) -> ListDevicesOutputTypeDef:
+        """
+        Returns a list of all devices on your Amazon Web Services account that have
+        Amazon Web Services Snow Device Management enabled in the Amazon Web Services
+        Region where the command is
+        run.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.list_devices)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#list_devices)
+        """
+
+    def list_executions(
+        self,
+        *,
+        taskId: str,
+        maxResults: int = ...,
+        nextToken: str = ...,
+        state: ExecutionStateType = ...,
+    ) -> ListExecutionsOutputTypeDef:
+        """
+        Returns the status of tasks for one or more target devices.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.list_executions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#list_executions)
+        """
+
+    def list_tags_for_resource(self, *, resourceArn: str) -> ListTagsForResourceOutputTypeDef:
+        """
+        Returns a list of tags for a managed device or task.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.list_tags_for_resource)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#list_tags_for_resource)
+        """
+
+    def list_tasks(
+        self, *, maxResults: int = ..., nextToken: str = ..., state: TaskStateType = ...
+    ) -> ListTasksOutputTypeDef:
+        """
+        Returns a list of tasks that can be filtered by state.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.list_tasks)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#list_tasks)
+        """
+
+    def tag_resource(
+        self, *, resourceArn: str, tags: Mapping[str, str]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Adds or replaces tags on a device or task.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.tag_resource)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#tag_resource)
+        """
+
+    def untag_resource(
+        self, *, resourceArn: str, tagKeys: Sequence[str]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Removes a tag from a device or task.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.untag_resource)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#untag_resource)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_device_resources"]
+    ) -> ListDeviceResourcesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_devices"]) -> ListDevicesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_executions"]) -> ListExecutionsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_tasks"]) -> ListTasksPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/client/#get_paginator)
+        """
---
+++ packages/mypy-boto3-snow-device-management/mypy_boto3_snow_device_management-1.35.0-py3-none-any.whl/mypy_boto3_snow_device_management/paginator.pyi
@@ -0,0 +1,119 @@
+"""
+Type annotations for snow-device-management service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_snow_device_management.client import SnowDeviceManagementClient
+    from mypy_boto3_snow_device_management.paginator import (
+        ListDeviceResourcesPaginator,
+        ListDevicesPaginator,
+        ListExecutionsPaginator,
+        ListTasksPaginator,
+    )
+
+    session = Session()
+    client: SnowDeviceManagementClient = session.client("snow-device-management")
+
+    list_device_resources_paginator: ListDeviceResourcesPaginator = client.get_paginator("list_device_resources")
+    list_devices_paginator: ListDevicesPaginator = client.get_paginator("list_devices")
+    list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+    list_tasks_paginator: ListTasksPaginator = client.get_paginator("list_tasks")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import ExecutionStateType, TaskStateType
+from .type_defs import (
+    ListDeviceResourcesOutputTypeDef,
+    ListDevicesOutputTypeDef,
+    ListExecutionsOutputTypeDef,
+    ListTasksOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "ListDeviceResourcesPaginator",
+    "ListDevicesPaginator",
+    "ListExecutionsPaginator",
+    "ListTasksPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListDeviceResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListDeviceResources)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listdeviceresourcespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        managedDeviceId: str,
+        type: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListDeviceResourcesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListDeviceResources.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listdeviceresourcespaginator)
+        """
+
+class ListDevicesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListDevices)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listdevicespaginator)
+    """
+
+    def paginate(
+        self, *, jobId: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListDevicesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListDevices.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listdevicespaginator)
+        """
+
+class ListExecutionsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListExecutions)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listexecutionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        taskId: str,
+        state: ExecutionStateType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListExecutionsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListExecutions.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listexecutionspaginator)
+        """
+
+class ListTasksPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListTasks)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listtaskspaginator)
+    """
+
+    def paginate(
+        self, *, state: TaskStateType = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListTasksOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/snow-device-management.html#SnowDeviceManagement.Paginator.ListTasks.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_snow_device_management/paginators/#listtaskspaginator)
+        """
