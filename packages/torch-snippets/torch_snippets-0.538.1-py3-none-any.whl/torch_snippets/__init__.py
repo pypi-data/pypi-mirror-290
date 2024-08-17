026e3c8fe515d30d8ae0f1e7296cf5e9df74f4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:06:03 +0000

    Add package torch-snippets/0.538.1/torch_snippets-0.538.1-py3-none-any.whl

---
+++ packages/torch-snippets/torch_snippets-0.538.1-py3-none-any.whl/torch_snippets/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.538.1"
+from .loader import *
+from .paths import *
+from .markup import *
+from .inspector import *
+from .load_defaults import *
+from .pdf_loader import PDF
+from .logger import *
+from .markup2 import AD
+from .registry import *
+
+try:
+    from .ipython import *
+except:
+    ...
+
+from .decorators import *
+from .misc import *
+from .dates import *
+
+try:
+    from .torch_loader import *
+except Exception as e:
+    ...
