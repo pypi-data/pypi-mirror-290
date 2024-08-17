f572a68010362a6aa9503ba0544d394f46e1067d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:04:49 +0000

    Add package conexionbddian/0.0.2/conexionbddian-0.0.2-py3-none-any.whl

---
+++ packages/conexionbddian/conexionbddian-0.0.2-py3-none-any.whl/conexionbddian/__init__.py.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+from decouple import Config, RepositoryEnv
+from cx_Oracle import Connection, connect, init_oracle_client
+from sqlalchemy import create_engine,text
+
+config = Config(RepositoryEnv(os.path.join(os.environ.get("HOMEPATH"), ".env")))
+user = config('USER')
+pwd = config('PASSWORD')
+server = config('SERVER')
+port = config('PORT')
+service = config('SERVICE_NAME')
+
+from .conexionbddian import *
