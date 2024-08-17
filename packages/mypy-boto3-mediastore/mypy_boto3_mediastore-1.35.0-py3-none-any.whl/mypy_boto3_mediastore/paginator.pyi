de15950c3d310574d3ae2008fce3222e6c95d572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:02 +0000

    Add package mypy-boto3-mediastore/1.35.0/mypy_boto3_mediastore-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-mediastore/mypy_boto3_mediastore-1.35.0-py3-none-any.whl/mypy_boto3_mediastore/paginator.pyi
@@ -0,0 +1,51 @@
+"""
+Type annotations for mediastore service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediastore/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_mediastore.client import MediaStoreClient
+    from mypy_boto3_mediastore.paginator import (
+        ListContainersPaginator,
+    )
+
+    session = Session()
+    client: MediaStoreClient = session.client("mediastore")
+
+    list_containers_paginator: ListContainersPaginator = client.get_paginator("list_containers")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import ListContainersOutputTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListContainersPaginator",)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListContainersPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore.html#MediaStore.Paginator.ListContainers)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediastore/paginators/#listcontainerspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListContainersOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore.html#MediaStore.Paginator.ListContainers.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediastore/paginators/#listcontainerspaginator)
+        """
