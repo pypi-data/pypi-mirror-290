194ea692b205863d5ee4459c5a86f83bf541077b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:51 +0000

    Add package mypy-boto3-cloud9/1.35.0/mypy_boto3_cloud9-1.35.0.tar.gz

---
+++ packages/mypy-boto3-cloud9/mypy_boto3_cloud9-1.35.0.tar.gz/mypy_boto3_cloud9-1.35.0/mypy_boto3_cloud9/paginator.pyi
@@ -0,0 +1,77 @@
+"""
+Type annotations for cloud9 service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloud9/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_cloud9.client import Cloud9Client
+    from mypy_boto3_cloud9.paginator import (
+        DescribeEnvironmentMembershipsPaginator,
+        ListEnvironmentsPaginator,
+    )
+
+    session = Session()
+    client: Cloud9Client = session.client("cloud9")
+
+    describe_environment_memberships_paginator: DescribeEnvironmentMembershipsPaginator = client.get_paginator("describe_environment_memberships")
+    list_environments_paginator: ListEnvironmentsPaginator = client.get_paginator("list_environments")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import PermissionsType
+from .type_defs import (
+    DescribeEnvironmentMembershipsResultTypeDef,
+    ListEnvironmentsResultTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("DescribeEnvironmentMembershipsPaginator", "ListEnvironmentsPaginator")
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class DescribeEnvironmentMembershipsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloud9.html#Cloud9.Paginator.DescribeEnvironmentMemberships)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloud9/paginators/#describeenvironmentmembershipspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        userArn: str = ...,
+        environmentId: str = ...,
+        permissions: Sequence[PermissionsType] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[DescribeEnvironmentMembershipsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloud9.html#Cloud9.Paginator.DescribeEnvironmentMemberships.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloud9/paginators/#describeenvironmentmembershipspaginator)
+        """
+
+class ListEnvironmentsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloud9.html#Cloud9.Paginator.ListEnvironments)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloud9/paginators/#listenvironmentspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListEnvironmentsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloud9.html#Cloud9.Paginator.ListEnvironments.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_cloud9/paginators/#listenvironmentspaginator)
+        """
