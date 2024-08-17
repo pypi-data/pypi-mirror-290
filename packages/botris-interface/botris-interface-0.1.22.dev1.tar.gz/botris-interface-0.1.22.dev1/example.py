fac1483d28b10229ee0bb2b4d68c8c9b64384fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:22 +0000

    Add package botris-interface/0.1.22.dev1/botris-interface-0.1.22.dev1.tar.gz

---
+++ packages/botris-interface/botris-interface-0.1.22.dev1.tar.gz/botris-interface-0.1.22.dev1/example.py
@@ -0,0 +1,32 @@
+import asyncio
+
+import botris
+
+TOKEN = "913b8eb2-10ce-43b4-a8c1-ffb7dd4a5376"
+ROOM_KEY = "wxzasc1mhqci3zdmnf3fd6ea"
+
+
+async def main():
+    itf: botris.Interface = await botris.connect(
+        TOKEN,
+        ROOM_KEY,
+        botris.bots.RandomBot(),
+        tracking=False,
+        threading=True,
+        daemon=True,
+    )
+    itf2: botris.Interface = await botris.connect(
+        TOKEN,
+        ROOM_KEY,
+        botris.bots.RandomBot(),
+        tracking=False,
+        threading=True,
+        daemon=True,
+    )
+    await asyncio.sleep(100)
+    # itf = botris.Interface.create(TOKEN, ROOM_KEY, botris.bots.BlockFish(node_limit=800), tracking=False)
+    # await itf.connect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
