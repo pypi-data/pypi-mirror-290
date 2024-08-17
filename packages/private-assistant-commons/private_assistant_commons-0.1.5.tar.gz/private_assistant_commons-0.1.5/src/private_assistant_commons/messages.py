869638927a0fc9f0cf150f5e8fd6bbd8d84a550c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:38 +0000

    Add package private-assistant-commons/0.1.5/private_assistant_commons-0.1.5.tar.gz

---
+++ packages/private-assistant-commons/private_assistant_commons-0.1.5.tar.gz/private_assistant_commons-0.1.5/src/private_assistant_commons/messages.py
@@ -0,0 +1,35 @@
+import uuid
+
+from pydantic import BaseModel, Field
+
+
+class SkillCertainty(BaseModel):
+    message_id: uuid.UUID
+    certainty: float
+    skill_id: str
+
+
+class SkillRegistration(BaseModel):
+    skill_id: str
+    feedback_topic: str
+
+
+class ClientRequest(BaseModel):
+    id: uuid.UUID
+    text: str
+    room: str
+    output_topic: str
+
+
+class NumberAnalysisResult(BaseModel):
+    number_token: int
+    previous_token: str | None = None
+    next_token: str | None = None
+
+
+class IntentAnalysisResult(BaseModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    client_request: ClientRequest
+    numbers: list[NumberAnalysisResult]
+    nouns: list[str]
+    verbs: list[str]
