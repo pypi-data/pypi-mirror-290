eb5bcf8aa4164350b1e471fa990d82ee38906143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:05:01 +0000

    Add package mth5/0.4.5/mth5-0.4.5.tar.gz

---
+++ packages/mth5/mth5-0.4.5.tar.gz/mth5-0.4.5/mth5/tables/channel_table.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 14:09:38 2022
+
+@author: jpeacock
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+import pandas as pd
+import numpy as np
+import h5py
+
+from mth5 import CHANNEL_DTYPE
+from mth5.tables import MTH5Table
+
+# =============================================================================
+
+
+class ChannelSummaryTable(MTH5Table):
+    """
+    Object to hold the channel summary and provide some convenience functions
+    like fill, to_dataframe ...
+
+    """
+
+    def __init__(self, hdf5_dataset):
+        super().__init__(hdf5_dataset, CHANNEL_DTYPE)
+
+    def to_dataframe(self):
+        """
+        Create a pandas DataFrame from the table for easier querying.
+
+        :return: Channel Summary
+        :rtype: :class:`pandas.DataFrame`
+
+        """
+
+        df = pd.DataFrame(self.array[()])
+        for key in [
+            "survey",
+            "station",
+            "run",
+            "component",
+            "measurement_type",
+            "units",
+        ]:
+            setattr(df, key, getattr(df, key).str.decode("utf-8"))
+        try:
+            df.start = pd.to_datetime(df.start.str.decode("utf-8"), format="mixed")
+            df.end = pd.to_datetime(df.end.str.decode("utf-8"), format="mixed")
+        except ValueError:
+            df.start = pd.to_datetime(df.start.str.decode("utf-8"))
+            df.end = pd.to_datetime(df.end.str.decode("utf-8"))
+
+        return df
+
+    def summarize(self):
+        """
+
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        self.clear_table()
+
+        def get_channel_entry(group, dtype=CHANNEL_DTYPE):
+            ch_entry = np.array(
+                [
+                    (
+                        group.parent.parent.parent.parent.attrs["id"],
+                        group.parent.parent.attrs["id"],
+                        group.parent.attrs["id"],
+                        group.parent.parent.attrs["location.latitude"],
+                        group.parent.parent.attrs["location.longitude"],
+                        group.parent.parent.attrs["location.elevation"],
+                        group.attrs["component"],
+                        group.attrs["time_period.start"],
+                        group.attrs["time_period.end"],
+                        group.size,
+                        group.attrs["sample_rate"],
+                        group.attrs["type"],
+                        group.attrs["measurement_azimuth"],
+                        group.attrs["measurement_tilt"],
+                        group.attrs["units"],
+                        group.ref,
+                        group.parent.ref,
+                        group.parent.parent.ref,
+                    )
+                ],
+                dtype=dtype,
+            )
+            return ch_entry
+
+        def recursive_get_channel_entry(group):
+            """
+            a function to get channel entry
+            """
+            if isinstance(group, (h5py._hl.group.Group, h5py._hl.files.File)):
+                for key, node in group.items():
+                    recursive_get_channel_entry(node)
+            elif isinstance(group, h5py._hl.dataset.Dataset):
+                try:
+                    ch_type = group.attrs["type"]
+                    if ch_type in ["electric", "magnetic", "auxiliary"]:
+                        ch_entry = get_channel_entry(group)
+                        try:
+                            self.add_row(ch_entry)
+                        except ValueError as error:
+                            msg = (
+                                f"{error}. "
+                                "it is possible that the OS that made the table is not the OS operating on it."
+                            )
+                            self.logger.warning(msg)
+
+                except KeyError:
+                    pass
+
+        recursive_get_channel_entry(self.array.parent)
