11028deb10e17519e1eb7111c282e1aa8fdfe911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 07:05:16 +0000

    Add package netboxlabs-netbox-branching/0.3.1/netboxlabs-netbox-branching-0.3.1.tar.gz

---
+++ packages/netboxlabs-netbox-branching/netboxlabs-netbox-branching-0.3.1.tar.gz/netboxlabs_netbox_branching-0.3.1/netbox_branching/constants.py
@@ -0,0 +1,11 @@
+# Name of the main (non-branch) PostgreSQL schema
+MAIN_SCHEMA = 'public'
+
+# HTTP cookie
+COOKIE_NAME = 'active_branch'
+
+# HTTP header for API requests
+BRANCH_HEADER = 'X-NetBox-Branch'
+
+# URL query parameter name
+QUERY_PARAM = '_branch'
