8f14f09fa66b51b884d9f04839d6831fe48928d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:05:37 +0000

    Add package jayna/0.1.0/jayna-0.1.0-py3-none-any.whl

---
+++ packages/jayna/jayna-0.1.0-py3-none-any.whl/jayna/utils/birfday.py
@@ -0,0 +1,12 @@
+from datetime import datetime, date
+
+def jayna_birfday():
+    today = date.today()
+    current_year = today.year
+    next_april_29 = date(current_year, 4, 29)
+
+    if today > next_april_29:
+        next_april_29 = date(current_year + 1, 4, 29)
+
+    days_remaining = (next_april_29 - today).days
+    return days_remaining
