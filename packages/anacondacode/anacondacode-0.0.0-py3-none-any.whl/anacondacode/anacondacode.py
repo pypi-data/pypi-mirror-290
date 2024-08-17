30ce50dc547312e095336bb4f711372c2943b13a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 03:06:07 +0000

    Add package anacondacode/0.0.0/anacondacode-0.0.0-py3-none-any.whl

---
+++ packages/anacondacode/anacondacode-0.0.0-py3-none-any.whl/anacondacode/anacondacode.py
@@ -0,0 +1,12 @@
+# anacondacode
+
+__version__ = "0.0.0"
+
+def main():
+    print(istr(2).is_even())
+    print(istr.is_even('2'))
+    print(istr(3).is_prime())
+
+
+if __name__ == "__main__":
+    main()
