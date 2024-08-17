d483568b48629ba5d0c193200a51e40786ed6e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:12 +0000

    Add package mypy-boto3-timestream-influxdb/1.35.0/mypy_boto3_timestream_influxdb-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-timestream-influxdb/mypy_boto3_timestream_influxdb-1.35.0-py3-none-any.whl/mypy_boto3_timestream_influxdb/paginator.py
@@ -0,0 +1,74 @@
+"""
+Type annotations for timestream-influxdb service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_timestream_influxdb/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_timestream_influxdb.client import TimestreamInfluxDBClient
+    from mypy_boto3_timestream_influxdb.paginator import (
+        ListDbInstancesPaginator,
+        ListDbParameterGroupsPaginator,
+    )
+
+    session = Session()
+    client: TimestreamInfluxDBClient = session.client("timestream-influxdb")
+
+    list_db_instances_paginator: ListDbInstancesPaginator = client.get_paginator("list_db_instances")
+    list_db_parameter_groups_paginator: ListDbParameterGroupsPaginator = client.get_paginator("list_db_parameter_groups")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    ListDbInstancesOutputTypeDef,
+    ListDbParameterGroupsOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("ListDbInstancesPaginator", "ListDbParameterGroupsPaginator")
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListDbInstancesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Paginator.ListDbInstances)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_timestream_influxdb/paginators/#listdbinstancespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListDbInstancesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Paginator.ListDbInstances.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_timestream_influxdb/paginators/#listdbinstancespaginator)
+        """
+
+
+class ListDbParameterGroupsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Paginator.ListDbParameterGroups)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_timestream_influxdb/paginators/#listdbparametergroupspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListDbParameterGroupsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Paginator.ListDbParameterGroups.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_timestream_influxdb/paginators/#listdbparametergroupspaginator)
+        """
