fc8e571b6434ba942af4de6adaa23fab99409330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:10 +0000

    Add package mypy-boto3-amplifybackend/1.35.0/mypy_boto3_amplifybackend-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-amplifybackend/mypy_boto3_amplifybackend-1.35.0-py3-none-any.whl/mypy_boto3_amplifybackend/paginator.py
@@ -0,0 +1,60 @@
+"""
+Type annotations for amplifybackend service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amplifybackend/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_amplifybackend.client import AmplifyBackendClient
+    from mypy_boto3_amplifybackend.paginator import (
+        ListBackendJobsPaginator,
+    )
+
+    session = Session()
+    client: AmplifyBackendClient = session.client("amplifybackend")
+
+    list_backend_jobs_paginator: ListBackendJobsPaginator = client.get_paginator("list_backend_jobs")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import ListBackendJobsResponseTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListBackendJobsPaginator",)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListBackendJobsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/amplifybackend.html#AmplifyBackend.Paginator.ListBackendJobs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amplifybackend/paginators/#listbackendjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        AppId: str,
+        BackendEnvironmentName: str,
+        JobId: str = ...,
+        Operation: str = ...,
+        Status: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListBackendJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/amplifybackend.html#AmplifyBackend.Paginator.ListBackendJobs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amplifybackend/paginators/#listbackendjobspaginator)
+        """
