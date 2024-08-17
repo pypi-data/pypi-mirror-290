111b74bb48750b685d83bf84f1e0eff59b4ef99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 03:07:11 +0000

    Add package fastanime/1.7.2/fastanime-1.7.2-py3-none-any.whl

---
+++ packages/fastanime/fastanime-1.7.2-py3-none-any.whl/fastanime/libs/anime_provider/__init__.py
@@ -0,0 +1,12 @@
+anime_sources = {
+    "allanime": "api.AllAnimeAPI",
+    "animepahe": "api.AnimePaheApi",
+}
+SERVERS_AVAILABLE = [
+    "sharepoint",
+    "dropbox",
+    "gogoanime",
+    "weTransfer",
+    "wixmp",
+    "kwik",
+]
