5322c1f268225e89a218594aa47826e5b021310a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:05:25 +0000

    Add package plover-run-applescript/0.3.7/plover_run_applescript-0.3.7.tar.gz

---
+++ packages/plover-run-applescript/plover_run_applescript-0.3.7.tar.gz/plover_run_applescript-0.3.7/plover_run_applescript/path/expander.py
@@ -0,0 +1,62 @@
+"""
+Expander - a module for dealing with expansion of ENV vars in a file path.
+"""
+import os
+import re
+
+from typing import Tuple
+
+
+_ENV_VAR = re.compile(r"(\$[A-Za-z_][A-Za-z_0-9]*)")
+_DEFAULT_SHELL = "bash"
+_VAR_DIVIDER = "##"
+_ENV_VAR_SYNTAX = "$"
+
+def expand(path: str) -> str:
+    """
+    Expands env vars in a file path.
+
+    Raises an error if a value for the env var cannot be found.
+    """
+    parts = re.split(_ENV_VAR, path)
+    shell = _fetch_shell()
+    expanded_parts = []
+    for part in parts:
+        if part.startswith(_ENV_VAR_SYNTAX):
+            expanded_parts.append(_perform_expansion(part, shell))
+        else:
+            expanded_parts.append(part)
+
+    return "".join(expanded_parts)
+
+def expand_list(filepath_list: list[str]) -> list[Tuple[str, str]]:
+    """
+    Returns a list of expanded filepaths from a list of filepaths.
+
+    Removes a filepath from the list if its value is blank.
+    """
+    filepaths = _VAR_DIVIDER.join(filepath_list)
+    shell = _fetch_shell()
+    expanded_filepaths = _perform_expansion(filepaths, shell)
+    expanded_filepath_list = list(zip(
+        filepath_list,
+        expanded_filepaths.split(_VAR_DIVIDER)
+    ))
+
+    return expanded_filepath_list
+
+def _fetch_shell() -> str:
+    # NOTE: Entire shell path cannot be used because Plover's shell location may
+    # not be the same as the user's machine.
+    return os.getenv("SHELL", _DEFAULT_SHELL).split("/")[-1]
+
+def _perform_expansion(target: str, shell: str) -> str:
+    # NOTE: Using an interactive mode command (bash/zsh/fish -ic) seemed to be
+    # the only way to access a user's env vars on a Mac outside Plover's
+    # environment.
+    expanded = os.popen(f"{shell} -ic 'echo {target}'").read().strip()
+
+    if not expanded:
+        raise ValueError(f"No value found for env var: {target}")
+
+    return expanded
---
+++ packages/plover-run-applescript/plover_run_applescript-0.3.7.tar.gz/plover_run_applescript-0.3.7/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    extras_require = {
+        "test": ["mac-pyxa==0.0.9"]
+    }
+)
