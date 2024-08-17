2b4b7575886e53318acffeec84e9a132618650f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 03:05:49 +0000

    Add package object-case-switcher/0.0.16/object_case_switcher-0.0.16-py3-none-any.whl

---
+++ packages/object-case-switcher/object_case_switcher-0.0.16-py3-none-any.whl/object_case_switcher/__init__.py
@@ -0,0 +1,8 @@
+"""
+You need only one function `transform_structure`
+"""
+
+from .transformFunctions import *
+from .transformPropModule import transform_structure
+from .TransformStructure import *
+from .utils import *
