e11ef1e4083b0e30c7164ad1854fd4d1d733306c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 07:05:24 +0000

    Add package SoftLayer/6.2.5/SoftLayer-6.2.5-py2.py3-none-any.whl

---
+++ packages/SoftLayer/SoftLayer-6.2.5-py2.py3-none-any.whl/SoftLayer/transports/fixture.py
@@ -0,0 +1,32 @@
+"""
+    SoftLayer.transports.fixture
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Fixture transport, used for unit tests
+
+    :license: MIT, see LICENSE for more details.
+"""
+
+import importlib
+
+
+class FixtureTransport(object):
+    """Implements a transport which returns fixtures."""
+
+    def __call__(self, call):
+        """Load fixture from the default fixture path."""
+        try:
+            module_path = 'SoftLayer.fixtures.%s' % call.service
+            module = importlib.import_module(module_path)
+        except ImportError as ex:
+            message = f'{call.service} fixture is not implemented'
+            raise NotImplementedError(message) from ex
+        try:
+            return getattr(module, call.method)
+        except AttributeError as ex:
+            message = f'{call.service}::{call.method} fixture is not implemented'
+            raise NotImplementedError(message) from ex
+
+    @staticmethod
+    def print_reproduceable(call):
+        """Not Implemented"""
+        return call.service
