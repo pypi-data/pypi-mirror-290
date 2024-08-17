e1318c74631b4b92dc0c29a721dcb178cc22224c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 03:05:02 +0000

    Add package jellyfaas/0.0.6/jellyfaas-0.0.6.tar.gz

---
+++ packages/jellyfaas/jellyfaas-0.0.6.tar.gz/jellyfaas-0.0.6/src/jellyfaas/__init__.py
@@ -0,0 +1,8 @@
+# src/jellyfaas/__init__.py
+
+from .jellyfaas import Client
+from .jellyfaas import JellyFaasException
+from .jellyfaas import AuthenticationFailedException
+from .jellyfaas import FunctionLookupException
+from .jellyfaas import SetRequestException
+from .jellyfaas import InvocationException
