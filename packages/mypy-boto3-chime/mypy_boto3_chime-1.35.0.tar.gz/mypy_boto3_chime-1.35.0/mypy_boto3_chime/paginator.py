854a0969cedd4bf8bc601e12abae9ca4efee008d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:50 +0000

    Add package mypy-boto3-chime/1.35.0/mypy_boto3_chime-1.35.0.tar.gz

---
+++ packages/mypy-boto3-chime/mypy_boto3_chime-1.35.0.tar.gz/mypy_boto3_chime-1.35.0/mypy_boto3_chime/paginator.py
@@ -0,0 +1,80 @@
+"""
+Type annotations for chime service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_chime/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_chime.client import ChimeClient
+    from mypy_boto3_chime.paginator import (
+        ListAccountsPaginator,
+        ListUsersPaginator,
+    )
+
+    session = Session()
+    client: ChimeClient = session.client("chime")
+
+    list_accounts_paginator: ListAccountsPaginator = client.get_paginator("list_accounts")
+    list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import UserTypeType
+from .type_defs import ListAccountsResponseTypeDef, ListUsersResponseTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListAccountsPaginator", "ListUsersPaginator")
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListAccountsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime.html#Chime.Paginator.ListAccounts)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_chime/paginators/#listaccountspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        Name: str = ...,
+        UserEmail: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime.html#Chime.Paginator.ListAccounts.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_chime/paginators/#listaccountspaginator)
+        """
+
+
+class ListUsersPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime.html#Chime.Paginator.ListUsers)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_chime/paginators/#listuserspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        AccountId: str,
+        UserEmail: str = ...,
+        UserType: UserTypeType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListUsersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime.html#Chime.Paginator.ListUsers.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_chime/paginators/#listuserspaginator)
+        """
