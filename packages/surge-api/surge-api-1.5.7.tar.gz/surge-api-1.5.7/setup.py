c8d7c16b772badb55ddbbc7b390776dfcd700a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 05:06:11 +0000

    Add package surge-api/1.5.7/surge-api-1.5.7.tar.gz

---
+++ packages/surge-api/surge-api-1.5.7.tar.gz/surge-api-1.5.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.5.7'
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='surge-api',
+      version=VERSION,
+      description='Surge Python SDK',
+      author='Surge',
+      author_email='team@surgehq.ai',
+      url='https://github.com/surge-ai/surge-python',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      python_requires='>=3.6',
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      install_requires=requirements,
+      tests_require=['pytest >= 6.0.0'])
