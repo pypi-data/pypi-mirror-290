a0f11ba3c4b63e8eb6ed75db68ef41c001304b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:48 +0000

    Add package lasutils/1.0.70/lasutils-1.0.70-py3-none-any.whl

---
+++ packages/lasutils/lasutils-1.0.70-py3-none-any.whl/lasutils/my_ffmpeg.py
@@ -0,0 +1,40 @@
+import logging
+from subprocess import Popen, PIPE
+import signal
+
+
+# Start FFmpeg
+def start_ffmpeg(params: list):
+    # Get basic FFmpeg params
+    ffmpeg_cmd = get_global_params() + params
+    ffmpeg_proc = Popen(ffmpeg_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return ffmpeg_proc
+
+
+# STOP FFmpeg
+def stop_ffmpeg(ffmpeg_proc):
+    try:
+        logging.info("Shutting down FFmpeg process...")
+
+        ffmpeg_proc.send_signal(signal.SIGTERM)
+        ffmpeg_proc.communicate(timeout=10)
+
+    except Exception as e:
+        logging.warning(f"FFmpeg was not shut down. Killing the process...{e}")
+        try:
+            ffmpeg_proc.kill()
+            logging.info("FFmpeg process killed!")
+        except Exception as e:
+            logging.warning("Failed to kill FFmpeg process. Was probably not running")
+
+
+# Basic FFmpeg params
+def get_global_params():
+    params_ffmpeg_cmd = ["ffmpeg"]
+    params_global = [
+        # "-loglevel", "repeat+level+warning",
+        "-loglevel",
+        "repeat+level+warning",
+        "-y",
+    ]
+    return params_ffmpeg_cmd + params_global
