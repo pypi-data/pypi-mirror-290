85f73320b36a69d0f1327f6394116175eea48bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:05:39 +0000

    Add package pyav/13.0.0rc1/pyav-13.0.0rc1.tar.gz

---
+++ packages/pyav/pyav-13.0.0rc1.tar.gz/pyav-13.0.0rc1/av/audio/layout.pyx
@@ -0,0 +1,52 @@
+cimport libav as lib
+
+
+cdef object _cinit_bypass_sentinel
+
+cdef AudioLayout get_audio_layout(lib.AVChannelLayout c_layout):
+    """Get an AudioLayout from Cython land."""
+    cdef AudioLayout layout = AudioLayout.__new__(AudioLayout, _cinit_bypass_sentinel)
+    layout._init(c_layout)
+    return layout
+
+
+cdef class AudioLayout:
+    def __init__(self, layout):
+        if layout is _cinit_bypass_sentinel:
+            return
+
+        if type(layout) is str:
+            ret = lib.av_channel_layout_from_string(&c_layout, layout)
+            if ret != 0:
+                raise ValueError(f"Invalid layout: {layout}")
+        elif isinstance(layout, AudioLayout):
+            c_layout = (<AudioLayout>layout).layout
+        else:
+            raise TypeError(f"layout must be of type: string | av.AudioLayout, got {type(layout)}")
+
+        self._init(c_layout)
+
+    cdef _init(self, lib.AVChannelLayout layout):
+        self.layout = layout
+
+    def __repr__(self):
+        return f"<av.{self.__class__.__name__} {self.name!r}>"
+
+    def __eq__(self, other):
+        return isinstance(other, AudioLayout) and self.name == other.name and self.nb_channels == other.nb_channels
+
+    @property
+    def nb_channels(self):
+        return self.layout.nb_channels
+
+    @property
+    def name(self):
+        """The canonical name of the audio layout."""
+        cdef char layout_name[128]  # Adjust buffer size as needed
+        cdef int ret
+
+        ret = lib.av_channel_layout_describe(&self.layout, layout_name, sizeof(layout_name))
+        if ret < 0:
+            raise RuntimeError(f"Failed to get layout name: {ret}")
+
+        return layout_name
