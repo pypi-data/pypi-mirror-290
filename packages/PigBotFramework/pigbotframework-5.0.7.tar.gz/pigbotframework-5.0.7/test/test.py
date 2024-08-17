a676f8e92286585d9cdb3233f587e4ef3a6bf2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 03:07:13 +0000

    Add package PigBotFramework/5.0.7/pigbotframework-5.0.7.tar.gz

---
+++ packages/PigBotFramework/pigbotframework-5.0.7.tar.gz/pigbotframework-5.0.7/test/test.py
@@ -0,0 +1,26 @@
+# Step 1: import config and modify it
+from pbf import config
+
+config.logs_level = "DEBUG"
+config.plugins_disabled = ["nsfw"]
+config.plugins_config["webui"] = {
+    "basic_auth": {
+        "username": "admin",
+        "password": "admin"
+    }
+}
+config.plugins_config["mcserver"] = {
+    "client_id": 123,
+    "client_secret": 123,
+}
+
+# Step 2: import setup and setup
+from pbf import setup
+
+setup.setup(__name__)
+
+# Step 3: import driver and start it
+if __name__ == "__main__":
+    from pbf.driver import Fastapi
+
+    Fastapi.start()
