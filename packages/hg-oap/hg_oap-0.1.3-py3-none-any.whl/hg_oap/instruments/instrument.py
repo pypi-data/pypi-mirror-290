99d6ec5b5e187a89f8fe662e865899a01355a4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:05:53 +0000

    Add package hg-oap/0.1.3/hg_oap-0.1.3-py3-none-any.whl

---
+++ packages/hg-oap/hg_oap-0.1.3-py3-none-any.whl/hg_oap/instruments/instrument.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import NewType
+
+from hg_oap.units.unit_system import UnitConversionContext
+from hg_oap.utils import ExprClass
+from hgraph import CompoundScalar, TimeSeriesSchema, TS
+
+__all__ = ("INSTRUMENT_ID", "Instrument")
+
+from hg_oap.assets.asset import Asset
+
+INSTRUMENT_ID = str  # A useful alias to help make it clear when a string is intended to represent an instrument id.
+
+
+@dataclass
+class InstrumentId(CompoundScalar):
+    """
+    Use this to represent an instrument with its identification type, this allows for identification of the symbology
+    the id is using. (For example FIGI, FactSet, etc.)
+    """
+    id_: INSTRUMENT_ID
+    id_type: str
+
+
+@dataclass(frozen=True)
+class Instrument(CompoundScalar, ExprClass, UnitConversionContext):
+    symbol: INSTRUMENT_ID
+
+
