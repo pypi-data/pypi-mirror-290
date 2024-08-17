fa6e3fc334eda9de9c27c2850d5927f28b72e240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 05:06:20 +0000

    Add package xingong-pypack/0.0.4/xingong_pypack-0.0.4-py2.py3-none-any.whl

---
+++ packages/xingong-pypack/xingong_pypack-0.0.4-py2.py3-none-any.whl/xingong_pypack/common.py
@@ -0,0 +1,15 @@
+"""The common module contains common functions and classes used by the other modules.
+"""
+
+# def hello_world():
+#     """Prints "Hello World!" to the console.
+#     """
+#     print("Hello World!")
+
+# def hello(name):
+#     """Prints "Hello, <name>!" to the console.
+
+#     Args:
+#         name (str): The name to say hello to.
+#     """
+#     print(f"Hello, {name}!")
