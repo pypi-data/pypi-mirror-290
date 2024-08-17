ae87376261449661ff395f162adb3b098cfde2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:06:03 +0000

    Add package threads-sdk/0.2.1/threads_sdk-0.2.1.tar.gz

---
+++ packages/threads-sdk/threads_sdk-0.2.1.tar.gz/threads_sdk-0.2.1/README.md
@@ -0,0 +1,41 @@
+# Threads SDK
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://github.com/louis70109/line-notify#contributing)
+[![pypi package](https://badge.fury.io/py/lotify.svg)](https://badge.fury.io/py/lotify)
+[![Python Version](https://img.shields.io/badge/Python-%3E%3D%203.5-blue.svg)](https://badge.fury.io/py/lotify)
+[![GitHub latest commit](https://badgen.net/github/last-commit/Naereen/Strapdown.js)](https://GitHub.com/Naereen/StrapDown.js/commit/)
+[![GitHub stars](https://img.shields.io/github/stars/Naereen/StrapDown.js.svg?style=social&label=Star&maxAge=2592000)](https://GitHub.com/Naereen/StrapDown.js/stargazers/)
+
+
+# Usage
+
+
+## Install package
+
+```
+pip install threads_sdk
+```
+
+
+## Environment variables
+
+Input those variables in your `.env` file or OS environment (or using `export`).
+
+Then you don't need to input any parameters in `initialize step`.
+
+```
+USER_ID
+ACCESS_TOKEN
+APP_SECRET
+```
+
+# Contributors
+
+<a href="https://github.com/louis70109/threads-sdk/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=louis70109/threads-sdk" />
+</a>
+
+# License
+
+[MIT](https://github.com/louis70109/threads-sdk/blob/master/LICENSE) © [NiJia Lin](https://nijialin.com/about/)
---
+++ packages/threads-sdk/threads_sdk-0.2.1.tar.gz/threads_sdk-0.2.1/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+from setuptools import setup, find_packages
+
+__version__ = ''
+with open('threads/__version__.py', 'r') as fd:
+    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+    for line in fd:
+        m = reg.match(line)
+        if m:
+            __version__ = m.group(1)
+            break
+
+
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='threads-sdk',
+    version=__version__,
+    description='Using Threads API more easily',
+    url='https://github.com/louis70109/threads-sdk',
+    author='NiJia Lin',
+    author_email='louis70109@gmail.com',
+    maintainer="NiJia Lin",
+    maintainer_email="louis70109@gmail.com",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords='Threads SDK',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=["requests>=2.0"],
+    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    project_urls={
+        'Bug Reports': 'https://github.com/louis70109/threads-sdk/issues',
+        'Source': 'https://github.com/louis70109/threads-sdk',
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development"
+    ]
+)
