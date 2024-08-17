4feb952326bc24ddd8708e880e4a9db7daa5aa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:05:15 +0000

    Add package jayna/0.1.2/jayna-0.1.2-py3-none-any.whl

---
+++ packages/jayna/jayna-0.1.2-py3-none-any.whl/jayna/drawing/bunny.py
@@ -0,0 +1,40 @@
+
+def cool_bunny():
+    bunny = r"""
+    (\(\  
+    ( -.-) 
+    o_(")(")
+    """
+    return bunny
+
+def surprised_bunny():
+    bunny = r"""
+    (\_/)
+    (o.o)
+    (")(")
+    """
+    return bunny
+
+def chubby_bunny():
+    bunny = r"""
+    (\_/)  
+    ( 'x' ) 
+    c(")(")
+    """
+    return bunny
+
+def shy_bunny():
+    bunny = r"""
+    (\_/) 
+    (o.o) 
+    (> <)
+    """
+    return bunny
+
+def sleepy_bunny():
+    bunny = r"""
+    (\_/) 
+    (-.-) 
+    z(_(")(")
+    """
+    return bunny
