8c7e38908e33e6bc31fc72e7148040670c7ab7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:09 +0000

    Add package mypy-boto3-rum/1.35.0/mypy_boto3_rum-1.35.0.tar.gz

---
+++ packages/mypy-boto3-rum/mypy_boto3_rum-1.35.0.tar.gz/mypy_boto3_rum-1.35.0/mypy_boto3_rum/paginator.pyi
@@ -0,0 +1,123 @@
+"""
+Type annotations for rum service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_rum.client import CloudWatchRUMClient
+    from mypy_boto3_rum.paginator import (
+        BatchGetRumMetricDefinitionsPaginator,
+        GetAppMonitorDataPaginator,
+        ListAppMonitorsPaginator,
+        ListRumMetricsDestinationsPaginator,
+    )
+
+    session = Session()
+    client: CloudWatchRUMClient = session.client("rum")
+
+    batch_get_rum_metric_definitions_paginator: BatchGetRumMetricDefinitionsPaginator = client.get_paginator("batch_get_rum_metric_definitions")
+    get_app_monitor_data_paginator: GetAppMonitorDataPaginator = client.get_paginator("get_app_monitor_data")
+    list_app_monitors_paginator: ListAppMonitorsPaginator = client.get_paginator("list_app_monitors")
+    list_rum_metrics_destinations_paginator: ListRumMetricsDestinationsPaginator = client.get_paginator("list_rum_metrics_destinations")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import MetricDestinationType
+from .type_defs import (
+    BatchGetRumMetricDefinitionsResponseTypeDef,
+    GetAppMonitorDataResponseTypeDef,
+    ListAppMonitorsResponseTypeDef,
+    ListRumMetricsDestinationsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    QueryFilterTypeDef,
+    TimeRangeTypeDef,
+)
+
+__all__ = (
+    "BatchGetRumMetricDefinitionsPaginator",
+    "GetAppMonitorDataPaginator",
+    "ListAppMonitorsPaginator",
+    "ListRumMetricsDestinationsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class BatchGetRumMetricDefinitionsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.BatchGetRumMetricDefinitions)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#batchgetrummetricdefinitionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        AppMonitorName: str,
+        Destination: MetricDestinationType,
+        DestinationArn: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[BatchGetRumMetricDefinitionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.BatchGetRumMetricDefinitions.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#batchgetrummetricdefinitionspaginator)
+        """
+
+class GetAppMonitorDataPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.GetAppMonitorData)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#getappmonitordatapaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        Name: str,
+        TimeRange: TimeRangeTypeDef,
+        Filters: Sequence[QueryFilterTypeDef] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[GetAppMonitorDataResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.GetAppMonitorData.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#getappmonitordatapaginator)
+        """
+
+class ListAppMonitorsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.ListAppMonitors)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#listappmonitorspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListAppMonitorsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.ListAppMonitors.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#listappmonitorspaginator)
+        """
+
+class ListRumMetricsDestinationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.ListRumMetricsDestinations)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#listrummetricsdestinationspaginator)
+    """
+
+    def paginate(
+        self, *, AppMonitorName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListRumMetricsDestinationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM.Paginator.ListRumMetricsDestinations.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rum/paginators/#listrummetricsdestinationspaginator)
+        """
