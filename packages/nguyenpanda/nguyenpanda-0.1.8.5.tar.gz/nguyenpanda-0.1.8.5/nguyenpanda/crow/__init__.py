89eca1ff6f9b9675de5c6875d76161d58c6a13f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:05:26 +0000

    Add package nguyenpanda/0.1.8.5/nguyenpanda-0.1.8.5.tar.gz

---
+++ packages/nguyenpanda/nguyenpanda-0.1.8.5.tar.gz/nguyenpanda-0.1.8.5/nguyenpanda/crow/__init__.py
@@ -0,0 +1,15 @@
+"""
+`crow` is a utility package designed to streamline the process of working
+on Machine Learning (ML) and Deep Learning (DL) projects within Jupyter notebooks.
+It provides a set of convenient functions to help manage datasets, directories,
+and other common tasks that are essential in a typical ML/DL workflow.
+"""
+
+from .jupyter_notebook import NoteBookUtils
+
+nb_utils: NoteBookUtils = NoteBookUtils()
+
+__all__ = [
+    'NoteBookUtils',
+    'nb_utils',
+]
