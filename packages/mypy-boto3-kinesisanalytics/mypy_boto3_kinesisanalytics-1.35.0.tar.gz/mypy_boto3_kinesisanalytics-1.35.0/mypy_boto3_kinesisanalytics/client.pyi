11cd33ba2529b5299996b509e112210916a5794f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:00 +0000

    Add package mypy-boto3-kinesisanalytics/1.35.0/mypy_boto3_kinesisanalytics-1.35.0.tar.gz

---
+++ packages/mypy-boto3-kinesisanalytics/mypy_boto3_kinesisanalytics-1.35.0.tar.gz/mypy_boto3_kinesisanalytics-1.35.0/mypy_boto3_kinesisanalytics/client.pyi
@@ -0,0 +1,334 @@
+"""
+Type annotations for kinesisanalytics service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_kinesisanalytics.client import KinesisAnalyticsClient
+
+    session = Session()
+    client: KinesisAnalyticsClient = session.client("kinesisanalytics")
+    ```
+"""
+
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from botocore.client import BaseClient, ClientMeta
+
+from .type_defs import (
+    ApplicationUpdateTypeDef,
+    CloudWatchLoggingOptionTypeDef,
+    CreateApplicationResponseTypeDef,
+    DescribeApplicationResponseTypeDef,
+    DiscoverInputSchemaResponseTypeDef,
+    InputConfigurationTypeDef,
+    InputProcessingConfigurationTypeDef,
+    InputStartingPositionConfigurationTypeDef,
+    InputTypeDef,
+    ListApplicationsResponseTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    OutputTypeDef,
+    ReferenceDataSourceTypeDef,
+    S3ConfigurationTypeDef,
+    TagTypeDef,
+    TimestampTypeDef,
+)
+
+__all__ = ("KinesisAnalyticsClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    CodeValidationException: Type[BotocoreClientError]
+    ConcurrentModificationException: Type[BotocoreClientError]
+    InvalidApplicationConfigurationException: Type[BotocoreClientError]
+    InvalidArgumentException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    ResourceInUseException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ResourceProvisionedThroughputExceededException: Type[BotocoreClientError]
+    ServiceUnavailableException: Type[BotocoreClientError]
+    TooManyTagsException: Type[BotocoreClientError]
+    UnableToDetectSchemaException: Type[BotocoreClientError]
+    UnsupportedOperationException: Type[BotocoreClientError]
+
+class KinesisAnalyticsClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        KinesisAnalyticsClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#exceptions)
+        """
+
+    def add_application_cloud_watch_logging_option(
+        self,
+        *,
+        ApplicationName: str,
+        CurrentApplicationVersionId: int,
+        CloudWatchLoggingOption: CloudWatchLoggingOptionTypeDef,
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_cloud_watch_logging_option)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#add_application_cloud_watch_logging_option)
+        """
+
+    def add_application_input(
+        self, *, ApplicationName: str, CurrentApplicationVersionId: int, Input: InputTypeDef
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_input)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#add_application_input)
+        """
+
+    def add_application_input_processing_configuration(
+        self,
+        *,
+        ApplicationName: str,
+        CurrentApplicationVersionId: int,
+        InputId: str,
+        InputProcessingConfiguration: InputProcessingConfigurationTypeDef,
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_input_processing_configuration)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#add_application_input_processing_configuration)
+        """
+
+    def add_application_output(
+        self, *, ApplicationName: str, CurrentApplicationVersionId: int, Output: OutputTypeDef
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_output)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#add_application_output)
+        """
+
+    def add_application_reference_data_source(
+        self,
+        *,
+        ApplicationName: str,
+        CurrentApplicationVersionId: int,
+        ReferenceDataSource: ReferenceDataSourceTypeDef,
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_reference_data_source)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#add_application_reference_data_source)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#can_paginate)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#close)
+        """
+
+    def create_application(
+        self,
+        *,
+        ApplicationName: str,
+        ApplicationDescription: str = ...,
+        Inputs: Sequence[InputTypeDef] = ...,
+        Outputs: Sequence[OutputTypeDef] = ...,
+        CloudWatchLoggingOptions: Sequence[CloudWatchLoggingOptionTypeDef] = ...,
+        ApplicationCode: str = ...,
+        Tags: Sequence[TagTypeDef] = ...,
+    ) -> CreateApplicationResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.create_application)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#create_application)
+        """
+
+    def delete_application(
+        self, *, ApplicationName: str, CreateTimestamp: TimestampTypeDef
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#delete_application)
+        """
+
+    def delete_application_cloud_watch_logging_option(
+        self,
+        *,
+        ApplicationName: str,
+        CurrentApplicationVersionId: int,
+        CloudWatchLoggingOptionId: str,
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_cloud_watch_logging_option)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#delete_application_cloud_watch_logging_option)
+        """
+
+    def delete_application_input_processing_configuration(
+        self, *, ApplicationName: str, CurrentApplicationVersionId: int, InputId: str
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_input_processing_configuration)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#delete_application_input_processing_configuration)
+        """
+
+    def delete_application_output(
+        self, *, ApplicationName: str, CurrentApplicationVersionId: int, OutputId: str
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_output)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#delete_application_output)
+        """
+
+    def delete_application_reference_data_source(
+        self, *, ApplicationName: str, CurrentApplicationVersionId: int, ReferenceId: str
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_reference_data_source)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#delete_application_reference_data_source)
+        """
+
+    def describe_application(self, *, ApplicationName: str) -> DescribeApplicationResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.describe_application)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#describe_application)
+        """
+
+    def discover_input_schema(
+        self,
+        *,
+        ResourceARN: str = ...,
+        RoleARN: str = ...,
+        InputStartingPositionConfiguration: InputStartingPositionConfigurationTypeDef = ...,
+        S3Configuration: S3ConfigurationTypeDef = ...,
+        InputProcessingConfiguration: InputProcessingConfigurationTypeDef = ...,
+    ) -> DiscoverInputSchemaResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.discover_input_schema)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#discover_input_schema)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#generate_presigned_url)
+        """
+
+    def list_applications(
+        self, *, Limit: int = ..., ExclusiveStartApplicationName: str = ...
+    ) -> ListApplicationsResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.list_applications)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#list_applications)
+        """
+
+    def list_tags_for_resource(self, *, ResourceARN: str) -> ListTagsForResourceResponseTypeDef:
+        """
+        Retrieves the list of key-value tags assigned to the application.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.list_tags_for_resource)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#list_tags_for_resource)
+        """
+
+    def start_application(
+        self, *, ApplicationName: str, InputConfigurations: Sequence[InputConfigurationTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.start_application)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#start_application)
+        """
+
+    def stop_application(self, *, ApplicationName: str) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.stop_application)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#stop_application)
+        """
+
+    def tag_resource(self, *, ResourceARN: str, Tags: Sequence[TagTypeDef]) -> Dict[str, Any]:
+        """
+        Adds one or more key-value tags to a Kinesis Analytics application.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.tag_resource)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#tag_resource)
+        """
+
+    def untag_resource(self, *, ResourceARN: str, TagKeys: Sequence[str]) -> Dict[str, Any]:
+        """
+        Removes one or more tags from a Kinesis Analytics application.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.untag_resource)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#untag_resource)
+        """
+
+    def update_application(
+        self,
+        *,
+        ApplicationName: str,
+        CurrentApplicationVersionId: int,
+        ApplicationUpdate: ApplicationUpdateTypeDef,
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.update_application)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kinesisanalytics/client/#update_application)
+        """
