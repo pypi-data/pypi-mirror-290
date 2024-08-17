3fe74c0c5ff23f615873d871b1f44b4c854bb677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:04:52 +0000

    Add package fastzdp-sqlmodel/0.1.0/fastzdp_sqlmodel-0.1.0.tar.gz

---
+++ packages/fastzdp-sqlmodel/fastzdp_sqlmodel-0.1.0.tar.gz/fastzdp_sqlmodel-0.1.0/fastzdp_sqlmodel/__init__.py
@@ -0,0 +1,29 @@
+from .add import add
+from .engine import get_engine
+from .table import init_table, create_table
+from .get import (
+    get_by_dict, get_all, get_by_dict_or,
+    get_by_dict_and, get,
+)
+from .update import (
+    update, update_id,
+)
+from .delete import (
+    delete, delete_id,
+)
+
+__all__ = [
+    "add",
+    "get_engine",
+    "init_table",
+    "create_table",
+    "get_all",
+    "get_by_dict",
+    "get_by_dict_or",
+    "get_by_dict_and",
+    "get",
+    "update",
+    "update_id",
+    "delete",
+    "delete_id",
+]
