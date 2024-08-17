cfabdeddcad14b675333a2c84e1cea1df0bf8bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:05:36 +0000

    Add package jb-module/0.1/jb_module-0.1.tar.gz

---
+++ packages/jb-module/jb_module-0.1.tar.gz/jb_module-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jb_module",  
+    version="0.1",   
+    author="Judha Benhur",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
