6970bb438d56c00c7cb3cd98c17df3f85aec71d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:04:53 +0000

    Add package atomic_dict/0.4.0/atomic_dict-0.4.0.tar.gz

---
+++ packages/atomic_dict/atomic_dict-0.4.0.tar.gz/atomic_dict-0.4.0/c_build.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, Extension, find_packages # type: ignore
+import sys
+
+cmd = ['build', '--build-lib', os.getcwd()]
+sys.argv.extend(cmd)
+
+atomic_dict_capi_module = Extension(
+    "atomic_dict.capi",
+    sources=["capi/init.c", "capi/methods.c"],
+    extra_compile_args=["-O3"]
+)
+
+setup(
+    name="atomic_dict",
+    version="0.4.0",
+    packages=find_packages(),
+    ext_modules=[atomic_dict_capi_module],
+)
