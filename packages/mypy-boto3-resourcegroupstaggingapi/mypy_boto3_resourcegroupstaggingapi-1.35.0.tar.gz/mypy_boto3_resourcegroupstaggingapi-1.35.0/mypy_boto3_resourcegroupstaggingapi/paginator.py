55209e560c92c5577158753dde13e10b7ffe326d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:08 +0000

    Add package mypy-boto3-resourcegroupstaggingapi/1.35.0/mypy_boto3_resourcegroupstaggingapi-1.35.0.tar.gz

---
+++ packages/mypy-boto3-resourcegroupstaggingapi/mypy_boto3_resourcegroupstaggingapi-1.35.0.tar.gz/mypy_boto3_resourcegroupstaggingapi-1.35.0/mypy_boto3_resourcegroupstaggingapi/paginator.py
@@ -0,0 +1,132 @@
+"""
+Type annotations for resourcegroupstaggingapi service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_resourcegroupstaggingapi.client import ResourceGroupsTaggingAPIClient
+    from mypy_boto3_resourcegroupstaggingapi.paginator import (
+        GetComplianceSummaryPaginator,
+        GetResourcesPaginator,
+        GetTagKeysPaginator,
+        GetTagValuesPaginator,
+    )
+
+    session = Session()
+    client: ResourceGroupsTaggingAPIClient = session.client("resourcegroupstaggingapi")
+
+    get_compliance_summary_paginator: GetComplianceSummaryPaginator = client.get_paginator("get_compliance_summary")
+    get_resources_paginator: GetResourcesPaginator = client.get_paginator("get_resources")
+    get_tag_keys_paginator: GetTagKeysPaginator = client.get_paginator("get_tag_keys")
+    get_tag_values_paginator: GetTagValuesPaginator = client.get_paginator("get_tag_values")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import GroupByAttributeType
+from .type_defs import (
+    GetComplianceSummaryOutputTypeDef,
+    GetResourcesOutputTypeDef,
+    GetTagKeysOutputTypeDef,
+    GetTagValuesOutputTypeDef,
+    PaginatorConfigTypeDef,
+    TagFilterTypeDef,
+)
+
+__all__ = (
+    "GetComplianceSummaryPaginator",
+    "GetResourcesPaginator",
+    "GetTagKeysPaginator",
+    "GetTagValuesPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class GetComplianceSummaryPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetComplianceSummary)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#getcompliancesummarypaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        TargetIdFilters: Sequence[str] = ...,
+        RegionFilters: Sequence[str] = ...,
+        ResourceTypeFilters: Sequence[str] = ...,
+        TagKeyFilters: Sequence[str] = ...,
+        GroupBy: Sequence[GroupByAttributeType] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[GetComplianceSummaryOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetComplianceSummary.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#getcompliancesummarypaginator)
+        """
+
+
+class GetResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetResources)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#getresourcespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        TagFilters: Sequence[TagFilterTypeDef] = ...,
+        TagsPerPage: int = ...,
+        ResourceTypeFilters: Sequence[str] = ...,
+        IncludeComplianceDetails: bool = ...,
+        ExcludeCompliantResources: bool = ...,
+        ResourceARNList: Sequence[str] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[GetResourcesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetResources.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#getresourcespaginator)
+        """
+
+
+class GetTagKeysPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetTagKeys)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#gettagkeyspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[GetTagKeysOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetTagKeys.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#gettagkeyspaginator)
+        """
+
+
+class GetTagValuesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetTagValues)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#gettagvaluespaginator)
+    """
+
+    def paginate(
+        self, *, Key: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[GetTagValuesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resourcegroupstaggingapi.html#ResourceGroupsTaggingAPI.Paginator.GetTagValues.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resourcegroupstaggingapi/paginators/#gettagvaluespaginator)
+        """
