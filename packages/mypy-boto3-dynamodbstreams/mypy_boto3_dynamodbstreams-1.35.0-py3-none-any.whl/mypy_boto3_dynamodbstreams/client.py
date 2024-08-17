3766b82f17946724c3f3756dd79b64fb59fd6fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:55 +0000

    Add package mypy-boto3-dynamodbstreams/1.35.0/mypy_boto3_dynamodbstreams-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-dynamodbstreams/mypy_boto3_dynamodbstreams-1.35.0-py3-none-any.whl/mypy_boto3_dynamodbstreams/client.py
@@ -0,0 +1,141 @@
+"""
+Type annotations for dynamodbstreams service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_dynamodbstreams.client import DynamoDBStreamsClient
+
+    session = Session()
+    client: DynamoDBStreamsClient = session.client("dynamodbstreams")
+    ```
+"""
+
+from typing import Any, Dict, Mapping, Type
+
+from botocore.client import BaseClient, ClientMeta
+
+from .literals import ShardIteratorTypeType
+from .type_defs import (
+    DescribeStreamOutputTypeDef,
+    GetRecordsOutputTypeDef,
+    GetShardIteratorOutputTypeDef,
+    ListStreamsOutputTypeDef,
+)
+
+__all__ = ("DynamoDBStreamsClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    ExpiredIteratorException: Type[BotocoreClientError]
+    InternalServerError: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    TrimmedDataAccessException: Type[BotocoreClientError]
+
+
+class DynamoDBStreamsClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        DynamoDBStreamsClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#can_paginate)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#close)
+        """
+
+    def describe_stream(
+        self, *, StreamArn: str, Limit: int = ..., ExclusiveStartShardId: str = ...
+    ) -> DescribeStreamOutputTypeDef:
+        """
+        Returns information about a stream, including the current status of the stream,
+        its Amazon Resource Name (ARN), the composition of its shards, and its
+        corresponding DynamoDB
+        table.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.describe_stream)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#describe_stream)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#generate_presigned_url)
+        """
+
+    def get_records(self, *, ShardIterator: str, Limit: int = ...) -> GetRecordsOutputTypeDef:
+        """
+        Retrieves the stream records from a given shard.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.get_records)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#get_records)
+        """
+
+    def get_shard_iterator(
+        self,
+        *,
+        StreamArn: str,
+        ShardId: str,
+        ShardIteratorType: ShardIteratorTypeType,
+        SequenceNumber: str = ...,
+    ) -> GetShardIteratorOutputTypeDef:
+        """
+        Returns a shard iterator.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.get_shard_iterator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#get_shard_iterator)
+        """
+
+    def list_streams(
+        self, *, TableName: str = ..., Limit: int = ..., ExclusiveStartStreamArn: str = ...
+    ) -> ListStreamsOutputTypeDef:
+        """
+        Returns an array of stream ARNs associated with the current account and
+        endpoint.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.list_streams)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_dynamodbstreams/client/#list_streams)
+        """
