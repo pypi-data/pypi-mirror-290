1ee4266cd2e9d4c37de1a1f9b074816e062fe0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:30 +0000

    Add package customized-table/0.1.17/customized_table-0.1.17-py2.py3-none-any.whl

---
+++ packages/customized-table/customized_table-0.1.17-py2.py3-none-any.whl/customized_table/__init__.py
@@ -0,0 +1,5 @@
+from .customized_table import CustomizedTable, tag_text, from_csv, generate_counts, display_multiple_columns
+from .docs import docs_formatting, docs_customizedtable, docs_general
+
+__version__ = "0.1.17"
+__author__ = "Johan Hagelbäck"
