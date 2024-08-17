9d0107b4ae4dbe6d5a57c878f57f49e848c3adfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:51 +0000

    Add package mypy-boto3-application-autoscaling/1.35.0/mypy_boto3_application_autoscaling-1.35.0.tar.gz

---
+++ packages/mypy-boto3-application-autoscaling/mypy_boto3_application_autoscaling-1.35.0.tar.gz/mypy_boto3_application_autoscaling-1.35.0/mypy_boto3_application_autoscaling/paginator.py
@@ -0,0 +1,139 @@
+"""
+Type annotations for application-autoscaling service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_application_autoscaling.client import ApplicationAutoScalingClient
+    from mypy_boto3_application_autoscaling.paginator import (
+        DescribeScalableTargetsPaginator,
+        DescribeScalingActivitiesPaginator,
+        DescribeScalingPoliciesPaginator,
+        DescribeScheduledActionsPaginator,
+    )
+
+    session = Session()
+    client: ApplicationAutoScalingClient = session.client("application-autoscaling")
+
+    describe_scalable_targets_paginator: DescribeScalableTargetsPaginator = client.get_paginator("describe_scalable_targets")
+    describe_scaling_activities_paginator: DescribeScalingActivitiesPaginator = client.get_paginator("describe_scaling_activities")
+    describe_scaling_policies_paginator: DescribeScalingPoliciesPaginator = client.get_paginator("describe_scaling_policies")
+    describe_scheduled_actions_paginator: DescribeScheduledActionsPaginator = client.get_paginator("describe_scheduled_actions")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import ScalableDimensionType, ServiceNamespaceType
+from .type_defs import (
+    DescribeScalableTargetsResponseTypeDef,
+    DescribeScalingActivitiesResponseTypeDef,
+    DescribeScalingPoliciesResponseTypeDef,
+    DescribeScheduledActionsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "DescribeScalableTargetsPaginator",
+    "DescribeScalingActivitiesPaginator",
+    "DescribeScalingPoliciesPaginator",
+    "DescribeScheduledActionsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class DescribeScalableTargetsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScalableTargets)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescalabletargetspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ServiceNamespace: ServiceNamespaceType,
+        ResourceIds: Sequence[str] = ...,
+        ScalableDimension: ScalableDimensionType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[DescribeScalableTargetsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScalableTargets.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescalabletargetspaginator)
+        """
+
+
+class DescribeScalingActivitiesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScalingActivities)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescalingactivitiespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ServiceNamespace: ServiceNamespaceType,
+        ResourceId: str = ...,
+        ScalableDimension: ScalableDimensionType = ...,
+        IncludeNotScaledActivities: bool = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[DescribeScalingActivitiesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScalingActivities.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescalingactivitiespaginator)
+        """
+
+
+class DescribeScalingPoliciesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScalingPolicies)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescalingpoliciespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ServiceNamespace: ServiceNamespaceType,
+        PolicyNames: Sequence[str] = ...,
+        ResourceId: str = ...,
+        ScalableDimension: ScalableDimensionType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[DescribeScalingPoliciesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScalingPolicies.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescalingpoliciespaginator)
+        """
+
+
+class DescribeScheduledActionsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScheduledActions)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescheduledactionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ServiceNamespace: ServiceNamespaceType,
+        ScheduledActionNames: Sequence[str] = ...,
+        ResourceId: str = ...,
+        ScalableDimension: ScalableDimensionType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[DescribeScheduledActionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/application-autoscaling.html#ApplicationAutoScaling.Paginator.DescribeScheduledActions.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_application_autoscaling/paginators/#describescheduledactionspaginator)
+        """
