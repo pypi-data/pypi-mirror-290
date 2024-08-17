d697bff7788f4f97fccab48e51fd831e22600169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 07:05:31 +0000

    Add package kye/0.1.1/kye-0.1.1.tar.gz

---
+++ packages/kye/kye-0.1.1.tar.gz/kye-0.1.1/kye/errors/exceptions.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import typing as t
+
+if t.TYPE_CHECKING:
+    from kye.parse.expressions import Token
+
+
+class KyeRuntimeError(RuntimeError):
+    token: Token
+
+    def __init__(self, token: Token, message: str):
+        super().__init__(message)
+        self.token = token
+
+class ParserError(Exception):
+    pass
