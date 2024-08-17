d0229e6e27aa3c7453964b0f43e89cb76bd2f001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:04 +0000

    Add package mypy-boto3-migrationhubstrategy/1.35.0/mypy_boto3_migrationhubstrategy-1.35.0.tar.gz

---
+++ packages/mypy-boto3-migrationhubstrategy/mypy_boto3_migrationhubstrategy-1.35.0.tar.gz/mypy_boto3_migrationhubstrategy-1.35.0/mypy_boto3_migrationhubstrategy/paginator.py
@@ -0,0 +1,167 @@
+"""
+Type annotations for migrationhubstrategy service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_migrationhubstrategy.client import MigrationHubStrategyRecommendationsClient
+    from mypy_boto3_migrationhubstrategy.paginator import (
+        GetServerDetailsPaginator,
+        ListAnalyzableServersPaginator,
+        ListApplicationComponentsPaginator,
+        ListCollectorsPaginator,
+        ListImportFileTaskPaginator,
+        ListServersPaginator,
+    )
+
+    session = Session()
+    client: MigrationHubStrategyRecommendationsClient = session.client("migrationhubstrategy")
+
+    get_server_details_paginator: GetServerDetailsPaginator = client.get_paginator("get_server_details")
+    list_analyzable_servers_paginator: ListAnalyzableServersPaginator = client.get_paginator("list_analyzable_servers")
+    list_application_components_paginator: ListApplicationComponentsPaginator = client.get_paginator("list_application_components")
+    list_collectors_paginator: ListCollectorsPaginator = client.get_paginator("list_collectors")
+    list_import_file_task_paginator: ListImportFileTaskPaginator = client.get_paginator("list_import_file_task")
+    list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import ApplicationComponentCriteriaType, ServerCriteriaType, SortOrderType
+from .type_defs import (
+    GetServerDetailsResponseTypeDef,
+    GroupTypeDef,
+    ListAnalyzableServersResponseTypeDef,
+    ListApplicationComponentsResponseTypeDef,
+    ListCollectorsResponseTypeDef,
+    ListImportFileTaskResponseTypeDef,
+    ListServersResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "GetServerDetailsPaginator",
+    "ListAnalyzableServersPaginator",
+    "ListApplicationComponentsPaginator",
+    "ListCollectorsPaginator",
+    "ListImportFileTaskPaginator",
+    "ListServersPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class GetServerDetailsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.GetServerDetails)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#getserverdetailspaginator)
+    """
+
+    def paginate(
+        self, *, serverId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[GetServerDetailsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.GetServerDetails.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#getserverdetailspaginator)
+        """
+
+
+class ListAnalyzableServersPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListAnalyzableServers)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listanalyzableserverspaginator)
+    """
+
+    def paginate(
+        self, *, sort: SortOrderType = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListAnalyzableServersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListAnalyzableServers.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listanalyzableserverspaginator)
+        """
+
+
+class ListApplicationComponentsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListApplicationComponents)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listapplicationcomponentspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        applicationComponentCriteria: ApplicationComponentCriteriaType = ...,
+        filterValue: str = ...,
+        groupIdFilter: Sequence[GroupTypeDef] = ...,
+        sort: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListApplicationComponentsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListApplicationComponents.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listapplicationcomponentspaginator)
+        """
+
+
+class ListCollectorsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListCollectors)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listcollectorspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListCollectorsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListCollectors.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listcollectorspaginator)
+        """
+
+
+class ListImportFileTaskPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListImportFileTask)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listimportfiletaskpaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListImportFileTaskResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListImportFileTask.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listimportfiletaskpaginator)
+        """
+
+
+class ListServersPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListServers)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listserverspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterValue: str = ...,
+        groupIdFilter: Sequence[GroupTypeDef] = ...,
+        serverCriteria: ServerCriteriaType = ...,
+        sort: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListServersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/migrationhubstrategy.html#MigrationHubStrategyRecommendations.Paginator.ListServers.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_migrationhubstrategy/paginators/#listserverspaginator)
+        """
