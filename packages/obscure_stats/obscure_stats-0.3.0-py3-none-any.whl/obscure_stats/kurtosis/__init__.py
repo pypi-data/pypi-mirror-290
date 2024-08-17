1141bae2fc27be765ecf70f921169dcd3fb112b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 15:05:23 +0000

    Add package obscure_stats/0.3.0/obscure_stats-0.3.0-py3-none-any.whl

---
+++ packages/obscure_stats/obscure_stats-0.3.0-py3-none-any.whl/obscure_stats/kurtosis/__init__.py
@@ -0,0 +1,21 @@
+"""Kurtosis module."""
+
+from .kurtosis import (
+    crow_siddiqui_kurt,
+    hogg_kurt,
+    l_kurt,
+    moors_kurt,
+    moors_octile_kurt,
+    reza_ma_kurt,
+    staudte_kurt,
+)
+
+__all__ = [
+    "crow_siddiqui_kurt",
+    "hogg_kurt",
+    "l_kurt",
+    "moors_kurt",
+    "moors_octile_kurt",
+    "reza_ma_kurt",
+    "staudte_kurt",
+]
