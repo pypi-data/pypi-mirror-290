460e4b75001e7c3aab414077ac56186204c82c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 14:04:51 +0000

    Add package canatax/1.2.1/canatax-1.2.1.tar.gz

---
+++ packages/canatax/canatax-1.2.1.tar.gz/canatax-1.2.1/canatax/tax_estimate.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TaxEstimate:
+    gross_income: float
+    federal_tax: float
+    provincial_tax: float
+    cpp: float
+    ei: float
+    total_tax: float
+    net_income: float
