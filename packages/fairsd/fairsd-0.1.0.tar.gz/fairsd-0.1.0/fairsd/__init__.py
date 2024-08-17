c1a953994131855d46a3d53a14f3cf7216e65bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:50 +0000

    Add package fairsd/0.1.0/fairsd-0.1.0.tar.gz

---
+++ packages/fairsd/fairsd-0.1.0.tar.gz/fairsd-0.1.0/fairsd/__init__.py
@@ -0,0 +1,10 @@
+from .sgdescription import Description
+from .sgdescription import Descriptor
+from .searchspace import SearchSpace
+from .searchspace import Discretizer
+from .qualitymeasures import QualityFunction
+
+from .algorithms import SubgroupDiscoveryTask
+from .algorithms import BeamSearch
+from .algorithms import DSSD
+from .algorithms import ResultSet
