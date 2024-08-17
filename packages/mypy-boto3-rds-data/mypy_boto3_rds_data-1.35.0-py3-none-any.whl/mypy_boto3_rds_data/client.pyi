e229f816806375ae63c4efee8ab32ea6efe23580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:05:25 +0000

    Add package mypy-boto3-rds-data/1.35.0/mypy_boto3_rds_data-1.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-rds-data/mypy_boto3_rds_data-1.35.0-py3-none-any.whl/mypy_boto3_rds_data/client.pyi
@@ -0,0 +1,193 @@
+"""
+Type annotations for rds-data service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_rds_data.client import RDSDataServiceClient
+
+    session = Session()
+    client: RDSDataServiceClient = session.client("rds-data")
+    ```
+"""
+
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from botocore.client import BaseClient, ClientMeta
+
+from .literals import RecordsFormatTypeType
+from .type_defs import (
+    BatchExecuteStatementResponseTypeDef,
+    BeginTransactionResponseTypeDef,
+    CommitTransactionResponseTypeDef,
+    ExecuteSqlResponseTypeDef,
+    ExecuteStatementResponseTypeDef,
+    ResultSetOptionsTypeDef,
+    RollbackTransactionResponseTypeDef,
+    SqlParameterTypeDef,
+)
+
+__all__ = ("RDSDataServiceClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    BadRequestException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    DatabaseErrorException: Type[BotocoreClientError]
+    DatabaseNotFoundException: Type[BotocoreClientError]
+    DatabaseUnavailableException: Type[BotocoreClientError]
+    ForbiddenException: Type[BotocoreClientError]
+    HttpEndpointNotEnabledException: Type[BotocoreClientError]
+    InternalServerErrorException: Type[BotocoreClientError]
+    InvalidSecretException: Type[BotocoreClientError]
+    NotFoundException: Type[BotocoreClientError]
+    SecretsErrorException: Type[BotocoreClientError]
+    ServiceUnavailableError: Type[BotocoreClientError]
+    StatementTimeoutException: Type[BotocoreClientError]
+    TransactionNotFoundException: Type[BotocoreClientError]
+    UnsupportedResultException: Type[BotocoreClientError]
+
+class RDSDataServiceClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        RDSDataServiceClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#exceptions)
+        """
+
+    def batch_execute_statement(
+        self,
+        *,
+        resourceArn: str,
+        secretArn: str,
+        sql: str,
+        database: str = ...,
+        schema: str = ...,
+        parameterSets: Sequence[Sequence[SqlParameterTypeDef]] = ...,
+        transactionId: str = ...,
+    ) -> BatchExecuteStatementResponseTypeDef:
+        """
+        Runs a batch SQL statement over an array of data.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.batch_execute_statement)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#batch_execute_statement)
+        """
+
+    def begin_transaction(
+        self, *, resourceArn: str, secretArn: str, database: str = ..., schema: str = ...
+    ) -> BeginTransactionResponseTypeDef:
+        """
+        Starts a SQL transaction.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.begin_transaction)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#begin_transaction)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#can_paginate)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#close)
+        """
+
+    def commit_transaction(
+        self, *, resourceArn: str, secretArn: str, transactionId: str
+    ) -> CommitTransactionResponseTypeDef:
+        """
+        Ends a SQL transaction started with the `BeginTransaction` operation and
+        commits the
+        changes.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.commit_transaction)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#commit_transaction)
+        """
+
+    def execute_sql(
+        self,
+        *,
+        dbClusterOrInstanceArn: str,
+        awsSecretStoreArn: str,
+        sqlStatements: str,
+        database: str = ...,
+        schema: str = ...,
+    ) -> ExecuteSqlResponseTypeDef:
+        """
+        Runs one or more SQL statements.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.execute_sql)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#execute_sql)
+        """
+
+    def execute_statement(
+        self,
+        *,
+        resourceArn: str,
+        secretArn: str,
+        sql: str,
+        database: str = ...,
+        schema: str = ...,
+        parameters: Sequence[SqlParameterTypeDef] = ...,
+        transactionId: str = ...,
+        includeResultMetadata: bool = ...,
+        continueAfterTimeout: bool = ...,
+        resultSetOptions: ResultSetOptionsTypeDef = ...,
+        formatRecordsAs: RecordsFormatTypeType = ...,
+    ) -> ExecuteStatementResponseTypeDef:
+        """
+        Runs a SQL statement against a database.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.execute_statement)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#execute_statement)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#generate_presigned_url)
+        """
+
+    def rollback_transaction(
+        self, *, resourceArn: str, secretArn: str, transactionId: str
+    ) -> RollbackTransactionResponseTypeDef:
+        """
+        Performs a rollback of a transaction.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html#RDSDataService.Client.rollback_transaction)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_rds_data/client/#rollback_transaction)
+        """
