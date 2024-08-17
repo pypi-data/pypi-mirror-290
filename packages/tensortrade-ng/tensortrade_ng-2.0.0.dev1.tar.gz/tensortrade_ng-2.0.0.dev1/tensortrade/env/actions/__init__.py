e29200884dc0b723903308900b6f50f3d1e64a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 07:05:24 +0000

    Add package tensortrade-ng/2.0.0.dev1/tensortrade_ng-2.0.0.dev1.tar.gz

---
+++ packages/tensortrade-ng/tensortrade_ng-2.0.0.dev1.tar.gz/tensortrade_ng-2.0.0.dev1/tensortrade/env/actions/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 The TensorTrade-NG Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+from tensortrade.env.actions.bsh import BSH
+from tensortrade.env.actions.simple_orders import SimpleOrders
+from tensortrade.env.actions.managed_risk_orders import ManagedRiskOrders
