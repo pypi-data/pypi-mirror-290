c1589a06f606f228099934638d32e91d979bef35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:05:10 +0000

    Add package scanspec/0.7.2/scanspec-0.7.2-py3-none-any.whl

---
+++ packages/scanspec/scanspec-0.7.2-py3-none-any.whl/scanspec/sphinxext.py
@@ -0,0 +1,45 @@
+from contextlib import contextmanager
+
+from matplotlib.sphinxext import plot_directive
+
+from . import __version__
+
+
+@contextmanager
+def always_create_figures():
+    """Force matplotlib PlotDirective to always create figures.
+
+    This is needed even if source rst hasn't changed, as we often use
+    example_spec from within docstrings
+    """
+    orig_f = plot_directive.out_of_date
+    # Patch the plot directive so it thinks all sources are out of date
+    plot_directive.out_of_date = lambda *args, **kwargs: True
+    try:
+        yield
+    finally:
+        plot_directive.out_of_date = orig_f
+
+
+class ExampleSpecDirective(plot_directive.PlotDirective):
+    """Runs `plot_spec` on the ``spec`` definied in the content."""
+
+    def run(self):
+        self.content = (
+            ["# Example Spec", "", "from scanspec.plot import plot_spec"]
+            + [str(x) for x in self.content]
+            + ["plot_spec(spec)"]
+        )
+        with always_create_figures():
+            return super().run()
+
+
+def setup(app):
+    """Setup this extension in sphinx."""
+    app.add_directive("example_spec", ExampleSpecDirective)
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
