7e67c55f22c546d7a03c14ccced35ad307754f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 03:05:46 +0000

    Add package cometx/2.1.7/cometx-2.1.7-py3-none-any.whl

---
+++ packages/cometx/cometx-2.1.7-py3-none-any.whl/cometx/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ****************************************
+#                              __
+#   _________  ____ ___  ___  / /__  __
+#  / ___/ __ \/ __ `__ \/ _ \/ __/ |/_/
+# / /__/ /_/ / / / / / /  __/ /__>  <
+# \___/\____/_/ /_/ /_/\___/\__/_/|_|
+#
+#
+#  Copyright (c) 2022 Cometx Development
+#      Team. All rights reserved.
+# ****************************************
+
+import os
+
+# To keep comet_ml logger silent:
+os.environ["COMET_LOGGING_CONSOLE"] = "CRITICAL"
+
+from ._version import __version__  # noqa
+from .api import API  # noqa
