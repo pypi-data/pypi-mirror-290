728da5530a343d1e1c8737bac1018f403891717c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 06:04:50 +0000

    Add package dirman/0.1.1/DirMan-0.1.1.tar.gz

---
+++ packages/dirman/DirMan-0.1.1.tar.gz/DirMan-0.1.1/build.bat
@@ -0,0 +1,4 @@
+maturin build --release
+
+@REM maturin build --release --target x86_64-pc-windows-gnu //Build For Windows From Mac
+@REM maturin build --release --target x86_64-pc-windows-gnu -i 3.12 <-(python version)
