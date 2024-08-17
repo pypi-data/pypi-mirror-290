ba27fd258ee7a7c4e3544fed2c5c22362c622752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-17 04:06:02 +0000

    Add package xingong-pypack/0.0.3/xingong_pypack-0.0.3.tar.gz

---
+++ packages/xingong-pypack/xingong_pypack-0.0.3.tar.gz/xingong_pypack-0.0.3/xingong_pypack/model.py
@@ -0,0 +1,9 @@
+# The FLDPLN model module
+
+def hello_model(name):
+    """Prints "Hello, <name>!" to the console.
+
+    Args:
+        name (str): The name to say hello to.
+    """
+    print(f"Hello, {name}! Thanks for using the model module!")
