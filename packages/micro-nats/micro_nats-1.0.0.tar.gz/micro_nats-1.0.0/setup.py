266f85bf1097b309e1fce42a8b205ea54d045fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:05:41 +0000

    Add package micro-nats/1.0.0/micro_nats-1.0.0.tar.gz

---
+++ packages/micro-nats/micro_nats-1.0.0.tar.gz/micro_nats-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='micro_nats',
+    version="1.0.0",
+    description='A MicroPython NATS client',
+    url='https://github.com/jordonsc/inu-py',
+    author='Jordon Scott',
+    author_email='jordonsc@gmail.com',
+    license='MIT',
+    packages=['micro_nats'],
+    install_requires=[],
+    classifiers=[],
+)
