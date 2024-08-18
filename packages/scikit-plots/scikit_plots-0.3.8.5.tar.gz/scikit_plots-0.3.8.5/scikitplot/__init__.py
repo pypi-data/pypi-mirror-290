5b95341dbe62832bb868bddf2cff499af015a0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:05:55 +0000

    Add package scikit-plots/0.3.8.5/scikit_plots-0.3.8.5.tar.gz

---
+++ packages/scikit-plots/scikit_plots-0.3.8.5.tar.gz/scikit_plots-0.3.8.5/scikitplot/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+from . import (
+    estimators,
+    decomposition,
+    metrics, cluster,
+    deciles, 
+)
+__version__ = '0.3.8.5'
+
+from scikitplot.classifiers import classifier_factory
+from scikitplot.clustering import clustering_factory
