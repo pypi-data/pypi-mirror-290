8e6e6b785196dc90e8cf2b4e52e4efe19d77828c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:05:45 +0000

    Add package mpgameserver/0.2.4/mpgameserver-0.2.4-py3-none-any.whl

---
+++ packages/mpgameserver/mpgameserver-0.2.4-py3-none-any.whl/mpgameserver/__init__.py
@@ -0,0 +1,44 @@
+
+__version__ = "0.2.1"
+
+from mpgameserver.auth import Auth
+from mpgameserver.client import UdpClient
+from mpgameserver.connection import SeqNum, ConnectionStatus, ProtocolError, RetryMode
+from mpgameserver.context import ServerContext
+from mpgameserver.crypto import EllipticCurvePrivateKey, EllipticCurvePublicKey
+from mpgameserver.handler import EventHandler
+from mpgameserver.logger import setupLogger
+from mpgameserver.serializable import SerializableType, Serializable, SerializableEnum, Default
+from mpgameserver.task import TaskPool
+from mpgameserver.timer import Timer
+from mpgameserver.twisted import TwistedServer, ThreadedServer
+from mpgameserver.http_server import path_join_safe, \
+    get, put, delete, post, websocket, \
+    Router, Resource, HTTPServer, \
+    Response, JsonResponse, SerializableResponse, \
+    WebSocketOpCode
+from mpgameserver.http_client import HTTPClient
+from mpgameserver.dispatch import ServerMessageDispatcher, ClientMessageDispatcher, \
+    server_event, client_event
+
+try:
+    from mpgameserver import pylon
+except ImportError as e: # pragma: no cover
+    pass
+
+# pygame is an optional dependency
+try:
+    from mpgameserver.graph import LineGraph, AreaGraph
+    from mpgameserver.guiserver import GuiServer
+except ImportError as e:  # pragma: no cover
+    # print(e.name)
+    # print(e.msg)
+    pass
+
+# pil + pygame are optional
+try:
+    from mpgameserver.captcha import Captcha
+except ImportError as e:  # pragma: no cover
+    # print(e.name)
+    # print(e.msg)
+    pass
