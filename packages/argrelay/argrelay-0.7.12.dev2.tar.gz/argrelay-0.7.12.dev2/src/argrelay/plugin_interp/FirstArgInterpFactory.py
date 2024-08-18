c7df5d4c35b7123be6e16988c289944d211f580c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:06:28 +0000

    Add package argrelay/0.7.12.dev2/argrelay-0.7.12.dev2.tar.gz

---
+++ packages/argrelay/argrelay-0.7.12.dev2.tar.gz/argrelay-0.7.12.dev2/src/argrelay/plugin_interp/FirstArgInterpFactory.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+
+from argrelay.composite_forest.CompositeForestExtractor import extract_zero_arg_interp_tree
+from argrelay.enum_desc.ReservedPropName import ReservedPropName
+from argrelay.misc_helper_common import eprint
+from argrelay.plugin_interp.FirstArgInterp import FirstArgInterp
+from argrelay.plugin_interp.FirstArgInterpFactoryConfigSchema import (
+    first_arg_interp_factory_config_desc,
+    first_arg_vals_to_next_interp_factory_ids_,
+    ignored_func_ids_list_,
+)
+from argrelay.plugin_interp.InterpTreeInterpFactory import InterpTreeInterpFactory
+from argrelay.plugin_interp.InterpTreeInterpFactoryConfigSchema import (
+    tree_path_interp_factory_config_desc,
+    interp_selector_tree_,
+)
+from argrelay.runtime_context.InterpContext import InterpContext
+from argrelay.runtime_data.ServerConfig import ServerConfig
+
+
+class FirstArgInterpFactory(InterpTreeInterpFactory):
+    """
+    `FirstArgInterp` was re-implemented in terms of `InterpTreeInterp` (FS_01_89_09_24).
+
+    Implements FS_42_76_93_51 first interp.
+    """
+
+    def __init__(
+        self,
+        server_config: ServerConfig,
+        plugin_instance_id: str,
+        plugin_config_dict: dict,
+    ):
+        first_arg_interp_factory_config_desc.validate_dict(plugin_config_dict)
+        converted_config = convert_FirstArgInterpConfig_to_InterpTreeInterpFactoryConfig(plugin_config_dict)
+        super().__init__(
+            server_config,
+            plugin_instance_id,
+            converted_config,
+        )
+
+    # TODO: TODO_10_72_28_05: This will go away together with switch to FS_33_76_82_84 composite forest config:
+    def _compare_config_with_composite_forest(
+        self,
+    ):
+        expected_dict = self.plugin_config_dict[interp_selector_tree_]
+        actual_dict = extract_zero_arg_interp_tree(
+            self.server_config.server_plugin_control.composite_forest,
+        )
+        eprint(f"expected_dict: {expected_dict}")
+        eprint(f"actual_dict: {actual_dict}")
+        assert expected_dict == actual_dict
+
+    def activate_plugin(
+        self,
+    ) -> None:
+        """
+        # NOTE: FS_42_76_93_51 first interp special case:
+        TODO: Think of anther way because `FirstArgInterp` is obsolete - this role here only extends its agony.
+        `FirstArgInterp` is used as the root and single instance which
+        triggers func loading of itself (and all others recursively).
+
+        Takes part in implementation of FS_01_89_09_24 interp tree.
+        """
+
+        # Build a map from `func_id` to `func_envelope`:
+        func_ids_to_func_envelopes: dict[str, dict] = {}
+        for delegator_plugin_id, action_delegator in self.server_config.action_delegators.items():
+            func_envelopes = action_delegator.get_supported_func_envelopes()
+            for func_envelope in func_envelopes:
+                func_id = func_envelope[ReservedPropName.func_id.name]
+                if func_id in func_ids_to_func_envelopes:
+                    raise RuntimeError(
+                        f"plugin_instance_id='{self.plugin_instance_id}': func_id='{func_id}' is published more than once by delegator_plugin_id='{delegator_plugin_id}'"
+                    )
+                func_ids_to_func_envelopes[func_id] = func_envelope
+
+        self.load_interp_tree_abs_paths([])
+
+        interp_tree_abs_path: tuple[str, ...] = tuple([])
+        mapped_func_ids: list[str] = self.load_func_envelopes(
+            interp_tree_abs_path,
+            func_ids_to_func_envelopes,
+        )
+
+        ignored_func_ids_list = self.plugin_config_dict.get(ignored_func_ids_list_, [])
+        for func_id in func_ids_to_func_envelopes.keys():
+            # TODO: TODO_19_67_22_89: remove `ignored_func_ids_list` - load as `FuncState.fs_unplugged`:
+            if func_id not in mapped_func_ids:
+                if func_id not in ignored_func_ids_list:
+                    raise RuntimeError(
+                        f"plugin_instance_id=`{self.plugin_instance_id}`: func_id=`{func_id}` is neither mapped anywhere nor is in `{ignored_func_ids_list_}`"
+                    )
+                else:
+                    # This `func_id` is ignored - skip:
+                    continue
+            else:
+                if func_id in ignored_func_ids_list:
+                    raise RuntimeError(
+                        f"plugin_instance_id=`{self.plugin_instance_id}`: func_id=`{func_id}` is already mapped but still listed in `{ignored_func_ids_list_}`"
+                    )
+                else:
+                    # This `func_id` is mapped - skip:
+                    continue
+
+    def load_interp_tree_abs_paths(
+        self,
+        this_plugin_instance_interp_tree_abs_paths: list[tuple[str, ...]],
+    ):
+        # TODO: TODO_18_51_46_14: refactor FS_42_76_93_51 zero_arg_interp into FS_15_79_76_85 line processor:
+        #       At the moment, `FirstArgInterpFactory` is not plugged into any tree
+        #       (while it should be part of composite forest likely as `CompositeNodeType.zero_arg_node`).
+        #       Instead, it is selected via `first_interp_factory_id` in server config.
+        #
+        #       When this func is called for `FirstArgInterpFactory`, it cannot be found plugged anywhere
+        #       within interp tree - instead, it walks the tree and invokes this func for other interps.
+        super().load_interp_tree_abs_paths([])
+
+    def create_interp(
+        self,
+        interp_ctx: InterpContext,
+    ) -> FirstArgInterp:
+        return FirstArgInterp(
+            self.plugin_instance_id,
+            # FS_42_76_93_51 first interp special case:
+            # We use multiple config clones (with customization) `tree_node_config_dict` for interps at
+            # different location on the FS_01_89_09_24 interp tree - these configs get cloned on `load_func_envelopes`.
+            # But first interp is always at the root of the interp tree.
+            # More over, first interp is the only one who triggers cascading calls to `load_func_envelopes`,
+            # but never receives such call itself.
+            # Instead of using `interp_tree_node_config_dict`, use `plugin_config_dict` directly:
+            self.plugin_config_dict,
+            interp_ctx,
+        )
+
+
+def convert_FirstArgInterpConfig_to_InterpTreeInterpFactoryConfig(
+    plugin_config_dict: dict,
+) -> dict:
+    """
+    `FirstArgInterp` was re-implemented in terms of `InterpTreeInterp` (FS_01_89_09_24).
+    """
+    interp_selector_tree = {}
+    for command_id, plugin_instance_id in plugin_config_dict[first_arg_vals_to_next_interp_factory_ids_].items():
+        interp_selector_tree[command_id] = plugin_instance_id
+
+    output_dict = {
+        interp_selector_tree_: interp_selector_tree,
+        ignored_func_ids_list_: plugin_config_dict.get(ignored_func_ids_list_, []),
+    }
+
+    tree_path_interp_factory_config_desc.validate_dict(output_dict)
+
+    return output_dict
---
+++ packages/argrelay/argrelay-0.7.12.dev2.tar.gz/argrelay-0.7.12.dev2/src/argrelay/runtime_context/AbstractPlugin.py
@@ -0,0 +1,85 @@
+import importlib
+from copy import deepcopy
+from typing import Type
+
+from argrelay.enum_desc.PluginType import PluginType
+from argrelay.runtime_data.PluginEntry import PluginEntry
+
+
+class AbstractPlugin:
+
+    def __init__(
+        self,
+        plugin_instance_id: str,
+        plugin_config_dict: dict,
+    ):
+        self.plugin_instance_id: str = plugin_instance_id
+        self.plugin_config_dict: dict = self.load_config(plugin_config_dict)
+
+        self.validate_config()
+
+    # TODO: TODO_10_72_28_05: This will go away together with switch to FS_33_76_82_84 composite forest config:
+    def _compare_config_with_composite_forest(
+        self,
+    ):
+        """
+        Compares `CompositeInfoType` extracted from `composite_forest` with
+        data manually specified in `plugin_config_dict`.
+        """
+        pass
+
+    def load_config(
+        self,
+        plugin_config_dict,
+    ) -> dict:
+        """
+        Pre-process the given config on load (e.g. populate default values).
+
+        The default implementation is to return `plugin_config_dict` as is.
+
+        The typical implementation is to call `TypeDesc.dict_from_input_dict`
+        so that defaults are populated according to the given `Schema`.
+        """
+        return deepcopy(plugin_config_dict)
+
+    def validate_config(
+        self,
+    ) -> None:
+        """
+        Validate schema or data for `plugin_config_dict`.
+
+        The typical implementation is to call `TypeDesc.validate_dict(self.plugin_config_dict)`.
+
+        If `load_config` is implemented via `TypeDesc.dict_from_input_dict`,
+        validating step is only necessary if it does anything extra beyond what `Schema` can validate.
+        """
+        pass
+
+    def get_plugin_type(
+        self,
+    ) -> PluginType:
+        """
+        Return one of the `PluginType`-s.
+        """
+        raise NotImplementedError
+
+    def activate_plugin(
+        self,
+    ) -> None:
+        """
+        One-time plugin activation callback during startup.
+
+        This is a chance to initialize (once) any resources plugin requires.
+        """
+        pass
+
+
+def import_plugin_class(
+    plugin_entry: PluginEntry,
+):
+    plugin_module = importlib.import_module(plugin_entry.plugin_module_name)
+    plugin_class: Type[AbstractPlugin] = getattr(
+        plugin_module,
+        plugin_entry.plugin_class_name,
+    )
+    return plugin_class
