b5cc8c5903c36c98e9265afaf05a4c4b023efdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:05:37 +0000

    Add package soupsavvy/0.2.1/soupsavvy-0.2.1-py3-none-any.whl

---
+++ packages/soupsavvy/soupsavvy-0.2.1-py3-none-any.whl/soupsavvy/__init__.py
@@ -0,0 +1,48 @@
+from .selectors import (
+    AncestorCombinator,
+    Anchor,
+    AndSelector,
+    AnyTagSelector,
+    AttributeSelector,
+    ChildCombinator,
+    ClassSelector,
+    DescendantCombinator,
+    HasSelector,
+    IdSelector,
+    NextSiblingCombinator,
+    NotSelector,
+    OrSelector,
+    ParentCombinator,
+    PatternSelector,
+    SelectorList,
+    SubsequentSiblingCombinator,
+    TypeSelector,
+    UniversalSelector,
+    XORSelector,
+)
+
+__version__ = "0.2.1"
+__author__ = "sewcio543"
+
+__all__ = [
+    "UniversalSelector",
+    "AttributeSelector",
+    "TypeSelector",
+    "PatternSelector",
+    "SelectorList",
+    "DescendantCombinator",
+    "AndSelector",
+    "NotSelector",
+    "ChildCombinator",
+    "NextSiblingCombinator",
+    "SubsequentSiblingCombinator",
+    "AncestorCombinator",
+    "ParentCombinator",
+    "HasSelector",
+    "Anchor",
+    "OrSelector",
+    "ClassSelector",
+    "IdSelector",
+    "XORSelector",
+    "AnyTagSelector",
+]
