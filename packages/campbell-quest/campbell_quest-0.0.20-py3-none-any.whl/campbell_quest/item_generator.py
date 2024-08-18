4ae0731e6ff457749e2f84658b3d71f1dc46d9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:06:31 +0000

    Add package campbell-quest/0.0.20/campbell_quest-0.0.20-py3-none-any.whl

---
+++ packages/campbell-quest/campbell_quest-0.0.20-py3-none-any.whl/campbell_quest/item_generator.py
@@ -0,0 +1,20 @@
+from .itemAgents import item_brainstormer, item_refiner, item_formatter
+import json
+
+def get_items(quest, required_items_schema, item_templates):
+    required_items = item_refiner.get_required_items(quest)
+    
+    formatted_required_items = item_formatter.get_formatted_required_items(required_items, required_items_schema)
+    required_items_json = json.loads(formatted_required_items)
+    
+    item_cues = required_items_json.get("items", [])
+    items = []
+    for item_cue in item_cues:
+        item_type = item_cue.get("item_type")
+        item_template = item_templates.get(item_type)
+        
+        item = item_brainstormer.generate_item(quest, item_cue, item_template)
+        item = item_formatter.get_formatted_item(item)
+        items.append(item)
+    
+    return items
